bdfc64a1af93be21e2fdf2c919ee6f87735a3e5c
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-01-23 10:23:19 -1000

    Adding documentation for #9

--- panoptes/mount.py
+++ panoptes/mount.py
@@ -12,6 +12,9 @@ class Mount:
         # Get the class for getting data from serial sensor
         self.serial = SerialData()
 
+        self.non_sidereal_available = False
+        self.PEC_available = False
+
         self.is_connected = False
         self.is_slewing = False
 
@@ -31,7 +34,10 @@ class Mount:
         return self.is_connected
 
     def is_connected(self):
-        """ Returns is_connected state """
+        """ 
+        Returns is_connected state 
+        Sends test communication to mount to check communications.
+        """
         
         return self.is_connected
 
@@ -45,18 +51,43 @@ class Mount:
         return self.serial.read()
 
     def is_slewing(self):
-        pass
+        """
+        Querys mount to determine if it is slewing.
+        For some mounts, this is a built in function. For mount which do not have it we will have to 
+        write something based on how the coordinates are changing.
+        """
+        return self.is_slewing
 
     def check_coordinates(self):
+        """
+        Querys the mount for the current position of the mount.
+        This will be useful in comparing the position of the mount to the orientation 
+        indicated by the accelerometer or by an astrometric plate solve.
+        """
         pass
 
     def sync_coordinates(self):
+        """
+        Takes as input, the actual coordinates (J2000) of the mount and syncs the mount on them.
+        Used after a plate solve.
+        Once we have a mount model, we would use sync only initially, 
+        then subsequent plate solves would be used as input to the model.
+        """
         pass
 
     def slew_to_coordinates(self):
+        """
+        Inputs:
+            HA and Dec
+            RA tracking rate (in arcsec per second, use 15.0 in absence of tracking model).
+            Dec tracking rate (in arcsec per second, use 0.0 in absence of tracking model).
+        """
         pass
 
     def slew_to_park(self):
+        """
+        No inputs, the park position should be defined in configuration
+        """
         pass
 
     def echo(self):
