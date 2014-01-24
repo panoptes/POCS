6a8502f55bf3eee1f753575f68a209702bd8185e
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-01-23 20:04:20 -1000

    - Issue #15 -- Adding @joshwalawender initial code for HA to Dec. --- Set it to a  more standard format

--- panoptes/utils.py
+++ panoptes/utils.py
@@ -8,6 +8,47 @@ import time
 import serial
 import logging
 
+import astropy.units as u
+import astropy.coordinates as coords
+from astropy.time import Time
+import ephem
+
+class Convert():
+    """
+        Convert convenience functions
+    """
+
+    def __init__(self):
+
+        # Attach to the logger
+        self.logger = Logger()
+
+    def HA_to_Dec(self,J2000_coordinate, site):
+        """
+            HA to Dec
+        """
+        assert type(J2000_coordinate) == coords.FK5
+        assert J2000_coordinate.equinox.value == 'J2000.000'
+
+        ## Coordinate precessed to Jnow (as an astropy coordinate object)
+        Jnow_coordinate = J2000_coordinate.precess_to(Time.now())
+
+        ## Coordinate as a pyephem coordinate (J2000)
+        RA_string, Dec_string = J2000_coordinate.to_string(precision=2, sep=':').split(' ')
+        ephem_coordinate = ephem.FixedBody(RA_string, Dec_string, epoch=ephem.J2000)
+        ephem_coordinate = ephem.readdb('Polaris,f|M|F7,{},{},2.02,2000'.format(RA_string, Dec_string))
+        ephem_coordinate.compute(site)
+
+        self.logger.debug('pyephem Jnow:  {} {}'.format(ephem_coordinate.ra, ephem_coordinate.dec))
+
+
+        self.logger.debug('RA decimal = {:f}'.format(ephem_coordinate.ra))
+        self.logger.debug('LST decimal = {:f}'.format(site.sidereal_time()))
+
+        HourAngle = ephem_coordinate.ra - site.sidereal_time()
+
+        return HourAngle
+
 class Logger():
 
     """
