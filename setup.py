19fa8e95212f6dd839d2bfd8f57100268e9d484c
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-27 10:24:16 -1000

    Cleanup more of setup

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 
-setup(
-    name='Panoptes',
-    version='0.0.1',
-    packages=['Panoptes']
-)
-
 from __future__ import print_function
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 import io
 import codecs
 import os
 import sys
 
-import sandman
+import panoptes
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -29,33 +22,16 @@ def read(*filenames, **kwargs):
 
 long_description = read('README.txt', 'CHANGES.txt')
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
-
 setup(
     name='Panoptes',
     version=panoptes.__version__,
     url='https://github.com/panoptes/POCS',
     author='Project Panoptes',
-    tests_require=['pytest'],
     install_requires=[],
-    cmdclass={'test': PyTest},
     author_email='info@projectpanoptes.org',
     description='Panoptic Astronomical Networked OPtical observatory for Transiting Exoplanets Survey',
     long_description=long_description,
     packages=['panoptes'],
     include_package_data=True,
     platforms='any',
-    test_suite='panoptes.test.test_panoptes',
-    extras_require={
-        'testing': ['pytest'],
-    }
 )
