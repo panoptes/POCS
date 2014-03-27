d51da671b3cdd93935b20b11e610462f9e9859f6
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-27 10:14:11 -1000

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,51 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from __future__ import print_function
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+
+import panoptes
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.txt', 'CHANGES.txt')
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
 
 setup(
     name='Panoptes',
-    version='0.0.1',
-    packages=['Panoptes']
+    version=panoptes.__version__,
+    url='https://github.com/panoptes/POCS',
+    author='Project Panoptes',
+    author_email='info@projectpanoptes.org',
+    description='Panoptic Astronomical Networked OPtical observatory for Transiting Exoplanets Survey',
+    long_description=long_description,
+    packages=['panoptes'],
+    include_package_data=True,
+    platforms='any',
 )
