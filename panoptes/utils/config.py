35e61bb61feb99b63215c1de3b53d493d2d8117a
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-30 12:28:30 -1000

    Don't use Error class here or we get stuck in cyclical loading with has_utils

--- panoptes/utils/config.py
+++ panoptes/utils/config.py
@@ -2,15 +2,12 @@ import yaml
 import os
 import warnings
 
-import panoptes.utils.error as error
-
-
 def load_config_file(filename):
 	""" Sets a new config file """
 	try:
 		assert filename > ''
 	except AssertionError as err:
-		raise InvalidConfig('Config filename cannot be blank')
+		warnings.warn('Problem: {}', err)
 
 	# Set our new config file
 	has_config._config_file = filename
@@ -42,7 +39,7 @@ def load_config(config=None):
 	    with open(config, 'r') as f:
 	        has_config._config.update(yaml.load(f.read()))
 	except FileNotFoundError as err:
-		raise error.InvalidConfig("Config file not found: {}".format(config))
+		warnings.warn('Problem: {}', err)
 
 # This is global
 has_config._config_file = '{}/../../panoptes_config.yaml'.format(os.path.dirname(__file__))
