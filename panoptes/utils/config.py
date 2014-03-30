a6d1931540046b5a41e03197dd5b3149b608bc06
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-30 09:14:00 -1000

    Config decorator

--- panoptes/utils/config.py
+++ panoptes/utils/config.py
@@ -1,26 +1,29 @@
 import yaml
 import warnings
+import os
 
-def has_config(Class, refresh=False, config_file='panoptes_config.yaml'):
+panoptes_config = '{}/../../config.yaml'.format(os.path.dirname(__file__))
+
+def has_config(Class):
 	""" Class Decorator: Adds a config singleton to class """
-
-	if refresh:
-		has_config._config = {}
-
     # If already read, simply return config
 	if not has_config._config:
-	    # If we haven't returned, we don't have a config so we read from file
-		try:
-		    with open(config_file, 'r') as f:
-		        has_conifg._config = yaml.load(f.read())
-		except FileNotFoundError as err:
-			warnings.warn('{}'.format(err))
+		load_config(config_file=panoptes_config)
 
     # Add the config to the class
 	if has_config._config:
 		setattr(Class, 'config', has_config._config)
 
 	return Class
+
+def load_config(refresh=False, config_file=panoptes_config):
+	""" Loads the config from a file """
+	if refresh or not has_config._config:
+		try:
+		    with open(config_file, 'r') as f:
+		        has_config._config = yaml.load(f.read())
+		except FileNotFoundError as err:
+			warnings.warn('{}'.format(err))
 
 # This is global
 has_config._config = dict()
