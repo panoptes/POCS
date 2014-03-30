1187f5e8ad8ca52dad814572427e8ec94d352afb
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-30 08:12:29 -1000

    Move site to observatory; Read from config; use do_logging decorator

--- panoptes/__init__.py
+++ panoptes/__init__.py
@@ -1,45 +1,51 @@
 import ephem
+import yaml
 
 import panoptes.utils.logger as logger
 import panoptes.observatory as observatory
 
+@logger.do_logging
 class Panoptes:
+
     """
     Base class for our unit. This is inherited by *every* object and is just
     used to set some base items for the application. Sets up logger, reads
     config file and starts up application.
     """
-    def __init__(self):
+
+    def __init__(self, config_file='config.yaml'):
         # Setup utils
-        self.logger = logger.Logger()
+        self.logger.info('Initializing panoptes unit')
 
-        self.logger.info('Initializing observatory')
-    
-        # Hilo, HI
-        self.site = ephem.Observer()
-        self.site.lat = '19:32:09.3876'
-        self.site.lon = '-155:34:34.3164'
-        self.site.elevation = float(3400)
-        self.site.horizon = '-12'
-        
-        # Pressure initially set to 0.  This could be updated later.
-        self.site.pressure = float(680)
+        self.config = self._config(config_file)
 
-        # Static Initializations
-        self.site.date = ephem.now()
-        self.sun = ephem.Sun()        
+        # This is mostly for debugging
+        if 'name' in self.config:
+            self.logger.info('Welcome {}'.format(self.config.get('name')))
 
         # Create our observatory, which does the bulk of the work
         # NOTE: Here we would pass in config options
-        self.observatory = observatory.Observatory( logger=self.logger )
-        
+        self.observatory = observatory.Observatory(logger=self.logger)
+
+    def _config(self, config_file='panoptes_config.yaml'):
+        """
+        Reads the yaml config file and returns
+        """
+        config = dict()
+        with open(config_file, 'r') as f:
+            config = yaml.load(f.read())
+
+        self.logger.info('Using parameters from config file')
+        return config
+
     def start_session(self):
         """
         Main starting point for panoptes application
         """
         self.observatory.start_observing()
 
+
 if __name__ == '__main__':
     panoptes = Panoptes()
     panoptes.logger.info("Panoptes created. Starting session")
-    # panoptes.start_session()        
+    # panoptes.start_session()
