f7c749ca210dd1fd6b5c867129204d221c3395b4
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-29 17:05:10 -1000

    Adding out site info to our observatory

--- panoptes/observatory.py
+++ panoptes/observatory.py
@@ -40,6 +40,20 @@ class Observatory():
         self.cameras = [self.create_camera(), self.create_camera()]
         self.weather_station = self.create_weather_station()
 
+        # Hilo, HI
+        self.site = ephem.Observer()
+        self.site.lat = '19:32:09.3876'
+        self.site.lon = '-155:34:34.3164'
+        self.site.elevation = float(3400)
+        self.site.horizon = '-12'
+        
+        # Pressure initially set to 0.  This could be updated later.
+        self.site.pressure = float(680)
+
+        # Static Initializations
+        self.site.date = ephem.now()
+        self.sun = ephem.Sun()        
+
         # State method mapper
         self.states = {
             'shutdown': self.while_shutdown,
