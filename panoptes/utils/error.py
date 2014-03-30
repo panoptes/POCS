bcf96552e18d54678a842b78cfb8be8a04baab11
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-30 13:13:55 -1000

    Log error message rather than exception so we don't get stack

--- panoptes/utils/error.py
+++ panoptes/utils/error.py
@@ -10,7 +10,7 @@ class Error(Exception):
 
     def exit_program(self, msg='No reason specified'):
         """ Kills running program """
-        self.logger.exception("TERMINATING: {}".format(msg))
+        self.logger.error("TERMINATING: {}".format(msg))
         sys.exit()
 
 
@@ -31,20 +31,24 @@ class NotFound(Error):
     """ Generic not found class """
 
     def __init__(self, msg):
-    	self.msg = msg
+        self.msg = msg
         self.exit_program(msg='Cannot find {}'.format(self.msg))
 
     def __str__(self):
         return self.msg
 
+
 class MountNotFound(Error):
+
     """ Mount cannot be import """
+
     def __init__(self, msg):
         super(MountNotFound, self).__init__(msg)
 
 
 class CameraNotFound(NotFound):
+
     """ Camera cannot be import """
+
     def __init__(self, msg):
         super(CameraNotFound, self).__init__(msg)
-
