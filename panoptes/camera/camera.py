d894636abd7ec97627345c0d3a811c6a80f141f2
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-03-30 08:14:14 -1000

    do_logging

--- panoptes/camera/camera.py
+++ panoptes/camera/camera.py
@@ -1,10 +1,10 @@
 import panoptes.utils.logger as logger
 
+@logger.do_logging
 class Camera:
 	"""
 	Main camera class
 	"""
 
-	def __init__(self,logger=None):
-
-	    self.logger = logger or logger.Logger()
+	def __init__(self):
+		self.logger.info('Setting up Camera')
