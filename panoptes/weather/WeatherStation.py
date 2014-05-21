0b7acf4efa979b3122cd4606a8961ed6bc69be7e
Josh Walawender <jmwalawender@gmail.com>
2014-05-20 16:10:32 -1000

    * Made log file in WeatherStation object based on UT date, not local date. * Added __init__.py

--- panoptes/weather/WeatherStation.py
+++ panoptes/weather/WeatherStation.py
@@ -1,7 +1,9 @@
-import os
 import datetime
+import os
+import sys
 
-import panoptes.utils.logger as logger
+from panoptes.utils import logger
+
 
 @logger.has_logger
 class WeatherStation():
@@ -10,12 +12,11 @@ class WeatherStation():
     """
 
     def __init__(self):
-        self.logger.info('Starting WeatherStation')
         ## Set up log file for weather telemetry
         self.telemetry_path = os.path.join('/', 'var', 'log', 'PanoptesWeather')
-        now = datetime.datetime.now()
-        self.telemetry_filename = 'weather_{}.txt'.format(now.strftime('%Y%m%d'))
+        now = datetime.datetime.utcnow()
+        self.telemetry_filename = 'weather_{}UT.txt'.format(now.strftime('%Y%m%d'))
         if not os.path.exists(self.telemetry_path): os.mkdir(self.telemetry_path)
         self.telemetry_file = os.path.join(self.telemetry_path, self.telemetry_filename)
-        
+
     
