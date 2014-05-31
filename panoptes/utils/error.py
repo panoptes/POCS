07b801f4955bcb2d9bd7dbeafd546c320c931de8
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-05-31 13:28:31 -1000

    Cleanup errors

--- panoptes/utils/error.py
+++ panoptes/utils/error.py
@@ -7,57 +7,40 @@ import panoptes.utils.logger as logger
 class Error(Exception):
 
     """ Base class for Panoptes errors """
+    def __init__(self, msg=None):
+        if msg:
+            self.logger.error('{}: {}'.format(self.__class__.__name__,msg))
+            self.msg = msg
 
     def exit_program(self, msg='No reason specified'):
         """ Kills running program """
         self.logger.error("TERMINATING: {}".format(msg))
         sys.exit()
 
-class InvalidConfig(Error):
-
-    """ Error raised if config file is invalid """
-
-    def __init__(self, msg):
-        super(InvalidConfig, self).__init__()
-        self.msg = msg
 
 class NotFound(Error):
-
     """ Generic not found class """
-
     def __init__(self, msg):
-        pass
+        # pass
         self.msg = msg
         self.exit_program(msg='Cannot find {}'.format(self.msg))
 
-class MountNotFound(NotFound):
-
-    """ Mount cannot be import """
- 
-    def __init__(self, msg):
-        super(MountNotFound, self).__init__(msg)
-        self.exit_program(msg='Problem with mount: {}'.format(msg))
+class InvalidConfig(Error):
+    """ Error raised if config file is invalid """
+    pass
 
 class InvalidMountCommand(Error):
-
     """ Error raised if attempting to send command that doesn't exist """
-
-    def __init__(self, msg):
-        super(InvalidMountCommand, self).__init__()
-        self.msg = msg
+    pass
 
 class BadSerialConnection(Error):
-
     """ Error raised when serial command is bad """
+    pass
 
-    def __init__(self, msg):
-        super(BadSerialConnection, self).__init__()
-        self.logger.error(msg)
-        self.msg = msg
-
+class MountNotFound(NotFound):
+    """ Mount cannot be import """
+    pass
+ 
 class CameraNotFound(NotFound):
-
     """ Camera cannot be import """
-
-    def __init__(self, msg):
-        super(CameraNotFound, self).__init__(msg)
+    pass
