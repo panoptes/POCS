6ad4796030aab2f6dbf8389b4030007d0fcf8761
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-05-30 15:47:48 -1000

    Update to test for mount setup

--- panoptes/test/mount/test_ioptron.py
+++ panoptes/test/mount/test_ioptron.py
@@ -11,6 +11,10 @@ class TestIOptron():
 		mount = Mount()
 
 	@raises(AssertionError)
-	def test_config_no_commands(self):
-		""" """
-		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } }, commands=dict())
+	def test_config_bad_commands(self):
+		""" Passes in a default config but blank commands, which should error """
+		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } }, commands={'foo': 'bar'})
+
+	def test_config_auto_commands(self):
+		""" Passes in config like above, but no commands, so they should read from defaults """
+		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } })
