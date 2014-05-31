558edad6c3200535e9d46ea55b7ace01afda599d
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-05-31 13:14:30 -1000

    Share the good config

--- panoptes/test/mount/test_ioptron.py
+++ panoptes/test/mount/test_ioptron.py
@@ -7,6 +7,9 @@ import panoptes.utils.error as error
 
 class TestIOptron():
 
+	
+	good_config = {'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' }}
+
 	@nose.tools.raises(AssertionError)
 	def test_no_config_no_commands(self):
 		""" Mount needs a config """
@@ -15,22 +18,22 @@ class TestIOptron():
 	@nose.tools.raises(AssertionError)
 	def test_config_bad_commands(self):
 		""" Passes in a default config but blank commands, which should error """
-		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } }, commands={'foo': 'bar'})
+		mount = Mount(config=self.good_config, commands={'foo': 'bar'})
 
 	def test_config_auto_commands(self):
 		""" Passes in config like above, but no commands, so they should read from defaults """
-		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } })
+		mount = Mount(config=self.good_config)
 
 	def test_default_settings(self):
 		""" Passes in config like above, but no commands, so they should read from defaults """
-		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } })
+		mount = Mount(config=self.good_config)
 		assert mount.is_connected is False
 		assert mount.is_initialized is False
 		assert mount.is_slewing is False
 
 	def test_port_set(self):
 		""" Passes in config like above, but no commands, so they should read from defaults """
-		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } })
+		mount = Mount(config=self.good_config)
 		nose.tools.eq_(mount.port, '/dev/ttyUSB0')
 
 	@nose.tools.raises(error.BadSerialConnection)
@@ -41,13 +44,13 @@ class TestIOptron():
 
 	@nose.tools.raises(error.InvalidMountCommand)
 	def test_bad_command(self):
-		""" Test connecting to the mount after setup """
-		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } })
+		""" Give a bad command to the telescope """
+		mount = Mount(config=self.good_config)
 		mount.get_command('foobar')
 
 	def test_connect(self):
-		""" Test connecting to the mount after setup """
-		mount = Mount(config={'mount': { 'model': 'ioptron', 'port':'/dev/ttyUSB0' } })
+		""" Test connecting to the mount after setup. If we are not connected, we skip tests """
+		mount = Mount(config=self.good_config)
 		try:
 			mount.connect()
 		except:
