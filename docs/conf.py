9aff366aedbfb32718b6e0239de26c2b1d7613ad
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-05-31 21:36:01 -1000

    Small update

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ copyright = u'2014, Project Panoptes'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.1'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.1'
 
