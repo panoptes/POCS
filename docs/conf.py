902d4896b0c9238ee6fcda357838740a1ded1612
W. Tyler Gee <wtylergee@gmail.com>
2014-06-10 11:01:22 -1000

    Removing docs requirements

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,6 @@ sys.path.insert(0, os.path.abspath('../'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.todo',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
