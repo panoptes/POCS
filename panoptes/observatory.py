bfd6fe486d87dffe9c895efafcb6cde98f88021c
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-06-26 10:27:10 -1000

    Small change to add def

--- panoptes/observatory.py
+++ panoptes/observatory.py
@@ -23,6 +23,20 @@ import panoptes.utils.logger as logger
 import panoptes.utils.config as config
 import panoptes.utils.error as error
 
+def list_connected_cameras(logger=None):
+    command = ['gphoto2', '--auto-detect']
+    result = subprocess.check_output(command)
+    lines = result.decode('utf-8').split('\n')
+    Ports = []
+    for line in lines:
+        MatchCamera = re.match('([\w\d\s_\.]{30})\s(usb:\d{3},\d{3})', line)
+        if MatchCamera:
+            cameraname = MatchCamera.group(1).strip()
+            port = MatchCamera.group(2).strip()
+            if logger: logger.info('Found "{}" on port "{}"'.format(cameraname, port))
+            Ports.append(port)
+    return Ports
+
 @logger.has_logger
 @config.has_config
 class Observatory():
