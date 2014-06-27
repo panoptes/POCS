881976ed76b208fb88e03defd32036f239e459ae
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-06-27 12:19:09 -1000

    Updated test script

--- test.py
+++ test.py
@@ -5,18 +5,18 @@ import panoptes
 
 pan = panoptes.Panoptes()
 
-target_ra = "".format(pan.observatory.sun.ra)
+target_ra = "{}".format(pan.observatory.sun.ra)
 target_dec = "+{}".format(pan.observatory.sun.dec)
 
 target = (target_ra, target_dec)
 
-pan.observatory.slew_to_coordinates(target)
+pan.observatory.mount.slew_to_coordinates(target)
 
 while pan.observatory.mount.is_slewing:
-	time.sleep(2)
+	time.sleep(1)
+
+# for camera in pan.observatory.cameras:
+#     camera.list_config()
+#     camera.simple_capture_and_download(1/10)
 
 pan.observatory.mount.serial_query('goto_home')
-
-for camera in pan.observatory.cameras:
-    camera.list_config()
-    camera.simple_capture_and_download(1/10)
