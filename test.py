2c86c1c5ea955c10d02c30b4b5a00804d01b4628
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-07-11 20:39:52 -1000

    Do no test with camera

--- test.py
+++ test.py
@@ -5,21 +5,23 @@ import panoptes
 
 pan = panoptes.Panoptes()
 
-for camera in pan.observatory.cameras:
-    camera.connect()
+# for camera in pan.observatory.cameras:
+#     camera.connect()
 
 target_ra = "{}".format(pan.observatory.sun.ra)
 target_dec = "+{}".format(pan.observatory.sun.dec)
 
 target = (target_ra, target_dec)
 
-pan.observatory.mount.slew_to_coordinates(target)
+print(target)
 
-while pan.observatory.mount.is_slewing:
-	pan.observatory.mount.check_coordinates()
-	time.sleep(1)
+# pan.observatory.mount.slew_to_coordinates(target)
 
-for camera in pan.observatory.cameras:
-    camera.simple_capture_and_download(1/10)
+# while pan.observatory.mount.is_slewing:
+# 	pan.observatory.mount.check_coordinates()
+# 	time.sleep(1)
+
+# for camera in pan.observatory.cameras:
+#     camera.simple_capture_and_download(1/10)
 
 pan.observatory.mount.serial_query('goto_home')
