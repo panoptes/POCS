47f3303ce2d39fe46612f986ca91bc9f5bf386a7
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-07-16 22:38:54 -1000

    Made the code easier to understand, which is good because it's tricky. Here be dragons.

--- panoptes/state/statemachine.py
+++ panoptes/state/statemachine.py
@@ -3,7 +3,7 @@ The StateMachine for the Panoptes Project. Inherits from smach (see ros.org).
 """
 import smach
 
-import panoptes.state.states as states
+import panoptes.state.states
 
 import panoptes.utils.logger as logger
 import panoptes.utils.error as error
@@ -28,26 +28,32 @@ class StateMachine(object):
 
         self.logger.info("Creating state machine")
 
-        # Create a state machine container. The only outcome for our state machine is Parked,
-        # otherwise machine keeps running
-        self.sm = smach.StateMachine(outcomes=['quit'])
-
-        # Attach the observatory to the state machine userdata
         self.observatory = observatory
-
         self.state_table = state_table
 
-        # Open our state machine container
+        # Create a state machine container. The only outcome for our state
+        # is 'quit' because it runs indefinitely.
+        self.sm = smach.StateMachine(outcomes=['quit'])
+
+        # Open our state machine container and build our state machine
         with self.sm:
 
-            # Build our state machien from the supplied state_table
+            # Build our state machien from the supplied state_table.
             for state, transitions in self.state_table.items():
 
+                # Class instances are all upper case
                 instance_name = state.upper()
-                state_class = getattr(states, state.title())
 
-                smach.StateMachine.add(instance_name, state_class(
-                    observatory=self.observatory), transitions=transitions)
+                # Get the class object from the states module
+                state_class = getattr(panoptes.states.states, state.title())
+
+                # Create an instance of the state
+                state_instance = state_class(observatory=self.observatory)
+
+                # Add an instance of the state to our state machine, including possible transitions.
+                # Transitions are outcome: instance_name pairings that are possible for this state.
+                smach.StateMachine.add(instance_name, state_instance, transitions=transitions)
+
 
     def execute(self):
         """
