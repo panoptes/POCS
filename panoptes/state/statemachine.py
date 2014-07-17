cb7eb037c670b24ec5497393cdfc1f92a3a796ca
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-07-16 22:55:23 -1000

    Clarifying a lot of the documentation and making the code more readable

--- panoptes/state/statemachine.py
+++ panoptes/state/statemachine.py
@@ -3,7 +3,7 @@ The StateMachine for the Panoptes Project. Inherits from smach (see ros.org).
 """
 import smach
 
-import panoptes.state.states as states
+import panoptes.state.states
 
 import panoptes.utils.logger as logger
 import panoptes.utils.error as error
@@ -15,8 +15,8 @@ class StateMachine(object):
 
     def __init__(self, observatory, state_table):
         """
-        Initialize the StateMachine with an `Observatory`
-        of the state into the `states` dict. Sets `current_state` to 'shutdown'
+        Initialize the state machine. For PANOPTES, a state machine runs indefinitely
+        so the only possible outcome is 'quit'.
 
         @param  observatory     An instance of panoptes.observatory.Observatory
         @param  state_table     A dict() of state/transitions pairs
@@ -28,31 +28,45 @@ class StateMachine(object):
 
         self.logger.info("Creating state machine")
 
-        # Create a state machine container. The only outcome for our state machine is Parked,
-        # otherwise machine keeps running
-        self.sm = smach.StateMachine(outcomes=['quit'])
-
-        # Attach the observatory to the state machine userdata
         self.observatory = observatory
-
         self.state_table = state_table
 
-        # Open our state machine container
+        # Create a state machine container. The only outcome for our state
+        # is 'quit' because it runs indefinitely.
+        self.sm = smach.StateMachine(outcomes=['quit'])
+
+        # Open our state machine container and build our state machine. See
+        # smach documentation for explanation:
+        #
+        # http://wiki.ros.org/smach/Documentation#Opening_Containers_for_Construction
         with self.sm:
 
-            # Build our state machien from the supplied state_table
+            # Build our state machine from the supplied state_table
             for state, transitions in self.state_table.items():
 
+                # Class instances are all upper case
                 instance_name = state.upper()
-                state_class = getattr(states, state.title())
 
-                smach.StateMachine.add(instance_name, state_class(
-                    observatory=self.observatory), transitions=transitions)
+                # Get the state class from the states module
+                state_class = getattr(panoptes.states.states, state.title())
+
+                # Create an instance of the state class. All states receive the observatory
+                state_instance = state_class(observatory=self.observatory)
+
+                # Add an instance of the state to our state machine, including possible transitions.
+                # Transitions are outcome: instance_name pairings that are possible for this state.
+                smach.StateMachine.add(instance_name, state_instance, transitions=transitions)
+
 
     def execute(self):
         """
-        Starts the execution of our state machine
+        Executes the state machine, returning the possible outcomes.
+
+        @retval   outcome   one of the outcomes of the state machine. For now this is only 'quit'.
         """
-        self.logger.info("Beginning execution of state machine")
+        self.logger.info("Executing state machine")
+
         outcome = self.sm.execute()
+
+        self.logger.info("State machine outcome: {}".format(outcome))
         return outcome
