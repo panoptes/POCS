eb9a0758c9d2308e53abd1c7d0eaacb72bf15a19
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-12-13 17:54:39 -1000

    Updates to some of the web admin interface

--- admin/web.py
+++ admin/web.py
@@ -1,19 +1,48 @@
+import tornado
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url
 
-class HelloHandler(RequestHandler):
+class WebCamHandler(RequestHandler):
     def get(self):
-		loader = template.Loader("/var/panoptes/POCS/admin/static/templates")
-		print loader.load("test.html").generate(myvalue="XXX")
+        self.render("webcams.html", myvalue="FooBar")
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class MainHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.write("Hello, " + name)
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.write('<html><body><form action="/login" method="post">'
+                   'Name: <input type="text" name="name">'
+                   '<input type="submit" value="Sign in">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/")
 
 def make_app():
     return Application([
-        url(r"/", HelloHandler),
-        ])
+        url(r"/", WebCamHandler),
+        ],
+            cookie_secret="PANOPTES_SUPER_SECRET",
+            template_path="admin/static/templates",
+            debug=True,
+        )
 
 def main():
+
     app = make_app()
-    app.listen(8888)
+    app.listen(8880)
     IOLoop.current().start()
 
 if __name__ == '__main__':
