2c6a1374cac88da67abb109797d797bbea40ade1
Wilfred T Gee <wtylergee@gmail.com>
2014-12-13 16:20:56 -1000

    Starting a web interface to view webcame images

---
+++ admin/web.py
@@ -0,0 +1,19 @@
+from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application, url
+
+class HelloHandler(RequestHandler):
+    def get(self):
+        self.write("<img src='/static/webcams/pier_west.png' />")
+
+def make_app():
+    return Application([
+        url(r"/", HelloHandler),
+        ])
+
+def main():
+    app = make_app()
+    app.listen(8888)
+    IOLoop.current().start()
+
+if __name__ == '__main__':
+    main()
