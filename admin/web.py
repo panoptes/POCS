4e4652ab3d74f305a335de43d106c85c0ba02559
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-12-14 12:36:57 -1000

    Fixing Authentication; Adding in motor library for async access to mongo; cleanup

--- admin/web.py
+++ admin/web.py
@@ -1,12 +1,66 @@
-import tornado
-from tornado.ioloop import IOLoop
-from tornado.web import RequestHandler, Application, url
+import os.path
+import uuid
+import logging
+import tornado.auth
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+import tornado.options as options
+
+from tornado.concurrent import Future
+from tornado import gen
+from tornado.options import define, options
+
+import motor  # MongoDB Async
+
+define("port", default=8888, help="port", type=int)
+define("db", default="panoptes", help="Name of the Mongo DB to use")
+define("collection", default="admin", help="Name of the Mongo Collection to use")
+define("debug", default=False, help="debug mode")
+
+
+class Application(tornado.web.Application):
+
+    """ The main Application entry for our PANOPTES admin interface """
+
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/login", AuthLoginHandler),
+            (r"/logout", AuthLogoutHandler),
+            (r"/webcams", WebCamHandler),
+        ]
+
+        # Create a global connection to Mongo
+        db = motor.MotorClient().panoptes
+
+        settings = dict(
+            cookie_secret="PANOPTES_SUPER_DOOPER_SECRET",
+            login_url="/login",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            db=db,
+            debug=options.debug,
+        )
+
+        super(Application, self).__init__(handlers, **settings)
 
 
 class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        """ Simple property to access the DB easier """
+        return self.settings['db']
 
+
+    @gen.coroutine
     def get_current_user(self):
-        return self.get_secure_cookie("user")
+        username = self.get_secure_cookie("username")
+        if not username: return None
+
+        user_data = yield db.test_collection.find_one({'username': username})
+        return user_data
 
 
 class MainHandler(BaseHandler):
@@ -24,38 +78,45 @@ class WebCamHandler(BaseHandler):
         self.render("webcams.html", myvalue="FooBar")
 
 
-class LoginHandler(BaseHandler):
+class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
+
+    @gen.coroutine
+    def get(self):
+        if self.get_argument("openid.mode", None):
+            user = yield self.get_authenticated_user()
+            self.set_secure_cookie("user",
+                                   tornado.escape.json_encode(user))
+            self.redirect("/")
+            return
+        self.authenticate_redirect(ax_attrs=["name"])
+
+
+class AuthLogoutHandler(BaseHandler):
 
     def get(self):
-        self.write('<html><body><form action="/login" method="post">'
-                   'Name: <input type="text" name="name">'
-                   '<input type="submit" value="Sign in">'
-                   '</form></body></html>')
-
-    def post(self):
-        self.set_secure_cookie("user", self.get_argument("name"))
-        self.redirect("/")
-
-
-def make_app():
-    return Application([
-        url(r"/", MainHandler),
-        url(r"/webcams", WebCamHandler),
-        url(r"/login", LoginHandler),
-    ],
-        cookie_secret="PANOPTES_SUPER_SECRET",
-        login_url="/login",
-        template_path="admin/templates",
-        static_path="admin/static",
-        debug=True,
-    )
+        self.clear_cookie("user")
+        self.write("You are now logged out")
 
 
 def main():
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/login", AuthLoginHandler),
+            (r"/logout", AuthLogoutHandler),
+            (r"/webcams", WebCamHandler),
+        ],
+        cookie_secret="PANOPTES_SUPER_DOOPER_SECRET",
+        login_url="/login",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+        debug=options.debug,
+    )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
-    app = make_app()
-    app.listen(8880)
-    IOLoop.current().start()
 
 if __name__ == '__main__':
     main()
