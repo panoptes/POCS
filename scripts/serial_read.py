582f0a71ca1b847f2e68868a43d93581157333e7
Wilfred T Gee <wtylergee@gmail.com>
2014-12-17 11:45:24 -1000

    Fixing the upsert

--- scripts/serial_read.py
+++ scripts/serial_read.py
@@ -69,7 +69,7 @@ class ArduinoSerialMonitor(object):
             })
 
             # Insert same reading as 'current'
-            self.collection.update({"date": "current"}, {"$set": {"data": sensor_data}}, {"upsert": True})
+            self.collection.update({"date": "current"}, {"$set": {"data": sensor_data}}, True)
 
             # self.socket.send_string(sensor_string)  # ZMQ
 
