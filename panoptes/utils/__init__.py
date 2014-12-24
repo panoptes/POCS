86687c4ae4120f2bc4e995f7cc6f976a33944011
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-12-23 15:33:03 -1000

    Update to database utility

--- panoptes/utils/__init__.py
+++ panoptes/utils/__init__.py
@@ -2,5 +2,5 @@ from . import config as config
 from . import logger as logger
 from . import serial as serial
 from . import messaging as messaging
-from . import param_server as param_server
+from . import database as database
 from . import error as error
