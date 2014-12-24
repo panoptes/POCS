e1fca8431be1e0990d3d1dabc67985769ab4a9ea
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-12-23 15:47:18 -1000

    Hard code some collections we want to access in the database and make everything else private

--- panoptes/utils/database.py
+++ panoptes/utils/database.py
@@ -1,34 +1,42 @@
 from pymongo import MongoClient
 
+
 class PanMongo(object):
-	""" Connection to the running MongoDB instance
 
-	This is a collection of parameters that are initialized when the unit
-	starts and can be read and updated as the project is running. The server
-	is a wrapper around a mongodb collection.
-	"""
+    """ Connection to the running MongoDB instance
 
-	def __init__(self, host='localhost', port=27017):
-		# Get the mongo client
-		self.client = MongoClient(host, port)
-		self.db = self.client.panoptes
+    This is a collection of parameters that are initialized when the unit
+    starts and can be read and updated as the project is running. The server
+    is a wrapper around a mongodb collection.
+    """
 
-	def get_item(self, key=None):
-		""" Gets a value from the param server.
+    def __init__(self, host='localhost', port=27017):
+        # Get the mongo client
+        self._client = MongoClient(host, port)
 
-		Args:
-			key: name of parameter.
+        # Setup static connections to the collections we want
+        self.sensors = self._client.panoptes.sensors
+        self.images = self._client.panoptes.images
+        self.admin = self._client.panoptes.admin
+        self.config = self._client.panoptes.config
+        self.params = self._client.panoptes.params
 
-		Returns:
-			A value for the named parameter. This can be any object that
-			is stored in a dict. If no key is specified, warning is given
-			and nothing is returned.
-		"""
+    def get_param(self, key=None):
+        """ Gets a value from the param server.
 
-		val = None
+        Args:
+                key: name of parameter.
 
-		if key is not None:
-			param = self.param_server.find_one({ key: { '$exists': True } })
-			val = param.get(key)
+        Returns:
+                A value for the named parameter. This can be any object that
+                is stored in a dict. If no key is specified, warning is given
+                and nothing is returned.
+        """
 
-		return val
+        val = None
+
+        if key is not None:
+            param = self._db.param_server.find_one({key: {'$exists': True}})
+            val = param.get(key)
+
+        return val
