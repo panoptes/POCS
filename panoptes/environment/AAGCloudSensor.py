98d34d4448a2122bea86cba0512ca11661f5e9eb
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-12-25 10:51:42 -1000

    No need for import now

--- panoptes/environment/AAGCloudSensor.py
+++ panoptes/environment/AAGCloudSensor.py
@@ -13,7 +13,6 @@ import numpy as np
 import astropy.units as u
 
 from panoptes.utils import logger, config, database, messaging, error
-from panoptes.environment import WeatherStation
 
 
 @logger.has_logger
