bc1b374884f64db7f1fc30b5232e48238366a233
Wilfred Tyler Gee <wtylergee@gmail.com>
2014-12-26 10:28:42 -1000

    Adjusting websocket for sensors

--- admin/web.py
+++ admin/web.py
@@ -103,24 +103,20 @@ class SensorSocket(sockjs.tornado.SockJSConnection):
     """
 
     # Class level variable
-    participants = set()
+    observers = set()
 
     def on_open(self, info):
-        # Send that someone joined
-        self.broadcast(self.participants, "Someone joined.")
-
         # Add client to the clients list
-        self.participants.add(self)
+        self.observers.add(self)
 
     def on_message(self, message):
         # Broadcast message
-        self.broadcast(self.participants, message)
+        self.broadcast(self.observers, message)
 
     def on_close(self):
         # Remove client from the clients list and broadcast leave message
-        self.participants.remove(self)
+        self.observers.remove(self)
 
-        self.broadcast(self.participants, "Someone left.")
 
 
 class WebCamHandler(BaseHandler):
