7c84051031c576abcec70a99a6a4af41a6bc84cd
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-03-06 16:57:27 -1000

    Cleanging up imports; NOTE: requires Python >=3.3

--- panoptes/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import config as config
-from . import logger as logger
-from . import serial as serial
-from . import messaging as messaging
-from . import database as database
-from . import error as error
