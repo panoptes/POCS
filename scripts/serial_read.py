cafc34c3686133ab4b727d96fd827381714d8a50
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-03-12 18:04:38 -1000

    Removing zmq from serial_reader; Adding more debug statements

--- scripts/serial_read.py
+++ scripts/serial_read.py
@@ -4,7 +4,6 @@ import time
 import datetime
 import json
 import bson.json_util as json_util
-import zmq
 import pymongo
 
 import sys
@@ -30,8 +29,8 @@ class ArduinoSerialMonitor(object):
         self.serial_readers = dict()
 
         # Try to connect to a range of ports
-        for i in range(5):
-            port = '/dev/ttyACM{}'.format(i)
+        for i in range(2):
+            port = '/dev/arduino_{}'.format(i)
             self.logger.info('Attempting to connect to serial port: {}'.format(port))
 
             serial_reader = serial.SerialData(port=port, threaded=True)
@@ -41,11 +40,6 @@ class ArduinoSerialMonitor(object):
                 self.serial_readers[port] = serial_reader
             except:
                 self.logger.warning('Could not connect to port: {}'.format(port))
-
-        # Create the messaging socket
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PUB)
-        self.socket.bind("tcp://*:6500")
 
         # Connect to sensors db
         self.sensors = database.PanMongo().sensors
@@ -59,7 +53,7 @@ class ArduinoSerialMonitor(object):
         try:
             while True:
                 sensor_data = self.get_reading()
-                self.logger.debug("{}".format(sensor_data))
+                self.logger.debug("sensor_data: {}".format(sensor_data))
 
                 message = {
                     "date": datetime.datetime.utcnow(),
@@ -67,13 +61,12 @@ class ArduinoSerialMonitor(object):
                     "data": sensor_data
                 }
 
-                # Send out message on ZMQ
-                self.socket.send_string(json_util.dumps(message))
-
                 # Mongo insert
+                self.logger.debug("Inserting data to mongo")
                 self.sensors.insert(message)
 
                 # Update the 'current' reading
+                self.logger.debug("Updating the 'current' value in mongo")
                 self.sensors.update(
                     {"status": "current"},
                     {"$set": {
@@ -85,9 +78,11 @@ class ArduinoSerialMonitor(object):
                     True
                 )
 
+                self.logger.debug("Sleeping for {} seconds".format(self._sleep_interval))
                 time.sleep(self._sleep_interval)
+
         except KeyboardInterrupt:
-            pass
+            self.logger.info("Shutting down serial reader")
 
     def get_reading(self):
         """
@@ -116,16 +111,20 @@ class ArduinoSerialMonitor(object):
         for port, reader in self.serial_readers.items():
 
             # Get the values
+            self.logger.debug("Reading next serial value")
             sensor_value = reader.next()
 
             if len(sensor_value) > 0:
                 try:
+                    self.logger.debug("Got sensor_value from {}".format(port))
                     data = json.loads(sensor_value.replace('nan', 'null'))
 
                     sensor_data[port] = data
 
                 except ValueError:
                     self.logger.warning("Bad JSON: {0}".format(sensor_value))
+            else:
+                self.logger.debug("sensor_value length is zero")
 
         return sensor_data
 
