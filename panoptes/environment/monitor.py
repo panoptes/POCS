47237b881e6e3c983d0f50c956fdce991bc41cbe
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-03-19 16:43:55 -1000

    Greatly simplify the environmental monitors

--- panoptes/environment/monitor.py
+++ panoptes/environment/monitor.py
@@ -13,95 +13,47 @@ class EnvironmentalMonitor(object):
     It handles having a generic stoppable thread.
 
     Args:
-        messaging (panoptes.messaging.Messaging): A messaging Object for creating new
-            sockets.
-        channel (str): The channel topic that this monitor is associated with. Defaults
-            to 'default'
+        serial_port (str): Serial port to get readings from
     """
 
-    def __init__(self, messaging=None, channel='default', name=None):
-
-        if messaging is None:
-            messaging = messaging.Messaging()
+    def __init__(self, serial_port=None, connect_on_startup=True, name="environmental_sensor"):
 
         self.sleep_time = 1
 
-        self.messaging = messaging
-        self.channel = channel
+        self.serial_port = serial_port
 
-        # Create the thread and start monitoring
-        self.thread = threads.Thread(target=self._start_monitoring, args=())
+        try:
+            self.serial_reader = serial.SerialData(port=self.serial_port, name=name)
+        except:
+            self.logger.warning("Cannot connect to environmental sensor")
 
-        if name is not None:
-            self.thread.name = name
+        if connect_on_startup:
+            try:
+                self.start_monitoring()
+            except:
+                self.logger.warning("Problem starting serial monitor")
 
     def start_monitoring(self):
         """
-        Starts the actual thread
+        Connects over the serial port and starts the thread listening
         """
-        self.logger.info("Starting {} monitoring".format(self.thread.name))
-        self.thread.start()
+        self.logger.info("Starting {} monitoring".format(self.serial_reader.thread.name))
+
+        try:
+            self.serial_reader.connect()
+            self.serial_reader.thread.start()
+        except:
+            self.logger.warning("Cannot connect to CameraEnclosure via serial port")
+
+    def get_reading(self):
+        """ Gets a reading from the sensor """
+        return self.serial_reader.get_reading()
 
     def stop(self):
         """ Stops the running thread """
-        self.logger.info("Stopping {} monitoring".format(self.thread.name))
-        self.thread.stop()
+        self.logger.info("Stopping {} monitoring".format(self.serial_reader.thread.name))
+        self.serial_reader.thread.stop()
 
-    def _start_monitoring(self):
-        """ Starts the actual monitoring of the thread.
-
-        Calls out to the public monitoring() method that is implemented
-        in child classes.
-
-        Runs the child monitor() method in a loop, checking whether the
-        thread has received a stop signal each time. Sleeps for self.sleep_time
-        between.
-        """
-
-        while not self.thread.is_stopped():
-            self.monitor()
-            self.thread.wait(self.sleep_time)
-
-    def monitor(self):
-        raise NotImplementedError()
-
-    def send_message(self, message, channel=None):
-        """
-        Sends a message over the socket. Checks to ensure message is not
-        zero-length.
-
-        Args:
-            message (str): Message to be sent
-            channel (str): Channel to send message on. Defaults to value set
-                for the instance.
-        """
-        if(message > ''):
-            if channel is None:
-                channel = self.channel
-
-            self._send_message(message, channel)
-
-    def _send_message(self, message='', channel=None):
-        """ Responsible for actually sending message. Appends the channel
-        and timestamp to outgoing message.
-
-        Args:
-            message (str): Message to be sent
-            channel (str): Channel to send message on. Defaults to value set
-                for the instance.
-        """
-        assert self.socket is not None, self.logger.warning("No socket, cannot send message")
-        assert message > '', self.logger.warning("Cannot send blank message")
-
-        if channel is None:
-            channel = self.channel
-
-        timestamp = datetime.datetime.now()
-
-        full_message = '{} {} {}'.format(channel, timestamp, message)
-
-        # Send the message
-        self.socket.send_string(full_message)
 
     def __del__(self):
         """ Shut down the monitor """
