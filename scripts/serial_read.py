3a191acda9133898a1d9ad43989591c649123f3d
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-03-19 15:48:16 -1000

    Fixing config for environment sensors; updating serial read to read from config

--- scripts/serial_read.py
+++ scripts/serial_read.py
@@ -19,27 +19,33 @@ class ArduinoSerialMonitor(object):
 
     """
         Monitors the serial lines and tries to parse any data recevied
-        as JSON. This script first checks the first five ttyACM nodes and
-        tries to connect. Also connects to our mongo instance to update values
+        as JSON.
+
+        Checks for the `camera_box` and `computer_box` entries in the config
+        and tries to connect. Values are updated in the mongo db.
     """
 
     def __init__(self):
+
+        assert 'environment' in self.config
 
         # Store each serial reader
         self.serial_readers = dict()
 
         # Try to connect to a range of ports
-        for i in range(2):
-            port = '/dev/arduino_{}'.format(i)
-            self.logger.info('Attempting to connect to serial port: {}'.format(port))
+        for sensor in self.config['environment'].keys():
+            port = self.config['environment'][sensor].get('serial_port', None)
+            self.logger.info('Attempting to connect to serial port: {} {}'.format(sensor, port))
 
-            serial_reader = serial.SerialData(port=port, threaded=True)
+            if port is not None:
+                serial_reader = serial.SerialData(port=port, threaded=True)
 
-            try:
-                serial_reader.connect()
-                self.serial_readers[port] = serial_reader
-            except:
-                self.logger.warning('Could not connect to port: {}'.format(port))
+                try:
+                    serial_reader.connect()
+                    self.serial_readers[port] = serial_reader
+                except:
+                    self.logger.warning('Could not connect to port: {}'.format(port))
+
 
         # Connect to sensors db
         self.sensors = database.PanMongo().sensors
@@ -47,11 +53,13 @@ class ArduinoSerialMonitor(object):
         self._sleep_interval = 1
 
     def run(self):
-        """Run by the thread, reads continuously from serial line
+        """
+        The actual method that is run for each thread. Loops over each connected sensor
+        and reads from the serial line.
         """
 
         try:
-            while True:
+            while True and len(self.serial_readers):
                 sensor_data = self.get_reading()
                 self.logger.debug("sensor_data: {}".format(sensor_data))
 
@@ -83,6 +91,8 @@ class ArduinoSerialMonitor(object):
 
         except KeyboardInterrupt:
             self.logger.info("Shutting down serial reader")
+        finally:
+            self.logger.info("Loop finished. (No sensors connected?)")
 
     def get_reading(self):
         """
