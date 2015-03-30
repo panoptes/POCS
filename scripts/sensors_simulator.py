93f9a69ce96e24b7ad854b8c72e6abb2cd1c48d1
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-03-29 18:02:03 -1000

    Simulate sensors

---
+++ scripts/sensors_simulator.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import time
+import datetime
+import json
+import bson.json_util as json_util
+import pymongo
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from panoptes.utils import config, logger, serial, error, database
+
+
+class SensorSimulator(object):
+
+    """ Accepts a date and loops through mongodb values, updating current
+
+    Args:
+        start_date (datetime.datetime): Date to start from. Defaults to start of year.
+        delay (int):                    Amount of time to delay between updates. Defaults
+            to 1 sec
+    """
+
+    def __init__(self, start_date=datetime.datetime(2015, 1, 1), delay=1.0):
+        self.start_date = start_date
+        self.delay = delay
+
+        self.db = database.PanMongo()
+
+    def run(self):
+        """ Starts updating the "current" reading """
+        self._cursor = self.db.sensors.find({
+            "type": "environment",
+            "computer_box": {"$exists": True},
+            "camera_box": {"$exists": True},
+            "date": {"$gte": self.start_date}
+        })
+
+        for record in self._cursor:
+            self.db.sensors.update(
+                {"status": "current", "type": "environment"},
+                {"$set": {
+                    "date": datetime.datetime.utcnow(),
+                    "type": "environment",
+                    "camera_box": record["camera_box"],
+                    "computer_box": record["computer_box"],
+                }
+                },
+                True
+            )
+
+            print("Updated {}".format(record))
+            time.sleep(self.delay)
+
+
+if __name__ == "__main__":
+    simulator = SensorSimulator()
+    simulator.run()
