6971a2823b1dac0af72a5d3f21f8a7a238761e8e
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-08-07 12:23:01 -1000

    Massively simplifying logging system by using the astropy log util. Will need to do some adjustments

--- panoptes/utils/logger.py
+++ panoptes/utils/logger.py
@@ -1,106 +1,12 @@
-import logging
-from panoptes.utils import config
+from astropy import log
 
-def has_logger(Class, level='warning'):
+def has_logger(Class, level='DEBUG'):
     """Class decorator to add logging
 
     Args:
         level (str): log level to set for the class wrapper, defaults to 'warning'
     """
-    has_logger.log.info("Adding {} logging to: {}".format(level, Class.__name__))
-    setattr(Class, 'logger', Logger(log_level=level,profile=Class.__name__))
+    log.setLevel(level)
+    log.info("Adding {} logging to: {}".format(level, Class.__name__))
+    setattr(Class, 'logger', log)
     return Class
-
-
-def set_log_level(level='info'):
-    """Sets the log level for the class
-
-    Args:
-        level (str): log level to set for the class wrapper, defaults to 'warning'
-    """
-    def decorator(Class):
-        has_logger.log.info('Setting log level to {}'.format(level))
-        Class.logger.logger.setLevel(log_levels.get(level))
-        Class.logger.log_fh.setLevel(log_levels.get(level))
-        return Class
-    return decorator
-
-
-log_levels = {
-    'critical': logging.CRITICAL,
-    'error': logging.ERROR,
-    'warning': logging.WARNING,
-    'info': logging.INFO,
-    'debug': logging.DEBUG,
-}
-
-@config.has_config
-class Logger(object):
-    """ Consistent logging class for application
-
-        The has_logger class decorator allows this to be
-        applited to classes within a project for consistent functionality
-    """
-
-    def __init__(self,log_level='warning',profile=None):
-        # Get log info from config
-        log_config = self.config.get('log', {})
-
-        self.log_dir = log_config.setdefault('log_dir', '/var/panoptes/log')
-        self.log_file = log_config.setdefault('log_file', 'panoptes.log')
-        self.log_level = log_config.setdefault('log_level', 'info')
-        self.log_format = log_config.setdefault('log_format', '%(asctime)23s %(name)15s %(levelname)8s: %(message)s')
-        self.log_profile = profile if profile is not None else log_config.setdefault('log_profile', 'PanoptesLogger')
-
-        self.logger = logging.getLogger(self.log_profile)
-        self.log_format = logging.Formatter(self.log_format)
-        self.logger.setLevel(log_levels[self.log_level])
-
-        # Set up file output
-        self.log_fh = logging.FileHandler(self.log_file)
-        self.log_fh.setLevel(log_levels[self.log_level])
-        self.log_fh.setFormatter(self.log_format)
-        self.logger.addHandler(self.log_fh)
-
-        # Set up console output
-        if self.config.get('debug_console'):
-            self.log_ch = logging.StreamHandler()
-            self.log_ch.setLevel(log_levels[self.log_level])
-            self.log_ch.setFormatter(self.log_format)
-            self.logger.addHandler(self.log_ch)
-
-    def debug(self, msg):
-        """ Send a debug message """
-
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        """ Send an info message """
-
-        self.logger.info(msg)
-
-    def error(self, msg):
-        """ Send an error message """
-
-        self.logger.warning(self.logger.findCaller())
-        self.logger.error(msg)
-
-    def warning(self, msg):
-        """ Send an warning message """
-
-        self.logger.warning(self.logger.findCaller())
-        self.logger.warning(msg)
-
-    def critical(self, msg):
-        """ Send an critical message """
-
-        self.logger.warning(self.logger.findCaller())
-        self.logger.critical(msg)
-
-    def exception(self, msg):
-        """ Send an exception message """
-
-        self.logger.warning(self.logger.findCaller())
-        self.logger.exception(msg)
-
-has_logger.log = Logger()
