5989c01ef9e5775e42e73053d26130cb8f01e8de
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-08-10 16:30:40 -1000

    Basic tests for observatory

--- panoptes/tests/test_observatory.py
+++ panoptes/tests/test_observatory.py
@@ -2,8 +2,15 @@ import pytest
 
 from ..observatory import Observatory
 
+from ..utils.config import load_config
+
+config = load_config()
 
 def test_no_config():
     """ Creates a blank Observatory """
     with pytest.raises(AssertionError):
         obs_1 = Observatory()
+
+def test_default_config():
+    """ Creates a default Observatory """
+    obs_1 = Observatory(config=config)
