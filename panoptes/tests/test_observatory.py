6d8b863c87123fc0c07e70bc2b02f0b56586bde4
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-08-10 21:35:15 -1000

    Adding simple tests

--- panoptes/tests/test_observatory.py
+++ panoptes/tests/test_observatory.py
@@ -1,16 +1,25 @@
 import pytest
 
-from ..observatory import Observatory
+import astropy.units as u
 
+from ..observatory import Observatory
 from ..utils.config import load_config
 
 config = load_config()
 
+obs = None
+
+
 def test_no_config():
     """ Creates a blank Observatory """
     with pytest.raises(AssertionError):
-        obs_1 = Observatory()
+        obs = Observatory()
+
 
 def test_default_config():
     """ Creates a default Observatory """
-    obs_1 = Observatory(config=config)
+    obs = Observatory(config=config)
+
+    assert obs.location is not None
+    assert obs.elevation == config['site']['elevation'] * u.meter
+    assert obs.horizon == config['site']['horizon'] * u.degree
