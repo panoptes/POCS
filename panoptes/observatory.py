b82ea682683653e25a6df751c589087a781468f1
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-08-10 15:35:26 -1000

    * Making the site details for an observatory more robust     * `observatory` knows about its location (coords.EarthLocation, including lat, long, elevation), air_pressure, utc_offset, etc.

--- panoptes/observatory.py
+++ panoptes/observatory.py
@@ -28,138 +28,37 @@ class Observatory(object):
         dates, mount, cameras, and weather station
         """
         assert config is not None, self.logger.warning("Config not set for observatory")
-        self.config = config
 
         self.logger.info('Initializing observatory')
 
+        self.config = config
+
+        self.location = None
+        self.horizon = 30 * u.degree
+        self.utc_offset = 0 * u.minutes
+        self.elevation = 0 * u.meters
+        self.air_pressure = 0 * u.millibarye
+
+        self.mount = None
+        self.scheduler = None
+        self.cameras = list()
+
        # Setup information about site location
-        self.logger.info('Setting up observatory site')
-        self.site = self.setup_site()
+        self.logger.info('\t Setting up observatory details')
+        self._setup_observatory()
 
         # Create default mount and cameras. Should be read in by config file
         self.logger.info('\t Setting up mount')
-        self.mount = self.create_mount()
+        self._create_mount()
 
-        # Read the targets from the file
-        targets_path = os.path.join(
-            self.config.get('base_dir'),
-            self.config.get('targets', 'default_targets.yaml')
-        )
+        self.logger.info('\t Setting up cameras')
+        self._create_cameras()
 
-        self.logger.info('\t Setting up scheduler: {}'.format(targets_path))
-        self.scheduler = scheduler.Scheduler(target_list_file=targets_path)
-
-        # self.cameras = self.create_cameras()
-
-    def setup_site(self, start_date=Time.now()):
-        """
-        Sets up the site and location details, for the observatory.
-
-        These items are read from the 'site' config directive and include:
-        * lat (latitude)
-        * lon (longitude)
-        * elevation
-        # * horizon
-
-        """
-        self.logger.info('Setting up site details of observatory')
-        earth_location = None
-
-        if 'site' in self.config:
-            config_site = self.config.get('site')
-
-            lat = config_site.get('lat') * u.degree
-            lon = config_site.get('lon') * u.degree
-
-            elevation = config_site.get('elevation', 0) * u.meter
-            horizon = config_site.get('horizon', 0)
-
-            # Create an astropy EarthLocation
-            earth_location = coords.EarthLocation(
-                lat=lat,
-                lon=lon,
-                height=elevation,
-            )
-        else:
-            raise error.Error(msg='Bad site information')
-
-        # Pressure initially set to 680.  This could be updated later.
-        # site.pressure = config_site.get('pressure', 680)
-
-        return earth_location
-
-
-    def create_mount(self, mount_info=None):
-        """Creates a mount object.
-
-        Details for the creation of the mount object are held in the
-        configuration file or can be passed to the method.
-
-        This method ensures that the proper mount type is loaded.
-
-        Note:
-            This does not actually make a serial connection to the mount. To do so,
-            call the 'mount.connect()' explicitly.
-
-        Args:
-            mount_info (dict):  Configuration items for the mount.
-
-        Returns:
-            panoptes.mount:     Returns a sub-class of the mount type
-        """
-        if mount_info is None:
-            mount_info = self.config.get('mount')
-
-        model = mount_info.get('model')
-
-        self.logger.info('Creating mount: {}'.format(model))
-
-        mount = None
-
-        # Actually import the model of mount
-        try:
-            module = importlib.import_module('.{}'.format(model), package='panoptes.mount')
-        except ImportError as err:
-            self.logger.warning('ImportError. Check that the mount module exists and that all dependencies are installed')
-            raise error.NotFound(model)
-
-        # Make the mount include site information
-        mount = module.Mount(config=mount_info, site=self.site)
-
-        return mount
-
-    def create_cameras(self, camera_info=None):
-        """Creates a camera object(s)
-
-        Creates a camera for each camera item listed in the config. Ensures the
-        appropriate camera module is loaded.
-
-        Note:
-            This does not actually make a usb connection to the camera. To do so,
-            call the 'camear.connect()' explicitly.
-
-        Args:
-            camera_info (dict): Configuration items for the cameras.
-
-        Returns:
-            list: A list of created camera objects.
-        """
-        camera_info = self.config.get('cameras')
-
-        cameras = []
-
-        for camera in camera_info:
-            # Actually import the model of camera
-            try:
-                module = importlib.import_module('.{}'.format(camera.get('model')), 'panoptes.camera')
-                cameras.append(module.Camera(config=camera))
-
-            except ImportError as err:
-                raise error.NotFound(msg=model)
-
-        return cameras
+        self.logger.info('\t Setting up scheduler')
+        self._create_scheduler()
 
     def get_target(self):
+        """ Gets the next target from the scheduler """
 
         target = self.scheduler.get_target(self)
 
@@ -205,3 +104,122 @@ class Observatory(object):
         self.is_dark = self.sun.alt < dark_horizon
 
         return self.is_dark
+
+
+    def _setup_observatory(self, start_date=Time.now()):
+        """
+        Sets up the site and location details, for the observatory.
+
+        These items are read from the 'site' config directive and include:
+        * lat (latitude)
+        * lon (longitude)
+        * elevation
+        # * horizon
+
+        """
+        self.logger.info('Setting up site details of observatory')
+        earth_location = None
+
+        if 'site' in self.config:
+            config_site = self.config.get('site')
+
+            lat = config_site.get('lat') * u.degree
+            lon = config_site.get('lon') * u.degree
+
+            self.elevation = config_site.get('elevation', 0) * u.meter
+            self.horizon = config_site.get('horizon', 0) * u.degree
+            self.air_pressure = config_site.get('pressure', 680) * u.millibarye
+
+            # Create an astropy EarthLocation
+            earth_location = coords.EarthLocation(
+                lat=lat,
+                lon=lon,
+                height=self.elevation,
+            )
+        else:
+            raise error.Error(msg='Bad site information')
+
+
+        self.location = earth_location
+
+
+    def _create_mount(self, mount_info=None):
+        """Creates a mount object.
+
+        Details for the creation of the mount object are held in the
+        configuration file or can be passed to the method.
+
+        This method ensures that the proper mount type is loaded.
+
+        Note:
+            This does not actually make a serial connection to the mount. To do so,
+            call the 'mount.connect()' explicitly.
+
+        Args:
+            mount_info (dict):  Configuration items for the mount.
+
+        Returns:
+            panoptes.mount:     Returns a sub-class of the mount type
+        """
+        if mount_info is None:
+            mount_info = self.config.get('mount')
+
+        model = mount_info.get('model')
+
+        self.logger.info('Creating mount: {}'.format(model))
+
+        mount = None
+
+        # Actually import the model of mount
+        try:
+            module = importlib.import_module('.{}'.format(model), package='panoptes.mount')
+        except ImportError as err:
+            self.logger.warning('ImportError. Check that the mount module exists and that all dependencies are installed')
+            raise error.NotFound(model)
+
+        # Make the mount include site information
+        mount = module.Mount(config=mount_info, site=self.site)
+
+        return mount
+
+    def _create_cameras(self, camera_info=None):
+        """Creates a camera object(s)
+
+        Creates a camera for each camera item listed in the config. Ensures the
+        appropriate camera module is loaded.
+
+        Note:
+            This does not actually make a usb connection to the camera. To do so,
+            call the 'camear.connect()' explicitly.
+
+        Args:
+            camera_info (dict): Configuration items for the cameras.
+
+        Returns:
+            list: A list of created camera objects.
+        """
+        camera_info = self.config.get('cameras')
+
+        for camera in camera_info:
+            # Actually import the model of camera
+            try:
+                module = importlib.import_module('.{}'.format(camera.get('model')), 'panoptes.camera')
+                cameras.append(module.Camera(config=camera))
+
+            except ImportError as err:
+                raise error.NotFound(msg=model)
+
+        self.cameras = cameras
+
+
+    def _create_scheduler(self):
+        """ Sets up the scheduler that will be used by the observatory """
+
+        # Read the targets from the file
+        targets_path = os.path.join(
+            self.config.get('base_dir'),
+            self.config.get('targets', 'default_targets.yaml')
+        )
+
+        self.logger.info('\t Scheduler file: {}'.format(targets_path))
+        self.scheduler = scheduler.Scheduler(target_list_file=targets_path)
