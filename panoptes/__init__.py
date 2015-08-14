798adc2250d00e950c59f151550ce6b4ef81b1db
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-08-14 07:21:44 -1000

    * Renaming main file to core.py to be more consistent with python best practices * Updating the __init__ files for importing cleaner

--- panoptes/__init__.py
+++ panoptes/__init__.py
@@ -12,4 +12,6 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from .panoptes import *
+    from .utils import *
+
+    from .core import *
---
+++ panoptes/utils/__init__.py
@@ -0,0 +1,5 @@
+from .config import *
+from .database import *
+from .error import *
+from .logger import *
+from .serial import *
