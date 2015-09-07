ae92f1e8c37e39f5dd7a871bad32ec95c3473eef
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-09-07 13:30:21 -1000

    * Splitting up the weather class. Each one can init differently and then should override the check_conditions method     based on how they connect or what they are. Panoptes class should decide (via config) which weather station(s) it loads.     This will make it easy to make different weather stations / alter the simluator, without messing with the basic mongo     connection.     * WeatherStation Base class     * WeatherStationMongo class     * WeatherStationSimulator Class.

--- panoptes/weather/weather_station.py
+++ panoptes/weather/weather_station.py
@@ -6,18 +6,89 @@ from datetime import timedelta as tdelta
 
 from panoptes.utils import config, database
 
-class WeatherStation():
+
+class WeatherStation(object):
     """
     This object is used to determine the weather safe/unsafe condition.
     """
+
+    def __init__(self):
+        '''
+        '''
+        self.safe = False
+        self.sensors = None
+
+    def check_conditions(self, stale=180):
+        ''' Determines whether current conditions are safe or not
+
+        Args:
+            stale(int): If reading is older than `stale` seconds, return False. Default 180 (seconds).
+
+        Returns:
+            bool:       Conditions are safe (True) or unsafe (False)
+        '''
+        is_safe = false
+        return is_safe
+
+
+class WeatherStationMongo():
+    """
+    This object is used to determine the weather safe/unsafe condition.
+
+    Queries a mongodb collection for most recent values.
+    """
+
+    def __init__(self):
+        ''' Initialize the weather station with a mongodb connection. '''
+        super().__init__(self)
+
+        self.sensors = database.PanMongo().sensors
+
+    def check_conditions(self, stale=180):
+        ''' Determines whether current conditions are safe or not
+
+        Args:
+            stale(int): If reading is older than `stale` seconds, return False. Default 180 (seconds).
+
+        Returns:
+            bool:       Conditions are safe (True) or unsafe (False)
+        '''
+        assert self.sensors is not None, self.logger.warning("No connection to sensors.")
+
+        is_safe = False
+        now = dt.utcnow()
+        try:
+            is_safe = self.sensors.find_one({'type': 'weather', 'status': 'current'})[
+                'data']['Safe']
+            timestamp = self.sensors.find_one({'type': 'weather', 'status': 'current'})['date']
+            age = (now - timestamp).total_seconds()
+        except:
+            is_safe = False
+        else:
+            if age > stale:
+                is_safe = False
+
+        return is_safe
+
+
+class WeatherStationSimulator(WeatherStation):
+    """
+    This object simulates safe/unsafe conditions.
+
+    Args:
+        simulator(path):    Set this to a file path to manually control safe/unsafe conditions.
+            If the file exists, the weather is unsafe.  If the file does not exist, then conditions
+            are safe.
+
+    Returns:
+
+    """
+
     def __init__(self, simulator=None):
         '''
         Keyword Arguments
         -----------------
         simulator:
-            set this to a file path to manually control safe/unsafe conditions.
-            If the file exists, the weather is unsafe.  If the file does not
-            exist, then conditions are safe.
         '''
         self.safe = False
         self.simulator = simulator
@@ -25,7 +96,6 @@ class WeatherStation():
 
         if not self.simulator:
             self.sensors = database.PanMongo().sensors
-
 
     def check_conditions(self, stale=180):
         '''
@@ -38,8 +108,9 @@ class WeatherStation():
         else:
             now = dt.utcnow()
             try:
-                safe = self.sensors.find_one( {'type': 'weather', 'status': 'current'} )['data']['Safe']
-                timestamp = self.sensors.find_one( {'type': 'weather', 'status': 'current'} )['date']
+                safe = self.sensors.find_one({'type': 'weather', 'status': 'current'})[
+                    'data']['Safe']
+                timestamp = self.sensors.find_one({'type': 'weather', 'status': 'current'})['date']
                 age = (now - timestamp).total_seconds()
             except:
                 safe = False
