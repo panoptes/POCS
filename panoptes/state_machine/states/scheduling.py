f12adfb74074965a23745ecaa3416b2ab7a75c1f
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-09-09 10:21:01 -1000

    * Scheduling state work

--- panoptes/state_machine/states/scheduling.py
+++ panoptes/state_machine/states/scheduling.py
@@ -2,4 +2,19 @@ from . import PanState
 
 class State(PanState):
     def main(self):
-        return 'slewing'
+
+        next_state = 'parking'
+
+        self.logger.info("Getting target")
+
+        target = self.panoptes.observatory.get_target()
+
+        if target:
+            if self.panoptes.observatory.mount.set_target_coordinates(target):
+                self.logger.info("Mount set to target: {}".format(target.name))
+                next_state = 'slewing'
+            else:
+                self.logger.warning("Target not properly set")
+
+
+        return next_state
