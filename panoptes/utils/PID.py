acb3c8092117386a10bccf6680e706576e6c458a
Josh Walawender <jmwalawender@gmail.com>
2015-09-16 22:42:57 -1000

    Have PID calculate interval between recalculate operations

--- panoptes/utils/PID.py
+++ panoptes/utils/PID.py
@@ -1,3 +1,6 @@
+from datetime import datetime as dt
+from datetime import timedelta as tdelta
+
 ##-----------------------------------------------------------------------------
 ## PID Class
 ##-----------------------------------------------------------------------------
@@ -31,15 +34,22 @@ class PID:
         self.output_limits = output_limits
         self.history = []
         self.max_age = max_age
+        self.last_recalc_time = None
 
 
-    def recalculate(self, value, dt=1.0,\
+    def recalculate(self, value, interval=None,\
                     reset_integral=False,\
                     new_set_point=None):
         if new_set_point:
             self.set_point = float(new_set_point)
         if reset_integral:
             self.history = []
+        if not interval:
+            if self.last_recalc_time:
+                now = dt.utcnow()
+                interval = (now-self.last_recalc_time).total_seconds()
+            else:
+                interval = 0.0
 
         ## Pval
         error = self.set_point - value
@@ -47,12 +57,12 @@ class PID:
 
         ## Ival
         for entry in self.history:
-            entry[2] += dt
+            entry[2] += interval
         for entry in self.history:
             if self.max_age:
                 if entry[2] > self.max_age:
                     self.history.remove(entry)
-        self.history.append([error, dt, 0])
+        self.history.append([error, interval, 0])
         new_Ival = 0
         for entry in self.history:
             new_Ival += entry[0]*entry[1]
@@ -60,7 +70,7 @@ class PID:
 
         ## Dval
         if self.previous_error:
-            self.Dval = (error - self.previous_error)/dt
+            self.Dval = (error - self.previous_error)/interval
 
         ## Output
         output = self.Kp*error + self.Ki*self.Ival + self.Kd*self.Dval
@@ -68,6 +78,9 @@ class PID:
             if output > max(self.output_limits): output = max(self.output_limits)
             if output < min(self.output_limits): output = min(self.output_limits)
         self.previous_error = error
+
+        self.last_recalc_time = dt.utcnow()
+
         return output
 
 
