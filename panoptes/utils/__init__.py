b130237e510704a896cee35e392d88bda1375442
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-09-20 10:13:30 -1000

    * Documenting listify

--- panoptes/utils/__init__.py
+++ panoptes/utils/__init__.py
@@ -6,6 +6,15 @@ from .io import *
 from .modules import *
 
 def listify(obj):
+    """ Given an object, return a list
+
+    Always returns a list. If obj is None, returns empty list,
+    if obj is list, just returns obj, otherwise returns list with
+    obj as single member.
+
+    Returns:
+        list:   You guessed it.
+    """
     if obj is None:
         return []
     else:
