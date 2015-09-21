28973e80ad333cd6bf98aadc60782e281e2ab8f7
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-09-20 15:23:57 -1000

    * Adding dcraw executable. Testing better.

--- panoptes/utils/images.py
+++ panoptes/utils/images.py
@@ -7,38 +7,57 @@ from . import has_logger
 from . import InvalidSystemCommand
 from . import listify
 
+
 @has_logger
 class Image(object):
     """ Class for working with images
 
     Note:
         Many functions need dcraw to be installed.
+
+    Args:
+        filenames(str or list):     A str or list of strings of filenames
+        dcraw(str):                 Location of dcraw executable. Defaults to `/usr/bin/dcraw`
     """
-    def __init__(self, filenames):
+
+    def __init__(self, filenames, dcraw='/usr/bin/dcraw'):
         super().__init__()
 
+        assert os.path.exists(dcraw), self.logger.warning("dcraw does not exist at location {}".format(dcraw))
+        self.dcraw = dcraw
+
         # Save the list of files
-        self.files = [f if os.path.exists(f) for f in listify(fname)]
+        self.files = [f if os.path.exists(f) else None for f in listify(filenames)]
         self.logger.debug("{} files loaded.".format(len(self.files)))
 
-    def to_pgm(self):
-        """ Converts CR2 to PGM using dcraw """
+    def cr2_to_pgm(self):
+        """ Converts CR2 to PGM using dcraw
+
+        Returns:
+            list:   PGM file names
+        """
+        pgm_files = []
 
         for f in self.files:
+            if f is None:
+                next
+
             self.logger.debug("Converting {} to pgm".format(f))
             try:
                 # Build the command for this file
-                command = 'dcraw -t 0 -D -4 {}'.format(self.fname)
+                command = '{} -t 0 -D -4 {}'.format(self.dcraw, f)
                 cmd_list = command.split()
                 self.logger.debug("CR2 to PGM Conversion command: \n {}".format(cmd_list))
 
                 # Run the command
                 if subprocess.check_call(cmd_list) == 0:
                     self.logger.debug("PGM Conversion command successful")
+                    pgm_files.append(f.replace('.cr2', '.pgm'))
 
             except subprocess.CalledProcessError as err:
                 raise InvalidSystemCommand(msg="File: {} \n err: {}".format(f, err))
 
+        return pgm_files
 
     def read_pgm(fname, byteorder='>'):
         """Return image data from a raw PGM file as numpy array.
