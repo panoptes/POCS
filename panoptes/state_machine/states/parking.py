fecb229d272215570b567f9e75b2c31ab6c4a54f
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-09-21 17:57:32 -1000

    * Smarter Parking state

--- panoptes/state_machine/states/parking.py
+++ panoptes/state_machine/states/parking.py
@@ -3,18 +3,26 @@ from . import PanState
 class State(PanState):
     def main(self):
 
+        next_state = 'parked'
+
         mount = self.panoptes.observatory.mount
 
-        mount.slew_to_home()
-        while mount.is_slewing:
-            self.sleep()
+        if mount.is_initialized and mount.is_connected:
+            mount.slew_to_home()
+            while mount.is_slewing:
+                self.logger.debug("Slewing to home...")
+                self.sleep(5)
 
-        # iOptrons currently need to be re-initialized so they can handle meridian.
-        mount.is_initialized = False
-        mount.initialize()
+            # iOptrons currently need to be re-initialized so they can handle meridian.
+            self.logger.debug("Re-initializing mount")
+            mount.is_initialized = False
+            mount.initialize()
+            self.logger.debug("Mount re-initialized")
 
-        mount.park()
-        while not mount.is_parked:
-            self.sleep()
+            self.logger.debug("Sending park command")
+            mount.park()
+            while not mount.is_parked:
+                self.logger.debug("Slewing to park")
+                self.sleep(5)
 
-        return 'parked'
+        return next_state
