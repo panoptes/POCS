fd60bd3f18f97748f1e158e5a3642e8729ca9854
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-10-03 08:00:07 -1000

    * Making AbstractCamera more abstract and removing dependency from PanIndiDevice * Getting rid of all get_* property methods. A better solution can be found.

--- panoptes/camera/camera.py
+++ panoptes/camera/camera.py
@@ -9,21 +9,18 @@ import yaml
 from ..utils.logger import has_logger
 from ..utils.config import load_config
 from ..utils import listify
-from ..utils.indi import PanIndiDevice
 
 @has_logger
-class AbstractCamera(PanIndiDevice):
-    """
-    Abstract Camera class
+class AbstractCamera(object):
+    """ Abstract Camera class
+
+    Args:
+        name(str):      Name for the camera, defaults to 'GenericCamera'
+        config(Dict):   Config key/value pairs, defaults to empty dict.
     """
     pass
 
     def __init__(self, name='GenericCamera', config=dict()):
-        """
-        Initialize the camera
-        """
-        super().__init__(name)
-
         self.config = load_config()
         # Create an object for just the mount config items
         self.camera_config = config if len(config) else dict()
@@ -31,18 +28,19 @@ class AbstractCamera(PanIndiDevice):
         # Check the config for required items
         assert self.camera_config.get('port') is not None, self.logger.error(
             'No camera port specified, cannot create camera\n {}'.format(self.camera_config))
+        self.port = port
 
         # Get the model and port number
         model = self.camera_config.get('model')
         port = self.camera_config.get('port')
-        self.logger.info('Creating camera: {} {}'.format(model, port))
 
+        self.logger.info('Creating camera: {} {}'.format(model, port))
+        self.name = name
+        self.model = model
+
+        self.properties = None
         self.cooled = True
         self.cooling = False
-        self.model = model
-        self.USB_port = port
-        self.name = None
-        self.properties = None
 
 ##################################################################################################
 # Methods
@@ -71,7 +69,7 @@ class AbstractCamera(PanIndiDevice):
         This should be the only user-accessible way to run commands on the camera.
 
         Args:
-            command(list):   Commands to be passed to the camera
+            command(List[str]):   Commands to be passed to the camera
 
         Returns:
             list:           UTF-8 decoded response from camera
@@ -86,8 +84,7 @@ class AbstractCamera(PanIndiDevice):
         """
         raise NotImplementedError
 
-
-    def get(self, property):
+    def get_property(self, property):
         """ Get a value for the given property
 
         Args:
@@ -98,7 +95,7 @@ class AbstractCamera(PanIndiDevice):
         """
         raise NotImplementedError
 
-    def set(self, property, value):
+    def set_property(self, property, value):
         """ Sets a property for the camera
 
         Args:
@@ -110,61 +107,6 @@ class AbstractCamera(PanIndiDevice):
         """
         raise NotImplementedError
 
-    def get_iso(self):
-        """
-        Queries the camera for the ISO setting and populates the self.iso
-        property with a string containing the ISO speed.
-
-        Also examines the output of the command to populate the self.iso_options
-        property which is a dictionary associating the iso speed (as a string)
-        with the numeric value used as input for the set_iso() method.  The keys
-        in this dictionary are the allowed values of the ISO for this camera.
-
-        Returns:
-            str:        The current ISO setting
-        """
-        raise NotImplementedError
-
-    def set_iso(self, iso=100):
-        """ Sets the ISO speed of the camera.
-
-        Checks that the input value (a string or int) is in the list of allowed values in
-        the self.iso_options dictionary.
-        """
-        raise NotImplementedError
-
-    def get_serial_number(self):
-        """
-        Gets the generic Serial Number property and populates the
-        self.serial_number property.
-
-        Note: Some cameras override this. See `canon.get_serial_number`
-        """
-        raise NotImplementedError
-
-    def get_model(self):
-        """
-        Gets the Camera Model string from the camera and populates the
-        self.model property.
-        """
-        raise NotImplementedError
-
-    def get_device_version(self):
-        """
-        Gets the Device Version string from the camera and populates the
-        self.device_version property.
-        """
-        raise NotImplementedError
-
-    def get_shutter_count(self):
-        """
-        Gets the shutter count value and populates the self.shutter_count
-        property.
-        """
-        raise NotImplementedError
-
     def start_cooling(self):
-        """
-        This does nothing for a Canon DSLR as it does not have cooling.
-        """
+        """ Starts cooling the camera """
         raise NotImplementedError
