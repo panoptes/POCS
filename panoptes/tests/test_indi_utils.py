310d54c65398c856d46fa3e26b636684b9a16ac2
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-10-03 08:11:09 -1000

    * Moving all tests into the `panoptes/tests/` dir rather than having multiple `test` subdirs.

---
+++ panoptes/tests/test_indi_utils.py
@@ -0,0 +1,23 @@
+import pytest
+
+import astropy.units as u
+
+from ..utils.config import load_config
+from ..utils.indi import PanIndiServer, PanIndiDevice
+
+config = load_config()
+
+obs = None
+
+def test_no_config():
+    """ Creates a blank Observatory with no config, which should fail """
+    with pytest.raises(AssertionError):
+        obs = Observatory()
+
+def test_default_config():
+    """ Creates a default Observatory and tests some of the basic parameters """
+    obs = Observatory(config=config)
+
+    assert obs.location is not None
+    assert obs.location.get('elevation') - config['location']['elevation'] * u.meter < 1. * u.meter
+    assert obs.location.get('horizon') == config['location']['horizon'] * u.degree
