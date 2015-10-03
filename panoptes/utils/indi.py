ecbd38f919ca7a9c6f196cc97727f75c22d89f14
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-10-02 14:51:52 -1000

    * Separate so there is an PanIndiDevice and PanIndiServer class. The Panoptes class will start the server and create devices. Better separation.

--- panoptes/utils/indi.py
+++ panoptes/utils/indi.py
@@ -32,15 +32,19 @@ class PanIndiServer(object):
 
         self.load_drivers(drivers)
 
-    def load_drivers(self, drivers):
-        """ Load all the drivers """
+    def load_drivers(self, devices=[]):
+        """ Load all the device drivers
+
+        Args:
+            devices(list):      A list of PanIndiDevice objects
+        """
         # Load the drivers
-        for name, driver in drivers.items():
+        for device in devices:
             try:
-                self.load_driver(driver, name)
+                self.load_driver(device.driver, device.name)
             except error.InvalidCommand as e:
                 self.logger.warning(
-                    "Problem loading {} ({}) driver. Skipping for now.".format(name, driver))
+                    "Problem loading {} ({}) driver. Skipping for now.".format(device.name, device.driver))
 
     def start(self, *args, **kwargs):
         """ Start an INDI server.
@@ -115,7 +119,7 @@ class PanIndiServer(object):
 
 
 @has_logger
-class PanIndi(object):
+class PanIndiDevice(object):
     """ Interface to INDI for controlling hardware
 
     This module is capable of both spawning a server as well as connecting
@@ -123,15 +127,8 @@ class PanIndi(object):
 
     """
 
-    def __init__(self, host='localhost', port=7624, devices={'PAN_CCD_SIMULATOR': {'driver': 'indi_simulator_ccd'}}):
-        self.logger.info('Creating an instance of PanIndi')
-
-        self.host = host
-        self.port = port
-
-        drivers = {dev_name: dev_config['driver'] for dev_name, dev_config in devices.items()}
-
-        self.server = PanIndiServer(self.host, self.port, drivers)
+    def __init__(self, name='PAN_CCD_SIMULATOR', driver='indi_simulator_ccd'):
+        self.logger.info('Creating a PanIndiDevice')
 
         self._getprop = shutil.which('indi_getprop')
         self._setprop = shutil.which('indi_setprop')
@@ -139,18 +136,17 @@ class PanIndi(object):
         assert self._getprop is not None, PanError("Can't find indi_getprop")
         assert self._setprop is not None, PanError("Can't find indi_setprop")
 
-        self.devices = {}
-        self._load_devices()
+        self.name = name
+        self.driver = driver
 
-    def get_property(self, device, property='*', element='*'):
+    def get_property(self, property='*', element='*'):
         """ Gets a property from a device
 
         Args:
-            device(str):    Name of device.
             property(str):  Name of property. Defaults to '*'
             element(str):   Name of element. Defaults to '*'
         """
-        cmd = [self._getprop, '{}.{}.{}'.format(device, property, element)]
+        cmd = [self._getprop, '{}.{}.{}'.format(self.name, property, element)]
         self.logger.debug(cmd)
 
         output = ''
@@ -164,16 +160,15 @@ class PanIndi(object):
 
         return output
 
-    def set_property(self, device, property, element, value):
+    def set_property(self, property, element, value):
         """ Sets a property from a device with a certain value
 
         Args:
-            device(str):    Name of device.
             property(str):  Name of property.
             element(str):   Name of element.
             value(str):     Value for element.
         """
-        cmd = [self._setprop, '{}.{}.{}={}'.format(device, property, element, value)]
+        cmd = [self._setprop, '{}.{}.{}={}'.format(self.name, property, element, value)]
         self.logger.debug(cmd)
 
         output = ''
@@ -187,7 +182,7 @@ class PanIndi(object):
 
         return output
 
-    def get_all(self):
+    def get_all_properties(self):
         """ Gets all the properties for all the devices
         Returns:
             dict:   Key value pairs of all properties for all devices
@@ -201,161 +196,3 @@ class PanIndi(object):
     def disconnect(self, device):
         """ Connect to device """
         self.set_property(device, 'CONNECTION', 'Disconnect', 'On')
-
-    def _load_devices(self):
-        """ Loads the devices from the indiserve and stores them locally """
-        devices = set(key.split('.')[0] for key in self.get_all().keys())
-
-        for device in devices:
-            self.devices[device] = {}
-
-    # def connect(self, wait=1):
-    #     """ Connect to the server """
-    #     self.logger.info("Connecting to server and waiting {} secs".format(wait))
-    #
-    #     connected = False
-    #     try:
-    #         connected = self.connectServer()
-    #     except:
-    #         cmd = "indiserver indi_simulator_telescope indi_simulator_ccd"
-    #         msg = "No indiserver running on {}: Try to run \n {}".format(self.host, self.port, cmd)
-    #         raise error.NotFound(msg, exit=True)
-    #     else:
-    #         time.sleep(wait)
-    #         self.logger.info("Connected")
-    #
-    #     return connected
-    #
-    # def newDevice(self, d):
-    #     """ Add a new device to list
-    #
-    #     Checks first to make sure item is not already in list
-    #
-    #     NOTE:
-    #         What about multiple devices? This is just storing the driver name
-    #     """
-    #     name = d.getDeviceName()
-    #     self.logger.info("New device: {}".format(name))
-    #     if name not in self.devices:
-    #         self.devices[name] = d
-    #
-    # def newProperty(self, p):
-    #     self.logger.debug("newProperty on {}: {}".format(p.getDeviceName(), p.getName()))
-    #
-    # def removeProperty(self, p):
-    #     self.logger.debug("removeProperty on {}: {}".format(p.getDeviceName(), p.getName()))
-    #
-    # def newBLOB(self, bp):
-    #     self.logger.info("new BLOB " + bp.name)
-    #
-    #     img = bp.getblobdata()
-    #
-    #     with open("frame.fit", "wb") as f:
-    #         f.write(img)
-    #
-    # def newSwitch(self, svp):
-    #     self.logger.debug("newSwitch on {}: {} \t {}".format(svp.device, svp.name, svp.nsp))
-    #
-    # def newNumber(self, nvp):
-    #     self.logger.debug("newNumber on {}: {} \t {}".format(nvp.device, nvp.name, nvp.nnp))
-    #
-    # def newText(self, tvp):
-    #     self.logger.debug("newText on {}: {} \t {}".format(tvp.device, tvp.name, tvp.ntp))
-    #
-    # def newLight(self, lvp):
-    #     self.logger.debug("newText on {}: {} \t {}".format(lvp.device, lvp.name, lvp.nlp))
-    #
-    # def newMessage(self, d, m):
-    #     self.logger.debug("newMessage on {}: \t {}".format(d.getDeviceName(), d.messageQueue(m)))
-    #
-    # def serverConnected(self):
-    #     self.logger.debug("serverConnected on {}:{}".format(self.getHost(), self.getPort()))
-    #
-    # def serverDisconnected(self, code):
-    #     self.logger.debug("serverDisconnected on {}:{}. Reason: {}".format(self.getHost(), self.getPort(), code))
-
-    # def list_device_properties(self, device):
-    #     self.logger.info("Getting properties for {}".format(device.getDeviceName()))
-    #     for prop in device.getProperties():
-    #         prop_name = prop.getName()
-    #         prop_type = prop.getType()
-    #
-    #         if prop_type == PyIndi.INDI_TEXT:
-    #             tpy = prop.getText()
-    #             for t in tpy:
-    #                 self.logger.debug("{} ({}) \t {}".format(t.name, t.label, t.text))
-    #         elif prop_type == PyIndi.INDI_NUMBER:
-    #             tpy = prop.getNumber()
-    #             for t in tpy:
-    #                 self.logger.debug("{} ({}) \t {}".format(t.name, t.label, t.value))
-    #         elif prop_type == PyIndi.INDI_SWITCH:
-    #             tpy = prop.getSwitch()
-    #             for t in tpy:
-    #                 self.logger.debug("{} ({}) \t {}".format(t.name, t.label, switch_lookup.get(t.s)))
-    #         elif prop_type == PyIndi.INDI_LIGHT:
-    #             tpy = prop.getLight()
-    #             for t in tpy:
-    #                 self.logger.debug("{} ({}) \t {}".format(t.name, t.label, state_lookup.get(t.s)))
-    #         elif prop_type == PyIndi.INDI_BLOB:
-    #             tpy = prop.getBLOB()
-    #             for t in tpy:
-    #                 self.logger.debug("{} ({}) \t {}".format(t.name, t.label, t.size))
-    #
-    # def check_device(self, device=None):
-    #     pass
-    #
-    # def get_property_value(self, device=None, prop=None, elem=None):
-    #     """ Puts the property value into a sane format depending on type """
-    #     assert device is not None
-    #     self.logger.debug("{}: {}".format("device",device))
-    #     self.logger.debug("{}: {}".format("prop",prop))
-    #     self.logger.debug("{}: {}".format("elem",elem))
-    #
-    #     # If we have a string, try to load property
-    #     if isinstance(prop, str):
-    #         self.logger.info("Looking up property: {} on {}".format(prop, device))
-    #         device = self.getDevice(device)
-    #         prop = device.getProperty(prop)
-    #         self.logger.info("Property: {}".format(prop))
-    #
-    #     assert prop is not None
-    #
-    #     prop_name = prop.getName()
-    #     prop_type = prop.getType()
-    #     prop_value = []
-    #
-    #     self.logger.info("'{}' type: {}".format(prop_name, type_lookup.get(prop_type)))
-    #
-    #     if prop_type == PyIndi.INDI_TEXT:
-    #         tpy = prop.getText()
-    #         for t in tpy:
-    #             self.logger.debug("{} ({}) \t {}".format(t.name, t.label, t.text))
-    #             if elem is None or elem == t.name:
-    #                 prop_value.append((t.label, t.text))
-    #     elif prop_type == PyIndi.INDI_NUMBER:
-    #         tpy = prop.getNumber()
-    #         for t in tpy:
-    #             self.logger.debug("{} ({}) \t {}".format(t.name, t.label, t.value))
-    #             if elem is None or elem == t.name:
-    #                 prop_value.append((t.label, t.value))
-    #     elif prop_type == PyIndi.INDI_SWITCH:
-    #         tpy = prop.getSwitch()
-    #         print(tpy)
-    #         for t in tpy:
-    #             self.logger.debug("{} ({}) \t {}".format(t.name, t.label, switch_lookup.get(t.s)))
-    #             if elem is None or elem == t.name:
-    #                 prop_value.append((t.label, switch_lookup.get(t.s, 'UNKNOWN')))
-    #     elif prop_type == PyIndi.INDI_LIGHT:
-    #         tpy = prop.getLight()
-    #         for t in tpy:
-    #             self.logger.debug("{} ({}) \t {}".format(t.name, t.label, state_lookup.get(t.s)))
-    #             if elem is None or elem == t.name:
-    #                 prop_value.append((t.label, state_lookup.get(t.s, 'UNKNOWN')))
-    #     elif prop_type == PyIndi.INDI_BLOB:
-    #         tpy = prop.getBLOB()
-    #         for t in tpy:
-    #             self.logger.debug("{} ({}) \t {}".format(t.name, t.label, t.size))
-    #             if elem is None or elem == t.name:
-    #                 prop_value.append((t.label, t.size))
-    #
-    #     return prop_value
