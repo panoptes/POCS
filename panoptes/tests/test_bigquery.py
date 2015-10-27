3ba5aa3ba43478c9b7d5d0def1b0a14a2bfc802b
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-10-26 18:07:14 -1000

    Adding bigquery integration and testing

---
+++ panoptes/tests/test_bigquery.py
@@ -0,0 +1,38 @@
+import pytest
+
+from astropy.time import Time
+from astropy.tests.helper import remote_data
+from ..utils.google.bigquery import PanBigQuery
+
+bq = PanBigQuery('panoptes-survey')
+
+now = Time.now().isot
+table_name = 'test_sandbox_{}'.format(now.split('T')[0].replace('-','_'))
+dataset_id = 'playground'
+
+
+class TestBiqQuery(object):
+
+    @remote_data
+    def test_new_table(self):
+        schema = {
+            "fields": [
+                {"type": "STRING", "name": "unit"},
+                {"type": "TIMESTAMP", "name": "ts"},
+                {"type": "STRING", "name": "state"}
+            ]
+        }
+
+        result = bq._new_table(dataset_id, table_name, schema)
+
+        assert result.get('id') is not None
+
+    def test_insert_data(self):
+        row = {
+            "unit": 'PAN001',
+            "ts": now,
+            "state": "parked",
+        }
+        result = bq.stream_row_to_bigquery(dataset_id, table_name, row)
+
+        assert result.get('insertErrors') is None
