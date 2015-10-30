98c6fbcddd90b36982bf9591627fd2926385dc1c
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-10-30 08:42:56 -1000

    pep8

--- panoptes/utils/config.py
+++ panoptes/utils/config.py
@@ -2,26 +2,27 @@ import yaml
 import os
 import warnings
 
+
 def load_config():
-	""" Returns the config information """
+    """ Returns the config information """
 
-	# This is global
-	_config_file = '{}/config.yaml'.format(os.getenv('POCS', '/var/panoptes/POCS'))
-	_local_config_file = '{}/config_local.yaml'.format(os.getenv('POCS', '/var/panoptes/POCS'))
-	_config = dict()
+    # This is global
+    _config_file = '{}/config.yaml'.format(os.getenv('POCS', '/var/panoptes/POCS'))
+    _local_config_file = '{}/config_local.yaml'.format(os.getenv('POCS', '/var/panoptes/POCS'))
+    _config = dict()
 
-	# Load the global config
-	try:
-	    with open(_config_file, 'r') as f:
-	        _config.update(yaml.load(f.read()))
-	except IOError as err:
-		warnings.warn('Cannot open config file. Please make sure $POCS environment variable is set: {}'.format(err))
+    # Load the global config
+    try:
+        with open(_config_file, 'r') as f:
+            _config.update(yaml.load(f.read()))
+    except IOError as err:
+        warnings.warn('Cannot open config file. Please make sure $POCS environment variable is set: {}'.format(err))
 
-	# If there is a local config load that
-	try:
-	    with open(_local_config_file, 'r') as f:
-	        _config.update(yaml.load(f.read()))
-	except IOError as err:
-		pass
+    # If there is a local config load that
+    try:
+        with open(_local_config_file, 'r') as f:
+            _config.update(yaml.load(f.read()))
+    except IOError as err:
+        pass
 
-	return _config
+    return _config
