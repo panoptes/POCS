44a05a3a4d8d1e71cc29e5541a5f752ee702b33e
Wilfred T Gee <wtylergee@gmail.com>
2015-11-01 17:26:46 -1000

    web interface listens on all hosts

--- panoptes/admin/web_app.py
+++ panoptes/admin/web_app.py
@@ -51,5 +51,5 @@ class WebAdmin(tornado.web.Application):
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(WebAdmin())
-    http_server.listen(tornado.options.options.port)
+    http_server.listen(tornado.options.options.port, address='0.0.0.0')
     tornado.ioloop.IOLoop.instance().start()
