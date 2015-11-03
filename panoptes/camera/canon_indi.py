7fcc73c410b20eee1c4af0afe13641fca534c10d
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-02 20:21:18 -1000

    Don't set copyright info on iamges

--- panoptes/camera/canon_indi.py
+++ panoptes/camera/canon_indi.py
@@ -1,5 +1,7 @@
 import datetime
 
+from astropy.time import Time
+
 from . import AbstractCamera
 
 from ..utils.logger import has_logger
@@ -14,28 +16,28 @@ class Camera(AbstractCamera):
         super().__init__(config)
 
         self.config['init_commands'] = {
-            "artist": {"artist": "Project PANOPTES"},
+            # "artist": {"artist": "Project PANOPTES"},
+            # "ownername": {"ownername": "Project PANOPTES"},
+            # "copyright": {"copyright": "Project PANOPTES All Rights Reserved"},
             "autoexposuremode": {"autoexposuremode4": "On"},
             "autofocusdrive": {"autofocusdrive0": "Off", "autofocusdrive1": "On"},
             "autopoweroff": {"autopoweroff": "0"},
             "CAPTURE_FORMAT": {"FORMAT9": "On"},
-            "capturetarget": {"capturetarget1": "On"},
+            "capturetarget": {"capturetarget0": "On", "capturetarget1": "Off"},
             "CCD_COMPRESSION": {"CCD_RAW": "On"},
             "CCD_INFO": {"CCD_PIXEL_SIZE": "4.3", "CCD_PIXEL_SIZE_X": "4.3", "CCD_PIXEL_SIZE_Y": "4.3"},
             "CCD_ISO": {"ISO1": "On"},
             "continuousaf": {"continuousaf0": "Off", "continuousaf1": "On"},
-            "copyright": {"copyright": "Project PANOPTES All Rights Reserved"},
             "imageformatcf": {"imageformatcf9": "On"},
             "imageformatsd": {"imageformatsd9": "On"},
-            "ownername": {"ownername": "Project PANOPTES"},
             "picturestyle": {"picturestyle1": "On", },
             "reviewtime": {"reviewtime0": "On", },
             'Transfer Format': {'FITS': 'On', 'Native': 'Off'},
-            "UPLOAD_MODE": {"UPLOAD_BOTH": "On"},
+            "UPLOAD_MODE": {"UPLOAD_CLIENT": "On"},
             "UPLOAD_SETTINGS": {"UPLOAD_DIR": "/var/panoptes/images/", "UPLOAD_PREFIX": "{}_XXX".format(self.name)},
             "viewfinder": {"viewfinder0": "Off", "viewfinder1": "On"},
-            # "WCS_CONTROL": {"WCS_ENABLE": "Off"},
-            'WCS_CONTROL': {'WCS_ENABLE': 'On'},
+            "WCS_CONTROL": {"WCS_ENABLE": "Off"},
+            # 'WCS_CONTROL': {'WCS_ENABLE': 'On'},
         }
 
         try:
@@ -63,6 +65,10 @@ class Camera(AbstractCamera):
         """ Take an exposure """
         self.logger.info("<<<<<<<< Exposure >>>>>>>>>")
         self.logger.info('Taking {} second exposure'.format(exptime))
+
+        self.set_property("UPLOAD_SETTINGS", {
+            "UPLOAD_DIR": "/var/panoptes/images/".format(Time.now().isot.split('T')[0].replace('-', ''))
+        })
 
         try:
             output = self.set_property('CCD_EXPOSURE', {'CCD_EXPOSURE_VALUE': '{:.03f}'.format(exptime)})
