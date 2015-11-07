c59786d75d87e5c00afc0856de9c235d31a07193
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-07 09:40:23 -1000

    Typos

--- panoptes/state_machine/states/shutdown.py
+++ panoptes/state_machine/states/shutdown.py
@@ -4,4 +4,4 @@ from . import PanState
 class State(PanState):
     def __init__(self, *arg, **kwargs):
         super().__init__(self, *arg, **kwargs)
-        self.logger.info("In shtudwon")
+        self.logger.info("In shutdown")
