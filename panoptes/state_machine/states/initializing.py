cccd75dd4812445a8f0ce9831057e96c02f204d2
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-07 22:44:30 -1000

    Small cleanup to initializing. I'm sure more should be done here.

--- panoptes/state_machine/states/initializing.py
+++ panoptes/state_machine/states/initializing.py
@@ -1,16 +1,18 @@
 from . import PanState
 
+
 class State(PanState):
+
     def main(self):
-        next_state = 'scheduling'
+        next_state = 'parking'
 
         mount = self.panoptes.observatory.mount
 
         try:
             mount.initialize()
             mount.unpark()
-        except:
-            self.logger.warning("Problem initializing mount")
-            next_state = 'parking'
+            next_state = 'scheduling'
+        except Exception as e:
+            self.logger.warning("Problem initializing mount: {}".format(e))
 
         return next_state
