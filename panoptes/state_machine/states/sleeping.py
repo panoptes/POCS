d3cd0ea8f8bbfb944253f573ae7e01e959e3074c
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-08 13:18:01 -1000

    Use built in sleep

--- panoptes/state_machine/states/sleeping.py
+++ panoptes/state_machine/states/sleeping.py
@@ -5,9 +5,13 @@ class State(PanState):
 
     def main(self):
 
-        next_state = 'exit'
+        next_state = 'initializing'
 
-        self.logger.say("Looks like the sun is up, time for me to sleep.")
+        sleep_time = 300
+
+        self.logger.say("Looks like the sun is up, time for me to sleep. Sleeping for {} seconds".format(sleep_time))
+
+        self.sleep(sleep_time)
 
         # mount = self.panoptes.observatory.mount
 
