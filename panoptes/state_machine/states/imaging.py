9409047929e7feb5f2e62baf72f0feb6edb9dbcb
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-08 02:06:55 -1000

    Better image handling

--- panoptes/state_machine/states/imaging.py
+++ panoptes/state_machine/states/imaging.py
@@ -1,9 +1,13 @@
 from . import PanState
 
+from ...utils import error
+
 
 class State(PanState):
 
     def main(self):
+
+        image_time = 10.0
 
         self.logger.info("I'm finding exoplanets!")
 
@@ -12,16 +16,18 @@ class State(PanState):
         mount = self.panoptes.observatory.mount
 
         if mount.is_tracking:
-            image_time = 120.0
 
             for cam in self.panoptes.observatory.cameras:
                 try:
-                    cam.take_exposure(seconds=image_time, callback=self.process_image)
+                    cam.take_exposure(seconds=image_time)
+
+                    while image_time:
+                        image_time = image_time - 5.0
+                        self.logger.info("I'm still taking a picture. Just waiting. ")
+                        self.sleep(5)
+                except error.InvalidCommand as e:
+                    self.logger.warning("{} is already running a command.".format(cam.name))
                 except Exception as e:
                     self.logger.warning("Problem with imaging: {}".format(e))
 
         return next_state
-
-    def process_image(self):
-        """ Process the image """
-        self.logger.debug("Inside imaging state process_image")
