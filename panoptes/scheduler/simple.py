f797ae18649859c9dd53484b88a7c1aab28c28d8
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-08 10:16:17 -1000

    Add `_read_target_list` to build targets from simple list; NOTE: Requires internet connection to connect. Could add position to skip

--- panoptes/scheduler/simple.py
+++ panoptes/scheduler/simple.py
@@ -1,6 +1,11 @@
+import yaml
+
+from astropy.coordinates import SkyCoord
 
 from ..utils.config import load_config
 from .core import Scheduler
+
+from .target import Target
 
 
 class Scheduler(Scheduler):
@@ -22,3 +27,37 @@ class Scheduler(Scheduler):
             self.read_target_list()
 
         return self.list_of_targets.pop()
+
+    def read_target_list(self):
+        """Reads the target database file and returns a list of target dictionaries.
+
+        Returns:
+            list: A list of dictionaries for input to the get_target() method.
+        """
+        self.logger.info('Reading targets from file: {}'.format(self.targets_file))
+
+        with open(self.targets_file, 'r') as yaml_string:
+            yaml_list = yaml.load(yaml_string)
+
+        self.logger.debug("Simple target list: {}".format(yaml_list))
+
+        targets = []
+        for target_dict in yaml_list:
+            target_name = target_dict.get('name')
+            self.logger.debug("Looking for {}".format(target_name))
+
+            try:
+                t = SkyCoord.from_name(target_name)
+
+                target_dict['position'] = t.to_string(style='hmsdms')
+                target_dict['frame'] = t.frame.name
+
+                target = Target(target_dict)
+            except Exception as e:
+                self.logger.warning("Problem with Target: {}".format(e))
+            else:
+                targets.append(target)
+
+        self.list_of_targets = targets
+
+        return targets
