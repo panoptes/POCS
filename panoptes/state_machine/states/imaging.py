9951016763edce54f1df9d38b74f34167ff7f19b
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-08 12:52:23 -1000

    Smarter imaging step size; NOTE: Do we want to block in the state or return same state for loop.

--- panoptes/state_machine/states/imaging.py
+++ panoptes/state_machine/states/imaging.py
@@ -17,14 +17,17 @@ class State(PanState):
 
         if mount.is_tracking:
 
+            step_time = image_time / 4
+
             for cam in self.panoptes.observatory.cameras:
                 try:
                     cam.take_exposure(seconds=image_time)
+                    self.logger.say("I'm taking a picture for {} seconds".format(image_time))
 
                     while image_time:
-                        image_time = image_time - 5.0
-                        self.logger.say("I'm still taking a picture. Just waiting. ")
-                        self.sleep(5)
+                        image_time = image_time - step_time
+                        self.sleep(step_time)
+                        self.logger.say("I'm still taking that picture. Just waiting. ")
                 except error.InvalidCommand as e:
                     self.logger.warning("{} is already running a command.".format(cam.name))
                 except Exception as e:
