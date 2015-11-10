7fe99b7ea55e8b6022c94de242ffd7a59c33b347
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-11-10 07:53:47 -1000

    Hooking up websocket again

--- web/handlers/base.py
+++ web/handlers/base.py
@@ -1,9 +1,11 @@
 import tornado.escape
 import tornado.web
+from tornado.websocket import WebSocketHandler
 
-# import zmq
-# import pymongo
-# import bson.json_util as json_util
+import zmq
+from zmq.eventloop.zmqstream import ZMQStream
+
+from panoptes.utils.logger import has_logger
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -12,6 +14,7 @@ class BaseHandler(tornado.web.RequestHandler):
     BaseHandler is inherited by all Handlers and is responsible for any
     global operations. Provides the `db` property and the `get_current_user`
     """
+
     def initialize(self):
         self.config = self.settings['config']
         self.db = self.settings['db']
@@ -40,3 +43,35 @@ class MainHandler(BaseHandler):
         user_data = self.get_current_user()
 
         self.render("main.html", user_data=user_data)
+
+@has_logger
+class MyWebSocket(WebSocketHandler):
+
+    def open(self):
+        messaging = self.settings['messaging']
+        self.pubsub = ZMQPubSub(self.on_data, messaging, self.settings['name'])
+        self.pubsub.connect()
+        self.logger.info("WS Opened")
+
+    def on_message(self, message):
+        self.logger.info("WS Sent: {}".format(message))
+
+    def on_close(self):
+        self.logger.info("WS Closed")
+
+    def on_data(self, data):
+        self.logger.info("WS Received: {}".format(data))
+        self.write_message(data)
+
+
+class ZMQPubSub(object):
+
+    def __init__(self, callback, messaging, channel):
+        self.callback = callback
+        self.messaging = messaging
+        self.channel = channel
+
+    def connect(self):
+        self.socket = self.messaging.create_subscriber(channel=self.channel)
+        self.stream = ZMQStream(self.socket)
+        self.stream.on_recv(self.callback)
