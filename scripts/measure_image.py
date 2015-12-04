c0bb2192e18d775d97be4816ec07260f7ad5a170
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-12-03 19:49:48 -1000

    autopep8

--- scripts/measure_image.py
+++ scripts/measure_image.py
@@ -8,24 +8,25 @@ import IQMon
 from IQMon.image import Image
 from IQMon.telescope import Telescope
 
-def measure_image(file,\
-                 clobber_logs=False,\
-                 verbose=False,\
-                 nographics=False,\
-                 analyze_image=True,\
-                 record=True,\
-                 zero_point=False,\
-                 ):
 
-    ##-------------------------------------------------------------------------
-    ## Create Telescope Object
-    ##-------------------------------------------------------------------------
+def measure_image(file,
+                  clobber_logs=False,
+                  verbose=False,
+                  nographics=False,
+                  analyze_image=True,
+                  record=True,
+                  zero_point=False,
+                  ):
+
+    # -------------------------------------------------------------------------
+    # Create Telescope Object
+    # -------------------------------------------------------------------------
     config_file = os.path.expanduser('~joshw/git/POCS/scripts/IQMon_config.yaml')
     tel = Telescope(config_file)
 
-    ##-------------------------------------------------------------------------
-    ## Perform Actual Image Analysis
-    ##-------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # Perform Actual Image Analysis
+    # -------------------------------------------------------------------------
     with Image(file, tel) as im:
         im.make_logger(verbose=verbose, clobber=clobber_logs)
         im.read_image()
@@ -38,8 +39,8 @@ def measure_image(file,\
 
             is_blank = (im.n_stars_SExtracted < 100)
             if is_blank:
-                im.logger.warning('Only {} stars found.  Image may be blank.'.format(\
-                                   im.n_stars_SExtracted))
+                im.logger.warning('Only {} stars found.  Image may be blank.'.format(
+                    im.n_stars_SExtracted))
 
             if not im.image_WCS and not is_blank:
                 im.solve_astrometry()
@@ -54,29 +55,29 @@ def measure_image(file,\
 
         if record and not nographics:
             p1, p2 = (1.50, 0.50)
-            small_JPEG = im.raw_file_basename+"_fullframe.jpg"
-            im.make_JPEG(small_JPEG, binning=2,\
-                         p1=p1, p2=p2,\
-                         make_hist=False,\
-                         mark_pointing=True,\
-                         mark_detected_stars=True,\
-                         mark_catalog_stars=True,\
-                         mark_saturated=False,\
-                         quality=70,\
+            small_JPEG = im.raw_file_basename + "_fullframe.jpg"
+            im.make_JPEG(small_JPEG, binning=2,
+                         p1=p1, p2=p2,
+                         make_hist=False,
+                         mark_pointing=True,
+                         mark_detected_stars=True,
+                         mark_catalog_stars=True,
+                         mark_saturated=False,
+                         quality=70,
                          )
-            cropped_JPEG = im.raw_file_basename+"_crop.jpg"
-            im.make_JPEG(cropped_JPEG,\
-                         p1=p1, p2=p2,\
-                         make_hist=False,\
-                         mark_pointing=True,\
-                         mark_detected_stars=True,\
-                         mark_catalog_stars=False,\
-                         mark_saturated=False,\
-                         crop=(int(im.nXPix/2)-512,\
-                               int(im.nYPix/2)-512,\
-                               int(im.nXPix/2)+512,\
-                               int(im.nYPix/2)+512),\
-                         quality=70,\
+            cropped_JPEG = im.raw_file_basename + "_crop.jpg"
+            im.make_JPEG(cropped_JPEG,
+                         p1=p1, p2=p2,
+                         make_hist=False,
+                         mark_pointing=True,
+                         mark_detected_stars=True,
+                         mark_catalog_stars=False,
+                         mark_saturated=False,
+                         crop=(int(im.nXPix / 2) - 512,
+                               int(im.nYPix / 2) - 512,
+                               int(im.nXPix / 2) + 512,
+                               int(im.nYPix / 2) + 512),
+                         quality=70,
                          )
 
         im.clean_up()
@@ -89,39 +90,38 @@ def measure_image(file,\
 
 
 def main():
-    ##-------------------------------------------------------------------------
-    ## Parse Command Line Arguments
-    ##-------------------------------------------------------------------------
-    ## create a parser object for understanding command-line arguments
+    # -------------------------------------------------------------------------
+    # Parse Command Line Arguments
+    # -------------------------------------------------------------------------
+    # create a parser object for understanding command-line arguments
     parser = ArgumentParser(description="Describe the script")
-    ## add flags
+    # add flags
     parser.add_argument("-v", "--verbose",
-        action="store_true", dest="verbose",
-        default=False, help="Be verbose! (default = False)")
+                        action="store_true", dest="verbose",
+                        default=False, help="Be verbose! (default = False)")
     parser.add_argument("--no-graphics",
-        action="store_true", dest="nographics",
-        default=False, help="Turn off generation of graphics")
+                        action="store_true", dest="nographics",
+                        default=False, help="Turn off generation of graphics")
     parser.add_argument("-z", "--zp",
-        action="store_true", dest="zero_point",
-        default=False, help="Calculate zero point")
+                        action="store_true", dest="zero_point",
+                        default=False, help="Calculate zero point")
     parser.add_argument("-n", "--norecord",
-        action="store_true", dest="no_record",
-        default=False, help="Do not record results")
-    ## add arguments
+                        action="store_true", dest="no_record",
+                        default=False, help="Do not record results")
+    # add arguments
     parser.add_argument("filename",
-        type=str,
-        help="File Name of Input Image File")
+                        type=str,
+                        help="File Name of Input Image File")
     args = parser.parse_args()
 
     record = not args.no_record
 
-    measure_image(args.filename,\
-                  nographics=args.nographics,\
-                  zero_point=args.zero_point,\
-                  record=record,\
+    measure_image(args.filename,
+                  nographics=args.nographics,
+                  zero_point=args.zero_point,
+                  record=record,
                   verbose=args.verbose)
 
 
 if __name__ == '__main__':
     main()
-
