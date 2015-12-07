ea08ca20876ef909fcef18a263e7a9d78ee81c0f
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-12-07 10:06:56 -1000

    Rearrange the order of initialization to make more sense and be more efficient

--- panoptes/state_machine/states/initializing.py
+++ panoptes/state_machine/states/initializing.py
@@ -5,18 +5,24 @@ from ... utils import error
 
 class State(PanState):
 
-    def main(self):
+    def main(self, event_data):
 
         self.panoptes.say("Getting ready! Woohoo!")
 
         try:
-            for cam in self.panoptes.observatory.cameras:
-                cam.connect()
-
+            # Initialize the mount
             self.panoptes.observatory.mount.initialize()
 
+            # If successful, unpark and slew to home.
             if self.panoptes.observatory.mount.is_initialized:
                 self.panoptes.observatory.mount.unpark()
+
+                # Slew to home
+                self.panoptes.observatory.mount.slew_to_home()
+
+                # Initialize each of the cameras while slewing
+                for cam in self.panoptes.observatory.cameras:
+                    cam.connect()
 
                 # We have successfully initialized so we transition to the schedule state
                 self.panoptes.schedule()
