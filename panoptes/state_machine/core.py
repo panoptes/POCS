bdb5eb17586e7088b91f196dc314e75386c58011
Wilfred Tyler Gee <wtylergee@gmail.com>
2015-12-27 16:06:59 -1000

    Simplify loading of states and transitions

--- panoptes/state_machine/core.py
+++ panoptes/state_machine/core.py
@@ -44,10 +44,7 @@ class PanStateMachine(transitions.Machine, PanStateLogic):
         self._initial = kwargs.get('initial', 'sleeping')
 
         # Setup Transitions
-        self._transitions = kwargs['transitions']
-        self.transitions = [self._load_transition(transition) for transition in self._transitions]
-
-        # Setup States
+        self.transitions = kwargs['transitions']
         self.states = kwargs['states']
 
         # Get the asyncio loop
@@ -174,21 +171,9 @@ class PanStateMachine(transitions.Machine, PanStateLogic):
             self.logger.debug("Stopping event loop")
             self._loop.stop()
             self.logger.debug("Powering down")
-            self.power_down()
+            self.park()
         except Exception as e:
             self.logger.error("Problem powering down. PLEASE MANUALLY INSPECT THE MOUNT.")
             self.logger.error("Error: {}".format(e))
         finally:
             sys.exit(0)
-
-    def _load_transition(self, transition):
-        self.logger.debug("Loading transition: {}".format(transition))
-
-        # # Make sure the transition has the weather_is_safe condition on it
-        # conditions = listify(transition.get('conditions', []))
-
-        # conditions.append('is_safe')
-        # transition['conditions'] = conditions
-
-        self.logger.debug("Returning transition: {}".format(transition))
-        return transition
