792f2a9034c73f30f6528a2f0cd9ac53daf4ae3b
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-01-14 15:15:17 -1000

    Adding data util file for downloads

---
+++ panoptes/utils/data.py
@@ -0,0 +1,21 @@
+import os.path
+from astropy.utils import data
+from astroplan import download_IERS_A
+
+
+def download_all_files():
+    download_IERS_A()
+
+    for i in range(4214, 4219):
+        fn = 'index-{}.fits'.format(i)
+        data_folder = "/var/panoptes/astrometry.net/data"
+        dest = "{}/{}".format(data_folder, fn)
+
+        if not os.path.exists(dest):
+            url = "http://data.astrometry.net/4200/{}".format(fn)
+            df = data.download_file(url)
+            os.rename(df, dest)
+
+
+if __name__ == '__main__':
+    download_all_files()
