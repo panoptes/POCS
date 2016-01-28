0be49c48a3fe27def1bf315c4d5679ef673a4c9f
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-01-28 08:12:27 -1000

    Only make thumb if link

--- scripts/cr2_to_jpg.sh
+++ scripts/cr2_to_jpg.sh
@@ -13,12 +13,11 @@ THUMB=${LATEST/.jpg/_tn.png}
 # Convert CR2 to JPG
 dcraw -c -q 3 -a -w -H 5 -b 5 ${FNAME} | cjpeg -quality 90 > ${JPG}
 
-# Make thumbnail from jpg. Roughly 1/8th original
-convert ${JPG} -thumbnail 651x450 -background black -fill red \
-    -font ubuntu -pointsize 12 label:"${NAME}" -gravity South -append ${THUMB}
-
-
 if [[ $LINK == 'link' ]]; then
+    # Make thumbnail from jpg. Roughly 1/8th original
+    convert ${JPG} -thumbnail 651x450 -background black -fill red \
+        -font ubuntu -pointsize 12 label:"${NAME}" -gravity South -append ${THUMB}
+
     # Remove symlink
     rm ${LATEST}
     ln -s ${JPG} ${LATEST}
