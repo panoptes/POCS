6f95f9f517f9d3cea40a9e30d8aab961b93ffdca
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-01-28 08:09:32 -1000

    Make thumbnails and links

--- scripts/cr2_to_jpg.sh
+++ scripts/cr2_to_jpg.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
 FNAME=$1
-LINK=$2
+NAME=$2
+LINK=$3
 
-dcraw -c -q 3 -a -w -H 5 -b 5 ${FNAME} | cjpeg -quality 80 > ${FNAME/cr2/jpg}
+JPG=${FNAME/cr2/jpg}
+LATEST=${PANDIR}/images/latest.jpg
+
+# We only make a thumb of latest
+THUMB=${LATEST/.jpg/_tn.png}
+
+# Convert CR2 to JPG
+dcraw -c -q 3 -a -w -H 5 -b 5 ${FNAME} | cjpeg -quality 90 > ${JPG}
+
+# Make thumbnail from jpg. Roughly 1/8th original
+convert ${JPG} -thumbnail 651x450 -background black -fill red \
+    -font ubuntu -pointsize 12 label:"${NAME}" -gravity South -append ${THUMB}
+
 
 if [[ $LINK == 'link' ]]; then
-    ln -s ${FNAME/cr2/jpg} /var/panoptes/images/latest.jpg
+    # Remove symlink
+    rm ${LATEST}
+    ln -s ${JPG} ${LATEST}
+    # Annotate the symlink
+    convert ${LATEST} -orient bottom-right -background black -fill red -font ubuntu -pointsize 72 \
+        label:"${NAME}" -gravity South -append ${LATEST}
 fi
