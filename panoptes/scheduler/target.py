c60b0d8ff4b2be987b821480a55e4521a0f9b422
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-01-29 22:56:54 -1000

    * Proper file name

--- panoptes/scheduler/target.py
+++ panoptes/scheduler/target.py
@@ -208,10 +208,10 @@ class Target(FixedTarget):
             self._dx.append(delta[1].value)
 
             # Add to plot
-            ax = fig.subplot2grid((self._max_row, self._max_col), (self._num_row, self._num_col))
+            ax = self._drift_fig.subplot2grid((self._max_row, self._max_col), (self._num_row, self._num_col))
             ax.imshow(d2, origin='lower', cmap=cm.Blues_r)
 
-            ax.set_title(img.split('/')[-1].replace('.cr2', ''))
+            ax.set_title(last_image['img_file'].split('/')[-1].replace('.cr2', ''))
 
             self._drift_fig.savefig('/var/panoptes/images/drift.png')
 
