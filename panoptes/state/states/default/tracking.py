98dd21ca371bdc2d6812883eeca0c9da18d31c86
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-02-05 00:05:34 -1000

    * Adjust rate first

--- panoptes/state/states/default/tracking.py
+++ panoptes/state/states/default/tracking.py
@@ -12,21 +12,14 @@ def on_enter(event_data):
     # Make sure we have a target
     if target.current_visit is not None:
 
+        delta_ra_rate = target._offset_info.get('delta_ra_rate', 0.0)
+        if delta_ra_rate != 0.0:
+            pan.logger.debug("Delta RA Rate: {}".format(delta_ra_rate))
+            pan.say("I'm adjusting the tracking rate")
+            pan.observatory.mount.set_tracking_rate(delta=delta_ra_rate)
+
         # Get the delay for the RA and Dec and adjust mount accordingly.
         for direction in ['dec', 'ra']:
-
-            # Adjust the rate
-            # Adjust RA rate. NOTE: Dec not implemented for iOptron yet
-            if direction == 'ra':
-                delta_ra_rate = target._offset_info.get('delta_ra_rate', 0.0)
-                if delta_ra_rate != 0.0:
-                    pan.logger.debug("Delta RA Rate: {}".format(delta_ra_rate))
-                    pan.say("I'm adjusting the tracking rate")
-                    pan.observatory.mount.set_tracking_rate(delta=delta_ra_rate)
-                    time.sleep(0.5)  # Make sure command goes through
-
-            # delta_dec_rate = target._offset_info.get('delta_dec_rate', 0.0)
-            # pan.logger.debug("Delta Dec Rate: {}".format(delta_dec_rate))
 
             # Now adjust for existing offset
             key = '{}_ms_offset'.format(direction)
