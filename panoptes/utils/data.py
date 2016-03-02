a218b56fe3f71de1d43eb4a5ad66ff4db24cf5c3
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-03-02 10:43:29 -1000

    Accept folder as path

--- panoptes/utils/data.py
+++ panoptes/utils/data.py
@@ -1,4 +1,5 @@
-import os.path
+import os
+import argparse
 from astropy.utils import data
 from astroplan import download_IERS_A
 
@@ -17,4 +18,15 @@ def download_all_files(data_folder="{}/astrometry/data".format(os.getenv('PANDIR
 
 
 if __name__ == '__main__':
-    download_all_files()
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('folder', help='Folder to place astrometry data')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.folder):
+        print("{} does not exist.".format(args.folder))
+
+    download_all_files(data_folder=args.folder)
