f6a6c383d94f859904970d875e7d732d2b457ba4
Wilfred T Gee <wtylergee@gmail.com>
2016-05-09 17:25:16 -1000

    Adding a basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PANOPTES Environmental Analysis System',
+      version='0.0.1',
+      description='Scripts and utilties for monitoring a PANOPTES environment',
+      author='Wilfred Gee',
+      author_email='wtylergee@gmail.com',
+      packages=[],
+     )
