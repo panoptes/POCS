91abba98d3fbd5a68e85894c8bbc696f4a09c0ee
Wilfred T Gee <wtylergee@gmail.com>
2016-05-11 18:56:51 -1000

    * Cleaning up the PanProcess     * Loop is not in the class itself

--- panoptes/utils/process.py
+++ panoptes/utils/process.py
@@ -1,16 +1,14 @@
-import os
-import time
 import multiprocessing
 
 from panoptes.utils.logger import get_root_logger
-from panoptes.utils.config import load_config
 
 
 class PanProcess(object):
     """ Creates a simple way to launch a separate process """
 
-    def __init__(self, name='PanProcess', **kwargs):
-        super().__init__()
+    def __init__(self, name='PanProcess', target_method=None, **kwargs):
+        assert target_method is not None
+
         self.name = name
 
         self.db = None
@@ -18,19 +16,16 @@ class PanProcess(object):
 
         self._loop_delay = kwargs.get('loop_delay', 60)
 
-        self.config = kwargs.get('config', load_config())
         self.logger = kwargs.get('logger', get_root_logger())
 
-        assert self.config is not None, self.logger.warning("Config not set for process")
         assert self.logger is not None, self.logger.warning("Logger not set for process")
 
+        self.logger.info("Creating separate process")
         # Setup the actual process
-        self.process = multiprocessing.Process(target=self._loop_capture)
+        self.process = multiprocessing.Process(target=target_method)
         self.process.daemon = True
         self.process.name = 'PanProcess_{}'.format(self.name).replace(' ', '_')
-
-        self._in_loop = False
-        self._is_capturing = False
+        self.logger.info("Separate process created")
 
     def start_capturing(self):
         """ Starts the capturing loop for the process
@@ -39,8 +34,7 @@ class PanProcess(object):
         typically call this method.
         """
 
-        self.is_capturing = True
-        self.logger.info("Staring capture loop for process {}".format(self.process.pid))
+        self.logger.info("Starting capture loop for process {}".format(self.process.pid))
         try:
             self.process.start()
         except AssertionError as err:
@@ -53,39 +47,5 @@ class PanProcess(object):
         typically call this method.
         """
         self.logger.info("Stopping capture loop for {}".format(self.process.pid))
-        self.is_capturing = False
-        while self._in_loop:
-            print("\tWaiting for loop to finish")
-            time.sleep(int(self._loop_delay / 2))
-        else:
-            print("Terminating process {} {}".format(self.name, self.process.pid))
-            self.process.terminate()
-            self.process.join()
-
-    @property
-    def process_exists(self):
-        """ Checks OS for running process ID (PID) and returns boolean """
-        return os.path.exists('/proc/{}'.format(self.process.pid))
-
-    @property
-    def is_capturing(self):
-        """ Whether or not the loop has started capturing """
-        return self._is_capturing
-
-    @is_capturing.setter
-    def is_capturing(self, value):
-        self._is_capturing = value
-
-    def step(self):
-        """ Called once per loop. This is the main method for a subclass to override """
-        raise NotImplementedError("Must implement `step` method")
-
-    def _loop_capture(self):
-        """ Calls commands to be performed each time through the loop """
-        while self.is_capturing:
-            self._in_loop = True
-            self.step()
-            time.sleep(self._loop_delay)
-        else:
-            self._in_loop = False
-            self.logger.info("Stopping loop for {}".format(self.name))
+        self.process.terminate()
+        self.process.join()
