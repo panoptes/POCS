f0207e2809fa434e61b9ec729eef2dee90aaefec
Wilfred T Gee <wtylergee@gmail.com>
2016-05-15 07:21:54 -1000

    * Fix up logic for `yesterday` flag * Simplified date creation (NOTE: Only 24 hour periods are allowed)

--- scripts/export_data.py
+++ scripts/export_data.py
@@ -4,30 +4,36 @@ import subprocess
 import warnings
 import shutil
 
-from datetime import datetime as dt
+from datetime import date, timedelta, datetime
 
 
-def main(start_date=None, end_date=None, database=None, collections=list(), verbose=False):
+def main(start_date=None, end_date=None, database=None, collections=list(), yesterday=False, verbose=False):
     me_cmd = shutil.which('mongoexport')
     assert me_cmd, warnings.warn("No mongoexport command found!")
 
-    if start_date is None:
-        start_dt = dt.utcnow()
+    if yesterday:
+        start_dt = date.today() - timedelta(1)
+        start = datetime(start_dt.year, start_dt.month, start_dt.day, 0, 0, 0, 0)
+        end = datetime(start_dt.year, start_dt.month, start_dt.day, 23, 59, 59, 0)
     else:
-        start_dt = dt.strptime('{} 23:59:59'.format(start_date), '%Y%m%dUT %H:%M:%S')
+        assert start_date, warnings.warn("start-date required if not using yesterday")
 
-    if end_date is None:
-        end_dt = start_dt
-    else:
-        end_dt = dt.strptime('{} 23:59:59'.format(end_date), '%Y%m%dUT %H:%M:%S')
+        y, m, d = [int(x) for x in start_date.split('-')]
+        start_dt = date(y, m, d)
 
-    start = dt(start_dt.year, start_dt.month, start_dt.day, 0, 0, 0, 0)
-    end = dt(end_dt.year, end_dt.month, end_dt.day, 23, 59, 59, 0)
+        if end_date is None:
+            end_dt = start_dt
+        else:
+            y, m, d = [int(x) for x in end_date.split('-')]
+            end_dt = date(y, m, d)
+
+        start = datetime.fromordinal(start_dt.toordinal())
+        end = datetime(end_dt.year, end_dt.month, end_dt.day, 23, 59, 59, 0)
 
     date_query = '{"date": {"$gte": "' + start.isoformat() + '", "$lte": "' + end.isoformat() + '"}})'
 
     for collection in collections:
-        out_file = '/var/panoptes/backup/{}_{}.json'.format(collection, start_date)
+        out_file = '/var/panoptes/backups/{}_{}.json'.format(collection, start_date)
         export_cmd = [me_cmd, '--quiet', '-d', database, '-c', collection, '-q', date_query, '--out', out_file]
 
         if verbose:
@@ -41,12 +47,14 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Make a plot of the weather for a give date.")
 
+    parser.add_argument('-y', '--yesterday', action="store_true", dest='yesterday', default=True,
+                        help="Yeserday\'s data, defaults to True if start-date is not provided, False otherwise.")
     parser.add_argument("-s", "--start-date", type=str, dest="start_date", default=None,
-                        help="Starting UT Date to plot. If no end-date is provided plots entire date")
+                        help="[yyyy-mm-dd] Start date, defaults to None; if provided, yesterday is ignored.")
     parser.add_argument("-e", "--end-date", type=str, dest="end_date", default=None,
-                        help="Ending UT Date to plot, defaults to None, causing start-date to plot entire day.")
+                        help="[yyyy-mm-dd] End date, defaults to None, causing start-date to exports full day.")
     parser.add_argument('-d', '--database', type=str, dest='database',
-                        default='panoptes', help="Mongo db to use for export.")
+                        default='panoptes', help="Mongo db to use for export, defaults to 'panoptes'")
     parser.add_argument('-c', '--collections', type=str, nargs='+',
                         dest='collections', help="Collections to export. One file per collection will be generated.")
     parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Be verbose.")
@@ -55,5 +63,8 @@ if __name__ == '__main__':
 
     if args.end_date is not None:
         assert args.start_date is not None, warnings.warn("Can't use an end date without a start date")
+
+    if args.start_date is not None:
+        args.yesterday = False
 
     main(**vars(args))
