f46cd86c0570ef46b601b7511628ad3c5bf6d6a9
Wilfred T Gee <wtylergee@gmail.com>
2016-05-17 03:24:19 -1000

    Getting rid of extra quotes on subprocess

--- peas/webcam.py
+++ peas/webcam.py
@@ -171,7 +171,7 @@ class Webcam(object):
         out_file = '{}/{}.mp4'.format(directory, self.port_name)
 
         cmd = [ffmpeg_cmd, '-f', 'image2', '-r', str(fps), '-pattern_type', 'glob',
-               '-i', '"{}{}*.jpeg"'.format(directory, self.port_name), '-c:v', 'libx264', '-pix_fmt', 'yuv420p', out_file]
+               '-i', '{}{}*.jpeg'.format(directory, self.port_name), '-c:v', 'libx264', '-pix_fmt', 'yuv420p', out_file]
 
         self.logger.debug("Timelapse command: {}".format(cmd))
         try:
