068e7029de18da73974d8c0104f794405d8dc230
Wilfred T Gee <wtylergee@gmail.com>
2016-05-26 06:16:31 -1000

    * Add a listener for web messages

--- pocs/utils/messaging.py
+++ pocs/utils/messaging.py
@@ -1,5 +1,8 @@
 import datetime
+import time
 import zmq
+
+from subprocess import Process
 
 from astropy import units as u
 from bson import ObjectId
@@ -80,7 +83,7 @@ class PanMessaging(object):
 
         return socket
 
-    def register_listener(self, channel='', callback=None, port=None, bind=False, connect=False):
+    def register_listener(self, channel='', callback=None, port=None, bind=False, connect=False, start_proc=False):
         """ Create a listener
 
         Args:
@@ -102,27 +105,29 @@ class PanMessaging(object):
 
         socket.setsockopt_string(zmq.SUBSCRIBE, channel)
 
-        # if callback is None:
-        #     self.logger.debug('Creating call back for messages')
+        if start_proc or callback is not None:
+            if callback is None:
+                self.logger.debug('Creating call back for messages')
 
-        #     def show_web_msg():
-        #         self.logger.info('In show_web_msg')
-        #         while True:
-        #             msg_type, msg = socket.recv_string().split(' ', maxsplit=1)
-        #             # if msg_type == channel or channel == '*':
-        #             self.logger.info("Web message: {} {}".format(msg_type, msg))
+                def show_web_msg():
+                    self.logger.info('In show_web_msg')
+                    while True:
+                        msg_type, msg = socket.recv_string().split(' ', maxsplit=1)
+                        # if msg_type == channel or channel == '*':
+                        self.logger.info("Web message: {} {}".format(msg_type, msg))
 
-        #             time.sleep(1)
+                        time.sleep(1)
 
-        #     proc = Process(target=show_web_msg)
-        # else:
-        #     # Create another process to call callback
-        #     proc = Process(target=callback, args=(socket,))
+                proc = Process(target=show_web_msg)
+            else:
+                # Create another process to call callback
+                proc = Process(target=callback, args=(socket,))
 
-        # proc.start()
-        # self.logger.debug("Starting listener process: {}".format(proc.pid))
+            proc.start()
+            self.logger.debug("Starting listener process: {}".format(proc.pid))
 
         self.logger.debug("Starting listener for channel: {}".format(channel))
+
         return socket
 
     def send_message(self, channel, message):
