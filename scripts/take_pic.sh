856717991744c22db6d4679dadbfe64ae709a1ef
Wilfred T Gee <wtylergee@gmail.com>
2016-05-27 16:07:07 -1000

    Filename

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -8,9 +8,10 @@ F=$3
 # echo "T=${T}"
 # echo "F=${F}"
 
-opts="--camera=\"Canon EOS 100D\" --debug --debug-logfile=$HOME/gphoto2.log --port ${P} --set-config eosremoterelease=Immediate --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=1s --filename=${F}"
 
 # echo $opts
 
 /usr/bin/gphoto2 --port $P --reset
-/usr/bin/gphoto2 ${opts} > /dev/null
+/usr/bin/gphoto2 --camera="Canon EOS 100D" --debug --debug-logfile=$HOME/gphoto2.log \
+    --port ${P} --set-config eosremoterelease=Immediate --wait-event=${T}s \
+    --set-config eosremoterelease=4 --wait-event-and-download=1s --filename="${F}%03n.cr2" > /dev/null
