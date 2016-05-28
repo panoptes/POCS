b9b859e7fd360bf0d68b6830de162ba7a4a43543
Wilfred T Gee <wtylergee@gmail.com>
2016-05-27 21:29:30 -1000

    Pass full filename

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -12,6 +12,6 @@ gphoto2 --port $P --reset
 gphoto2 --camera="Canon EOS 100D" --port $P --set-config eosremoterelease=Immediate &> /dev/null
 sleep ${T}
 gphoto2 --port $P --set-config eosremoterelease=4 &> /dev/null
-gphoto2 --port $P --wait-event-and-download=1s --filename "${F}_%H%M%S.cr2" &> /dev/null
+gphoto2 --port $P --wait-event-and-download=1s --filename "${F}" &> /dev/null
 
 echo "Done with pic"
