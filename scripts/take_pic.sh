5a09afecf23a6ab0e0c0c608009d4fe514f8eaad
Panoptes PAN002 <panoptes@PAN002.subaru.nao.ac.jp>
2016-05-27 21:28:15 -1000

    Updating capture script

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -4,13 +4,14 @@ P=$1
 T=$2
 F=$3
 echo 'Taking picture'
-# echo "P=${P}"
-# echo "T=${T}"
-# echo "F=${F}"
-
-# echo $opts
+echo "P=${P}"
+echo "T=${T}"
+echo "F=${F}"
 
 gphoto2 --port $P --reset
-gphoto2 --camera="Canon EOS 100D" --debug --debug-logfile=$HOME/gphoto2.log \
-    --port ${P} --set-config eosremoterelease=Immediate --wait-event=${T}s \
-    --set-config eosremoterelease=4 --wait-event-and-download=1s --filename="${F}%03n.cr2" > /dev/null
+gphoto2 --camera="Canon EOS 100D" --port $P --set-config eosremoterelease=Immediate &> /dev/null
+sleep ${T}
+gphoto2 --port $P --set-config eosremoterelease=4 &> /dev/null
+gphoto2 --port $P --wait-event-and-download=1s --filename "${F}_%H%M%S.cr2" &> /dev/null
+
+echo "Done with pic"
