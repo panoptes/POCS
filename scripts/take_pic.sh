fa5823728137d44d1a32858167e085def408bdc8
Wilfred T Gee <wtylergee@gmail.com>
2016-05-27 18:19:17 -1000

    Don't specify gphoto2 path

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -3,15 +3,14 @@
 P=$1
 T=$2
 F=$3
-# echo 'Taking picture'
+echo 'Taking picture'
 # echo "P=${P}"
 # echo "T=${T}"
 # echo "F=${F}"
 
-
 # echo $opts
 
-/usr/bin/gphoto2 --port $P --reset
-/usr/bin/gphoto2 --camera="Canon EOS 100D" --debug --debug-logfile=$HOME/gphoto2.log \
+gphoto2 --port $P --reset
+gphoto2 --camera="Canon EOS 100D" --debug --debug-logfile=$HOME/gphoto2.log \
     --port ${P} --set-config eosremoterelease=Immediate --wait-event=${T}s \
     --set-config eosremoterelease=4 --wait-event-and-download=1s --filename="${F}%03n.cr2" > /dev/null
