797e39ec7c9c715933b630e4b186755d036c20b9
Wilfred T Gee <wtylergee@gmail.com>
2016-07-06 13:32:50 -1000

    Trying take_pic script

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -9,16 +9,20 @@ echo "T=${T}"
 echo "F=${F}"
 
 # gphoto2 --port $P --reset
-# gphoto2 --camera="Canon EOS 100D" --port $P --set-config eosremoterelease=Immediate &> /dev/null
-# sleep ${T}
-# gphoto2 --port $P --set-config eosremoterelease=4 &> /dev/null
-# gphoto2 --port $P --wait-event-and-download=1s --filename "${F}" &> /dev/null
+gphoto2 --camera="Canon EOS 100D" --port `readlink -f /dev/canon0 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=Immediate &> /dev/null
+gphoto2 --camera="Canon EOS 100D" --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=Immediate &> /dev/null
+sleep ${T}
+gphoto2 --port `readlink -f /dev/canon0 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=4 &> /dev/null
+gphoto2 --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=4 &> /dev/null
+
+gphoto2 --port `readlink -f /dev/canon0 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --wait-event-and-download=1s --filename "${F}" &> /dev/null
+gphoto2 --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --wait-event-and-download=1s --filename "${F}" &> /dev/null
 
 #gphoto2 --port $P --reset
 
 # Open shutter
-gphoto2 --camera="Canon EOS 100D" --port $P --set-config eosremoterelease=Immediate \
-        --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=2s \
-        --filename "${F}"
+# gphoto2 --camera="Canon EOS 100D" --port $P --set-config eosremoterelease=Immediate \
+#         --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=2s \
+#         --filename "${F}"
 
 echo "Done with pic"
