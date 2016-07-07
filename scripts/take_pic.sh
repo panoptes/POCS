beeab12dead516d5ee6ca5a62d75af474bc955bd
Wilfred T Gee <wtylergee@gmail.com>
2016-07-06 16:32:12 -1000

    This is a lot of coding around the issue (which is probably a power cable issue)

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-P=$1
-T=$2
-F=$3
+T=$1
 echo 'Taking picture'
-echo "P=${P}"
-echo "T=${T}"
-echo "F=${F}"
+echo "T = ${T}s"
 
 # gphoto2 --port $P --reset
 gphoto2 --camera="Canon EOS 100D" --port `readlink -f /dev/canon0 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=Immediate &> /dev/null
-gphoto2 --camera="Canon EOS 100D" --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=Immediate &> /dev/null
+# gphoto2 --camera="Canon EOS 100D" --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=Immediate &> /dev/null
 sleep ${T}
 gphoto2 --port `readlink -f /dev/canon0 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=4 &> /dev/null
-gphoto2 --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=4 &> /dev/null
+# gphoto2 --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --set-config eosremoterelease=4 &> /dev/null
 
-gphoto2 --port `readlink -f /dev/canon0 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --wait-event-and-download=1s --filename "${F}" &> /dev/null
-gphoto2 --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --wait-event-and-download=1s --filename "${F}" &> /dev/null
-
-#gphoto2 --port $P --reset
-
-# Open shutter
-# gphoto2 --camera="Canon EOS 100D" --port $P --set-config eosremoterelease=Immediate \
-#         --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=2s \
-#         --filename "${F}"
+gphoto2 --port `readlink -f /dev/canon0 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --wait-event-and-download=2s --filename "${F}" &> /dev/null
+# gphoto2 --port `readlink -f /dev/canon1 | cut -d "/" -f4-6 | sed 's/\//:/' | sed 's/\//,/'` --wait-event-and-download=1s --filename "${F}" &> /dev/null
 
 echo "Done with pic"
