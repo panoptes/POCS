ce719cc491073c4a6f6fdc5cc09e84888ab32ebb
Wilfred T Gee <wtylergee@gmail.com>
2016-07-17 15:21:10 +1000

    Actually don't show any zmq messages; will log elsewhere

--- pocs/utils/messaging.py
+++ pocs/utils/messaging.py
@@ -116,9 +116,6 @@ class PanMessaging(object):
 
         full_message = '{} {}'.format(channel, msg_object)
 
-    a    if channel != 'weather':
-            self.logger.debug("Sending message: {}".format(full_message))
-
         # Send the message
         self.publisher.send_string(full_message, flags=zmq.NOBLOCK)
 
