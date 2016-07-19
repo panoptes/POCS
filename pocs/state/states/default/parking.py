4a66dd89dc5d048cb65e17990a5510b306996cde
Wilfred T Gee <wtylergee@gmail.com>
2016-07-19 13:32:40 +1000

    Hack for parking mount

--- pocs/state/states/default/parking.py
+++ pocs/state/states/default/parking.py
@@ -8,6 +8,10 @@ def on_enter(event_data):
         while not pan.observatory.mount.is_parked:
             pan.sleep()
 
+        # The mount is currently not parking in correct position so we manually move it there.
+        pan.observatory.mount.unpark()
+        pan.observatory.mount.move_direction(direction='south', seconds=11.0)
+
         pan.next_state = 'parked'
 
     except Exception as e:
