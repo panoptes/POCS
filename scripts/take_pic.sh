dac023bb6f6e03659893086d8153a27d3b266d35
Wilfred T Gee <wtylergee@gmail.com>
2016-07-20 15:07:15 +1000

    * While the port keeps resetting it is cleaner to just use model name

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -7,7 +7,7 @@ echo 'Taking picture'
 echo "T = ${T}s"
 
 # Open shutter
-gphoto2 --camera="Canon EOS 100D" --port $P --set-config eosremoterelease=Immediate \
+gphoto2 --camera="Canon EOS 100D" --set-config eosremoterelease=Immediate \
          --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=2s \
          --filename "${F}"
 
