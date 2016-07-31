01d5ac89de59e05d90fc45b899466b0d1a16b52f
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-07-31 12:39:38 +1000

    Adding a setup-env script that can set environment variables and create dirs as needed. Also better checking for setup environment

--- pocs/__init__.py
+++ pocs/__init__.py
@@ -14,19 +14,10 @@ from .utils.config import load_config
 from .utils.logger import get_root_logger
 from warnings import warn
 
-if sys.version_info[:2] < (3, 0):
-    warn("POCS requires Python 3.x to run")
-
 try:
     from .version import version as __version__
 except ImportError:
-    # TODO: Issue a warning using the logging framework
     __version__ = ''
-try:
-    from .version import githash as __githash__
-except ImportError:
-    # TODO: Issue a warning using the logging framework
-    __githash__ = ''
 
 ##################################################################################################
 # Private Methods
@@ -42,22 +33,33 @@ def _check_environment():
 
         POCS    Base directory for PANOPTES
     """
+    if sys.version_info[:2] < (3, 0):
+        warn("POCS requires Python 3.x to run")
+
+    pandir = os.getenv('PANDIR')
     pocs = os.getenv('POCS')
     if pocs is None:
         sys.exit('Please make sure $POCS environment variable is set')
+
+    if not os.path.exists(pocs):
+        sys.exit("$POCS dir does not exist or is empty: {}".format(pocs))
+
+    if not os.path.exists("{}/logs".format(pandir)):
+        print("Creating log dir at {}/logs".format(pandir))
+        os.makedirs("{}/logs".format(pandir))
 
 
 def _check_config(temp_config):
     """ Checks the config file for mandatory items """
 
     if 'directories' not in temp_config:
-        warn('directories must be specified in config_local.yaml')
+        sys.exit('directories must be specified in config')
 
     if 'mount' not in temp_config:
-        warn('Mount must be specified in config')
+        sys.exit('Mount must be specified in config')
 
     if 'state_machine' not in temp_config:
-        warn('State Table must be specified in config')
+        sys.exit('State Table must be specified in config')
 
     return temp_config
 
