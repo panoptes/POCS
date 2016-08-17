7e34ce40fd4257a7ae14c7f2a61b2667c7a3a232
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-08-18 09:47:47 +1000

    Cleanup of pointing

--- pocs/state/states/default/pointing.py
+++ pocs/state/states/default/pointing.py
@@ -5,7 +5,7 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
 
-from ....utils import images
+from ....utils import current_time
 
 
 def on_enter(event_data):
@@ -24,37 +24,41 @@ def on_enter(event_data):
     """
     pocs = event_data.model
 
+    # This should all move to the `states.pointing` module or somewhere else
+    point_config = pocs.config.get('pointing', {})
+    pointing_exptime = point_config.get('exptime', 30) * u.s
+
+    pocs.next_state = 'parking'
+
     try:
-        pocs.say("Taking guide picture.")
+        pocs.say("Taking pointing picture.")
 
-        # Get guide image
-        guide_camera = pocs.observatory.primary_camera
-        filename = 'guide_{:03.0f}.cr2'.format(pocs._pointing_iteration)
-        pocs.logger.debug("Path for guide: {}".format(filename))
+        primary_camera = pocs.observatory.primary_camera
+        observation = pocs.observatory.current_observation
 
-        # Take guide picture and wait for result
+        filename = "{}/".format(observation.name, primary_camera.uid, observation.seq_time, current_time(flatten=True))
+
+        # Take pointing picture and wait for result
         try:
-            proc = guide_camera.take_exposure(seconds=pocs._pointing_exptime, filename=filename)
-            pocs.logger.debug("Waiting for guide image: {}".format(filename))
-            pocs.logger.debug("Proc: {}".format(proc))
+            proc = primary_camera.take_exposure(seconds=pointing_exptime, filename=filename)
+            pocs.logger.debug("Waiting for pointing: PID {} File {}".format(proc.pid, filename))
             proc.wait(timeout=1.5 * pocs._pointing_exptime.value)
         except subprocess.TimeoutExpired:
             pocs.logger.debug("Killing camera, timeout expired")
             proc.terminate()
-            pocs.next_state = 'park'
         except Exception as e:
             pocs.logger.error("Problem waiting for images: {}".format(e))
-            pocs.next_state = 'park'
         else:
-            # Sync the image. This will start a slew
-            sync_coordinates(pocs, filename)
+            # Image object methods go here
+            # sync_coordinates(pocs, filename, point_config)
+
+            pocs.next_state = 'tracking'
 
     except Exception as e:
         pocs.say("Hmm, I had a problem checking the pointing error. Sending to parking. {}".format(e))
-        pocs.next_state = 'park'
 
 
-def sync_coordinates(pocs, fname):
+def sync_coordinates(pocs, fname, point_config):
     """ Adjusts pointing error from the most recent image.
 
     Uses utility function to return pointing error. If the error is off by some
@@ -73,7 +77,10 @@ def sync_coordinates(pocs, fname):
     u.Quantity
         The separation between the center of the solved image and the target.
     """
-    pocs.say("Ok, I've got the guide picture, let's see how close we are.")
+    pocs.say("Ok, I've got the pointing picture, let's see how close we are.")
+
+    pointing_threshold = point_config.get('threshold', 0.01) * u.deg
+
     pocs.logger.debug("Getting pointing error")
     pocs.logger.debug("Processing image: {}".format(fname))
 
@@ -83,31 +90,33 @@ def sync_coordinates(pocs, fname):
     target = pocs.observatory.current_target
     pocs.logger.debug("Target: {}".format(target))
 
-    fits_headers = pocs.observatory._get_standard_headers(target=target)
-    pocs.logger.debug("Guide headers: {}".format(fits_headers))
+    fits_headers = pocs.observatory.get_standard_headers(target=target)
+    pocs.logger.debug("pointing headers: {}".format(fits_headers))
 
     kwargs = {}
     kwargs['ra'] = target.ra.value
     kwargs['dec'] = target.dec.value
     kwargs['radius'] = 15.0
 
+    ############################################################################
+    # Image object method replaces following
+    ############################################################################
     pocs.logger.debug("Processing CR2 files with kwargs: {}".format(kwargs))
     processed_info = images.process_cr2(fname, fits_headers=fits_headers, timeout=45, **kwargs)
-    # pocs.logger.debug("Processed info: {}".format(processed_info))
 
     # Use the solve file
     fits_fname = processed_info.get('solved_fits_file', None)
 
     if os.path.exists(fits_fname):
-        pocs.logger.debug("Solved guide file: {}".format(fits_fname))
+        pocs.logger.debug("Solved pointing file: {}".format(fits_fname))
         # Get the WCS info and the HEADER info
         pocs.logger.debug("Getting WCS and FITS headers for: {}".format(fits_fname))
 
         wcs_info = images.get_wcsinfo(fits_fname)
 
-        # Save guide wcsinfo to use for future solves
-        target.guide_wcsinfo = wcs_info
-        pocs.logger.debug("WCS Info: {}".format(target.guide_wcsinfo))
+        # Save pointing wcsinfo to use for future solves
+        target.pointing_wcsinfo = wcs_info
+        pocs.logger.debug("WCS Info: {}".format(target.pointing_wcsinfo))
 
         target = None
         with fits.open(fits_fname) as hdulist:
@@ -126,20 +135,14 @@ def sync_coordinates(pocs, fname):
 
         pocs.logger.debug("Solved separation: {}".format(separation))
     else:
-        pocs.logger.warning("Could not solve guide image")
+        pocs.logger.warning("Could not solve pointing image")
 
-    if separation < pocs._pointing_threshold:
-        pocs.say("I'm pretty close to the target, starting track.")
-        pocs.next_state = 'tracking'
+    ############################################################################
+    # End replacement
+    ############################################################################
 
-    elif pocs._pointing_iteration >= pocs._max_iterations:
-        pocs.say("I've tried to get closer to the target but can't. I'll just observe where I am.")
-        pocs.next_state = 'tracking'
-
-    else:
+    if separation > pointing_threshold:
         pocs.say("I'm still a bit away from the target so I'm going to try and get a bit closer.")
-
-        pocs._pointing_iteration = pocs._pointing_iteration + 1
 
         # Tell the mount we are at the target, which is the center
         pocs.say("Syncing with the latest image...")
@@ -150,5 +153,3 @@ def sync_coordinates(pocs, fname):
         if has_target:
             if target is not None:
                 pocs.observatory.mount.set_target_coordinates(target)
-
-        pocs.next_state = 'slewing'
