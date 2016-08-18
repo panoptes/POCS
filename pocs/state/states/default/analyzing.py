66b9fead07525ddff3d62d4d06f338970bc23fa4
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-08-18 15:36:57 +1000

    State Cleanup * Analyzing state

--- pocs/state/states/default/analyzing.py
+++ pocs/state/states/default/analyzing.py
@@ -5,20 +5,17 @@ def on_enter(event_data):
     pocs.say("Analyzing image...")
 
     try:
-        target = pocs.observatory.current_target
-        pocs.logger.debug("For analyzing: Target: {}".format(target))
+        observation = pocs.observatory.current_observation
 
         image_info = pocs.observatory.analyze_recent()
-        # TODO: Handle Quantity correctly
-        # pocs.db.insert_current('images', image_info)
 
         pocs.logger.debug("Image information: {}".format(image_info))
 
-        if target.current_visit.done_exposing and target.done_visiting:
-            # We have successfully analyzed this visit, so we go to next
+        pocs.logger.debug("Observation exposure: {} / {}".format(observation.current_exp, observation.min_nexp))
+        if (observation.current_exp - observation.min_nexp) % observation.exp_set_size == 0:
             pocs.next_state = 'scheduling'
         else:
             pocs.next_state = 'tracking'
     except Exception as e:
         pocs.logger.error("Problem in analyzing: {}".format(e))
-        pocs.park()
+        pocs.next_state = 'parking'
