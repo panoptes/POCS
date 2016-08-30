0394da99778beaee4ff0ba0361b951cee48114cd
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-08-30 18:34:32 +1000

    Removing old utility method for moving mount

--- pocs/observatory.py
+++ pocs/observatory.py
@@ -529,33 +529,3 @@ class Observatory(PanBase):
                 raise error.NotFound(msg=e)
         else:
             self.logger.warning("Fields file does not exist: {}".format(fields_file))
-
-
-##################################################################################################
-# Private Utility Methods
-##################################################################################################
-
-    def _track_target(self, target, hours=2.0):
-        """ Track a target for set amount of time.
-
-        This is a utility method that will track a given `target` for a certain number of `hours`.
-
-        WARNING:
-            This is a blocking method! It is a utility method only.
-
-        Args:
-            target(SkyCoord):   An astropy.coordinates.SkyCoord.
-            hours(float):       Number of hours to track for.
-        """
-        self.logger.info("Tracking target {} for {} hours".format(target, hours))
-
-        self.mount.set_target_coordinates(target)
-        self.mount.slew_to_target()
-
-        self.logger.info("Slewing to {}".format(target))
-        while self.mount.is_slewing:
-            time.sleep(5)
-
-        self.logger.info("Tracking target. Sleeping for {} hours".format(hours))
-        time.sleep(hours * 60 * 60)
-        self.logger.info("I just finished tracking {}".format(target))
