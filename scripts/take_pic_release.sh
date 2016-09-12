6b792a31725b6379b65bc789863332338882ed73
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-09-12 16:10:43 +1000

    Remove Camera param and fix release script

--- scripts/take_pic_release.sh
+++ scripts/take_pic_release.sh
@@ -4,6 +4,6 @@ F=$1
 echo 'Taking picture'
 
 # Open shutter
-gphoto2 --camera="Canon EOS 100D"--set-config eosremoterelease=4 --wait-event-and-download=5s --filename "${F}"
+gphoto2 --set-config eosremoterelease=4 --wait-event-and-download=5s --filename "${F}"
 
 echo "Done with pic"
