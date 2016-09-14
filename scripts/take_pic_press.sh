734887247fb1977be16edfe7fe9374613f45590b
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-09-14 21:56:00 +1000

    Updating pic taking scripts (this is in develop alread)

--- scripts/take_pic_press.sh
+++ scripts/take_pic_press.sh
@@ -3,6 +3,6 @@
 echo 'Taking picture'
 
 # Open shutter
-gphoto2 --camera="Canon EOS 100D" --set-config eosremoterelease=Immediate 
+gphoto2 --set-config eosremoterelease=Immediate 
 
 echo "Shutter pressed"
--- scripts/take_pic_release.sh
+++ scripts/take_pic_release.sh
@@ -2,10 +2,8 @@
 
 F=$1
 echo 'Taking picture'
-echo "T = ${T}s"
 
 # Open shutter
-gphoto2 --camera="Canon EOS 100D"--set-config eosremoterelease=4 --wait-event-and-download=2s \
-         --filename "${F}"
+gphoto2 --set-config eosremoterelease=4 --wait-event-and-download=2s --filename "${F}"
 
 echo "Done with pic"
