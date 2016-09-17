9453d941e68b1812c912f566fa0a5d059b5e88f0
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-09-18 09:59:54 +1000

    Adding config load for `config_local.yaml`

--- peas/__init__.py
+++ peas/__init__.py
@@ -1,14 +1,32 @@
 import os
+import sys
 import yaml
 
 
-def load_config(fn='config.yaml'):
-    config = dict()
-    try:
-        path = '{}/{}'.format(os.getenv('PEAS', '/var/panoptes/PEAS'), fn)
-        with open(path, 'r') as f:
-            config = yaml.load(f.read())
-    except IOError:
-        pass
+def load_config():
+    """ Returns the config information """
+    _config = dict()
 
-    return config
+    # This is global
+    _config_file = '{}/config.yaml'.format(os.getenv('PEAS', '/var/panoptes/PEAS'))
+    _local_config_file = '{}/config_local.yaml'.format(os.getenv('PEAS', '/var/panoptes/PEAS'))
+
+    if not os.path.exists(_config_file):
+        sys.exit("Problem loading config file, check that it exists: {}".format(_config_file))
+
+    _add_to_conf(_config, _config_file)
+
+    if os.path.exists(_local_config_file):
+        _add_to_conf(_config, _local_config_file)
+
+    return _config
+
+
+def _add_to_conf(config, fn):
+    try:
+        with open(fn, 'r') as f:
+            c = yaml.load(f.read())
+            if c is not None:
+                config.update(c)
+    except IOError:  # pragma: no cover
+        pass
