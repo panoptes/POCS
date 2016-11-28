9c3dd3d9a93b3f95f0715db6e99ee083834fa3d4
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-11-28 12:33:56 +1100

    Default to zero tracking rate offset

--- pocs/mount/ioptron.py
+++ pocs/mount/ioptron.py
@@ -175,7 +175,7 @@ class Mount(AbstractSerialMount):
 ##################################################################################################
     def _set_initial_rates(self):
         # Make sure we start at sidereal
-        self.set_tracking_rate(delta=+0.003)
+        self.set_tracking_rate()
 
         self.logger.debug("Mount guide rate: {}".format(self.serial_query('get_guide_rate')))
         self.serial_query('set_guide_rate', '090')
