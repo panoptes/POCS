78b3fec2c15b1635fefb3cbbd6cf1542058f7ef0
Wilfred Tyler Gee <wtylergee@gmail.com>
2016-12-09 09:50:37 +1100

    Clear the current observation when parking

--- pocs/state/states/default/parking.py
+++ pocs/state/states/default/parking.py
@@ -2,6 +2,9 @@ def on_enter(event_data):
     """ """
     pocs = event_data.model
     try:
+        # Clear any current observation
+        pocs.observatory.current_observation = None
+
         pocs.say("I'm takin' it on home and then parking.")
         pocs.observatory.mount.home_and_park()
 
