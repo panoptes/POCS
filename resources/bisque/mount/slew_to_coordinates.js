09c793b2a560e21a380d5891bd4bc43de06b03cf
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-02-16 16:59:44 +1100

    Cleaning up some of the javascript for bisque software

--- resources/bisque/mount/slew_to_coordinates.js
+++ resources/bisque/mount/slew_to_coordinates.js
@@ -1,14 +1,13 @@
 /* Java Script */
-Out = '';
 sky6Utils.ConvertStringToRA("$ra");
 var ra = sky6Utils.dOut0;
 
 sky6Utils.ConvertStringToDec("$dec");
 var dec = sky6Utils.dOut0;
 
-sky6RASCOMTele.Asynchronous = $async;
+sky6RASCOMTele.Asynchronous = false;
 sky6RASCOMTele.Abort();
-sky6RASCOMTele.SlewToRaDec(ra, dec, 'radec_coords');
+sky6RASCOMTele.SlewToRaDec(ra, dec, '');
 
 while (!sky6RASCOMTele.IsSlewComplete) {
     sky6Web.Sleep(1000);
@@ -16,6 +15,6 @@ while (!sky6RASCOMTele.IsSlewComplete) {
 Out = JSON.stringify({
     "ra": ra,
     "dec": dec,
-    "slewing": sky6RASCOMTele.IsSlewComplete,
-    "success": true,
+    "success": sky6RASCOMTele.IsSlewComplete,
+    "msg": "Slew complete"
 });    
