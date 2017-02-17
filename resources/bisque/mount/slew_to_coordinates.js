8462bf178a9cf7ee8db264be58454b0c87875180
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-02-17 12:28:34 +1100

    Slew * Adding a bunch of log output to try and figure out #11

--- resources/bisque/mount/slew_to_coordinates.js
+++ resources/bisque/mount/slew_to_coordinates.js
@@ -7,11 +7,19 @@ var dec = sky6Utils.dOut0;
 
 sky6RASCOMTele.Asynchronous = false;
 sky6RASCOMTele.Abort();
+RunJavaScriptOutput.writeLine("Start [async=" + sky6RASCOMTele.Asynchronous + "]");
+RunJavaScriptOutput.writeLine("Before slew call");
+RunJavaScriptOutput.writeLine("Slew complete: " + sky6RASCOMTele.IsSlewComplete);
 sky6RASCOMTele.SlewToRaDec(ra, dec, '');
-
+RunJavaScriptOutput.writeLine("After slew call");
+RunJavaScriptOutput.writeLine("Slew Complete: " + sky6RASCOMTele.IsSlewComplete);
+RunJavaScriptOutput.writeLine("Going to loop");
 while (!sky6RASCOMTele.IsSlewComplete) {
+	RunJavaScriptOutput.writeLine("Slew Complete: " + sky6RASCOMTele.IsSlewComplete);
     sky6Web.Sleep(1000);
 }
+RunJavaScriptOutput.writeLine("Out of loop");
+	RunJavaScriptOutput.writeLine("Slew Complete: " + sky6RASCOMTele.IsSlewComplete);
 Out = JSON.stringify({
     "ra": ra,
     "dec": dec,
