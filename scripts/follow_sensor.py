10bbb5a328b5ade04f9ba1f3c31d39e7bdfe7c18
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-03-28 00:10:52 -1000

    Adding simple script to follow sensor

---
+++ scripts/follow_sensor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from peas.sensors import ArduinoSerialMonitor
+
+
+def main(watch_key=None, **kwargs):
+    monitor = ArduinoSerialMonitor()
+
+    keys = watch_key.split('.')
+
+    while True:
+        data = monitor.capture(use_mongo=False)
+        for key in keys:
+            data = data[key]
+
+        print(data)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Follow some serial keys.")
+
+    parser.add_argument('watch_key', help="Key to watch with dot separation, e.g. telemetry_board.amps")
+
+    args = parser.parse_args()
+
+    try:
+        main(**vars(args))
+    except KeyboardInterrupt:
+        print("Stopping...")
