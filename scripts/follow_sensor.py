703f690d1561cb40171584ac8dcfa60bf93e8ad4
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-03-28 00:39:52 -1000

    This time with messaging!

--- scripts/follow_sensor.py
+++ scripts/follow_sensor.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
-from peas.sensors import ArduinoSerialMonitor
+from pocs.utils.messaging import PanMessaging
 
 
-def main(sensor=None, watch_key=None, **kwargs):
-    monitor = ArduinoSerialMonitor()
+def main(sensor=None, watch_key=None, channel=None, port=6511, **kwargs):
+    sub = PanMessaging.create_subscriber(port)
 
     while True:
         try:
-            data = monitor.capture(use_mongo=False)[sensor]
+            msg_channel, msg_data = sub.receive_message()
         except KeyError:
             continue
         else:
+            if msg_channel != channel:
+                continue
+
+            data = msg_data['data'][sensor]
+
             if watch_key in data:
-                print(data[watch_key])
-            else:
-                print(data)
+                data = data[watch_key]
+
+        print(data)
 
 
 if __name__ == '__main__':
@@ -24,6 +29,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Follow some serial keys.")
 
     parser.add_argument('sensor', help="Sensor to watch")
+    parser.add_argument('--channel', default='environment', help="Which channel to monitor, e.g. environment, weather")
     parser.add_argument('--watch-key', default=None, help="Key to watch, e.g. amps")
 
     args = parser.parse_args()
