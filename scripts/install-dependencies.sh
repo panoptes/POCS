11d26a122eae2166d25deec8992cefb03f3f485d
jamessynge <james.synge@gmail.com>
2017-11-19 17:23:41 -0500

    wip - Starting on creating some setup scripts. Need to coordinate with Wilfred who is doing a similar exercise with the Raspberry Pi.

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,103 @@
+#!/bin/bash -e
+
+if [[ -z "${PANDIR}" || -z "${POCS}" || -z "${PAWS}" || -z "${PANLOG}" ||
+      -z "${PANUSER}" || ! -d "${PANDIR}" ]] ; then
+  echo "Please set the Panoptes environment variables, then re-run this script."
+  exit 1
+fi
+
+THIS_DIR="$(dirname $(readlink -f "${0}"))"
+APT_PKGS="$(egrep -v '^#|^ *$' "${THIS_DIR}/apt-packages-list.txt"|sort|uniq)"
+
+cd "${PANDIR}"
+mkdir -p tmp
+
+echo
+echo "Running sudo apt-get install, you may be prompted for your password."
+echo
+
+(set -x ; sudo apt-get install --yes ${APT_PKGS})
+
+exit
+
+# If conda is not present, install latest version of Miniconda (Anaconda with
+# fewer packages; any that are needed can then be installed).
+set -x
+CONDA_BIN=$(which conda)  # Assuming is for python3 if found.
+if [[ -z "${CONDA_BIN}" ]] ; then
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O tmp/miniconda.sh
+  bash miniconda.sh -b -p "${PANDIR}/miniconda"
+fi
+
+exit
+
+dist: trusty
+sudo: required
+language: python
+services:
+  - mongodb
+python:
+  - "3.6"
+cache:
+  - pip
+env:
+  - POCS=$TRAVIS_BUILD_DIR PANDIR=/var/panoptes
+before_install:
+    - sudo mkdir /var/panoptes && sudo chmod 777 /var/panoptes
+    - mkdir $PANDIR/logs
+    - ln -s $TRAVIS_BUILD_DIR /var/panoptes/POCS
+    - pip install coveralls
+    - pip install -U pip
+    - cd $HOME
+    - wget http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz
+    - tar zxf cfitsio_latest.tar.gz
+    - cd cfitsio
+    - ./configure
+    - make
+    - make fpack
+    - make funpack
+    - sudo make install
+    - sudo mkdir -p /var/panoptes/astrometry/data
+    - sudo chmod -R 777 /var/panoptes/astrometry/
+addons:
+  apt:
+    packages:
+    - gphoto2
+    - libcairo2-dev 
+    - libnetpbm10-dev 
+    - netpbm
+    - libpng12-dev
+    - libjpeg-dev
+    - python-numpy
+    - python-pyfits
+    - python-dev 
+    - zlib1g-dev 
+    - libbz2-dev 
+    - swig 
+    - cfitsio-dev
+install:
+  - wget http://astrometry.net/downloads/astrometry.net-0.72.tar.gz
+  - tar zxvf astrometry.net-0.72.tar.gz
+  - cd astrometry.net-0.72 && make && make py && make install INSTALL_DIR=/var/panoptes/astrometry
+  - echo 'add_path /var/panoptes/astrometry/data' | sudo tee --append /var/panoptes/astrometry/etc/astrometry.cfg
+  - wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  - bash miniconda.sh -b -p $HOME/miniconda
+  - export PATH="$HOME/miniconda/bin:$HOME/cfitsio/bin:/var/panoptes/astrometry/bin:$PATH"
+  - hash -r
+
+  - conda config --set always_yes yes --set changeps1 no
+  - conda update -q conda
+  - conda info -a # Useful for debugging any issues with conda
+  - conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
+  - source activate test-environment
+
+  - cd $TRAVIS_BUILD_DIR
+  - pip install -r requirements.txt
+  - python setup.py install
+script:
+  - coverage run setup.py test
+  - coverage combine .coverage*
+after_success:
+    - if [[ $TRAVIS_PYTHON_VERSION == 3.6* ]]; then
+          bash <(curl -s https://codecov.io/bash);
+      fi
