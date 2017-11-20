8dc2835b6dce5dab678232c802251bc4ff62e525
jamessynge <james.synge@gmail.com>
2017-11-19 21:27:36 -0500

    wip - Install cfitsio.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# TODO(jamessynge): Add flags to control behavior, such as skipping apt-get.
+
 if [[ -z "${PANDIR}" || -z "${POCS}" || -z "${PAWS}" || -z "${PANLOG}" ||
       -z "${PANUSER}" || ! -d "${PANDIR}" ]] ; then
   echo "Please set the Panoptes environment variables, then re-run this script."
@@ -18,16 +20,54 @@ echo
 
 (set -x ; sudo apt-get install --yes ${APT_PKGS})
 
-exit
+add_to_PATH() {
+  local -r the_dir="$(readlink -f "${1}")"
+  echo >>"${HOME}/.bashrc" "
+# Added by PANOPTES install-dependencies.sh
+PATH=\"${the_dir}:\${PATH}\""
+  PATH="${the_dir}:${PATH}"
+}
 
 # If conda is not present, install latest version of Miniconda (Anaconda with
 # fewer packages; any that are needed can then be installed).
 set -x
 CONDA_BIN=$(which conda)  # Assuming is for python3 if found.
 if [[ -z "${CONDA_BIN}" ]] ; then
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O tmp/miniconda.sh
-  bash miniconda.sh -b -p "${PANDIR}/miniconda"
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+       -O tmp/miniconda.sh
+  bash tmp/miniconda.sh -b -p "${PANDIR}/miniconda"
+  rm tmp/miniconda.sh
+  add_to_PATH miniconda/bin
 fi
+
+if [[ ! -d cfitsio ]] ; then
+  wget \
+    http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz \
+    -O tmp/cfitsio_latest.tar.gz
+  tar zxf tmp/cfitsio_latest.tar.gz
+  cd cfitsio
+  ./configure
+  make
+  make utils stand_alone shared
+fi
+
+# Upgrade pip itself before installing other python packages.
+pip install -U pip
+pip install -r "${THIS_DIR}/../requirements.txt"
+
+exit
+
+- cd cfitsio
+- ./configure
+- make
+- make fpack
+- make funpack
+- sudo make install
+
+
+
+
+
 
 exit
 
