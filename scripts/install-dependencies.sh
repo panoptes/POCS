0dafb45afb9e48c3cb0491b001cef3df36e86f72
jamessynge <james.synge@gmail.com>
2017-11-24 15:05:58 -0500

    Installation script for PANOPTES. Ready for review AND testing (perhaps in a docker container).

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -16,27 +16,32 @@ cd "${PANDIR}"
 ASTROMETRY_VERSION="0.72"
 INSTALL_PREFIX="/usr/local"
 DO_APT_GET=1
+DO_MONGODB=1
 DO_CONDA=1
-DO_CFITSIO=1
+DO_CFITSIO=0
 DO_ASTROMETRY=1
 DO_ASTROMETRY_INDICES=1
+DO_ASTROMETRY_FULL_INDICES=0
 DO_PIP_REQUIREMENTS=1
 
 function echo_bar() {
-  echo <<EOF
-################################################################################
-EOF
+  eval $(resize|grep COLUMNS=)
+  printf "%${COLUMNS:-80}s\n" | tr ' ' '#'
 }
 
 # Append $1 to PATH and write command to do the same to .bashrc.
 function add_to_PATH() {
   local -r the_dir="$(readlink -f "${1}")"
-  echo >>"${HOME}/.bashrc" "
+  if [[ -z "$(egrep -- "PATH=.*${the_dir}" ~/.bashrc)" ]] ; then
+    echo >>"${HOME}/.bashrc" "
 # Added by PANOPTES install-dependencies.sh
 PATH=\"${the_dir}:\${PATH}\""
-  PATH="${the_dir}:${PATH}"
-  echo
-  echo "Added ${the_dir} to PATH in .bashrc"
+    PATH="${the_dir}:${PATH}"
+    echo
+    echo "Added ${the_dir} to PATH in .bashrc"
+  else
+    echo ".bashrc already adds ${the_dir} to PATH"
+  fi
 }
 
 # Given the path to a pkg-config file (.pc), extract the version number.
@@ -45,6 +50,55 @@ function extract_version_from_pkg_config() {
     egrep '^Version:' "${1}" | cut '-d:' -f2
   else
     echo ""
+  fi
+}
+
+function install_apt_packages() {
+  # Remove all the comments from the package list and install the packages whose
+  # names are left.
+  APT_PKGS="$(cut '-d#' -f1 "${THIS_DIR}/apt-packages-list.txt" | sort | uniq)"
+  echo
+  echo "Running sudo apt-get install, you may be prompted for your password."
+  echo
+  (set -x ; sudo apt-get update)
+  (set -x ; sudo apt-get install --yes ${APT_PKGS})
+}
+
+function install_mongodb() {
+  # This is based on https://www.howtoforge.com/tutorial/install-mongodb-on-ubuntu-16.04/
+  # Note this function does not configure mongodb itself, i.e. no users or
+  # security settings.
+  echo_bar
+  echo "
+Installing mongodb, for which several commands require sudo, so
+you may be prompted for you password.
+"
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+  echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" \
+     | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+  sudo apt-get update
+  sudo apt-get install -y mongodb-org
+  echo "[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+Documentation=https://docs.mongodb.org/manual
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /lib/systemd/system/mongod.service
+
+  sudo systemctl daemon-reload
+  sudo systemctl start mongod
+  sudo systemctl enable mongod
+}
+
+function maybe_install_mongodb() {
+  if [[ -z "$(which mongo)" || -z "$(systemctl | grep mongo)" ]] ; then
+    install_mongodb
   fi
 }
 
@@ -68,14 +122,20 @@ function install_conda_if_missing() {
   CONDA_BIN="$(which conda)"  # Assuming is a python3 version if found.
   if [[ -z "${CONDA_BIN}" ]] ; then
     install_conda
+  else
+    echo_bar
+    echo
+    echo "Updating conda installation."
+    conda update conda
   fi
 }
 
-# Fetches and builds the latest version of cfitsio.
-function fetch_and_build_cfitsio() {
+# Fetches and configures the latest version of cfitsio; this allows us to
+# extract the version from the package config file (cfitsio.pc).
+function fetch_and_configure_cfitsio() {
   echo_bar
   echo
-  echo "Fetching and building latest cfitsio release."
+  echo "Fetching and configuring latest cfitsio release..."
   echo
   # Unpack into PANDIR/tmp/cfitsio
   cd "${PANDIR}"
@@ -88,6 +148,14 @@ function fetch_and_build_cfitsio() {
   tar zxf cfitsio_latest.tar.gz
   cd cfitsio
   ./configure --prefix=${INSTALL_PREFIX}
+}
+
+# Builds the downloaded version of cfitsio.
+function build_cfitsio() {
+  echo
+  echo "Building cfitsio..."
+  echo
+  cd "${PANDIR}/tmp/cfitsio"
   make
   make utils stand_alone shared
 }
@@ -114,7 +182,7 @@ function sudo_install_cfitsio() {
 
 # Install the latest versio of cfitsio if it is not already installed.
 function install_latest_cfitsio() {
-  (fetch_and_build_cfitsio)
+  (fetch_and_configure_cfitsio)
   local -r LATEST_CFITSIO_VERSION="$(latest_cfitsio_version)"
   local -r INSTALLED_CFITSIO_VERSION="$(installed_cfitsio_version)"
   if [[ "${LATEST_CFITSIO_VERSION}" == "${INSTALLED_CFITSIO_VERSION}" ]] ; then
@@ -126,6 +194,7 @@ function install_latest_cfitsio() {
       echo "Installing cfitsio version ${LATEST_CFITSIO_VERSION}, replacing" \
         "installed version ${OLD_CFITSIO_VERSION}."
     fi
+    (build_cfitsio)
     (sudo_install_cfitsio)
   fi
   rm -rf tmp/cfitsio_latest.tar.gz tmp/cfitsio
@@ -160,7 +229,6 @@ function install_astrometry() {
   mkdir -p "${INSTALL_TO}"
   echo "Installing into ${INSTALL_TO}"
   make install "INSTALL_DIR=${INSTALL_TO}"
-  echo "add_path ${INSTALL_TO}/data" >> "${INSTALL_TO}/etc/astrometry.cfg"
 
   add_to_PATH "${INSTALL_TO}/bin"
   cd "${PANDIR}"
@@ -170,13 +238,23 @@ function install_astrometry() {
 function install_astrometry_indices() {
   mkdir -p "${PANDIR}/astrometry/data"
   cd "${PANDIR}/astrometry/data"
-  if [[ ! -f index-4107.fits ]] ; then
-   curl --remote-name \
-        http://broiler.astrometry.net/~dstn/4100/index-41[07-19].fits
-  fi
-  if [[ ! -f astrometry/data/index-4208.fits ]] ; then
-    curl --remote-name \
-         http://broiler.astrometry.net/~dstn/4200/index-42[08-19].fits
+  # See http://astrometry.net/doc/readme.html#getting-index-files
+  # for info on the index files you might need.
+  if [[ "${DO_ASTROMETRY_FULL_INDICES}" -eq 1 ]] ; then
+    if [[ ! -f index-4107.fits ]] ; then
+      curl --remote-name \
+           http://broiler.astrometry.net/~dstn/4100/index-41[07-19].fits
+    fi
+    if [[ ! -f astrometry/data/index-4208.fits ]] ; then
+      curl --remote-name \
+           http://broiler.astrometry.net/~dstn/4200/index-42[08-19].fits
+    fi
+  else
+    # Install wide field indices. If we change to using longer focal length
+    if [[ ! -f index-4116.fits ]] ; then
+      curl --remote-name \
+           http://broiler.astrometry.net/~dstn/4100/index-411[6-9].fits
+    fi
   fi
 }
 
@@ -188,12 +266,17 @@ function show_help() {
   echo "${THIS_PROGRAM} [options]"
   echo " "
   echo "options:"
-  echo "-h, --help               show brief help"
-  echo "--no-apt-get             don't run apt-get to install Linux packages"
-  echo "--no-cfitsio             don't install the latest version of cfitsio"
-  echo "--no-astrometry          don't install astrometry.net software"
-  echo "--no-astrometry-indices  don't install astrometry.net indices"
-  echo "--no-pip-requirements    don't install python packages"
+  echo "-h, --help                 show brief help"
+  echo "-x                         turn on bash debug output"
+  echo "--run <function>           run the named function and exit, for debugging"
+  echo "--no-apt-get               don't run apt-get to install Linux packages"
+  echo "--no-mongodb               don't install and start mongodb server"
+  echo "--no-cfitsio               don't install the latest version of cfitsio"
+  echo "--no-conda                 don't install the latest version of Anaconda"
+  echo "--no-astrometry            don't install astrometry.net software"
+  echo "--no-astrometry-indices    don't install astrometry.net indices"
+  echo "--astrometry-full-indices  install full indices, for narrow and wide fields"
+  echo "--no-pip-requirements      don't install python packages"
 }
 
 cd "${PANDIR}"
@@ -205,8 +288,26 @@ while test ${#} -gt 0; do
       show_help
       exit 0
       ;;
+    -x)
+      set -x
+      shift
+      ;;
+    --run)
+      if [ $# -lt 2 ]; then
+        echo "Function name missing after ${1}."
+        show_help
+        exit 1
+      fi
+      shift
+      (${1})
+      exit
+      ;;
     --no-apt-get)
       DO_APT_GET=0
+      shift
+      ;;
+    --no-mongodb)
+      DO_MONGODB=0
       shift
       ;;
     --no-cfitsio)
@@ -223,14 +324,16 @@ while test ${#} -gt 0; do
       ;;
     --no-astrometry-ind*)
       DO_ASTROMETRY_INDICES=0
+      DO_ASTROMETRY_FULL_INDICES=0
+      shift
+      ;;
+    --astrometry-full-ind*)
+      DO_ASTROMETRY_FULL_INDICES=1
+      DO_ASTROMETRY_INDICES=1
       shift
       ;;
     --no-pip-requirements)
       DO_PIP_REQUIREMENTS=0
-      shift
-      ;;
-    -x)
-      set -x
       shift
       ;;
     *)
@@ -244,10 +347,12 @@ done
 
 
 if [[ "${DO_APT_GET}" -eq 1 ]] ; then
-  echo
-  echo "Running sudo apt-get install, you may be prompted for your password."
-  echo
-  (set -x ; sudo apt-get install --yes ${APT_PKGS})
+  install_apt_packages
+fi
+
+
+if [[ "${DO_MONGODB}" -eq 1 ]] ; then
+  maybe_install_mongodb
 fi
 
 
@@ -274,6 +379,11 @@ fi
 if [[ "${DO_PIP_REQUIREMENTS}" -eq 1 ]] ; then
   # Upgrade pip itself before installing other python packages.
   pip install -U pip
+  # TODO(jamessynge): Move this script and needed text files into an install
+  # directory.
+  # TODO(wtgee): Consider whether to inline the needed text files into this
+  # file, and add git clone of the PANOPTES repos, so that the user can do
+  # an install with just about two commands (wget script, then run script).
   pip install -r "${THIS_DIR}/../requirements.txt"
 fi
 
@@ -282,233 +392,3 @@ rmdir --ignore-fail-on-non-empty "${PANDIR}/tmp"
 
 exit
 
-
-
-
-
-
-exit
-
-
-
-
-
-  install_astrometry
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-fi
-
-
-if [[ "${DO_ASTROMETRY_INDICES}" -eq 1 ]] ; then
-  install_astrometry_indices
-fi
-
-
-set -x
-CONDA_BIN=$(which conda)  # Assuming is for python3 if found.
-fi
-
-
-
-
-
-  echo
-  echo "Running sudo apt-get install, you may be prompted for your password."
-  echo
-  (set -x ; sudo apt-get install --yes ${APT_PKGS})
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-APT_PKGS="$(egrep -v '^#|^ *$' "${THIS_DIR}/apt-packages-list.txt"|sort|uniq)"
-
-cd "${PANDIR}"
-mkdir -p tmp
-
-echo
-echo "Running sudo apt-get install, you may be prompted for your password."
-echo
-
-(set -x ; sudo apt-get install --yes ${APT_PKGS})
-
-# If conda is not present, install latest version of Miniconda (Anaconda with
-# fewer packages; any that are needed can then be installed).
-set -x
-CONDA_BIN=$(which conda)  # Assuming is for python3 if found.
-if [[ -z "${CONDA_BIN}" ]] ; then
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-       -O tmp/miniconda.sh
-  bash tmp/miniconda.sh -b -p "${PANDIR}/miniconda"
-  rm tmp/miniconda.sh
-  add_to_PATH miniconda/bin
-fi
-
-if [[ ! -f /usr/local/include/fitsio.h ]] ; then
-  install_cfitsio() {
-    wget \
-      http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz \
-      -O tmp/cfitsio_latest.tar.gz
-    tar zxf tmp/cfitsio_latest.tar.gz
-    cd cfitsio
-    ./configure --prefix=/usr/local
-    make
-    make utils stand_alone shared
-    echo
-    echo "Running sudo make install for cfitsio, you may be prompted for your password."
-    echo
-    (set -x ; sudo make install)
-    rm tmp/cfitsio_latest.tar.gz
-    rmdir --ignore-fail-on-non-empty tmp
-  }
-  if [[ -d cfitsio ]] ; then
-    rm -rf cfitsio
-  fi
-  (install_cfitsio)
-fi
-
-exit
-
-
-if [[ ! -d astrometry/bin ]] ; then
-  # This uses a directory in tmp to download and build astrometry, then
-  # installs the binaries into $PANDIR/astrometry.
-  install_astrometry() {
-    local -r DIR="astrometry.net-${ASTROMETRY_VERSION}"
-    local -r FN="${DIR}.tar.gz"
-    local -r URL="http://astrometry.net/downloads/${FN}"
-    local -r TMP_DIR="$(realpath "${TMPDIR:-${PANDIR}/tmp}")"
-    local -r SCRATCH_DIR="$(mktemp -d "${TMP_DIR}/install-astrometry.XXXXXXXXXXXX")"
-    local -r INSTALL_TO="${PANDIR}/astrometry"
-    cd ${SCRATCH_DIR}
-    echo "Fetching astrometry into directory $(pwd)"
-    wget "${URL}"
-    tar zxvf "${FN}"
-    cd "${DIR}" && make && make py && make install "INSTALL_DIR=${INSTALL_TO}"
-    echo "add_path ${INSTALL_TO}/data" >> "${INSTALL_TO}/etc/astrometry.cfg"
-  }
-  mkdir -p astrometry
-  (install_astrometry)
-fi
-
-if [[ ! -f astrometry/data/index-4107.fits ]] ; then
-  mkdir -p astrometry/data
-  (cd astrometry/data ;
-   curl --remote-name \
-     http://broiler.astrometry.net/~dstn/4100/index-41[07-19].fits )
-fi
-
-if [[ ! -f astrometry/data/index-4208.fits ]] ; then
-  mkdir -p astrometry/data
-  (cd astrometry/data ;
-   curl --remote-name \
-     http://broiler.astrometry.net/~dstn/4200/index-42[08-19].fits )
-fi
-
-# Upgrade pip itself before installing other python packages.
-pip install -U pip
-pip install -r "${THIS_DIR}/../requirements.txt"
-
-exit
-
-- cd cfitsio
-- ./configure
-- make
-- make fpack
-- make funpack
-- sudo make install
-
-
-
-
-
-
-exit
-
-dist: trusty
-sudo: required
-language: python
-services:
-  - mongodb
-python:
-  - "3.6"
-cache:
-  - pip
-env:
-  - POCS=$TRAVIS_BUILD_DIR PANDIR=/var/panoptes
-before_install:
-    - sudo mkdir /var/panoptes && sudo chmod 777 /var/panoptes
-    - mkdir $PANDIR/logs
-    - ln -s $TRAVIS_BUILD_DIR /var/panoptes/POCS
-    - pip install coveralls
-    - pip install -U pip
-    - cd $HOME
-    - wget http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz
-    - tar zxf cfitsio_latest.tar.gz
-    - cd cfitsio
-    - ./configure
-    - make
-    - make fpack
-    - make funpack
-    - sudo make install
-    - sudo mkdir -p /var/panoptes/astrometry/data
-    - sudo chmod -R 777 /var/panoptes/astrometry/
-addons:
-  apt:
-    packages:
-    - gphoto2
-    - libcairo2-dev 
-    - libnetpbm10-dev 
-    - netpbm
-    - libpng12-dev
-    - libjpeg-dev
-    - python-numpy
-    - python-pyfits
-    - python-dev 
-    - zlib1g-dev 
-    - libbz2-dev 
-    - swig 
-    - cfitsio-dev
-install:
-  - wget http://astrometry.net/downloads/astrometry.net-0.72.tar.gz
-  - tar zxvf astrometry.net-0.72.tar.gz
-  - cd astrometry.net-0.72 && make && make py && make install INSTALL_DIR=/var/panoptes/astrometry
-  - echo 'add_path /var/panoptes/astrometry/data' | sudo tee --append /var/panoptes/astrometry/etc/astrometry.cfg
-  - wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-  - bash miniconda.sh -b -p $HOME/miniconda
-  - export PATH="$HOME/miniconda/bin:$HOME/cfitsio/bin:/var/panoptes/astrometry/bin:$PATH"
-  - hash -r
-
-  - conda config --set always_yes yes --set changeps1 no
-  - conda update -q conda
-  - conda info -a # Useful for debugging any issues with conda
-  - conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
-  - source activate test-environment
-
-  - cd $TRAVIS_BUILD_DIR
-  - pip install -r requirements.txt
-  - python setup.py install
-script:
-  - coverage run setup.py test
-  - coverage combine .coverage*
-after_success:
-    - if [[ $TRAVIS_PYTHON_VERSION == 3.6* ]]; then
-          bash <(curl -s https://codecov.io/bash);
-      fi
