ba2a8c614599048462aabe1ecb0c64dbb8814121
jamessynge <james.synge@gmail.com>
2017-11-27 18:53:07 -0500

    test_rs232.py works w.r.t. installing serial handler for test://

--- pocs/tests/test_rs232.py
+++ pocs/tests/test_rs232.py
@@ -7,7 +7,7 @@ from pocs.utils.config import load_config
 from pocs.tests.serial_handlers import protocol_test
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def handler():
     # Install our test handlers for the duration
     serial.protocol_handler_packages.append('pocs.tests.serial_handlers')
@@ -17,13 +17,20 @@ def handler():
 
 
 def test_basic(handler):
-    """  """
+    """Confirm we can create the SerialData object."""
     ser = rs232.SerialData(port="test://")
     assert ser.ser
     assert not ser.is_connected
+    assert not ser.is_listening
 
 
-def test_bad_handler(handler):
-    """  """
+def test_without_handler():
+    """If the handlers aren't installed, it should fail."""
+    with pytest.raises(ValueError):
+        rs232.SerialData(port="test://")
+
+
+def test_another_handler(handler):
+    """And even when installed, the wrong name won't work."""
     with pytest.raises(ValueError):
         rs232.SerialData(port="another://")
