015c630fa66911f9663134501192e7bd8613dc07
Anthony Horton <anthony.horton@aao.gov.au>
2017-11-29 19:45:45 +1100

    Finally fixed test_hdr properly (though it's still not very good)

--- pocs/tests/test_hdr.py
+++ pocs/tests/test_hdr.py
@@ -1,4 +1,5 @@
 import pytest
+import astropy.units as u
 from astropy.coordinates import SkyCoord
 
 from gunagala.imager import create_imagers
@@ -16,7 +17,7 @@ def test_target_list(imagers):
     base = SkyCoord("16h52m42.2s -38d37m12s")
 
     for imager_name in imagers:
-        for filter_name in imager_name.filters:
+        for filter_name in imagers[imager_name].filters:
             exposure_parameters = {'filter_name': filter_name,
                                    'shortest_exp_time': 5 * u.second,
                                    'longest_exp_time': 600 * u.second,
@@ -27,7 +28,7 @@ def test_target_list(imagers):
                                           imagers=imagers,
                                           primary_imager=imager_name,
                                           base_position=base,
-                                          exposure_paramters=exposure_parameters)
+                                          exposure_parameters=exposure_parameters)
 
 
 def test_target_list_bad(imagers):
@@ -35,11 +36,19 @@ def test_target_list_bad(imagers):
     base = SkyCoord("16h52m42.2s -38d37m12s")
 
     for imager_name in imagers:
-        with pytest.raises(ValueError):
-            targets = hdr.get_target_list(target_name=name,
-                                          imagers=imagers,
-                                          primary_imager=42,
-                                          base_position=base)
+        for filter_name in imagers[imager_name].filters:
+            exposure_parameters = {'filter_name': filter_name,
+                                   'shortest_exp_time': 5 * u.second,
+                                   'longest_exp_time': 600 * u.second,
+                                   'num_long_exp': 1,
+                                   'exp_time_ratio': 2.0,
+                                   'snr_target': 5.0}
+            with pytest.raises(ValueError):
+                targets = hdr.get_target_list(target_name=name,
+                                              imagers=imagers,
+                                              primary_imager=42,
+                                              base_position=base,
+                                              exposure_parameters=exposure_parameters)
 
 
 def test_target_list_string(imagers):
@@ -47,7 +56,7 @@ def test_target_list_string(imagers):
     base = "16h52m42.2s -38d37m12s"
 
     for imager_name in imagers:
-        for filter_name in imager_name.filters:
+        for filter_name in imagers[imager_name].filters:
             exposure_parameters = {'filter_name': filter_name,
                                    'shortest_exp_time': 5 * u.second,
                                    'longest_exp_time': 600 * u.second,
@@ -58,4 +67,4 @@ def test_target_list_string(imagers):
                                           imagers=imagers,
                                           primary_imager=imager_name,
                                           base_position=base,
-                                          exposure_paramters=exposure_parameters)
+                                          exposure_parameters=exposure_parameters)
