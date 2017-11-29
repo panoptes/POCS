8a89b260598408200efed442fcdf91f190d49049
Anthony Horton <anthony.horton@aao.gov.au>
2017-11-29 11:33:25 +1100

    Removed duplicated code in test_hdr, gunagala non-editable

--- pocs/tests/test_hdr.py
+++ pocs/tests/test_hdr.py
@@ -33,13 +33,6 @@ def test_target_list_bad(imagers):
                                           primary_imager=42,
                                           base_position=base)
 
-    for imager_name in imagers:
-        with pytest.raises(ValueError):
-            targets = hdr.get_target_list(target_name=name,
-                                          imagers=imagers,
-                                          primary_imager=name,
-                                          base_position=42)
-
 
 def test_target_list_string(imagers):
     name = 'M6 Toll'
