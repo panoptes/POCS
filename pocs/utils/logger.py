a494ff76e56ed9ee69082e29c762d3c929588318
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-12-19 13:11:15 +1100

    Only show logger init message once (#215)

--- pocs/utils/logger.py
+++ pocs/utils/logger.py
@@ -116,6 +116,7 @@ def get_root_logger(profile='panoptes', log_config=None):
         pass
 
     logger = PanLogger(logger)
+    logger.info('{:*^80}'.format(' Starting PanLogger '))
     all_loggers[logger_key] = logger
     return logger
 
