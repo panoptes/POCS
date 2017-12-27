df8c5be7a5bf78cb164ea7852eac6df199f1e1fe
Wilfred Tyler Gee <wtylergee@gmail.com>
2017-12-27 12:43:36 +1100

    Logging updates * Replace PanLogger with custom `LogRecord` class 	* Allow logger to use either `%` or `{}` (str.format) style for actual log messages. * Log messages show correct file/lineno * Reorder log.yaml
    Fixes #253

--- pocs/utils/logger.py
+++ pocs/utils/logger.py
@@ -9,47 +9,28 @@ from tempfile import gettempdir
 
 from pocs.utils.config import load_config
 
-
-class PanLogger(object):
-    """ Logger for PANOPTES with format style strings """
-
-    def __init__(self, logger):
-        super(PanLogger, self).__init__()
-        self.logger = logger
-
-    def _process_str(self, fmt, *args, **kwargs):
-        """ Pre-process the log string
-
-        This allows for `format` style specifiers, e.g. `{:02f}` and
-        `{:40s}`, which otherwise aren't supported by python's default
-        log formatting.
-        """
-        log_str = fmt
-        if len(args) > 0 or len(kwargs) > 0:
-            log_str = fmt.format(*args, **kwargs)
-
-        return log_str
-
-    def debug(self, fmt, *args, **kwargs):
-        self.logger.debug(self._process_str(fmt, *args, **kwargs))
-
-    def info(self, fmt, *args, **kwargs):
-        self.logger.info(self._process_str(fmt, *args, **kwargs))
-
-    def warning(self, fmt, *args, **kwargs):
-        self.logger.warning(self._process_str(fmt, *args, **kwargs))
-
-    def error(self, fmt, *args, **kwargs):
-        self.logger.error(self._process_str(fmt, *args, **kwargs))
-
-    def critical(self, fmt, *args, **kwargs):
-        self.logger.critical(self._process_str(fmt, *args, **kwargs))
-
-
 # We don't want to create multiple root loggers that are "identical",
 # so track the loggers in a dict keyed by a tuple of:
 #    (profile, json_serialized_logger_config).
 all_loggers = {}
+
+
+class StrFormatLogRecord(logging.LogRecord):
+    """
+    Even though you can select '{' as the style for the formatter class,
+    you still can't use {} formatting for your message.
+
+    From: https://goo.gl/Cyt5NH
+    """
+
+    def getMessage(self):
+        msg = str(self.msg)
+        if self.args:
+            try:
+                msg = msg % self.args
+            except (TypeError, ValueError):
+                msg = msg.format(*self.args)
+        return msg
 
 
 def get_root_logger(profile='panoptes', log_config=None):
@@ -87,11 +68,11 @@ def get_root_logger(profile='panoptes', log_config=None):
         full_log_fname = '{}/{}-{}.log'.format(log_dir, log_fname, handler)
         log_config['handlers'][handler].setdefault('filename', full_log_fname)
 
-        # Setup the TimeedRotatingFileHandler for middle of day
+        # Setup the TimedRotatingFileHandler for middle of day
         log_config['handlers'][handler].setdefault('atTime', datetime.time(hour=11, minute=30))
 
         if handler == 'all':
-            # Symlink the log file to $PANDIR/logs/panoptes.log
+            # Symlink the log file to log_symlink
             try:
                 os.unlink(log_symlink)
             except FileNotFoundError:
@@ -109,13 +90,15 @@ def get_root_logger(profile='panoptes', log_config=None):
     # we have our own way of logging state transitions
     logging.getLogger('transitions.core').setLevel(logging.WARNING)
 
+    # Set out custom LogRecord
+    logging.setLogRecordFactory(StrFormatLogRecord)
+
     try:
         import coloredlogs
         coloredlogs.install()
     except Exception:  # pragma: no cover
         pass
 
-    logger = PanLogger(logger)
     logger.info('{:*^80}'.format(' Starting PanLogger '))
     all_loggers[logger_key] = logger
     return logger
