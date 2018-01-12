210cdc02bd1fddbd2408bb4446c1c9ac43393d30
Unknown <tigergoogs@hotmail.com>
2018-01-12 12:04:30 +1100

    edit _make_pretty_from_fits
    *using the function in make_pretty_from_fits file
    *delete make_pretty_from_fits file

--- pocs/utils/images/__init__.py
+++ pocs/utils/images/__init__.py
@@ -6,7 +6,10 @@ matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from warnings import warn
 
+from astropy.io import fits
+from astropy.wcs import WCS
 from astropy.io.fits import getdata
+from astropy.visualization import (MinMaxInterval, LogStretch, ImageNormalize)
 
 from ffmpy import FFmpeg
 from glob import glob
@@ -92,7 +95,39 @@ def _make_pretty_from_fits(fname, **kwargs):
     new_filename = fname.replace('.fits', '.jpg')
 
     data = getdata(fname)
-    plt.imshow(data, cmap='cubehelix_r', origin='lower')
+
+    norm = ImageNormalize(interval=MinMaxInterval(), stretch=LogStretch())
+
+    wcs = False
+
+    """
+    try:
+        wcs = WCS(fname)
+    except:
+        wcs = False
+    ### Why is this commented out? --> See Pull Request
+    """
+
+    if wcs:
+        ax = plt.subplot(projection=wcs)
+
+        ax.coords.grid(True, color='white', ls='solid')
+        ax.coords[0].set_axislabel('Galactic(?) Longitude')
+        ax.coords[1].set_axislabel('Galactic(?) Latitude')
+
+        overlay = ax.get_coords_overlay('fk5') # What does this fk5 mean?
+        overlay.grid(color='white', ls='dotted')
+        overlay[0].set_axislabel('Right Ascension (J2000)')
+        overlay[1].set_axislabel('Declination (J2000)')
+    else:
+        ax = plt.subplot()
+
+        ax.grid(True, color='white', ls='solid')
+        ax.set_xlabel('Pixel x-axis')
+        ax.set_ylabel('Pixel y-axis')
+
+    ax.imshow(data, norm=norm, cmap='inferno', origin='lower')
+
     plt.title(title)
     plt.savefig(new_filename)
 
