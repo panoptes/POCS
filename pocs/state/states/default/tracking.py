7e8f6f29fcd425fa84ce06cb73416ec5efb6f624
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-01-14 08:09:16 +1100

    Wait for tracking adjustment
    The tracking adjustment for ioptron uses `move_ms_<dir>`, which places
    the mount in `guide` mode (as indicated by the status) but is non-blocking.
    If the tracking adjustment is too long (~ > 4-5 seconds) and no blocking
    is done the machine will try to transition to next state. There is a
    condition on the state machine that the mount must be in `tracking`
    to `observe` so it gets stuck and then tries again. After 5 iterations
    of same state without successful transition the machine will give up
    and park the mount.
    
    Here we just wait out for up to 30 seconds until back in `tracking`
    mode.
    
    Note: Ideally we should never have to have an adjustment of 4-5 seconds,
    so not entirely sure what is going on there. Seems to happen most on
    first adjustment after pointing but also depends on where mount is poining.
    This is a problem with PAN001 that we have had and might not be an issue
    on other units.

--- pocs/state/states/default/tracking.py
+++ pocs/state/states/default/tracking.py
@@ -1,14 +1,27 @@
+from pocs.utils import current_time
+
+
 def on_enter(event_data):
     """ The unit is tracking the target. Proceed to observations. """
     pocs = event_data.model
+    pocs.next_state = 'parking'
     pocs.say("Checking our tracking")
 
     try:
         ra_info, dec_info = pocs.observatory.update_tracking()
         pocs.say("Correcting drift: RA {} {:.02f}".format(ra_info[0], ra_info[1]))
         pocs.say("Correcting drift: Dec {} {:.02f}".format(dec_info[0], dec_info[1]))
+
+        # Adjust tracking for up to 30 seconds then fail if not done.
+        start_tracking_time = current_time()
+        while pocs.observatory.mount.is_tracking is False:
+            if (current_time() - start_tracking_time).sec > 30:
+                raise Exception("Trying to adjust tracking for more than 30 seconds")
+
+            pocs.logger.debug("Waiting for tracking adjustment")
+            pocs.sleep(delay=0.5)
+
+        pocs.say("Done with tracking adjustment, going to observe")
+        pocs.next_state = 'observing'
     except Exception as e:
         pocs.logger.warning("Problem adjusting tracking: {}".format(e))
-
-    pocs.say("Done with tracking adjustment, going to observe")
-    pocs.next_state = 'observing'
