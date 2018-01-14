b47aa3e0e4c7c8d7ef57c81bf5c291b01f81b515
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-01-14 13:37:48 +1100

    Don't adjust tracking if coming from pointing

--- pocs/state/states/default/tracking.py
+++ pocs/state/states/default/tracking.py
@@ -7,21 +7,23 @@ def on_enter(event_data):
     pocs.next_state = 'parking'
     pocs.say("Checking our tracking")
 
-    try:
-        ra_info, dec_info = pocs.observatory.update_tracking()
-        pocs.say("Correcting drift: RA {} {:.02f}".format(ra_info[0], ra_info[1]))
-        pocs.say("Correcting drift: Dec {} {:.02f}".format(dec_info[0], dec_info[1]))
+    # If we came from pointing then don't try to adjust
+    if event_data.transition.source != 'pointing':
+        try:
+            ra_info, dec_info = pocs.observatory.update_tracking()
+            pocs.say("Correcting drift: RA {} {:.02f}".format(ra_info[0], ra_info[1]))
+            pocs.say("Correcting drift: Dec {} {:.02f}".format(dec_info[0], dec_info[1]))
 
-        # Adjust tracking for up to 30 seconds then fail if not done.
-        start_tracking_time = current_time()
-        while pocs.observatory.mount.is_tracking is False:
-            if (current_time() - start_tracking_time).sec > 30:
-                raise Exception("Trying to adjust tracking for more than 30 seconds")
+            # Adjust tracking for up to 30 seconds then fail if not done.
+            start_tracking_time = current_time()
+            while pocs.observatory.mount.is_tracking is False:
+                if (current_time() - start_tracking_time).sec > 30:
+                    raise Exception("Trying to adjust tracking for more than 30 seconds")
 
-            pocs.logger.debug("Waiting for tracking adjustment")
-            pocs.sleep(delay=0.5)
+                pocs.logger.debug("Waiting for tracking adjustment")
+                pocs.sleep(delay=0.5)
 
-        pocs.say("Done with tracking adjustment, going to observe")
-        pocs.next_state = 'observing'
-    except Exception as e:
-        pocs.logger.warning("Problem adjusting tracking: {}".format(e))
+            pocs.say("Done with tracking adjustment, going to observe")
+            pocs.next_state = 'observing'
+        except Exception as e:
+            pocs.logger.warning("Problem adjusting tracking: {}".format(e))
