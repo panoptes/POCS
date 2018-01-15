774154a34bfebfe9bdb01ef3e7e6cba652d1883d
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-01-15 11:02:00 +1100

    Add a test for setting `equinox=None`

--- pocs/tests/test_field.py
+++ pocs/tests/test_field.py
@@ -18,6 +18,11 @@ def test_create_field_name():
     assert field.name == 'Test Field - 32b'
 
 
+def test_equinox_none():
+    field = Field('Test Field - 32b', '20h00m43.7135s +22d42m39.0645s', equinox=None)
+    assert field.coord.equinox == 'J2000'
+
+
 def test_create_field_Observation_name():
     field = Field('Test Field - 32b', '20h00m43.7135s +22d42m39.0645s')
     assert field.field_name == 'TestField32B'
