85ea77a48900ff517afeb358df51cecd33b80e8e
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-01-16 10:12:08 +1100

    Adding codestyle check for peas.
    Fixing an error it didn't like.

--- pocs/tests/test_codestyle.py
+++ pocs/tests/test_codestyle.py
@@ -5,6 +5,9 @@ import pytest
 def test_conformance():
     """Test that we conform to PEP-8."""
     style = pycodestyle.StyleGuide(quiet=False, ignore=['E501', 'E402'])
-    style.input_dir('pocs')
-    result = style.check_files()
-    assert result.total_errors == 0, "Found code style errors (and warnings)."
+    dirs_to_check = ['pocs', 'peas']
+
+    for d in dirs_to_check:
+        style.input_dir(d)
+        result = style.check_files()
+        assert result.total_errors == 0, "Found code style errors (and warnings)."
