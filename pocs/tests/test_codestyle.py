e77a4e6be8f667719091007a51bf4278ff80d1ef
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-01-16 16:32:30 +1100

    Correct codestyle check for peas and pocs dir

--- pocs/tests/test_codestyle.py
+++ pocs/tests/test_codestyle.py
@@ -2,12 +2,16 @@ import pycodestyle
 import pytest
 
 
-def test_conformance():
+@pytest.fixture(scope='module', params=['pocs', 'peas'])
+def dirs_to_check(request):
+    return request.param
+
+
+def test_conformance(dirs_to_check):
     """Test that we conform to PEP-8."""
     style = pycodestyle.StyleGuide(quiet=False, ignore=['E501', 'E402'])
-    dirs_to_check = ['pocs', 'peas']
 
-    for d in dirs_to_check:
-        style.input_dir(d)
-        result = style.check_files()
-        assert result.total_errors == 0, "Found code style errors (and warnings)."
+    print(dirs_to_check)
+    style.input_dir(dirs_to_check)
+    result = style.check_files()
+    assert result.total_errors == 0, "Found code style errors (and warnings)."
