6b51ddf85dbeb4c7cb0d3595957cecc6a12a103b
James Synge <jamessynge@users.noreply.github.com>
2018-02-05 08:35:26 -0500

    Fix flakiness of test_messaging.py (#440)
    Use two Event objects from a multiprocessing.SyncManager
    to coordinate the usage of the messaging forwarder.
    In particular, once the forwarder has created the sockets we
    can create the publisher and subscriber. And when shutting
    down, we should wait until the sockets have been closed else
    we may find that we can't create the ports for the next test.
    
    Sometimes receive_message() wasn't get the message
    because the subscriber hadn't been properly hooked
    up to the publisher through zeromq. This commit
    combines the pub and sub fixtures into a single
    pub_and_sub fixture that guarantees that the subscriber
    is created first, and that there is a pause after
    creating both the subscriber and the publisher.
    
    Unfortunately, as the zeromq manual makes clear,
    there isn't any guarantee about how long one must
    wait before a subscriber is fully connected to a publisher.
    
    Nonetheless, local testing finds this change to be 100%
    reliable; specifically, I ran the tests 1000 time each with
    no failures; that is better than any previous attempt at
    fixing issue #131.
    
    Fixes #131.

--- pocs/tests/test_messaging.py
+++ pocs/tests/test_messaging.py
@@ -1,54 +1,66 @@
+import multiprocessing
 import pytest
 import time
 
 from datetime import datetime
-from multiprocessing import Process
 from pocs.utils.messaging import PanMessaging
 
 
+@pytest.fixture(scope='module')
+def mp_manager():
+    return multiprocessing.Manager()
+
+
 @pytest.fixture(scope='function')
-def forwarder():
+def forwarder(mp_manager):
+    ready = mp_manager.Event()
+    done = mp_manager.Event()
+
     def start_forwarder():
-        PanMessaging.create_forwarder(12345, 54321)
+        PanMessaging.create_forwarder(
+            12345, 54321, ready_fn=lambda: ready.set(), done_fn=lambda: done.set())
 
-    messaging = Process(target=start_forwarder)
-
+    messaging = multiprocessing.Process(target=start_forwarder)
     messaging.start()
+
+    if not ready.wait(timeout=10.0):
+        raise Exception('Forwarder failed to become ready!')
+    # Wait a moment for the forwarder to start using those sockets.
+    time.sleep(0.05)
+
     yield messaging
+
+    # Stop the forwarder. Since we use the same ports in multiple
+    # tests, we wait for the process to shutdown.
     messaging.terminate()
-
-
-@pytest.fixture(scope='function')
-def sub():
-    messaging = PanMessaging.create_subscriber(54321)
-
-    yield messaging
-    messaging.close()
-
-
-@pytest.fixture(scope='function')
-def pub():
-    messaging = PanMessaging.create_publisher(12345, bind=False, connect=True)
-    time.sleep(2)  # Wait for publisher to start up
-    yield messaging
-    messaging.close()
-
-
-# def test_publisher_receive(pub):
-#     with pytest.raises(AssertionError):
-#         pub.receive_message()
-
-
-# def test_subscriber_send(sub):
-#     with pytest.raises(AssertionError):
-#         sub.send_message('FOO', 'BAR')
+    for _ in range(100):
+        # We can't be sure that the sub-process will succeed in
+        # calling the done_fn, so we also check for the process
+        # ending.
+        if done.wait(timeout=0.01):
+            break
+        if not messaging.is_alive():
+            break
 
 
 def test_forwarder(forwarder):
     assert forwarder.is_alive() is True
 
 
-def test_messaging(forwarder, sub, pub):
+@pytest.fixture(scope='function')
+def pub_and_sub(forwarder):
+    # Ensure that the subscriber is created first.
+    sub = PanMessaging.create_subscriber(54321)
+    time.sleep(0.05)
+    pub = PanMessaging.create_publisher(12345, bind=False, connect=True)
+    time.sleep(0.05)
+    yield (pub, sub)
+    pub.close()
+    sub.close()
+
+
+def test_send_string(pub_and_sub):
+    pub, sub = pub_and_sub
     pub.send_message('TEST-CHANNEL', 'Hello')
     msg_type, msg_obj = sub.receive_message()
 
@@ -58,18 +70,16 @@ def test_messaging(forwarder, sub, pub):
     assert msg_obj['message'] == 'Hello'
 
 
-def test_send_datetime(forwarder, sub, pub):
-    pub.send_message('TEST-CHANNEL', {
-        'date': datetime(2017, 1, 1)
-    })
+def test_send_datetime(pub_and_sub):
+    pub, sub = pub_and_sub
+    pub.send_message('TEST-CHANNEL', {'date': datetime(2017, 1, 1)})
     msg_type, msg_obj = sub.receive_message()
     assert msg_obj['date'] == '2017-01-01T00:00:00'
 
 
-def test_storage_id(forwarder, sub, pub, config, db):
-
+def test_storage_id(pub_and_sub, config, db):
     id0 = db.insert_current('config', {'foo': 'bar'}, store_permanently=False)
-
+    pub, sub = pub_and_sub
     pub.send_message('TEST-CHANNEL', db.get_current('config'))
     msg_type, msg_obj = sub.receive_message()
     assert '_id' in msg_obj
