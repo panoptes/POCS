3c7984290b9d3ca1f17bf7860c4d1b2679ae2e56
Anthony Horton <anthony.horton@aao.gov.au>
2018-02-09 14:29:17 +1100

    Use SBIG serial numbers in log messages, not handle

--- pocs/camera/sbigudrv.py
+++ pocs/camera/sbigudrv.py
@@ -281,10 +281,12 @@ class SBIGDriver(PanBase):
                                results=query_status_results)
 
         if query_status_results.status != status_codes['CS_IDLE']:
-            self.logger.warning('Attempt to start exposure on {} while camera busy!'.format(handle))
+            self.logger.warning('Attempt to start exposure on {} while camera busy!'.format(
+                self._ccd_info[handle]['serial number']))
             # Wait until camera is idle
             while query_status_results.status != status_codes['CS_IDLE']:
-                self.logger.warning('Waiting for exposure on {} to complete'.format(handle))
+                self.logger.warning('Waiting for exposure on {} to complete'.format(
+                    self._ccd_info[handle]['serial_number']))
                 time.sleep(1)
                 with self._command_lock:
                     self._set_handle(handle)
@@ -297,10 +299,12 @@ class SBIGDriver(PanBase):
         if temp_status.coolingEnabled:
             if abs(temp_status.imagingCCDTemperature - temp_status.ccdSetpoint) > 0.5 or \
                temp_status.imagingCCDPower == 100.0:
-                self.logger.warning('Unstable CCD temperature in {}'.format(handle))
+                self.logger.warning('Unstable CCD temperature in {}'.format(
+                    self._ccd_info[handle]['serial_number']))
 
         # Start exposure
-        self.logger.debug('Starting {} second exposure on {}'.format(seconds, handle))
+        self.logger.debug('Starting {} second exposure on {}'.format(seconds,
+                                                                     self._ccd_info[handle]['serial_number']))
         with self._command_lock:
             self._set_handle(handle)
             self._send_command('CC_START_EXPOSURE2', params=start_exposure_params)
@@ -355,7 +359,8 @@ class SBIGDriver(PanBase):
 
         # Poll if needed.
         while query_status_results.status != status_codes['CS_INTEGRATION_COMPLETE']:
-            self.logger.debug('Waiting for exposure on {} to complete'.format(handle))
+            self.logger.debug('Waiting for exposure on {} to complete'.format(
+                self._ccd_info[handle]['serial_number']))
             time.sleep(0.1)
             with self._command_lock:
                 self._set_handle(handle)
@@ -363,7 +368,7 @@ class SBIGDriver(PanBase):
                                    params=query_status_params,
                                    results=query_status_results)
 
-        self.logger.debug('Exposure on {} complete'.format(handle))
+        self.logger.debug('Exposure on {} complete'.format(self._ccd_info[handle]['serial_number']))
 
         # Readout data
         with self._command_lock:
@@ -376,7 +381,7 @@ class SBIGDriver(PanBase):
                                        params=readout_line_params,
                                        results=as_ctypes(image_data[i]))
                 except RuntimeError as err:
-                    message = 'Readout error on {}: expected {} rows, got{}!'.format(handle,
+                    message = 'Readout error on {}: expected {} rows, got{}!'.format(self._ccd_info[handle]['serial_number'],
                                                                                      height,
                                                                                      i)
                     self.logger.error(message)
@@ -387,10 +392,11 @@ class SBIGDriver(PanBase):
             try:
                 self._send_command('CC_END_READOUT', params=end_readout_params)
             except RuntimeError as err:
-                message = "Error ending readout on {}: {}".format(handle, err)
+                message = "Error ending readout on {}: {}".format(self._ccd_info[handle]['serial_number'],
+                                                                  err)
                 self.logger.error(message)
             else:
-                self.logger.debug('Readout on {} complete'.format(handle))
+                self.logger.debug('Readout on {} complete'.format(self._ccd_info[handle]['serial_number']))
             finally:
                 fits_utils.write_fits(image_data, header, filename, self.logger, exposure_event)
 
