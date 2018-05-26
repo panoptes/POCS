999ffdcf15ea3be2baf66971dcdf966e501668af
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-05-27 08:57:47 +1000

    * Allow `solve-field` to be set by an env variable

--- scripts/solve_field.sh
+++ scripts/solve_field.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-SOLVE_FIELD="${PANDIR}/astrometry/bin/solve-field"
 if [ ! -f $SOLVE_FIELD ]
 then
-  echo 2>&1 ""
-  echo 2>&1 "Please install astrometry into ${PANDIR}/astrometry to enable $0"
-  echo 2>&1 ""
-  exit 3 # Command not found
+	SOLVE_FIELD="${PANDIR}/astrometry/bin/solve-field"
+	if [ ! -f $SOLVE_FIELD ]
+	then
+	  echo 2>&1 ""
+	  echo 2>&1 "Please install astrometry into ${PANDIR}/astrometry to enable $0"
+	  echo 2>&1 ""
+	  exit 3 # Command not found
+	 fi
 fi
 
 if [[ $# == 1 ]]; then
     echo "Using options: --guess-scale --no-plots --downsample 3 --overwrite"
     ${SOLVE_FIELD} --guess-scale --no-plots --downsample 3 --overwrite $1
 else
-    ${SOLVE_FIELD} $@
+    ${SOLVE_FIELD} --config /etc/astrometry.cfg $@
 fi
