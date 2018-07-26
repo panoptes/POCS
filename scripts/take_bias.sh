6c21cc124761cf276eb5a4d11817f3e06901d51b
Wilfred Gee <wtylergee@gmail.com>
2018-07-27 08:14:56 +1000

    Allow SHUTTER_INDEX and NUM_FRAMES to be passed as positional parameters.

--- scripts/take_bias.sh
+++ scripts/take_bias.sh
@@ -9,11 +9,12 @@
 ##################################################
 
 P=$1
+
 SAVE_FILE=${2:-"bias-%Y%m%d-%H%M%S.cr2"}
 echo "Taking bias frames on port ${P}: ${SAVE_FILE}"
 
-SHUTTER_INDEX=52
-NUM_FRAMES=5
+NUM_FRAMES=${4:-5}
+SHUTTER_INDEX=${3:-52} # 1/4000s on EOS 100D
 
 # Set to fast speed
 gphoto2 --port=${P} --set-config-index shutterspeed=${SHUTTER_INDEX}
