150e086e1704bb6e10d8f12d4e6ac6b124c026cd
Wilfred Gee <wtylergee@gmail.com>
2018-08-25 12:17:45 +1000

    Adding shutterspeed and capture target to base file

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -7,8 +7,13 @@ echo 'Taking picture'
 echo "T = ${T}s"
 
 # Open shutter
-gphoto2 --port=${P} --set-config eosremoterelease=Immediate \
-         --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=2s \
-         --filename "${F}"
+gphoto2 --port=${P} \
+		--set-config shutterspeed=0 \
+		--set-config capturetarget=0 \
+		--set-config eosremoterelease=Immediate \
+        --wait-event=${T}s \
+        --set-config eosremoterelease=4 \
+        --wait-event-and-download=2s \
+        --filename "${F}"
 
 echo "Done with pic"
