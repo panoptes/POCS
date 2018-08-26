52504c22761836899b27a591183a31e693736869
jamessynge <james.synge@gmail.com>
2018-08-26 17:26:57 -0400

    Respond to feedback:
    Use pocs.utils.current_time() and pocs.utils.Timeout
    for working with time.
    
    Sleep for short amounts of time on each loop, but only
    emit debug log messages or status every 30 and 10 seconds,
    respectively.
    
    Extend pocs.utils.Timeout with support for Astropy durations
    (e.g. astropy.units.second, .minute or .hour).

--- pocs/state/states/default/observing.py
+++ pocs/state/states/default/observing.py
@@ -1,53 +1,61 @@
+from astropy import units as u
 import time
 
-from pocs.utils import error
+from pocs import utils as pocs_utils
 
-min_sleeping_interval = 0.1
-waiting_msg_interval = 15
-
-# Why is this not passed in based on the length of the current observation?
-timeout = 150.
+SLEEP_SECONDS = 1.0
+STATUS_INTERVAL = 10. * u.second
+WAITING_MSG_INTERVAL = 30. * u.second
+MAX_EXTRA_TIME = 60 * u.second
 
 
 def on_enter(event_data):
-    """ """
+    """Wait for camera exposures to complete.
+
+    Frequently check for the exposures to complete, the observation to be
+    interrupted, messages to be received. Periodically post to the STATUS
+    channel and to the debug log.
+     """
     pocs = event_data.model
     pocs.say("I'm finding exoplanets!")
     pocs.next_state = 'parking'
 
     try:
-        # Start the observing
-        start_time = time.time()
+        maximum_duration = pocs.observatory.current_observation.exp_time + MAX_EXTRA_TIME
+
+        # Start the observing.
+        start_time = pocs_utils.current_time()
         camera_events = pocs.observatory.observe()
 
-        timeout_time = start_time + timeout
-        next_msg_time = start_time + waiting_msg_interval
+        timeout = pocs_utils.Timeout(maximum_duration)
+        next_status_time = start_time + STATUS_INTERVAL
+        next_msg_time = start_time + WAITING_MSG_INTERVAL
         while not all([event.is_set() for event in camera_events.values()]):
             pocs.check_messages()
             if pocs.interrupted:
                 pocs.say("Observation interrupted!")
                 break
 
-            now = time.time()
+            now = pocs_utils.current_time()
             if now >= next_msg_time:
+                elapsed_secs = (now - start_time).to(u.second).value
                 pocs.logger.debug(
-                    'Waiting for images: {} seconds elapsed'.format(round(now - start_time)))
+                    'Waiting for images: {} seconds elapsed'.format(round(elapsed_secs)))
+                next_msg_time += WAITING_MSG_INTERVAL
+                now = pocs_utils.current_time()
+
+            if now >= next_status_time:
                 pocs.status()
-                next_msg_time += waiting_msg_interval
-                now = time.time()
+                next_status_time += STATUS_INTERVAL
+                now = pocs_utils.current_time()
 
-            if now >= timeout_time:
-                raise error.Timeout
+            if timeout.expired():
+                raise pocs_utils.error.Timeout
 
-            # Sleep until almost the time for the next message, or just
-            # before the timeout time. We assume that checking the
-            # camera events and messages will take a small fraction of
-            # a second
-            sleep_time = min(next_msg_time - now, timeout_time - now)
-            sleep_time = max(min_sleeping_interval, sleep_time - min_sleeping_interval)
-            time.sleep(sleep_time)
+            # Sleep for a little bit.
+            time.sleep(SLEEP_SECONDS)
 
-    except error.Timeout as e:
+    except pocs_utils.error.Timeout as e:
         pocs.logger.warning(
             "Timeout while waiting for images. Something wrong with camera, going to park.")
     except Exception as e:
