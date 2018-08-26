72cd9fd26c1715acd1d42f93feed0ec01e6b59b3
Wilfred Gee <wtylergee@gmail.com>
2018-08-26 14:17:55 +1000

    Adding PONG to POCS

---
+++ pong/clouddb.py
@@ -0,0 +1,242 @@
+import os
+
+from warnings import warn
+
+import psycopg2
+from astropy.wcs import WCS
+
+try:
+    host_lookup = {
+        'panoptes-meta': os.environ['METADB_IP'],
+        'tess-catalog': os.environ['TESSDB_IP'],
+    }
+except KeyError:
+    host_lookup = dict()
+
+
+def get_db_proxy_conn(
+        host='127.0.0.1',
+        db_name='panoptes',
+        db_user='panoptes',
+        port=5432):
+    """Return postgress connection to local proxy.
+
+    Args:
+        host (str, optional): Hostname, default localhost.
+        db_user (str, optional): Name of db user, default 'panoptes'.
+        db_name (str, optional): Name of db, default 'postgres'.
+        port (int, optional): DB port.
+
+    Returns:
+        `psycopg2.Connection`: DB connection object.
+    """
+    try:
+        pg_pass = os.environ['PGPASSWORD']
+    except KeyError:
+        warn("DB password has not been set")
+        return None
+
+    conn_params = {
+        'host': host,
+        'port': port,
+        'user': db_user,
+        'dbname': db_name,
+        'password': pg_pass,
+    }
+    conn_str = ' '.join("{}={}".format(k, v) for k, v in conn_params.items())
+
+    conn = psycopg2.connect(conn_str)
+    return conn
+
+
+def get_db_conn(instance='panoptes-meta',
+                db_name='panoptes',
+                db_user='panoptes',
+                port=5432):
+    """Gets a connection to the Cloud SQL db.
+
+    Args:
+        instance (str, optional): Cloud SQL instance to connect to.
+        db_user (str, optional): Name of db user, default 'panoptes'.
+        db_name (str, optional): Name of db, default 'panoptes'.
+        port (int, optional): DB port.
+
+    Returns:
+        `psycopg2.Connection`: DB connection handle.
+    """
+    try:
+        pg_pass = os.environ['PGPASSWORD']
+    except KeyError:
+        warn("DB password has not been set")
+        return None
+
+    ssl_root_cert = os.path.join(os.environ['SSL_KEYS_DIR'], instance, 'server-ca.pem')
+    ssl_client_cert = os.path.join(os.environ['SSL_KEYS_DIR'], instance, 'client-cert.pem')
+    ssl_client_key = os.path.join(os.environ['SSL_KEYS_DIR'], instance, 'client-key.pem')
+
+    conn_params = {
+        'sslmode': 'verify-full',
+        'sslrootcert': ssl_root_cert,
+        'sslcert': ssl_client_cert,
+        'sslkey': ssl_client_key,
+        'hostaddr': host_lookup[instance],
+        'host': instance,
+        'port': port,
+        'user': db_user,
+        'dbname': db_name,
+        'password': pg_pass,
+    }
+    conn_str = ' '.join("{}={}".format(k, v) for k, v in conn_params.items())
+
+    conn = psycopg2.connect(conn_str)
+    return conn
+
+
+def get_cursor(**kwargs):
+    """Get a Cursor object.
+
+    Args:
+        **kwargs: Passed to `get_db_conn`
+
+    Returns:
+        `psycopg2.Cursor`: Cursor object.
+    """
+    conn = get_db_proxy_conn(**kwargs)
+    cur = conn.cursor()
+
+    return cur
+
+
+def meta_insert(table, conn=None, logger=None, **kwargs):
+    """Inserts arbitrary key/value pairs into a table.
+
+    Args:
+        table (str): Table in which to insert.
+        conn (None, optional): DB connection, if None then `get_db_proxy_conn`
+            is used.
+        logger (None, optional): A logger.
+        **kwargs: List of key/value pairs corresponding to columns in the
+            table.
+
+    Returns:
+        tuple|None: Returns the inserted row or None.
+    """
+    if conn is None:
+        conn = get_db_proxy_conn()
+
+    cur = conn.cursor()
+
+    col_names = ','.join(kwargs.keys())
+    col_val_holders = ','.join(['%s' for _ in range(len(kwargs))])
+
+    insert_sql = 'INSERT INTO {} ({}) VALUES ({}) ON CONFLICT DO NOTHING RETURNING *'.format(
+        table, col_names, col_val_holders)
+
+    try:
+        cur.execute(insert_sql, list(kwargs.values()))
+        conn.commit()
+        return cur.fetchone()
+    except Exception as e:
+        conn.rollback()
+        warn("Error on fetch: {}".format(e))
+        if logger:
+            logger.log_text("Can't insert row: {}".format(e))
+        return None
+
+
+def add_header_to_db(header, conn=None, logger=None):
+    """Add FITS image info to metadb.
+
+    Note:
+        This function doesn't check header for proper entries and
+        assumes a large list of keywords. See source for details.
+
+    Args:
+        header (dict): FITS Header data from an observation.
+        conn (None, optional): DB connection, if None then `get_db_proxy_conn`
+            is used.
+        logger (None, optional): A logger.
+
+    Returns:
+        str: The image_id.
+    """
+    unit_id = int(header['OBSERVER'].strip().replace('PAN', ''))
+    seq_id = header['SEQID'].strip()
+    img_id = header['IMAGEID'].strip()
+    camera_id = header['INSTRUME'].strip()
+
+    unit_data = {
+        'id': unit_id,
+        'name': header['OBSERVER'].strip(),
+        'lat': float(header['LAT-OBS']),
+        'lon': float(header['LONG-OBS']),
+        'elevation': float(header['ELEV-OBS']),
+    }
+    meta_insert('units', conn=conn, logger=logger, **unit_data)
+
+    camera_data = {
+        'unit_id': unit_id,
+        'id': camera_id,
+    }
+    meta_insert('cameras', conn=conn, logger=logger, **camera_data)
+
+    seq_data = {
+        'id': seq_id,
+        'unit_id': unit_id,
+        'start_date': header['SEQID'].split('_')[-1],
+        'exp_time': header['EXPTIME'],
+        'ra_rate': header['RA-RATE'],
+        'pocs_version': header['CREATOR'],
+        'piaa_state': header['PSTATE'],
+    }
+    logger.log_text("Inserting sequence: {}".format(seq_data))
+    try:
+        bl, tl, tr, br = WCS(header).calc_footprint()  # Corners
+        seq_data['coord_bounds'] = '(({}, {}), ({}, {}))'.format(
+            bl[0], bl[1],
+            tr[0], tr[1]
+        )
+        meta_insert('sequences', conn=conn, logger=logger, **seq_data)
+        logger.log_text("Sequence inserted: {}".format(seq_id))
+    except Exception as e:
+        logger.log_text("Can't get bounds: {}".format(e))
+        if 'coord_bounds' in seq_data:
+            del seq_data['coord_bounds']
+        try:
+            meta_insert('sequences', conn=conn, logger=logger, **seq_data)
+        except Exception as e:
+            logger.log_text("Can't insert sequence: {}".format(seq_id))
+            raise e
+
+    image_data = {
+        'id': img_id,
+        'sequence_id': seq_id,
+        'date_obs': header['DATE-OBS'],
+        'moon_fraction': header['MOONFRAC'],
+        'moon_separation': header['MOONSEP'],
+        'ra_mnt': header['RA-MNT'],
+        'ha_mnt': header['HA-MNT'],
+        'dec_mnt': header['DEC-MNT'],
+        'airmass': header['AIRMASS'],
+        'exp_time': header['EXPTIME'],
+        'iso': header['ISO'],
+        'camera_id': camera_id,
+        'cam_temp': header['CAMTEMP'].split(' ')[0],
+        'cam_colortmp': header['COLORTMP'],
+        'cam_circconf': header['CIRCCONF'].split(' ')[0],
+        'cam_measrggb': header['MEASRGGB'],
+        'cam_red_balance': header['REDBAL'],
+        'cam_blue_balance': header['BLUEBAL'],
+        'file_path': header['FILEPATH']
+    }
+
+    # Add plate-solved info.
+    try:
+        image_data['center_ra'] = header['CRVAL1']
+        image_data['center_dec'] = header['CRVAL2']
+    except KeyError:
+        pass
+
+    img_row = meta_insert('images', conn=conn, logger=logger, **image_data)
+
+    return img_row
---
+++ pong/storage.py
@@ -0,0 +1,267 @@
+import os
+import re
+import time
+import subprocess
+import shutil
+from warnings import warn
+
+import google.cloud.exceptions
+from google.cloud import storage
+
+from pocs.utils.images import fits as fits_utils
+
+
+def get_bucket(name):
+    """Get Storage bucket object.
+
+    Args:
+        name (str): Name of Storage bucket.
+
+    Returns:
+        google.cloud.storage.client.Client|None: Stroage Client or None
+            if bucket does not exist.
+    """
+    client = storage.Client()
+    bucket = None
+
+    try:
+        bucket = client.get_bucket(name)
+    except google.cloud.exceptions.NotFound:
+        print('Sorry, that bucket does not exist!')
+
+    return bucket
+
+
+def get_observation_blob(blob_name, bucket_name='panoptes-survey'):
+    """Returns an individual Blob.
+
+    Args:
+        blob_name (str): Full name of Blob to be fetched.
+        bucket_name (str, optional): Name of Storage bucket where Observation is
+            stored, default `panoptes-survey` (Shouldn't need to change).
+
+    Returns:
+        google.cloud.storage.blob.Blob|None: Blob object or None.
+    """
+    return get_bucket(bucket_name).get_blob(blob_name)
+
+
+def get_observation_blobs(prefix, include_pointing=False, bucket_name='panoptes-survey'):
+    """Returns the list of Storage blobs (files) matching the prefix.
+
+    Note:
+        The prefix can be any pattern, e.g:
+            - 'PAN006'  # All images for unit
+            - 'Hd189733'  # All images for field
+            - 'PAN006/Hd189733/7bab97' # All images for unit, field, camera
+            - 'PAN006/*/7bab97' # All images for unit, camera
+            - 'PAN006/Hd189733/7bab97/20180327T071126/' # Specific observation
+
+    Args:
+        prefix (str): Path in storage to observation sequence, see note.
+        include_pointing (bool, optional): Whether or not to include the pointing file,
+            default False.
+        bucket_name (str, optional): Name of Storage bucket where Observation is
+            stored, default `panoptes-survey` (Shouldn't need to change).
+
+    Returns:
+        google.cloud.storage.blob.Blob: Blob object.
+    """
+
+    # The bucket we will use to fetch our objects
+    bucket = get_bucket(bucket_name)
+
+    objs = list()
+    for f in bucket.list_blobs(prefix=prefix):
+        if 'pointing' in f.name and not include_pointing:
+            continue
+        elif f.name.endswith('.fz') is False:
+            continue
+        else:
+            objs.append(f)
+
+    return sorted(objs, key=lambda x: x.name)
+
+
+def download_fits_file(img_blob, save_dir='.', force=False, unpack=False, callback=None):
+    """Downloads (and uncompresses) the image blob data.
+
+    Args:
+        img_blob (str|google.cloud.storage.blob.Blob): Blob object corresponding to FITS file.
+            If just the blob name is given then file will be downloaded.
+        save_dir (str, optional): Path for saved file, defaults to current directory.
+        force (bool, optional): Force a download even if file exists locally, default False.
+        unpack (bool, optional): If file should be uncompressed, default False.
+        callback (callable, optional): A callable object that gets called at end of
+        function.
+
+    Returns:
+        str: Path to local (uncompressed) FITS file
+    """
+    if isinstance(img_blob, str):
+        img_blob = get_observation_blob(img_blob)
+
+    output_path = os.path.join(
+        save_dir,
+        img_blob.name.replace('/', '_')
+    )
+
+    # Make dir if needed
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # If we don't  have file (or force=True) then download directly to
+    # (compressed) FITS file.
+    if not os.path.exists(output_path):
+        with open(output_path, 'wb') as f:
+            img_blob.download_to_file(f)
+
+    # Wait for download
+    timeout = 10
+    while os.path.exists(output_path) is False:
+        timeout -= 1
+        if timeout < 0:
+            return None
+        time.sleep(1)
+
+    # Once downloaded, uncompress
+    if os.path.exists(output_path) and unpack:
+        output_path = fits_utils.fpack(output_path, unpack=True)
+
+    # User supplied callback (e.g. logging)
+    if callback is not None:
+        try:
+            callback()
+        except TypeError as e:
+            warn('callback must be callable')
+
+    return output_path
+
+
+def upload_fits_file(img_path, bucket_name='panoptes-survey'):
+    """Uploads an image to the storage bucket.
+
+    Args:
+        img_path (str): Path to local file to upload.
+        bucket_name (str, optional): Bucket to upload, default 'panoptes-survey'.
+
+    """
+    bucket = get_bucket(bucket_name)
+
+    # Replace anything before the unit id
+    bucket_path = re.sub(r'^.*PAN', 'PAN', img_path).replace('_', '/')
+
+    try:
+        blob = bucket.blob(bucket_path)
+        blob.upload_from_filename(img_path)
+    except Exception as e:
+        warn("Can't upload file: {}".format(e))
+
+
+def get_header(img_blob, parse_line=None):
+    """Read the FITS header from storage.
+
+    FITS Header Units are stored in blocks of 2880 bytes consisting of 36 lines
+    that are 80 bytes long each. The Header Unit always ends with the single
+    word 'END' on a line (not necessarily line 36).
+
+    Here the header is streamed from Storage until the 'END' is found, with
+    each line given minimal parsing.
+
+    See https://fits.gsfc.nasa.gov/fits_primer.html for overview of FITS format.
+
+    Args:
+        img_blob (google.cloud.storage.blob.Blob): Blob object corresponding to stored FITS file.
+        parse_line (callable, optional): A callable function which will be passed an 80 character
+            string corresponding to each line in the header.
+
+    Returns:
+        dict: FITS header as a dictonary.
+    """
+    i = 1
+    if img_blob.name.endswith('.fz'):
+        i = 2  # We skip the compression header info
+
+    headers = dict()
+
+    streaming = True
+    while streaming:
+        # Get a header card
+        start_byte = 2880 * (i - 1)
+        end_byte = (2880 * i) - 1
+        b_string = img_blob.download_as_string(start=start_byte, end=end_byte)
+
+        # Loop over 80-char lines
+        for j in range(0, len(b_string), 80):
+            item_string = b_string[j:j + 80].decode()
+
+            # End of FITS Header, stop streaming
+            if item_string.startswith('END'):
+                streaming = False
+                break
+
+            # If custom parse function supplied, call that
+            if parse_line is not None:
+                try:
+                    parse_line(item_string)
+                except TypeError as e:
+                    warn('parse_line must be callable')
+                continue
+
+            # Get key=value pairs (skip COMMENTS and HISTORY)
+            if item_string.find('=') > 0:
+                k, v = item_string.split('=')
+
+                # Remove FITS comment
+                if ' / ' in v:
+                    v = v.split(' / ')[0]
+
+                v = v.strip()
+
+                # Cleanup and discover type in dumb fashion
+                if v.startswith("'") and v.endswith("'"):
+                    v = v.replace("'", "").strip()
+                elif v.find('.') > 0:
+                    v = float(v)
+                elif v == 'T':
+                    v = True
+                elif v == 'F':
+                    v = False
+                else:
+                    v = int(v)
+
+                headers[k.strip()] = v
+
+        i += 1
+
+    return headers
+
+
+def upload_to_bucket(bucket, local_path, remote_path):
+    """ Upload to Google Storage Bucket using gsutil.
+
+    Note:
+        This function does no sanity checking on `remote_path`.
+
+    See also: `upload_fits_file()`
+
+    Args:
+        bucket (str): Bucket name.
+        local_path (str): Path to local file.
+        remote_path (str): Path to remote file.
+    """
+    gsutil = shutil.which('gsutil')
+    assert gsutil is not None, "gsutil command line utility not found"
+
+    bucket = 'gs://{}/'.format(bucket)
+    # normpath strips the trailing slash so add here so we place in directory
+    run_cmd = [gsutil, '-mq', 'cp', local_path, bucket + remote_path]
+    print("Running: {}".format(run_cmd))
+
+    try:
+        completed_process = subprocess.run(run_cmd, stdout=subprocess.PIPE)
+
+        if completed_process.returncode != 0:
+            print("Problem uploading")
+            print(completed_process.stdout)
+    except Exception as e:
+        print("Problem uploading: {}".format(e))
