c07969052050d7564626f6c82c011484680a249d
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-09-04 12:00:03 -1000

    Clean config items related to pointing

--- pocs/state/states/default/pointing.py
+++ pocs/state/states/default/pointing.py
@@ -6,8 +6,6 @@ from pocs.utils import error
 wait_interval = 3.
 timeout = 150.
 
-num_pointing_images = 2
-
 
 def on_enter(event_data):
     """Pointing State
@@ -17,6 +15,12 @@ def on_enter(event_data):
     pocs = event_data.model
 
     pocs.next_state = 'parking'
+
+    # Get pointing parameters
+    pointing_config = pocs.config['pointing']
+    num_pointing_images = pointing_config.get('max_iterations', 3)
+    should_correct = pointing_config.get('auto_correct', False)
+    pointing_threshold = pointing_config.get('threshold', False)
 
     try:
         pocs.say("Taking pointing picture.")
@@ -29,9 +33,11 @@ def on_enter(event_data):
         fits_headers['POINTING'] = 'True'
         pocs.logger.debug("Pointing headers: {}".format(fits_headers))
 
+        # Loop over maximum number of pointing iterations
         for img_num in range(num_pointing_images):
             camera_events = dict()
 
+            # Take pointing image with primary camera
             for cam_name, camera in pocs.observatory.cameras.items():
                 if camera.is_primary:
                     pocs.logger.debug("Exposing for camera: {}".format(cam_name))
@@ -49,6 +55,7 @@ def on_enter(event_data):
                     except Exception as e:
                         pocs.logger.error("Problem waiting for images: {}".format(e))
 
+            # Wait for images to complete
             wait_time = 0.
             while not all([event.is_set() for event in camera_events.values()]):
                 pocs.check_messages()
@@ -65,8 +72,9 @@ def on_enter(event_data):
                 sleep(wait_interval)
                 wait_time += wait_interval
 
+            # Analyze pointing
             if pocs.observatory.current_observation is not None:
-                pointing_id, pointing_path = pocs.observatory.current_observation.last_exposure
+                pointing_id, pointing_path = observation.last_exposure
                 pointing_image = Image(
                     pointing_path,
                     location=pocs.observatory.earth_location
@@ -84,13 +92,16 @@ def on_enter(event_data):
 
                 separation = pointing_image.pointing_error.magnitude.value
 
-                if separation > pocs.config.get('pointing_threshold', 0.05):
-                    pocs.say("I'm still a bit away from the field so I'm going to get a bit closer.")
+                # Correct the pointing
+                if should_correct and separation > pointing_threshold:
+                    pocs.say("I'm still a bit away from the field so I'm going to get closer.")
 
                     # Tell the mount we are at the field, which is the center
                     pocs.say("Syncing with the latest image...")
                     has_field = pocs.observatory.mount.set_target_coordinates(pointing_image.pointing)
                     pocs.logger.debug("Coords set, calibrating")
+
+                    # Sync the mount
                     pocs.observatory.mount.query('calibrate_mount')
 
                     # Now set back to field
@@ -99,6 +110,10 @@ def on_enter(event_data):
                             pocs.logger.debug("Slewing back to target")
                             pocs.observatory.mount.set_target_coordinates(observation.field)
                             pocs.observatory.mount.slew_to_target()
+                else:
+                    # Either we want to correct pointing or we are close enough
+                    # so we stop the pointing loop correction.
+                    break
 
         pocs.next_state = 'tracking'
 
