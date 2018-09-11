707d3b3d2ca54b288d0150d8f9e7b64749b2cf48
Wilfred Gee <wtylergee@gmail.com>
2018-09-12 09:50:42 +1000

    Basic cleanup of take_pic script

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-P=$1
-T=$2
-F=$3
+usage() {
+  echo -n "##################################################
+# Take a picture via gphoto2.
+# 
+# This will change the camera setting to bulb and then take
+# an exposure for the requested amount of time.
+##################################################
+ $ $(basename $0) PORT EXPTIME FILENAME
+ 
+ Options:
+  PORT              USB port as reported by gphoto2 --auto-detect, e.g. usb:001,004.
+  EXPTIME			Exposure time in seconds, should be greater than 1 second.
+  OUTFILE           Output filename (with .cr2 extension).
+"
+}
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+PORT=$1
+EXPTIME=$2
+FILENAME=$3
 echo 'Taking picture'
-echo "T = ${T}s"
-
-
-# Make sure bulb is set
-gphoto2 --port=${P} --set-config-index=0 --set-config-index capturetarget=0
+echo "PORT = ${PORT}"
+echo "TIME = ${TIME}s"
+echo "FILE = ${FILENAME}"
 
 # Open shutter
-gphoto2 --port=${P} --set-config eosremoterelease=Immediate \
-         --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=2s \
-         --filename "${F}"
+gphoto2 --port=${PORT} \
+		--set-config shutterspeed=0 \
+		--set-config capturetarget=0 \
+		--set-config eosremoterelease=Immediate \
+		--wait-event=${EXPTIME}s \
+		--set-config eosremoterelease=4 \
+		--wait-event-and-download=2s \
+		--filename "${FILENAME}"
 
 echo "Done with pic"
