233b0ba83e9bb3a144dcf5bb973e164b84731dd7
Wilfred Gee <wtylergee@gmail.com>
2018-09-12 09:43:22 +1000

    Make sure bulb setting is active (can get switched with bias scripts)

--- scripts/take_pic.sh
+++ scripts/take_pic.sh
@@ -6,6 +6,10 @@ F=$3
 echo 'Taking picture'
 echo "T = ${T}s"
 
+
+# Make sure bulb is set
+gphoto2 --port=${P} --set-config-index=0 --set-config-index capturetarget=0
+
 # Open shutter
 gphoto2 --port=${P} --set-config eosremoterelease=Immediate \
          --wait-event=${T}s --set-config eosremoterelease=4 --wait-event-and-download=2s \
