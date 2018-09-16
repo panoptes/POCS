bfe97f3f950cc9f42b3481cd98328ffefea6791d
jamessynge <james.synge@gmail.com>
2018-09-15 20:25:52 -0400

    Address feedback.

--- pocs/utils/messaging.py
+++ pocs/utils/messaging.py
@@ -189,7 +189,7 @@ class PanMessaging(object):
         if isinstance(message, str):
             message = {
                 'message': message,
-                'timestamp': current_time().isot.replace('T', ' ').split('.')[0]
+                'timestamp': current_time(pretty=True),
             }
         elif isinstance(message, dict):
             message = self.scrub_message(message)
