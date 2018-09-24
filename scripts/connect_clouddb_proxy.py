277e114a4b0241ec6eb749456aeaf60dee02a689
Wilfred Gee <wtylergee@gmail.com>
2018-09-25 08:00:44 +1000

    Option to use one proxy to connect to all available instances.

--- scripts/connect_clouddb_proxy.py
+++ scripts/connect_clouddb_proxy.py
@@ -16,15 +16,20 @@ from pprint import pprint
 from pocs.utils.config import load_config
 
 
-def main(instance_id, local_port, verbose=False):
+def main(instances, key_file, verbose=False):
+    assert os.path.isfile(key_file)
+
     proxy_cmd = os.path.join(os.environ['POCS'], 'bin', 'cloud_sql_proxy')
-
-    connection_str = '{}=tcp:{}'.format(instance_id, local_port)
-    instances_arg = '-instances={}'.format(connection_str)
-
     assert os.path.isfile(proxy_cmd)
 
-    run_proxy_cmd = [proxy_cmd, instances_arg]
+    connection_str = ','.join(instances)
+    instances_arg = '-instances={}'.format(connection_str)
+    credentials_arg = '-credential_file={}'.format(key_file)
+
+    run_proxy_cmd = [proxy_cmd, instances_arg, credentials_arg]
+
+    if verbose:
+        print("Running command: {}".format(run_proxy_cmd))
 
     stdout_handler = subprocess.PIPE
     if verbose:
@@ -41,10 +46,15 @@ if __name__ == '__main__':
 
     # Get the command line option
     parser = argparse.ArgumentParser(description="Connect to a google CloudSQL via a local proxy")
-    parser.add_argument('-d', '--database', default=None, required=True,
-                        help="Database, currently 'meta' or 'tess' from the config.")
-    parser.add_argument('-v', '--verbose', action='store_true', default=False,
-                        help="Print results to stdout")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--from_config', default=True, action='store_true',
+                       help="Connect to all instances listed in the config file, default True.")
+    group.add_argument('--database', default=None,
+                       help="Connect to a specific database listed in the config.")
+    parser.add_argument('--key_file', default=None, required=True,
+                        help="JSON service account key location.")
+    parser.add_argument('--verbose', action='store_true', default=False,
+                        help="Print results to stdout, default False.")
     args = parser.parse_args()
 
     config = load_config()
@@ -53,22 +63,31 @@ if __name__ == '__main__':
         if args.verbose:
             print("Found config:")
             pprint(network_config)
-        instance_info = network_config['cloudsql_instances'][args.database]
+
+        project_id = network_config['project_id']
 
         # Get connection details
-        project_id = network_config['project_id']
-        server_location = instance_info['location']
-        db_name = instance_info['database']
-        local_port = instance_info['local_port']
+        connect_instances = list()
+        for db_name in network_config['cloudsql_instances']:
+            instance_info = network_config['cloudsql_instances'][db_name]
 
-        instance_id = '{}:{}:{}'.format(project_id, server_location, db_name)
+            if args.database and args.database != db_name:
+                continue
+
+            instance_name = instance_info['instance']
+            location = instance_info['location']
+            local_port = instance_info['local_port']
+
+            conn_str = '{}:{}:{}=tcp:{}'.format(project_id, location, instance_name, local_port)
+            connect_instances.append(conn_str)
 
     except KeyError as e:
-        print("Invalid configuration. Check panoptes_network config.")
+        print("Invalid configuration. Check panoptes_network config. {}".format(e))
         print(e)
         sys.exit(1)
 
     if args.verbose:
-        print("Connecting to {} on local port {}".format(instance_id, local_port))
+        print("Connecting to the following instances:")
+        pprint(connect_instances)
 
-    main(instance_id, local_port, verbose=args.verbose)
+    main(connect_instances, args.key_file, verbose=args.verbose)
