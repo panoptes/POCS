705ab69d3657560818e978f566ba0ad5f6ae1978
Wilfred Gee <wtylergee@gmail.com>
2018-09-24 16:59:36 +1000

    Removing all `spline_smoothing` parameters

--- pocs/focuser/focuser.py
+++ pocs/focuser/focuser.py
@@ -177,7 +177,6 @@ class AbstractFocuser(PanBase):
                   merit_function=None,
                   merit_function_kwargs=None,
                   mask_dilations=None,
-                  spline_smoothing=None,
                   coarse=False,
                   plots=True,
                   blocking=False):
@@ -210,8 +209,6 @@ class AbstractFocuser(PanBase):
                 keyword arguments for the merit function.
             mask_dilations (int, optional): Number of iterations of dilation to perform on the
                 saturated pixel mask (determine size of masked regions), default 10
-            spline_smoothing (float, optional): smoothing parameter for the spline fitting to
-                the autofocus data, 0.0 to 1.0, smaller values mean *less* smoothing, default 0.4
             coarse (bool, optional): Whether to begin with coarse focusing, default False.
             plots (bool, optional: Whether to write focus plots to images folder, default True.
             blocking (bool, optional): Whether to block until autofocus complete, default False.
@@ -297,7 +294,6 @@ class AbstractFocuser(PanBase):
             'merit_function': merit_function,
             'merit_function_kwargs': merit_function_kwargs,
             'mask_dilations': mask_dilations,
-            'spline_smoothing': spline_smoothing,
             'plots': plots,
             'start_event': None,
             'finished_event': None,
@@ -338,7 +334,6 @@ class AbstractFocuser(PanBase):
                    merit_function,
                    merit_function_kwargs,
                    mask_dilations,
-                   spline_smoothing,
                    plots,
                    coarse,
                    start_event,
