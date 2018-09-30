73a6537ffc2ce8b9a8e7a3234ff0c808409e5fe6
Wilfred Gee <wtylergee@gmail.com>
2018-10-01 07:41:11 +1000

    Logic cleanup suggested in review

--- pocs/observatory.py
+++ pocs/observatory.py
@@ -242,11 +242,11 @@ class Observatory(PanBase):
         self.logger.debug("Getting observation for observatory")
 
         # If observation list is empty or a reread is requested
-        reread_fields_file = False
-        if (self.scheduler.has_valid_observations is False or
-                kwargs.get('reread_fields_file', False) or
-                self.config['scheduler'].get('check_file', False)):
-            reread_fields_file = True
+        reread_fields_file = (
+            self.scheduler.has_valid_observations is False or
+            kwargs.get('reread_fields_file', False) or
+            self.config['scheduler'].get('check_file', False)
+        )
 
         # This will set the `current_observation`
         self.scheduler.get_observation(reread_fields_file=reread_fields_file, *args, **kwargs)
