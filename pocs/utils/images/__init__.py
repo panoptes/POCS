2a41e390438c81d3ede7737d2bd559d1c3966e17
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-10-02 19:01:29 +1000

    Revert "Image Upload Improvements" (#660)
    This reverts commit 61c561d64735f0a6f2e284c6c494b550e879336f.

--- pocs/utils/images/__init__.py
+++ pocs/utils/images/__init__.py
@@ -378,3 +378,51 @@ def clean_observation_dir(dir_name, *args, **kwargs):
                     warn('Could not delete file: {!r}'.format(e))
     except Exception as e:
         warn('Problem with cleanup creating timelapse: {!r}'.format(e))
+
+
+def upload_observation_dir(pan_id, dir_name, bucket='panoptes-survey', **kwargs):
+    """Upload an observation directory to google cloud storage.
+
+    Note:
+        This requires that the command line utility `gsutil` be installed
+        and that authentication has properly been set up.
+
+    Args:
+        pan_id (str): A string representing the unit id, e.g. PAN001.
+        dir_name (str): Full path to observation directory.
+        bucket (str, optional): The bucket to place the images in, defaults
+            to 'panoptes-survey'.
+        **kwargs: Optional keywords: verbose
+    """
+    assert os.path.exists(dir_name)
+    assert re.match(r'PAN\d\d\d', pan_id) is not None
+
+    verbose = kwargs.get('verbose', False)
+
+    def _print(msg):
+        if verbose:
+            print(msg)
+
+    dir_name = dir_name.replace('//', '/')
+    _print("Uploading {}".format(dir_name))
+
+    gsutil = shutil.which('gsutil')
+
+    img_path = os.path.join(dir_name, '*.fz')
+    if glob(img_path):
+        field_dir = dir_name.split('/fields/')[-1]
+        remote_path = os.path.normpath(os.path.join(pan_id, field_dir))
+
+        bucket = 'gs://{}/'.format(bucket)
+        # normpath strips the trailing slash so add here so we place in directory
+        run_cmd = [gsutil, '-mq', 'cp', '-r', img_path, bucket + remote_path + '/']
+        _print("Running: {}".format(run_cmd))
+
+        try:
+            completed_process = subprocess.run(run_cmd, stdout=subprocess.PIPE)
+
+            if completed_process.returncode != 0:
+                warn("Problem uploading")
+                warn(completed_process.stdout)
+        except Exception as e:
+            warn("Problem uploading: {}".format(e))
