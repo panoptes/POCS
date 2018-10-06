bd1dbe250ff05f44ba35aa660c165a7f6b8ad755
Wilfred Gee <wtylergee@gmail.com>
2018-10-06 11:17:05 +1000

    Make sure tests are always using certain config

--- pocs/tests/conftest.py
+++ pocs/tests/conftest.py
@@ -29,8 +29,9 @@ def config(images_dir):
     if not _one_time_config:
         _one_time_config = load_config(ignore_local=True, simulator=['all'])
         _one_time_config['db']['name'] = 'panoptes_testing'
-        _one_time_config['directories']['images'] = images_dir
-        _one_time_config['scheduler']['fields_file'] = 'simulator.yaml'
+
+    _one_time_config['directories']['images'] = images_dir
+    _one_time_config['scheduler']['fields_file'] = 'simulator.yaml'
 
     return copy.deepcopy(_one_time_config)
 
