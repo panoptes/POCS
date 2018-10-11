d0a6da364dfa23590b79e60a1e2daad7964e129f
Wilfred Gee <wtylergee@gmail.com>
2018-10-11 11:49:51 +1100

    Removing option for uploading headers from script (it happens on server side automatically if something is uploaded)

--- scripts/upload_image_dir.py
+++ scripts/upload_image_dir.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 import os
 
-from glob import glob
-from astropy.io import fits
-
 from pocs.utils.config import load_config
 from pocs.utils.images import clean_observation_dir
 from pocs.utils.google.storage import upload_observation_to_bucket
-from pocs.utils.db.postgres import get_db_proxy_conn
-from pocs.utils.db.postgres import add_header_to_db
 from pocs.utils import error
 
 
@@ -17,7 +12,6 @@ def main(directory,
          remove_jpgs=False,
          overwrite=False,
          make_timelapse=False,
-         send_headers=False,
          db_pass=None,
          verbose=False,
          **kwargs):
@@ -38,8 +32,12 @@ def main(directory,
 
     _print("Cleaning observation directory: {}".format(directory))
     try:
-        clean_observation_dir(directory, remove_jpgs=remove_jpgs, include_timelapse=make_timelapse,
-                              timelapse_overwrite=overwrite, verbose=verbose, **kwargs)
+        clean_observation_dir(directory,
+                              remove_jpgs=remove_jpgs,
+                              include_timelapse=make_timelapse,
+                              timelapse_overwrite=overwrite,
+                              verbose=verbose,
+                              **kwargs)
     except Exception as e:
         raise error.PanError('Cannot clean observation dir: {}'.format(e))
 
@@ -53,22 +51,6 @@ def main(directory,
             exclude_files='upload_manifest.log',
             verbose=verbose, **kwargs)
 
-    if send_headers:
-        _print("Making connection to Meta DB")
-
-        try:
-            metadb_conn = get_db_proxy_conn(db_name='metadata', db_pass=db_pass)
-        except error.GoogleCloudError:
-            _print("Can't make connection to Meta db, skipping headers")
-        else:
-            for fn in glob(os.path.join(directory, '*.fz')):
-                _print("Sending FITS header: {}".format(fn))
-                h0 = fits.getheader(fn, ext=1)
-                try:
-                    add_header_to_db(h0, conn=metadb_conn)
-                except Exception as e:
-                    _print("Problem with fits header: {}".format(e))
-
     return directory
 
 
@@ -81,8 +63,6 @@ if __name__ == '__main__':
                         help='Directory to be cleaned and uploaded.')
     parser.add_argument('--upload', default=False, action='store_true',
                         help='If images should be uploaded, default False.')
-    parser.add_argument('--send_headers', default=False, action='store_true',
-                        help='If FITS headers should be sent to metadb, default False.')
     parser.add_argument('--remove_jpgs', default=False, action='store_true',
                         help='If images should be removed after making timelapse, default False.')
     parser.add_argument('--make_timelapse', action='store_true', default=False,
