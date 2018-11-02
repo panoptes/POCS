23b4e06e42fb3aa27f815ff238b853cbe6bc72e3
James Synge <jamessynge@google.com>
2018-11-02 17:38:28 -0400

    Reduce number of RUN commands for apt-get operations.
    Split apt-packages-list.txt into two files, one for
    use in ci, the other with the remaining packages.
    
    This is still a WIP.

--- scripts/install/install-apt-packages.sh
+++ scripts/install/install-apt-packages.sh
@@ -4,19 +4,27 @@ THIS_DIR="$(dirname "$(readlink -f "${0}")")"
 
 source "${THIS_DIR}/install-functions.sh"
 
-# Install all of the packages specified in the apt-packages-list file.
+# Install all of the packages specified in the in the args.
 function install_apt_packages() {
   echo
   echo_running_sudo "apt-get update"
   echo
   my_sudo apt-get update
-  # Remove all the comments from the package list and install the packages whose
-  # names are left.
-  APT_PKGS="$(cut '-d#' -f1 "${THIS_DIR}/apt-packages-list.txt" | sort | uniq)"
-  echo
-  echo_running_sudo "apt-get install"
-  echo
-  my_sudo apt-get install --yes ${APT_PKGS}
+  for APT_PKGS_FILE in "$@"
+  do
+    # Remove all the comments from the package list and install the packages whose
+    # names are left.
+    APT_PKGS="$(cut '-d#' -f1 "${APT_PKGS_FILE}" | sort | uniq)"
+    echo
+    echo_running_sudo "apt-get install for the files in ${APT_PKGS_FILE}"
+    echo
+    my_sudo apt-get install --yes ${APT_PKGS}
+  done
 }
 
-install_apt_packages
+if [ $# -eq 0 ] ; then
+  install_apt_packages "${THIS_DIR}"/apt-packages-list.for-*.txt
+else  
+  install_apt_packages "$@"
+fi
+
