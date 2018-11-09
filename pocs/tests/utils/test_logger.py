260ac218af6c211932af4de6269752fe66e2db07
James Synge <jamessynge@users.noreply.github.com>
2018-11-09 09:23:57 -0500

    Improve robustness of log formatter (#708)
    * Improve robustness of log formatter
    
    Attempt to undo the special casing that the standard
    logging package applies to a dict as the second of
    two args to a logging method (i.e. format_str, dict_value).
    
    Annoyingly complicated. Makes me wonder if we should
    simply require use of legacy formatting. Oh well, wasted
    the effort to fix it, so let's try it out.
    
    Add tests of log formatting code.
    
    * Fix codestyle violation
    
    * Remove dead code, fix coverage
    
    * Respond to feedback, primarily adding docstrings.

---
+++ pocs/tests/utils/test_logger.py
@@ -0,0 +1,105 @@
+import pytest
+
+from pocs.utils.logger import field_name_to_key
+from pocs.utils.logger import format_has_reference_keys
+from pocs.utils.logger import logger_msg_formatter
+
+
+def test_field_name_to_key():
+    assert not field_name_to_key('.')
+    assert not field_name_to_key('[')
+    assert field_name_to_key('abc') == 'abc'
+    assert field_name_to_key(' abc ') == ' abc '
+    assert field_name_to_key('abc.def') == 'abc'
+    assert field_name_to_key('abc[1].def') == 'abc'
+
+
+def test_logger_msg_formatter_1_dict():
+    d = dict(abc='def', xyz=123)
+
+    tests = [
+        # Single anonymous reference, satisfied by the entire dict.
+        ('{}', "{'abc': 'def', 'xyz': 123}"),
+
+        # Single anonymous reference, satisfied by the entire dict.
+        ('{!r}', "{'abc': 'def', 'xyz': 123}"),
+
+        # Position zero references, satisfied by the entire dict.
+        ('{0} {0}', "{'abc': 'def', 'xyz': 123} {'abc': 'def', 'xyz': 123}"),
+
+        # Reference to a valid key in the dict.
+        ('{xyz}', "123"),
+
+        # Invalid modern reference, so %s format applied.
+        ('%s {1}', "{'abc': 'def', 'xyz': 123} {1}"),
+
+        # Valid legacy format applied to whole dict.
+        ('%r', "{'abc': 'def', 'xyz': 123}"),
+        ('%%', "%"),
+    ]
+
+    for fmt, msg in tests:
+        assert logger_msg_formatter(fmt, d) == msg, fmt
+
+    # Now tests with entirely invalid formats, so warnings should be issued.
+    tests = [
+        '%(2)s',
+        '{def}',
+        '{def',
+        'def}',
+        '%d',
+        # Bogus references either way.
+        '{0} {1} %(2)s'
+    ]
+
+    for fmt in tests:
+        with pytest.warns(UserWarning):
+            assert logger_msg_formatter(fmt, d) == fmt
+
+
+def test_logger_msg_formatter_1_non_dict():
+    d = ['abc', 123]
+
+    tests = [
+        # Single anonymous reference, satisfied by first element.
+        ('{}', "abc"),
+
+        # Single anonymous reference, satisfied by first element.
+        ('{!r}', "'abc'"),
+
+        # Position references, satisfied by elements.
+        ('{1} {0!r}', "123 'abc'"),
+
+        # Valid modern reference, %s ignored.
+        ('%s {1}', "%s 123"),
+
+        # Valid legacy format applied to whole list.
+        ('%r', "['abc', 123]"),
+
+        # Valid legacy format applied to whole list.
+        ('%s', "['abc', 123]"),
+    ]
+
+    for fmt, msg in tests:
+        assert logger_msg_formatter(fmt, d) == msg, fmt
+
+    # Now tests with entirely invalid formats, so warnings should be issued.
+    tests = [
+        # We only have two args, so a reference to a third should fail.
+        '{2}',
+        '%(2)s',
+        # Unknown key
+        '{def}',
+        '%(def)s',
+        # Malformed key
+        '{2',
+        '{',
+        '2}',
+        '}',
+        '{}{}{}',
+        '%d',
+    ]
+
+    for fmt in tests:
+        with pytest.warns(UserWarning):
+            assert logger_msg_formatter(fmt, d) == fmt
--- pocs/utils/logger.py
+++ pocs/utils/logger.py
@@ -1,11 +1,15 @@
-import json
-import os
-import sys
-import time
+import collections
 import datetime
+import json
 import logging
 import logging.config
+import os
+import re
+import string
+import sys
 from tempfile import gettempdir
+import time
+from warnings import warn
 
 from pocs.utils.config import load_config
 
@@ -15,30 +19,165 @@ from pocs.utils.config import load_config
 all_loggers = {}
 
 
+def field_name_to_key(field_name):
+    """Given a field_name from Formatter.parse(), extract the argument key.
+
+    Args:
+        field_name: expression used to identify the source of the value for
+            a field. See string.Formatter.parse for more info.
+
+    Returns:
+        The name or index at the start of the field_name.
+    """
+    assert isinstance(field_name, str)
+    assert len(field_name)
+    m = re.match(r'^([^.[]+)', field_name)
+    if not m:
+        return None
+    arg_name = m.group(1)
+    if arg_name.isdigit():
+        return int(arg_name)
+    else:
+        return arg_name
+
+
+def format_has_reference_keys(fmt, args):
+    """Does fmt have named references in it?
+
+    Args:
+        fmt: A format string of the type supported by string.Formatter.
+        args: A dictionary which *may* be providing values to be formatted
+            according to the format fmt.
+
+    Returns:
+        True if fmt has any format substitution field that references an
+        entry in args by a string key. False otherwise.
+    """
+    assert isinstance(args, dict)
+    try:
+        for literal_text, field_name, format_spec, conversion in string.Formatter().parse(fmt):
+            if field_name:
+                key = field_name_to_key(field_name)
+                if isinstance(key, str) and key in args:
+                    return True
+    except Exception:
+        pass
+    return False
+
+
+def format_has_legacy_style(fmt):
+    """Does fmt have a % in it? I.e. is it a legacy style?
+
+    We replace two %%'s in a row with nothing, see if any percents are
+    left.
+    """
+    fmt = fmt.replace('%%', '')
+    return '%' in fmt
+
+
+# formatting_methods encapsulates the different ways that we can apply
+# a format string to a dictionary of args. Those starting with legacy_
+# use the original printf style operator '%'. Those starting with
+# modern_ use the Advanced String Formatting method defined in PEP 3101.
+formatting_methods = dict(
+    legacy_direct=lambda fmt, args: fmt % args,
+    legacy_tuple=lambda fmt, args: fmt % (args, ),
+    modern_direct=lambda fmt, args: fmt.format(args),
+    modern_args=lambda fmt, args: fmt.format(*args),
+    modern_kwargs=lambda fmt, args: fmt.format(**args),
+)
+
+
+def logger_msg_formatter(fmt, args):
+    """Returns the formatted logger message.
+
+    Python's logger package uses the old printf style formatting
+    strings, rather than the newer PEP-3101 "Advanced String Formatting"
+    style of formatting strings.
+
+    This function supports using either style, though not both in one
+    string. It examines msg to look for which style is in use,
+    and is exposed as a function for easier testing.
+
+    The logging package assumes that if the sole argument to the logger
+    call is a dict, that the caller intends to use that dict as a source
+    for mapping key substitutions in the formatting operation, so
+    discards the sequence that surrounded the dict (as part of *args),
+    keeping only the dict as the value of logging.LogRecord.args here.
+    It happens that the old style formatting operator '%' would detect
+    whether the string included keys mapping into the dict on the right
+    hand side of the % operator, and if so would look them up; however,
+    if the formatting string didn't include mapping keys, then a sole
+    dict arg was treated as a single value, thus permitting a single
+    substitution (e.g. 'This is the result: %r' % some_dict).
+
+    The .format() method of strings doesn't have the described behavior,
+    so this formatter class attempts to provide it.
+    """
+    if not args:
+        return fmt
+
+    # There are args, so fmt must be a format string. Select the
+    # formatting methods to try based on the contents.
+    method_names = []
+    may_have_legacy_subst = format_has_legacy_style(fmt)
+    args_are_mapping = isinstance(args, collections.Mapping)
+    if '{' in fmt:
+        # Looks modern.
+        if args_are_mapping:
+            if format_has_reference_keys(fmt, args):
+                method_names.append('modern_kwargs')
+            else:
+                method_names.append('modern_direct')
+        else:
+            method_names.append('modern_args')
+    if may_have_legacy_subst:
+        # Looks old school.
+        method_names.append('legacy_direct')
+
+    # Add fallback methods.
+    def add_fallback(name):
+        if name not in method_names:
+            method_names.append(name)
+    if '{' in fmt:
+        add_fallback('modern_direct')
+    if may_have_legacy_subst:
+        add_fallback('legacy_tuple')
+    elif '%' in fmt:
+        add_fallback('legacy_direct')
+
+    # Now try to format:
+    for method_name in method_names:
+        try:
+            method = formatting_methods[method_name]
+            return method(fmt, args)
+        except Exception:
+            pass
+
+    warn(f'Unable to format log.')
+    warn(f'Log message (format string): {fmt!r}')
+    warn('Log args type: %s' % type(args))
+    try:
+        warn(f'Log args: {args!r}')
+    except Exception:  # pragma: no cover
+        warn('Unable to represent log args in string form.')
+    return fmt
+
+
 class StrFormatLogRecord(logging.LogRecord):
-    """ Allow for `str.format` style log messages
+    """Allow for `str.format` style log messages
 
     Even though you can select '{' as the style for the formatter class,
     you still can't use {} formatting for your message. The custom
     `getMessage` tries new format, then falls back to legacy format.
 
-    From: https://goo.gl/Cyt5NH
+    Originally inspired by https://goo.gl/Cyt5NH but much changed since
+    then.
     """
 
     def getMessage(self):
         msg = str(self.msg)
-        if self.args:
-            if '{' in msg:
-                try:
-                    msg = msg.format(*self.args)
-                except (TypeError, ValueError):
-                    msg = msg % self.args
-            else:
-                try:
-                    msg = msg % self.args
-                except (TypeError, ValueError):
-                    msg = msg.format(*self.args)
-        return msg
+        return logger_msg_formatter(msg, self.args)
 
 
 def get_root_logger(profile='panoptes', log_config=None):
@@ -72,9 +211,12 @@ def get_root_logger(profile='panoptes', log_config=None):
         # missing from the log_config. It is hard to understand how
         # this could occur given that none of the callers of
         # get_root_logger pass in their own log_config.
-        if 'formatters' not in log_config and sys.stdout.isatty():
-            import pdb
-            pdb.set_trace()
+        if 'formatters' not in log_config:  # pragma: no cover
+            # TODO(jamessynge): Raise a custom exception in this case instead
+            # of issuing a warning; after all, a standard dict will throw a
+            # KeyError in the for loop below if 'formatters' is missing.
+            warn('formatters is missing from log_config!')
+            warn(f'log_config: {log_config!r}')
         for name, formatter in log_config['formatters'].items():
             log_config['formatters'][name].setdefault('()', _UTCFormatter)
         log_fname_datetime = datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
@@ -135,6 +277,5 @@ def get_root_logger(profile='panoptes', log_config=None):
 
 
 class _UTCFormatter(logging.Formatter):
-
     """ Simple class to convert times to UTC in the logger """
     converter = time.gmtime
