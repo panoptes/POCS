0de12d7cf88726ed7d87404cc5119ddd571f1869
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-11-24 10:12:37 +1100

    Simplifying the plot to only require the generated positions

--- pocs/utils/dither.py
+++ pocs/utils/dither.py
@@ -5,8 +5,6 @@ from astropy.coordinates import SkyOffsetFrame
 from astropy.coordinates import ICRS
 from astropy.wcs import WCS
 
-from astroplan.plots import plot_finder_image
-
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
@@ -101,17 +99,12 @@ def get_dither_positions(base_position,
     return dither_coords
 
 
-def plot_dither_pattern(base_position, dither_positions, include_finder_chart=False):
+def plot_dither_pattern(dither_positions):
     """Utility function to generate a plot of the dither pattern.
 
     Args:
-        base_position (SkyCoord or compatible): base position for the dither pattern,
-            either a SkyCoord or an object that can be converted to one by the SkyCoord
-            constructor (e.g. string).
         dither_positions (SkyCoord): SkyCoord positions to be plotted as generated from
             `get_dither_positions`.
-        include_finder_chart (bool, optional): If the plot should include a finder
-            chart as background, default False.
 
     Returns:
         `matplotlib.figure.Figure`: The matplotlib plot.
@@ -121,12 +114,14 @@ def plot_dither_pattern(base_position, dither_positions, include_finder_chart=Fa
             tuples, in units of the pattern_offset. If given pattern_offset must also
             be specified.
     """
+    fig = Figure()
+    FigureCanvas(fig)
+
+    base_position = dither_positions[0]
+
     dummy_wcs = WCS(naxis=2)
     dummy_wcs.wcs.ctype = ['RA---TAN', 'DEC--TAN']
     dummy_wcs.wcs.crval = [base_position.ra.value, base_position.dec.value]
-
-    fig = Figure()
-    FigureCanvas(fig)
     ax = fig.add_subplot(111, projection=dummy_wcs)
 
     ax.plot(dither_positions.ra, dither_positions.dec, 'b*-', transform=ax.get_transform('world'))
@@ -141,6 +136,7 @@ def plot_dither_pattern(base_position, dither_positions, include_finder_chart=Fa
     ax.grid()
 
     ax.set_title(base_position.to_string('hmsdms'))
+
     fig.set_size_inches(8, 8.5)
 
     return fig
