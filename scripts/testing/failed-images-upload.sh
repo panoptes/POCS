4674d1df546af112e1f7fd3759a92725cf3bec78
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-11-24 19:21:49 +1100

    I sure do love testing things on travis one upload at a time!

--- scripts/testing/failed-images-upload.sh
+++ scripts/testing/failed-images-upload.sh
@@ -6,4 +6,6 @@ echo "Zipping files found in ${UPLOAD_DIR}"
 tar zcf failed-images.tgz $UPLOAD_DIR
 
 echo "Uploading public temporary hosting site"
-curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz
+echo "Files can be downloaded from:" > temp.txt
+curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz >> temp.txt
+cat temp.txt
