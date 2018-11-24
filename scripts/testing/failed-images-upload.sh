46a8179bafcce7e9c7df313aa61ce52cd99b74aa
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-11-24 18:53:07 +1100

    Minor typos (sigh)

--- scripts/testing/failed-images-upload.sh
+++ scripts/testing/failed-images-upload.sh
@@ -6,4 +6,4 @@ echo "Zipping files found in ${UPLOAD_DIR}"
 tar zcf failed-images.tgz $UPLOAD_DIR
 
 echo "Uploading public temporary hosting site"
-curl --upload_file failed-images.tgz https://transfer.sh/failed-images.tgz
+curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz
