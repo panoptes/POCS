dc484d2c570bd0e1085b1a24a457dbb2be6895e5
Wilfred Gee <wtylergee@gmail.com>
2018-11-25 08:08:03 +1100

    Still trying to simply output the url

--- scripts/testing/failed-images-upload.sh
+++ scripts/testing/failed-images-upload.sh
@@ -6,6 +6,6 @@ echo "Zipping files found in ${UPLOAD_DIR}"
 tar zcf failed-images.tgz $UPLOAD_DIR
 
 echo "Uploading public temporary hosting site"
-echo "Files can be downloaded from:" > temp.txt
-curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz >> temp.txt
+curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz -o temp.txt
+echo "Files can be downloaded from:"
 cat temp.txt
