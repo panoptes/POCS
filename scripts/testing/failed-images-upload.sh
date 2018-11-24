2b54e19e3c947824221f9005f1447f2c996c47eb
Wilfred Gee <wtylergee@gmail.com>
2018-11-25 09:37:34 +1100

    Ok, I lied. I'm trying agian.
    I suspect it's the text or the style that is screwed up so also testing
    text here.

--- scripts/testing/failed-images-upload.sh
+++ scripts/testing/failed-images-upload.sh
@@ -6,4 +6,7 @@ echo "Zipping files found in ${UPLOAD_DIR}"
 tar zcf failed-images.tgz $UPLOAD_DIR
 
 echo "Uploading public temporary hosting site"
-curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz -o "-"
+echo "Download failed images from:" > temp.txt
+curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz >> temp.txt
+echo "\n\nThere will be one subfolder per failed test." >> temp.txt
+cat temp.txt
