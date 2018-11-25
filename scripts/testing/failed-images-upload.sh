45dc0cb767cbfe47cebed354a5b09196258929b5
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-11-25 14:20:23 +1100

    Revert "Put failed image upload directly in travis"
    This reverts commit a7721e104b5d15ce160f9247f189fd1988f23dd3.

---
+++ scripts/testing/failed-images-upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash +e
+
+UPLOAD_DIR=$1
+echo "Zipping files found in ${UPLOAD_DIR}"
+
+tar zcf failed-images.tgz $UPLOAD_DIR
+
+echo "Uploading public temporary hosting site"
+echo "Download failed images from:" > temp.txt
+curl --upload-file failed-images.tgz https://transfer.sh/failed-images.tgz >> temp.txt
+echo '\n There will be one subfolder per failed test.' >> temp.txt
+cat temp.txt
