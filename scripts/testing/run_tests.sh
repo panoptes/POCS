66071454e7db3a1364d10748b38361883435c83f
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-04-29 09:46:02 +1000

    Fixing add

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -3,10 +3,11 @@
 export PYTHONPATH="$PYTHONPATH:$PANDIR/POCS/scripts/coverage"
 export COVERAGE_PROCESS_START=.coveragerc
 coverage run $(which pytest) -v --test-databases all
-coverage combine
 
 # Only worry about coverage if on travis.
 if [[ $TRAVIS ]]; then
 	coverage combine
 	bash <(curl -s https://codecov.io/bash)
 fi
+
+exit 0
