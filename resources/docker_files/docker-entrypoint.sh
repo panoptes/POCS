402768880e9b81cac95766323d2af79997362b01
Wilfred Gee <wtylergee@gmail.com>
2019-05-11 19:13:39 +1000

    Fixing conda paths (or lack thereof)

--- resources/docker_files/docker-entrypoint.sh
+++ resources/docker_files/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-source /opt/conda/etc/profile.d/conda.sh
-conda activate panoptes-env
+# source /opt/conda/etc/profile.d/conda.sh
+# conda activate panoptes-env
 
 # Run the remaining comands
 exec "$@"
