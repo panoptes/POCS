def093810459fb402a26ad5a22494d606f983c22
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-05-22 07:32:43 +1000

    Docker updates
    * Install arduino-cli.
    * Split into separate images.

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
-SOURCE_DIR=${POCS}
+SOURCE_DIR="${POCS}"
 
-gcloud builds submit --timeout="5h" --config ${SOURCE_DIR}/docker/cloudbuild.yaml --async ${SOURCE_DIR}
+gcloud builds submit \
+    --timeout="5h" \
+    --config "${SOURCE_DIR}/docker/cloudbuild.yaml"
+    --async \
+    "${SOURCE_DIR}"
 
