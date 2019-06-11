db2bae39463abb8993bec4aba9bb27218e2ed37d
Demezhan Marikov <demezhan1998@gmail.com>
2019-06-11 10:17:03 +0600

    Update pocs/tests/test_pocs.py
    Co-Authored-By: Wilfred Tyler Gee <wtylergee@gmail.com>

--- pocs/tests/test_pocs.py
+++ pocs/tests/test_pocs.py
@@ -13,7 +13,7 @@ from pocs.scheduler import create_scheduler_from_config
 from pocs.utils import CountdownTimer
 from pocs.utils import current_time
 from pocs.utils import error
-from pocs.utils.location import setup_site_location_details_from_config
+from pocs.utils.location import create_location_from_config
 from pocs.utils.messaging import PanMessaging
 
 
