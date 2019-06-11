017b043367b4ff9758ee482dba098ede4063e42e
Demezhan Marikov <demezhan1998@gmail.com>
2019-06-11 10:16:48 +0600

    Update pocs/tests/test_observatory.py
    Co-Authored-By: Wilfred Tyler Gee <wtylergee@gmail.com>

--- pocs/tests/test_observatory.py
+++ pocs/tests/test_observatory.py
@@ -166,7 +166,7 @@ def test_default_config(observatory):
     """ Creates a default Observatory and tests some of the basic parameters """
 
     assert observatory.location is not None
-    assert observatory.location.get('elevation') - observatory.config['location']['elevation'] < 1. * u.meter
+    assert observatory.location.get('elevation') == pytest.approx(observatory.config['location']['elevation'], rel=1 * u.meter)
     assert observatory.location.get('horizon') == observatory.config['location']['horizon']
     assert hasattr(observatory, 'scheduler')
     assert isinstance(observatory.scheduler, Scheduler)
