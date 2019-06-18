510d3461ea14d650750cc2440e0939cc163a92f6
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-06-18 10:24:57 +1000

    Trying to make multi-container compose file work in a sane way

--- scripts/pocs-docker.sh
+++ scripts/pocs-docker.sh
@@ -3,11 +3,11 @@
 usage() {
   echo -n "##################################################
 # Start POCS via Docker.
-# 
+#
 ##################################################
 
  $ $(basename $0) [COMMAND]
- 
+
  Options:
   COMMAND 	These options are passed at the end of the docker-compose command.
   			To start all service simply pass 'up'.
@@ -15,13 +15,16 @@ usage() {
  Examples:
 
 	# Start all services in the foreground.
-	$POCS/scripts/start-pocs-docker.sh up
+	$POCS/scripts/pocs-docker.sh up
 
  	# Start config-server and messaging-hub serivces in the background.
-	$POCS/scripts/start-pocs-docker.sh up --no-deps -d config-server messaging-hub
+	$POCS/scripts/pocs-docker.sh up --no-deps -d config-server messaging-hub
 
  	# Read the logs from the config-server
-	$POCS/scripts/start-pocs-docker.sh logs config-server
+	$POCS/scripts/pocs-docker.sh logs config-server
+
+    # Run the software tests (no hardware)
+    $POCS/scripts/pocs-docker.sh up
 "
 }
 
@@ -31,9 +34,10 @@ if [ ${START} = 'help' ] || [ ${START} = '-h' ] || [ ${START} = '--help' ]; then
 	exit 1
 fi
 
+cd $PANDIR
 docker-compose \
-	-f $PANDIR/panoptes-utils/docker/docker-compose.yaml \
-	-f $PANDIR/PAWS/docker/docker-compose.yaml \
-	-f $PANDIR/POCS/docker/docker-compose.yaml \
+	-f panoptes-utils/docker/docker-compose.yaml \
+	-f PAWS/docker/docker-compose.yaml \
+	-f POCS/docker/docker-compose.yaml \
 	-p panoptes "$@"
 
