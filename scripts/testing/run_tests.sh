c9611b3929590852aaa7b488ecf5f5bce9fccf4d
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-06-25 21:37:17 +1000

    Add comments to test run

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -5,6 +5,7 @@ pip install -r requirements.txt
 
 export PYTHONPATH="$PYTHONPATH:$PANDIR/POCS/scripts/coverage"
 export COVERAGE_PROCESS_START=.coveragerc
+# Die on first test for now
 coverage run $(which pytest) -xvrs --test-databases all
 
 # Only worry about coverage if on travis.
