4344b3bb9cb76e2ce3d31f57faa9f03513fe275d
Demezhan Marikov <demezhan1998@gmail.com>
2019-06-26 14:10:46 +0600

    Minor changes.

--- pocs/observatory.py
+++ pocs/observatory.py
@@ -214,7 +214,7 @@ class Observatory(PanBase):
             self.logger.info('Adding scheduler')
             self.scheduler = scheduler
         else:
-            raise TypeError("scheduler is not instance of BaseScheduler class, cannot add.")
+            raise TypeError("Scheduler is not instance of BaseScheduler class, cannot add.")
 
     def remove_scheduler(self):
         """Remove scheduler. """
