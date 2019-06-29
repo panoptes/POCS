7ceb942133d02295d8ab80184840270161e1c9d8
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-06-29 13:15:31 +1000

    Allow for eacy building of just amd files

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 SOURCE_DIR="${POCS}"
+CLOUD_FILE="cloudbuild-${1:-all}.yaml"
+
+echo "Using ${CLOUD_FILE}"
 
 gcloud builds submit \
     --timeout="5h" \
-    --config "${SOURCE_DIR}/docker/cloudbuild.yaml" \
+    --config "${SOURCE_DIR}/docker/${CLOUD_FILE}" \
     --async \
     "${SOURCE_DIR}"
 
