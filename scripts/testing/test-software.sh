2ab7779279608ff86172d69d19c1560a41fd5e9e
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-07-02 08:18:04 +1000

    Adding basic testing script

---
+++ scripts/testing/test-software.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+clear;
+
+cat << EOF
+Beginning test of POCS software.  This will start a single docker container, mapping the
+host $PANDIR into the running docker container, which allows for testing of any local changes.
+
+You can view the output for the tests in a separate terminal:
+
+grc tail -F ${PANDIR}/log/pytest-all.log
+
+The tests will start by updating: ${PANDIR}/POCS/requirements.txt
+
+Tests will begin in 10 seconds. Press Ctrl-c to cancel.
+EOF
+
+sleep 10;
+
+docker run --rm -it -v /var/panoptes/:/var/panoptes gcr.io/panoptes-survey/pocs scripts/testing/run-tests.sh
+
