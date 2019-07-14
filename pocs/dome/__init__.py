a567ef579429c481a118712fde4e17d8d86a90b3
Wilfred Gee <wtylergee@gmail.com>
2019-07-14 15:02:11 +1000

    Fixing dome simulator

--- pocs/dome/__init__.py
+++ pocs/dome/__init__.py
@@ -3,6 +3,7 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 from pocs.base import PanBase
 from panoptes.utils.library import load_module
 from panoptes.utils.config.client import get_config
+from panoptes.utils.config.client import set_config
 from panoptes.utils.logger import get_root_logger
 
 
@@ -38,11 +39,10 @@ def create_dome_simulator(config_port=6563, logger=None, *args, **kwargs):
     if not logger:
         logger = get_root_logger()
 
-    dome_config = get_config('dome', port=config_port)
+    brand = 'Simulacrum'
+    driver = 'simulator'
 
-    brand = dome_config['brand']
-    driver = dome_config['driver']
-
+    set_config('dome', dict(brand=brand, driver=driver), port=config_port)
     logger.debug('Creating dome simulator: brand={}, driver={}'.format(brand, driver))
 
     module = load_module(f'pocs.dome.{driver}')
