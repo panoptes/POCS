6846fdfba2d4c27a1055b7c34bfbf357a183b8c5
Wilfred Gee <wtylergee@gmail.com>
2019-12-02 10:05:56 -1000

    Simplify the state loading tests again.

--- pocs/tests/test_state_machine.py
+++ pocs/tests/test_state_machine.py
@@ -4,28 +4,14 @@ import yaml
 
 from pocs.core import POCS
 from pocs.observatory import Observatory
-from pocs.camera import create_cameras_from_config
-from pocs.mount import create_mount_from_config
-from pocs.scheduler import create_scheduler_from_config
-from pocs.utils.location import create_location_from_config
 
 from pocs.utils import error
 
 
 @pytest.fixture
-def observatory(config_with_simulated_mount, images_dir):
+def observatory():
     """Return a valid Observatory instance with a specific config."""
-    config = config_with_simulated_mount
-    site_details = create_location_from_config(config)
-    scheduler = create_scheduler_from_config(config, observer=site_details['observer'])
-    mount = create_mount_from_config(config)
-    obs = Observatory(config=config,
-                      scheduler=scheduler,
-                      mount=mount,
-                      ignore_local_config=True)
-    cameras = create_cameras_from_config(config)
-    for cam_name, cam in cameras.items():
-        obs.add_camera(cam_name, cam)
+    obs = Observatory()
 
     return obs
 
@@ -46,8 +32,6 @@ def test_load_state_info(observatory):
     pocs = POCS(observatory)
 
     pocs._load_state('ready', state_info={'tags': ['at_twilight']})
-    pocs.next_state = 'ready'
-    pocs.goto_next_state()
 
 
 def test_state_machine_absolute(temp_file):
