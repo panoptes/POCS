d350cfef2934b3e817f86bdfd1b4440c8d4f2642
Wilfred Gee <wtylergee@gmail.com>
2020-02-15 23:36:46 +1100

    Try to download the IERS table before starting travis test

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -2,8 +2,14 @@
 
 export PYTHONPATH="$PYTHONPATH:$PANDIR/POCS/scripts/coverage"
 export COVERAGE_PROCESS_START=.coveragerc
+
+
+python "${PANDIR}/panoptes-utils/panoptes/utils/data.py --no-wide-field --no-narrow-field"
+
+cd $POCS
+
 # Die on first test for now
-coverage run "$(command -v pytest)" -xvvrs --test-databases all
+coverage run "$(command -v pytest)" -xvvrst --test-databases file
 
 # Only worry about coverage if on travis.
 if [[ $TRAVIS ]]; then
