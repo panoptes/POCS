f1d0c99702471b2063689af57c10fc95958f94d3
zacharyt20 <56666090+zacharyt20@users.noreply.github.com>
2020-02-26 21:19:12 -0800

    Formatting, Mount Detection, Arduino Detection
    Mount detection and crude Arduino detection. The Arduino detection uses a hardware difference between the control and camera boards. The control board has three ds18 temperature sensors, while the camera board does not. A sketcht hat tries to read these sensors is uploaded to either the camera board or control board. A disconnected sensor, and therefore all of the camera board's readings, will always have a temp of -127.00 C. This output allows the script to distinguish which Arduino is the camera board and which is the control board. This script is setup to prompt the user to automatically upload an Arduino sketch (power_board.ino / camera_board.ino) to the boards, however it's not fully automatic because of the way the arduino-cli is built. 
    
    There are two limitations to the Arduino detection. 
    1. The Arduino micro build will cause errors with the arduino-cli fqbn string system. This could be avoided with a simple if statement, but I need to see the output of either lsusb or arduino-cli board list on a system running a micro.
    2. The arduino-cli sucks at uploading sketches in a simple way. Without setting up a config file, it requires a fqnb string, but even more annoyingly it needs to be called on a folder containing an Arduino sketch and non-Arduino files the cli creates during the compiling of a sketch. It won't make these files from a sketch created in the Arduino IDE, only when a sketch is made by the arduino-cli and compiled through the arduino-cli. I also think that these files are system-specific, but I am not sure. 
    
    There is a simpler command just called "arduino" (I think it comes with the arduino IDE) that supports uploading sketches given only a sketch path and usb port. It would make things much simpler if it was included in the build.

--- scripts/lsusbplus.py
+++ scripts/lsusbplus.py
@@ -1,76 +1,196 @@
+#!/usr/bin/env python3
 import os
 import sys
 from subprocess import Popen, PIPE
 import fcntl
+import serial
+import time
+
+from pocs.mount import create_mount_from_config
+from panoptes.utils.error import MountNotFound
+
+from panoptes.utils.config.client import set_config
+
+from peas.sensors import detect_board_on_port
 
 BASEDIR = 'cd /sys/devices/pci0000:00'
 DataStorageMatrix = []
 TemporaryDataStorageList = []
 
-#Finding dev path and relating it to bus number and dev number
-StartLocationProbe = BASEDIR + ' ; sudo find -maxdepth 2 -name "usb*"'
-StartLocation = Popen(StartLocationProbe, shell=True, bufsize=64, stdin=PIPE, stdout=PIPE,close_fds=True).stdout.read().strip().decode('utf-8').split('\n')
-NEWBASEDIR = BASEDIR + ' ; cd ' + StartLocation[0] #I believe that on NUC usb1 holds information about all real devices, this entire section can be put in while loop to search for other usb directories if needed
+# Finding dev path and relating it to bus number and dev number
+StartLocationProbe = BASEDIR + ' ; find -maxdepth 2 -name "usb*"'
+StartLocation = Popen(
+    StartLocationProbe,
+    shell=True,
+    bufsize=64,
+    stdin=PIPE,
+    stdout=PIPE,
+    close_fds=True).stdout.read().strip().decode('utf-8').split('\n')
+NEWBASEDIR = BASEDIR + ' ; cd ' + StartLocation[0]
 SearchLocationProbe = NEWBASEDIR + ' ; find . -name "*-*:*"'
-SearchLocations = Popen(SearchLocationProbe, shell=True, bufsize=64, stdin=PIPE, stdout=PIPE,close_fds=True).stdout.read().strip().decode('utf-8').split('\n')
+SearchLocations = Popen(
+    SearchLocationProbe,
+    shell=True,
+    bufsize=64,
+    stdin=PIPE,
+    stdout=PIPE,
+    close_fds=True).stdout.read().strip().decode('utf-8').split('\n')
 Num_Searches = len(SearchLocations)
 i = Num_Searches
 while i > 0:
-   CurrentIndex = Num_Searches - i
-   SearchLocation = NEWBASEDIR + ' ; cd ' + SearchLocations[CurrentIndex]
-   DevPathProbe = SearchLocation + ' ; find -maxdepth 2 ! -name "tty"  -name "tty*"'
-   DEVPATH = Popen(DevPathProbe, shell=True, bufsize=64, stdin=PIPE, stdout=PIPE,close_fds=True).stdout.read().strip().decode('utf-8')
-   if (DEVPATH == ""):
-      i -= 1
-      continue
-   if (DEVPATH.startswith("./tty/")):
-      PARSEDDEVPATH = DEVPATH.split("./tty/")
-      PARSEDDEVPATH.remove("")
-   if (DEVPATH != ""):
-      if (DEVPATH.startswith("./tty/")):
-         PARSEDDEVPATH = DEVPATH.split("./tty/")
-      elif (DEVPATH.startswith("./tty")):
-         PARSEDDEVPATH = DEVPATH.split("./")
-      PARSEDDEVPATH.remove("")
-      BUSNUMDEVNUMPROBE = SearchLocation + " ; cd .. ; cat busnum ; cat devnum"
-      BUSNUMDEVNUM = Popen(BUSNUMDEVNUMPROBE, shell=True, bufsize=64, stdin=PIPE, stdout=PIPE,close_fds=True).stdout.read().strip().decode('utf-8')
-      TemporaryDataStorageList = BUSNUMDEVNUM.split("\n")
-      TemporaryDataStorageList.insert(0, PARSEDDEVPATH[0])
-      DataStorageMatrix.append(TemporaryDataStorageList)
-   i -= 1
-
-#Parsing lsusb for matching and reprinting
-
-lsusb = Popen("lsusb", shell=True, bufsize=64, stdin=PIPE, stdout=PIPE,close_fds=True).stdout.read().strip().decode('utf-8')
-lsusblines = lsusb.split("\n")
-num_devices = len(lsusblines)
-l = num_devices
-DataStorageMatrixLength = len(DataStorageMatrix)
-while l > 0:
-   line = num_devices - l
-   lineinformation = lsusblines[line].split(" ")
-   devicelineinformation = lineinformation[3].split(":")
-   bus = str(int(lineinformation[1]))
-   dev = str(int(devicelineinformation[0]))
-   #Pre-parsing lsusb output for cleaner dev path text insertion
-   lsusblines[line] = lsusblines[line].split(": ")
-   lsusblines[line][0] = lsusblines[line][0] + ": DevPath "
-    k = DataStorageMatrixLength
-   FoundScore = 0
-   while k > 0:
-      DataSet = DataStorageMatrixLength - k
-      if (bus == DataStorageMatrix[DataSet][1] and dev == DataStorageMatrix[DataSet][2]):
-         lsusblines[line][0] = lsusblines[line][0] + "\033[1;32;40m" + DataStorageMatrix[DataSet][0] + " \033[1;37;40m"
-      elif (bus != DataStorageMatrix[DataSet][1] or dev != DataStorageMatrix[DataSet][2]):
-         FoundScore += 1
-      if ((bus != DataStorageMatrix[DataSet][1] or dev != DataStorageMatrix[DataSet][2]) and FoundScore == 4):
-         lsusblines[line][0] = lsusblines[line][0] + "\033[1;31;40mno-path\033[1;37;40m "
-      k -= 1
-   l -= 1
-
-#Print out that parsed info!
-for x in lsusblines:
-   print("")
-   for y in x:
-      print(y, end='')
+    CurrentIndex = Num_Searches - i
+    SearchLocation = NEWBASEDIR + ' ; cd ' + SearchLocations[CurrentIndex]
+    DevPathProbe = SearchLocation + ' ; find -maxdepth 2 ! -name "tty"  -name "tty*"'
+    DEVPATH = Popen(
+        DevPathProbe,
+        shell=True,
+        bufsize=64,
+        stdin=PIPE,
+        stdout=PIPE,
+        close_fds=True).stdout.read().strip().decode('utf-8')
+    if (DEVPATH == ""):
+        i -= 1
+        continue
+    if (DEVPATH.startswith("./tty/")):
+        PARSEDDEVPATH = DEVPATH.split("./tty/")
+        PARSEDDEVPATH.remove("")
+    if (DEVPATH != ""):
+        if (DEVPATH.startswith("./tty/")):
+            PARSEDDEVPATH = DEVPATH.split("./tty/")
+        elif (DEVPATH.startswith("./tty")):
+            PARSEDDEVPATH = DEVPATH.split("./")
+    PARSEDDEVPATH.remove("")
+    BUSNUMDEVNUMPROBE = SearchLocation + ' ; cd .. ; echo -n "Bus: " ; cat busnum ; echo -n " Device: " ; cat devnum ; echo -n " ID " ; cat idVendor ; echo -n ":" ; cat idProduct ; echo -n " " ; cat manufacturer ; echo -n ", " ; cat product'
+    BUSNUMDEVNUM = Popen(
+        BUSNUMDEVNUMPROBE,
+        shell=True,
+        bufsize=64,
+        stdin=PIPE,
+        stdout=PIPE,
+        close_fds=True).stdout.read().strip().decode('utf-8')
+    TemporaryDataStorageList = BUSNUMDEVNUM.split("\n")
+    TemporaryDataStorageList.insert(0, PARSEDDEVPATH[0])
+    TemporaryDataStorageList.insert(1, " ")
+    DataStorageMatrix.append(TemporaryDataStorageList)
+    i -= 1
+# Print out that parsed info!
+for x in DataStorageMatrix:
+    print("")
+    for y in x:
+        print(y, end='')
 print("")
+
+DevPaths = []
+DataStorageMatrixLength = len(DataStorageMatrix)
+k = DataStorageMatrixLength
+while k > 0:
+    DataSet = DataStorageMatrixLength - k
+    if (DataStorageMatrix[DataSet][0].startswith("ttyUSB")):
+        DevPaths.append(DataStorageMatrix[DataSet][0])
+        k -= 1
+        continue
+    k -= 1
+
+# Identify Mount
+for port in DevPaths:
+    usb_port = f'/dev/{port}'
+    set_config('mount.serial.port', usb_port)
+    mount_info = {
+        'driver': 'ioptron',
+        'serial': {
+            'port': usb_port
+        }
+    }
+
+    mount = create_mount_from_config(mount_info=mount_info)
+
+    try:
+        mount.initialize()
+    except MountNotFound:
+        continue
+
+    print(f'\033[1;32;40mFound mount on {usb_port}, saving to config\033[1;37;40m')
+    set_config('mount.serial.port', usb_port)
+    break
+
+# Identify Weather Sensor
+
+
+# Identify Arduinos
+# Isolate Arduino device paths
+k = DataStorageMatrixLength
+DevPaths.clear()
+while k > 0:
+    DataSet = DataStorageMatrixLength - k
+    if (DataStorageMatrix[DataSet][0].startswith("ttyACM")):
+        DevPaths.append(DataStorageMatrix[DataSet][0])
+        k -= 1
+        continue
+    k -= 1
+
+# Setup arduino-cli commands for this docker image
+os.system("arduino-cli core update-index")
+fqbn_raw = os.popen("arduino-cli board list | awk '{ print $7 }'").read()
+fqbn = fqbn_raw.split('\n')[1]
+fqbnCoreElements = fqbn.split(":")
+fqbnCore = fqbnCoreElements[0] + ":" + fqbnCoreElements[1]
+os.system(f'arduino-cli core install {fqbnCore}')
+# Switch to the arduino_files directory to have the sketch upload properly
+os.chdir('/var/panoptes/POCS/resources/arduino_files')
+for port in DevPaths:
+    # Upload sketch
+    usb_port = f'/dev/{port}'
+    print(f'Uploading identifier Arduino sketch to {usb_port}.')
+    os.system(f'arduino-cli upload -p {usb_port} --fqbn {fqbn} identifier')
+    print(f'Sketch uploaded to Arduino on {usb_port}')
+
+    # Get and parse serial output
+    with serial.Serial(usb_port, 9600, timeout=3) as ser:
+        time.sleep(10)
+        # 53 chars since one full temp data is 27, 53 ensures full capture of
+        # at least one full temp report
+        identifier_output = ser.read(53)
+        identifier_output = identifier_output.decode("utf-8")
+    serial_lines = identifier_output.split('\n')
+    for line in serial_lines:
+        if(len(line) > 26):
+            full_reading = line
+            break
+    if (full_reading == '"temps":[-127.00,-127.00,-127.00]'):
+        print(f'\033[1;32;40mFound camera_board Arduino on {usb_port}, saving to config.\033[1;37;40m')
+        set_config('environment.camera_board.serial_port', usb_port)
+        # Ask user if they want to upload the Arduino script, don't force
+        # in case the build is different
+        while True:
+            response = input(
+                "Automatically upload the Arduino sketch for the camera board Arduino (camera_board.ino) [y/n]?")
+            if response.lower().startswith('y'):
+                print("Uploading sketch...")
+                os.chdir('/var/panoptes/POCS/resources/arduino_files')
+                os.system(f'arduino-cli upload -p {usb_port} --fqbn {fqbn} camera_board')
+                print('\033[1;32;40mSketch uploaded.\033[1;37;40m')
+                break
+            elif response.lower().startswith('n'):
+                print("Exiting and printing collected information.")
+                break
+            else:
+                print("A 'yes' or 'no' response is required")
+    elif (full_reading != '"temps":[-127.00,-127.00,-127.00]'):
+        print(f'\033[1;32;40mFound power_board Arduino on {usb_port}, saving to config.\033[1;37;40m')
+        set_config('environment.control_board.serial_port', usb_port)
+        while True:
+            response = input(
+                "Automatically upload the Arduino sketch for the control board Arduino (power_board.ino) [y/n]?")
+            if response.lower().startswith('y'):
+                print("Uploading sketch...")
+                os.chdir('/var/panoptes/POCS/resources/arduino_files')
+                os.system(f'arduino-cli upload -p {usb_port} --fqbn {fqbn} power_board')
+                print('\033[1;32;40mSketch uploaded.\033[1;37;40m')
+                break
+            elif response.lower().startswith('n'):
+                print("Exiting and printing collected information.")
+                break
+            else:
+                print("A 'yes' or 'no' response is required")
+    else:
+        print(f'\033[91mProblem detecting board with Arduino on {usb_port}!\033[00m')
