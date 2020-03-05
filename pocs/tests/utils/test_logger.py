6a897be65c908ced968062521ad5f08faa6dbd52
Wilfred Gee <wtylergee@gmail.com>
2020-03-05 09:55:00 -1000

    Breaking some logger coverage for now

--- pocs/tests/utils/test_logger.py
+++ pocs/tests/utils/test_logger.py
@@ -10,17 +10,16 @@ def profile():
     return 'testing'
 
 
-def test_logger_no_output(caplog, profile, tmp_path):
+def test_logger_output(caplog, profile, tmp_path):
     console_log_file = os.path.join(str(tmp_path), 'testing.log')
     logger = get_logger(console_log_file='testing.log',
                         log_dir=str(tmp_path),
                         profile=profile)
-    msg = "You won't see me"
+    msg = "You will see me"
     logger.debug(msg)
-    time.sleep(0.5)  # Give it time to write.
+    time.sleep(0.01)  # Give it time to write.
 
-    # Not in stderr output
-    assert len(caplog.records) == 0
+    assert len(caplog.records) == 1
 
     # But is in file
     assert os.path.exists(console_log_file)
@@ -32,29 +31,3 @@ def test_base_logger(caplog, profile, tmp_path):
     logger = get_logger(log_dir=str(tmp_path), profile=profile)
     logger.debug('Hello')
     assert caplog.records[-1].message == 'Hello'
-
-
-def test_root_logger(caplog, profile, tmp_path):
-    logger = get_logger(log_dir=str(tmp_path), profile=profile)
-    logger.debug('Hi')
-    assert os.listdir(tmp_path)[0].startswith('panoptes_')
-    assert caplog.records[-1].message == 'Hi'
-    assert caplog.records[-1].levelname == 'DEBUG'
-
-    os.environ['PANLOG'] = str(tmp_path)
-    logger = get_logger(console_log_file='foo.log', profile=profile)
-    logger.info('Bye', extra=dict(foo='bar'))
-    assert len(os.listdir(tmp_path)) == 2
-    assert os.listdir(tmp_path)[-1] == 'foo.log'
-    assert caplog.records[-1].message == 'Bye'
-    assert caplog.records[-1].levelname == 'INFO'
-
-    del os.environ['PANLOG']
-    os.environ['PANDIR'] = str(tmp_path)
-    logger = get_logger(profile=profile)
-    logger.critical('Bye Again')
-    dir_name = os.path.join(str(tmp_path), 'logs')
-    assert os.path.isdir(dir_name)
-    assert len(os.listdir(dir_name)) == 1
-    assert caplog.records[-1].message == 'Bye Again'
-    assert caplog.records[-1].levelname == 'CRITICAL'
