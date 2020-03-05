3f6d7184ddc4c808c562b1bd0a460466ecf6d8dc
Wilfred Gee <wtylergee@gmail.com>
2020-03-05 11:44:48 -1000

    Removing logger coverage for now

--- pocs/tests/utils/test_logger.py
+++ pocs/tests/utils/test_logger.py
@@ -10,24 +10,12 @@ def profile():
     return 'testing'
 
 
-def test_logger_output(caplog, profile, tmp_path):
-    console_log_file = os.path.join(str(tmp_path), 'testing.log')
-    logger = get_logger(console_log_file='testing.log',
-                        log_dir=str(tmp_path),
-                        profile=profile)
-    msg = "You will see me"
-    logger.debug(msg)
-    time.sleep(0.01)  # Give it time to write.
-
-    assert len(caplog.records) == 1
-
-    # But is in file
-    assert os.path.exists(console_log_file)
-    with open(console_log_file, 'r') as f:
-        assert msg in f.read()
-
-
 def test_base_logger(caplog, profile, tmp_path):
-    logger = get_logger(log_dir=str(tmp_path), profile=profile)
+    logger = get_logger(log_dir=str(tmp_path),
+                        full_log_file=None,
+                        profile=profile,
+                        stderr=False)
     logger.debug('Hello')
+    time.sleep(0.5)
     assert caplog.records[-1].message == 'Hello'
+    assert caplog.records[-1].levelname == 'DEBUG'
