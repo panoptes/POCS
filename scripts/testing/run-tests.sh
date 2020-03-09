cea1dadc48999fa1627dd298264402128f76ca13
Wilfred Gee <wtylergee@gmail.com>
2020-03-09 06:57:18 -1000

    Moving coverage upload to gh actions.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -5,11 +5,4 @@ export COVERAGE_PROCESS_START="${PANDIR}/POCS/.coveragerc"
 
 coverage run "$(command -v pytest)" -x -vv -rfes --test-databases all
 
-# Only worry about coverage if on travis.
-if [[ $TRAVIS ]]; then
-    chmod 777 .coverage*
-    coverage combine
-    bash <(curl -s https://codecov.io/bash)
-fi
-
 exit 0
