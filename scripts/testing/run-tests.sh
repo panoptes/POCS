5c2ccd629d5559dccefe2bfe2c8343a38be6f8dd
Wilfred Gee <wtylergee@gmail.com>
2020-03-09 08:29:14 -1000

    GHA tests

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -5,4 +5,6 @@ export COVERAGE_PROCESS_START="${PANDIR}/POCS/.coveragerc"
 
 coverage run "$(command -v pytest)" -x -vv -rfes --test-databases all
 
+cp "${PANDIR}/logs/panoptes.log" "${POCS}/"
+
 exit 0
