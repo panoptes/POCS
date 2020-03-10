f913eb106f1d16a03a65a233bc7369b1b1df1fcf
Wilfred Gee <wtylergee@gmail.com>
2020-03-10 07:08:26 -1000

    Try to debug random temperature spikes

--- pocs/camera/simulator_sdk/ccd.py
+++ pocs/camera/simulator_sdk/ccd.py
@@ -69,6 +69,8 @@ class Camera(AbstractSDKCamera, Camera):
 
         delta_temp = limit_temp - self._last_temp
         temperature = limit_temp - delta_temp * math.exp(-delta_time)
+        add_temp = random.uniform(-self._temp_var / 2, self._temp_var / 2)
+        self.logger.debug(f'Simulator randomly adding {add_temp:.02f}° C')
         temperature += random.uniform(-self._temp_var / 2, self._temp_var / 2)
 
         return temperature
