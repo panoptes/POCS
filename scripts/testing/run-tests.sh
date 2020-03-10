899d20d2b485370c4df58184ad08b70cfc9ba7b4
Wilfred Gee <wtylergee@gmail.com>
2020-03-10 08:40:57 -1000

    Coverage is combined at the time it is run.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -3,6 +3,8 @@
 export PYTHONPATH="${PYTHONPATH}:${PANDIR}/POCS/scripts/coverage"
 export COVERAGE_PROCESS_START="${PANDIR}/POCS/.coveragerc"
 
+# Run coverage over the pytest suite
 coverage run "$(command -v pytest)" -x -vv -rfes --test-databases all
+coverage combine
 
 exit 0
