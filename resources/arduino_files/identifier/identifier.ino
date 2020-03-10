9a08e4311257892fb2fa2dc9793c582950f5e360
zacharyt20 <56666090+zacharyt20@users.noreply.github.com>
2020-03-10 15:13:44 -0700

    Identifier Sketch and Libraries

---
+++ resources/arduino_files/identifier/identifier.ino
@@ -0,0 +1,37 @@
+#include "OneWire.h"
+#include "DallasTemperature.h"
+
+//Declare suspected ds18 pin
+const int DS18_PIN = 13;
+
+//Declare number of ds18 sensors
+const int NUM_DS18 = 3;
+
+uint8_t sensors_address[NUM_DS18][8];
+
+OneWire ds(DS18_PIN);
+DallasTemperature sensors(&ds);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.flush();
+
+
+  sensors.begin();
+}
+
+void loop() {
+  delay(100);
+  sensors.requestTemperatures();
+
+  float temps[3];
+  for (int x = 0; x < NUM_DS18; x++) {
+  temps[x] = sensors.getTempCByIndex(x);
+  }
+  Serial.print("\n");
+  Serial.print("\"temps\":[");
+  Serial.print(temps[0], 2); Serial.print(',');
+  Serial.print(temps[1], 2); Serial.print(',');
+  Serial.print(temps[2], 2);
+  Serial.print("]");
+}
