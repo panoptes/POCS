40e02397e8f9c95970e5b321e7135a9cd20d5ed9
Wilfred Gee <wtylergee@gmail.com>
2020-03-12 11:56:28 -1000

    Add some echoes

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -4,7 +4,10 @@ export PYTHONPATH="${PYTHONPATH}:${PANDIR}/POCS/scripts/coverage"
 export COVERAGE_PROCESS_START="${PANDIR}/POCS/.coveragerc"
 
 # Run coverage over the pytest suite
+echo "Staring tests"
 coverage run "$(command -v pytest)" -x -vv -rfes --test-databases all
+
+echo "Combining coverage"
 coverage combine
 
 exit 0
