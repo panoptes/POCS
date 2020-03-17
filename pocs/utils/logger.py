64559de086ffbd52474137290359288083e34c4f
Wilfred Gee <wtylergee@gmail.com>
2020-03-17 06:55:17 -1000

    Use `PanLogger` from the utils.

--- pocs/utils/logger.py
+++ pocs/utils/logger.py
@@ -1,26 +1,6 @@
 import os
-from contextlib import suppress
-from loguru import logger
-
-
-class PanLogger:
-
-    """Custom formatter to have dynamic widths for logging.
-
-    See https://loguru.readthedocs.io/en/stable/resources/recipes.html#dynamically-formatting-messages-to-properly-align-values-with-padding
-
-    """
-
-    def __init__(self):
-        self.padding = 0
-        self.fmt = "<lvl>{level:.1s}</lvl> <light-blue>{time:MM-DD HH:mm:ss.ss!UTC}</> <blue>({time:HH:mm:ss.ss})</> | <c>{name} {function}:{line}{extra[padding]}</c> | <lvl>{message}</lvl>\n"
-        self.handlers = dict()
-
-    def format(self, record):
-        length = len("{name}:{function}:{line}".format(**record))
-        self.padding = max(self.padding, length)
-        record["extra"]["padding"] = " " * (self.padding - length)
-        return self.fmt
+from panoptes.utils.logger import PanLogger
+from panoptes.utils.logger import logger
 
 
 LOGGER_INFO = PanLogger()
