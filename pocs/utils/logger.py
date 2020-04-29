197fb3caa30c1dfff96c3766b9b6f1cb8d35b5e3
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-04-29 06:38:22 -1000

    Bringing `PanLogger` (dynamic padding and handler info) from `panoptes-utils`.

--- pocs/utils/logger.py
+++ pocs/utils/logger.py
@@ -1,8 +1,34 @@
 import os
-from panoptes.utils.logger import PanLogger
 from panoptes.utils.logger import logger
 
 
+class PanLogger:
+    """Custom formatter to have dynamic widths for logging.
+
+    Also provides a `handlers` dictionary to track attached handlers by id.
+
+    See https://loguru.readthedocs.io/en/stable/resources/recipes.html#dynamically-formatting-messages-to-properly-align-values-with-padding
+
+    """
+
+    def __init__(self):
+        self.padding = 0
+        # Level Time_UTC Time_Local dynamic_padding Message
+        self.fmt = "<lvl>{level:.1s}</lvl> " \
+                   "<light-blue>{time:MM-DD HH:mm:ss.ss!UTC}</>" \
+                   "<blue>({time:HH:mm:ss.ss})</> " \
+                   "| <c>{name} {function}:{line}{extra[padding]}</c> | " \
+                   "<lvl>{message}</lvl>\n"
+        self.handlers = dict()
+
+    def format(self, record):
+        length = len("{name}:{function}:{line}".format(**record))
+        self.padding = max(self.padding, length)
+        record["extra"]["padding"] = " " * (self.padding - length)
+        return self.fmt
+
+
+# Create a global singleton to hold the handlers and padding info.
 LOGGER_INFO = PanLogger()
 
 
