640cae159633eec47af8e65fe79a83b6906746e6
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-05-27 11:45:54 -1000

    Fixing tests.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -2,12 +2,14 @@
 
 REPORT_FILE=${REPORT_FILE:-coverage.xml}
 
-export PYTHONPATH="$PYTHONPATH:${POCS}/scripts/coverage"
-export COVERAGE_PROCESS_START="${POCS}/setup.cfg"
+export PYTHONPATH="${PYTHONPATH}:/var/panoptes/pocs/scripts/testing/coverage"
+export COVERAGE_PROCESS_START="/var/panoptes/pocs/setup.cfg"
+
+coverage erase
 
 # Run coverage over the pytest suite
 echo "Starting tests"
-coverage run "$(command -v pytest)" -x -vv -rfes --test-databases all
+coverage run "$(command -v pytest)" -x -vv -rfes
 
 echo "Combining coverage"
 coverage combine
--- scripts/testing/test-software.sh
+++ scripts/testing/test-software.sh
@@ -1,41 +1,29 @@
 #!/bin/bash -e
 
-# TODO Make sure we are being run from $POCS root.
 clear;
 
 cat << EOF
-POCS Software Testing
+Beginning test of pocs software. This software is run inside a virtualized docker
+container that has all of the required dependencies installed.
 
-This script runs the POCS testing suite in a virtualized environment using docker images.
-
-The pocs:testing image will be built on your local machine if needed, then the tests will be
-run inside the virtualized container.
-
-The $PANDIR directory will be mapped into the running docker container, which allows for testing local changes.
+This will start a single docker container, mapping the host $PANDIR into the running docker
+container, which allows for testing of any local changes.
 
 You can view the output for the tests in a separate terminal:
 
-tail -F ${PANDIR}/log/panoptes-testing.log
+grc tail -F ${PANDIR}/log/pytest-all.log
 
-The tests will start by updating: ${POCS}/requirements.txt
+The tests will start by updating: ${PANDIR}/pocs/requirements.txt inside the container.
 
 Tests will begin in 5 seconds. Press Ctrl-c to cancel.
 EOF
 
 sleep 5;
 
-# Build the testing container.
-docker build \
-    -t pocs:testing \
-    -f docker/latest.Dockerfile \
-    .
-
-# TODO Have the option to map just $POCS instead of $PANDIR.
-
 docker run --rm -it \
-    -e PANDIR=/var/panoptes \
-    -e POCS=/var/panoptes/POCS \
     -e LOCAL_USER_ID=$(id -u) \
-    -v $PANDIR:/var/panoptes \
+    -v /var/panoptes/pocs:/var/panoptes/pocs \
+    -v /var/panoptes/logs:/var/panoptes/logs \
     pocs:testing \
-    "/var/panoptes/POCS/scripts/testing/run-tests.sh"
+    "/var/panoptes/pocs/scripts/testing/run-tests.sh"
+
