ff20135322916e33eb504d1862151a6fbac59cfc
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-06-03 09:46:19 -1000

    Development environment (#972)
    * * Image name changed from `pocs` -> `panoptes-pocs`
    
    * Better install options for extra features.
    
    * * Developer option Docker moving from panoptes-utils to pocs.
    
    * * Remove TODO about docker install. That should be handled by the script.
    
    * * Update test files.
    
    * * Cleaning up docker images:
      * `latest` installs the `panoptes-pocs` module from pip
      * `develop` installs via `pip install -e[google.testing]` and is used for running the CI tests.
      `developer-env` installs locally but with all options, i.e. `pip install -e[google,testing,plotting,developer]`. Also builds jupyterlab and other developer tools. Starts a jupyterlab instance by default.
    
    * * Changelog updates.
    
    * * `develop` image installs the module as the root user.
    
    * * Docker build ignore git directory unless buliding locally (needed by pyscaffold and we are buliding a local develop image anyway).
    * Use new `arduino-cli` installer.
    * Add `bin/panoptes-develop` and `bin/wait-for-it.sh` to installed scripts.
    * Adding a convenience script for building local images.
    
    * Docker image updates
    
    * * Pass git folder to build context for testing images.
    
    * * $PANUSER owns $PANDIR.
    
    * * Ignore log files..
    
    * * Fix log files on GHA. I don't think this should be needed.
    
    * * PANUSER owns PANDIR in develop.
    * Adding the SOLVE_FIELD env var.
    
    * * Give astrometry data all readable.
    
    * * Docker docs
    
    * * Updated install script.
    
    * * Update changelog

---
+++ docker/setup-local-environment.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+POCS=${POCS:-/var/panoptes/POCS}
+TAG="${1:-develop}"
+
+cd "${POCS}"
+
+echo "Building local panoptes-pocs:latest"
+docker build \
+    --quiet \
+    -t "panoptes-pocs:latest" \
+    -f "${POCS}/docker/latest.Dockerfile" \
+    "${POCS}"
+
+# In the local develop we need to pass git to the docker build context.
+sed -i s'/^\.git$/\!\.git/' .dockerignore
+
+echo "Building local panoptes-pocs:develop"
+docker build \
+    --quiet \
+    --build-arg IMAGE_URL="panoptes-pocs:latest" \
+    -t "panoptes-pocs:develop" \
+    -f "${POCS}/docker/develop.Dockerfile" \
+    "${POCS}"
+
+echo "Building local panoptes-pocs:developer-env"
+docker build \
+    --quiet \
+    --build-arg IMAGE_URL="panoptes-pocs:develop" \
+    -t "panoptes-pocs:developer-env" \
+    -f "${POCS}/docker/developer-env.Dockerfile" \
+    "${POCS}"
+
+# Revert our .dockerignore changes.
+sed -i s'/^!\.git$/\.git/' .dockerignore
+
+cat <<EOF
+Done building the local images.  To run the development environment enter:
+
+bin/panoptes-develop up
+
+To run the tests enter:
+
+scripts/testing/test-software.sh
+EOF
