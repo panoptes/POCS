7b206f70571ad013280f0705402a0c30a053c0b5
Wilfred Gee <wtylergee@gmail.com>
2020-06-02 15:11:53 -1000

    Docker image updates

--- docker/setup-local-environment.sh
+++ docker/setup-local-environment.sh
@@ -12,11 +12,10 @@ docker build \
     -f "${POCS}/docker/latest.Dockerfile" \
     "${POCS}"
 
-echo "Building local panoptes-pocs:develop"
-
 # In the local develop we need to pass git to the docker build context.
 sed -i s'/^\.git$/\!\.git/' .dockerignore
 
+echo "Building local panoptes-pocs:develop"
 docker build \
     --quiet \
     --build-arg IMAGE_URL="panoptes-pocs:latest" \
@@ -24,7 +23,23 @@ docker build \
     -f "${POCS}/docker/develop.Dockerfile" \
     "${POCS}"
 
+echo "Building local panoptes-pocs:developer-env"
+docker build \
+    --quiet \
+    --build-arg IMAGE_URL="panoptes-pocs:develop" \
+    -t "panoptes-pocs:developer-env" \
+    -f "${POCS}/docker/developer-env.Dockerfile" \
+    "${POCS}"
+
 # Revert our .dockerignore changes.
 sed -i s'/^!\.git$/\.git/' .dockerignore
 
-echo "Done building local images"
+cat <<EOF
+Done building the local images.  To run the development environment enter:
+
+bin/panoptes-develop up
+
+To run the tests enter:
+
+scripts/testing/test-software.sh
+EOF
