6f939faae9e9bf290b992134f962f8ba9ddf52ce
Wilfred Gee <wtylergee@gmail.com>
2020-06-09 15:59:54 -1000

    * Updates to tests and sitecustomize for coverage to match panoptes-utils.

--- scripts/testing/run-tests.sh
+++ scripts/testing/run-tests.sh
@@ -1,20 +1,23 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 REPORT_FILE=${REPORT_FILE:-coverage.xml}
 
-export PYTHONPATH="${PYTHONPATH}:/var/panoptes/POCS/scripts/testing/coverage"
-export COVERAGE_PROCESS_START="/var/panoptes/POCS/setup.cfg"
+# This assumes we are always running in a docker container.
+export COVERAGE_PROCESS_START="/var/panoptes/panoptes-pocs/setup.cfg"
 
 coverage erase
 
-# Run coverage over the pytest suite
+# Run coverage over the pytest suite.
 echo "Starting tests"
-coverage run "$(command -v pytest)" -x -vv -rfes
+coverage run "$(command -v pytest)"
 
 echo "Combining coverage"
 coverage combine
 
 echo "Making XML coverage report at ${REPORT_FILE}"
 coverage xml -o "${REPORT_FILE}"
+
+coverage report --show-missing
 
 exit 0
