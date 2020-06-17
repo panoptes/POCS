dedb7b7c6df81a01841f576ca31ce37d2c0e37c7
Wilfred Gee <wtylergee@gmail.com>
2020-06-17 07:48:16 -1000

    Fix logger test.

--- src/panoptes/pocs/tests/utils/test_logger.py
+++ src/panoptes/pocs/tests/utils/test_logger.py
@@ -11,8 +11,7 @@ def profile():
 
 def test_base_logger(caplog, profile, tmp_path):
     logger = get_logger(log_dir=str(tmp_path),
-                        full_log_file=None,
-                        profile=profile)
+                        full_log_file=None)
     logger.debug('Hello')
     time.sleep(0.5)
     assert caplog.records[-1].message == 'Hello'
