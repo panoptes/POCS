96bfdee83a24bbf1f5052dfd4a0c8c2e8149a45e
Wilfred Gee <wtylergee@gmail.com>
2020-06-17 16:30:25 -1000

    Slight test adjustment.

--- src/panoptes/pocs/tests/utils/test_logger.py
+++ src/panoptes/pocs/tests/utils/test_logger.py
@@ -13,6 +13,6 @@ def test_base_logger(caplog, profile, tmp_path):
     logger = get_logger(log_dir=str(tmp_path),
                         full_log_file=None)
     logger.debug('Hello')
-    time.sleep(0.5)
+    time.sleep(1)  # Wait for log to make it there.
     assert caplog.records[-1].message == 'Hello'
     assert caplog.records[-1].levelname == 'DEBUG'
