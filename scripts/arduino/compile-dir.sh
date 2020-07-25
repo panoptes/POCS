85afbcedf403b8992d11de023b18d12683f1c841
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-07-25 11:24:16 -1000

    Remove arduino files
    * Moving toward placing firmata on any MCU that we have so that we can
     have consistent control via python.

--- scripts/arduino/compile-dir.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash -e 
-
-BOARD=${1:-uno}
-
-arduino-cli compile --fqbn "arduino:avr:${BOARD}" .
-
--- scripts/arduino/upload.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash -e 
-
-DEVICE=${1:-/dev/ttyACM0}
-BOARD=${2:-uno}
-
-arduino-cli upload -p "${DEVICE}" --fqbn "arduino:avr:${BOARD}" .
-
-
