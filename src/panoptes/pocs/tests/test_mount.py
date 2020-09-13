5cd3d8a707c1422a73c78a6678414ba0047a7847
Wilfred Gee <wtylergee@gmail.com>
2020-09-13 07:33:02 -1000

    * Remove the config reset in test_mount. I have no idea why that was there.

--- src/panoptes/pocs/tests/test_mount.py
+++ src/panoptes/pocs/tests/test_mount.py
@@ -10,21 +10,6 @@ from panoptes.pocs.utils.location import create_location_from_config
 from panoptes.utils import error
 from panoptes.utils.config.client import get_config
 from panoptes.utils.config.client import set_config
-from panoptes.utils.serializers import to_json
-
-import requests
-
-config_host = 'localhost'
-config_port = 6563
-url = f'http://{config_host}:{config_port}/reset-config'
-
-
-def reset_conf():
-    response = requests.post(url,
-                             data=to_json({'reset': True}),
-                             headers={'Content-Type': 'application/json'}
-                             )
-    assert response.ok
 
 
 def test_create_mount_simulator():
@@ -41,7 +26,6 @@ def test_create_mount_simulator_with_config():
 
     mount = create_mount_from_config()
     assert isinstance(mount, AbstractMount) is True
-    reset_conf()
 
 
 def test_create_mount_without_mount_info():
@@ -50,8 +34,6 @@ def test_create_mount_without_mount_info():
     set_config('simulator', hardware.get_all_names(without=['mount']))
     with pytest.raises(error.MountNotFound):
         create_mount_from_config(mount_info=None)
-
-    reset_conf()
 
 
 def test_create_mount_with_mount_info():
@@ -64,8 +46,6 @@ def test_create_mount_with_mount_info():
     set_config('simulator', hardware.get_all_names(without=['mount']))
     assert isinstance(create_mount_from_config(mount_info=mount_info), AbstractMount) is True
 
-    reset_conf()
-
 
 def test_create_mount_with_earth_location():
     # Get location to pass manually.
@@ -75,14 +55,11 @@ def test_create_mount_with_earth_location():
     set_config('simulator', hardware.get_all_names())
     assert isinstance(create_mount_from_config(earth_location=loc['earth_location']), AbstractMount) is True
 
-    reset_conf()
-
 
 def test_create_mount_without_earth_location():
     set_config('location', None)
     with pytest.raises(error.PanError):
         create_mount_from_config(earth_location=None)
-    reset_conf()
 
 
 def test_bad_mount_port():
@@ -96,7 +73,6 @@ def test_bad_mount_port():
     set_config('mount.serial.port', 'foobar')
     with pytest.raises(error.MountNotFound):
         create_mount_from_config()
-    reset_conf()
 
 
 def test_bad_mount_driver():
@@ -110,4 +86,3 @@ def test_bad_mount_driver():
     set_config('mount.serial.driver', 'foobar')
     with pytest.raises(error.MountNotFound):
         create_mount_from_config()
-    reset_conf()
