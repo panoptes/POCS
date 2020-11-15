09c1ec9e988459deaee51d6829d822c47401f57f
Wilfred Gee <wtylergee@gmail.com>
2020-11-14 18:38:19 -1000

    * Setting up the pins

--- src/panoptes/peas/power.py
+++ src/panoptes/peas/power.py
@@ -29,19 +29,50 @@ class PowerBoard(PanBase):
         self.logger.success(f'Power board')
 
     def setup_pins(self, analog_pins=None, digital_pins=None, analog_read_callback=None):
-        """Set the pin modes."""
+        """Set the pin modes.
+
+        Args:
+            analog_pins (dict or None): Analog pin mapping. If not provided,
+                uses 'analog' pins from `self.pin_mapping`.
+            digital_pins (dict or None): Digital pin mapping. If not provided,
+                uses 'digital' pins from `self.pin_mapping`.
+            analog_read_callback (callable or None): A callback for the analog
+                input pins. If not provided a default logging callback is used.
+                Callback should accept a single list parameter, which will be
+                populated with: pin_type, pin_number, pin_value, time_stamp.
+        """
         digital_pins = digital_pins or self.pin_mapping['digital']
         analog_pins = analog_pins or self.pin_mapping['analog']
 
         # Generic logging callback if none is provided.
         if analog_read_callback is None:
-            def analog_callback(pin_type, pin_number, pin_value, raw_time_stamp):
-                self.logger.trace(f'Analog read: {pin_type} {pin_number} {pin_value} {raw_time_stamp}')
+            def analog_callback(data):
+                pin_type, pin, value, time_stamp = data
+                self.logger.trace(f'Analog read: {pin_type} {pin} {value} {time_stamp}')
 
             analog_read_callback = analog_callback
 
-        self.logger.debug(f'Setting analog pin modes for {self.name}')
+        self.logger.debug(f'Setting analog pins for {self.name}')
         for pin_number, label in analog_pins:
+            self.logger.debug(f'Setting pin_number={pin_number} as analog input.')
             self.board.set_pin_mode_analog_input(pin_number, callback=analog_read_callback)
 
-        self.logger.debug(f'Setting digital pin modes for {self.name}')
+        # All our pins are output on PowerBoard.
+        self.logger.debug(f'Setting digital pin for {self.name}')
+        for pin_number, pin_config in digital_pins:
+            pin_mode = pin_config['mode']
+            pin_name = pin_config['name']
+            initial_pin_state = pin_config['initial']
+
+            self.logger.debug(f'Setting digital pin_number={pin_number} ({pin_name}) as {pin_mode}')
+            self.board.set_pin_mode_digital_output(pin_number)
+
+            # Set initial state on digital output pin.
+            self.logger.debug(f'Setting digital output pin {pin_number} to {initial_pin_state}')
+            self.board.digital_write(pin_number, initial_pin_state)
+
+    def __str__(self):
+        return f'Power Distribution Board - {self.name}'
+
+    def __del__(self):
+        self.board.shutdown()
