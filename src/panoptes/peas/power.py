4ba5c1a465884d5b88610fb76de8ae2a3edbc739
Wilfred Gee <wtylergee@gmail.com>
2020-11-15 10:00:22 -1000

    * Cleaning up the power board specification. Using enums specified in the file rather than a more generic config-based approach. This is mostly because of the specific needs of the trucker board.

--- src/panoptes/peas/power.py
+++ src/panoptes/peas/power.py
@@ -9,13 +9,40 @@ from panoptes.pocs.base import PanBase
 
 class PinState(IntEnum):
     LOW = 0
+    OFF = 0
     HIGH = 1
+    ON = 1
+
+
+class RelayPins(IntEnum):
+    RELAY_0 = 17
+    RELAY_1 = 3
+    RELAY_2 = 4
+    RELAY_3 = 7
+    RELAY_4 = 8
+
+
+class CurrentSensePins(IntEnum):
+    IS_0 = 0  # Analog 0
+    IS_1 = 1  # Analog 1
+    IS_2 = 2  # Analog 2
+
+
+class CurrentSelectPins(IntEnum):
+    DSEL_0 = 2
+    DSEL_1 = 6
+
+
+class CurrentEnablePins(IntEnum):
+    DEN_0 = 18
+    DEN_1 = 5
+    DEN_2 = 9
 
 
 class PowerBoard(PanBase):
     """Power distribution and monitoring"""
 
-    def __init__(self, name='Power Board', arduino_instance_id=None, channels=None, pins=None, *args, **kwargs):
+    def __init__(self, name='Power Board', arduino_instance_id=None, relays=None, pins=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.name = name
@@ -24,7 +51,7 @@ class PowerBoard(PanBase):
         self.config = self.get_config('environment.power')
 
         arduino_instance_id = arduino_instance_id or self.config.get('arduino_instance_id', default=1)
-        self.channels = channels or self.config.get('channels', default=dict())
+        self.relays = relays or self.config.get('relays', default=dict())
 
         # Get the pin mapping from the config if not provided.
         self.pin_mapping = pins or self.config.get('pins', default=dict())
@@ -33,103 +60,45 @@ class PowerBoard(PanBase):
         self.logger.debug(f'Setting up Power board connection')
         self.board = pymata_express.PymataExpress(arduino_instance_id=arduino_instance_id)
 
-        self.setup_pins()
+        # Set initial pin modes.
+        self.set_pin_modes()
+
+        # Set initial pin states.
+        for relay_name, relay_config in self.relays:
+            label = relay_config['label']
+            initial_state = relay_config['initial_state'].upper()
+
+            self.set_pin_state(RelayPins[relay_name], initial_state)
+
+            # Set an attribute with the relay label for easy access.
+            self.set_relay_name(RelayPins[relay_name], label)
 
         self.logger.success(f'Power board initialized')
 
-    def setup_pins(self, analog_pins=None, digital_pins=None):
-        """Set the pin modes.
+    def set_pin_modes(self):
+        """Set the pin modes for the Arduino Uno + Infineon Uno 24V shield."""
+        self.logger.debug(f'Setting up current reading pins')
+        for pin_number in CurrentSensePins:
+            self.board.set_pin_mode_analog_input(pin_number)
 
-        Args:
-            analog_pins (dict or None): Analog pin mapping. If not provided,
-                uses 'analog' pins from `self.pin_mapping`.
-            digital_pins (dict or None): Digital pin mapping. If not provided,
-                uses 'digital' pins from `self.pin_mapping`.
-        """
-        digital_pins = digital_pins or self.pin_mapping['digital']
-        analog_pins = analog_pins or self.pin_mapping['analog']
+        self.logger.debug(f'Setting up current enable pins')
+        for pin_number in CurrentEnablePins:
+            self.board.set_pin_mode_digital_output(pin_number)
 
-        self.logger.debug(f'Setting analog pins for {self.name}')
-        for pin_number, pin_name in analog_pins:
-            self.logger.debug(f'Setting pin_number={pin_number} as analog input.')
-            await self.board.set_pin_mode_analog_input(pin_number)
+        self.logger.debug(f'Setting up current select pins')
+        for pin_number in CurrentSelectPins:
+            self.board.set_pin_mode_digital_output(pin_number)
 
-            # Set an attribute for the pin name.
-            self.set_pin_name(pin_number, pin_name)
-
-        # All our pins are output on PowerBoard.
-        self.logger.debug(f'Setting digital pin for {self.name}')
-        for pin_number, pin_config in digital_pins:
-            pin_mode = pin_config['mode']
-            pin_name = pin_config['name']
-            initial_pin_state = pin_config['initial']
-
-            self.logger.debug(f'Setting digital pin_number={pin_number} ({pin_name}) as {pin_mode}')
-            await self.board.set_pin_mode_digital_output(pin_number)
-
-            # Set an attribute for the pin name.
-            self.set_pin_name(pin_number, pin_name)
-
-            # Also set an attribute name for the channel label.
-            with suppress(KeyError):
-                channel_name = self.channels[pin_name]
-                if channel_name != 'unused':
-                    self.set_pin_name(pin_number, channel_name)
-
-            # Set initial state on digital output pin.
-            self.set_pin_state(pin_number, initial_pin_state)
-
-    def set_pin_name(self, number, name):
-        """Sets an attribute with the given name that returns the pin number.
-
-        Args:
-            number (int): The pin number to set an attribute name for.
-            name (str): The name for the pin.
-        """
-        self.logger.debug(f'Adding {name} as board attribute with pin_number={number}')
-        setattr(self, name, number)
-
-    def get_channel_pin(self, name):
-        """Get the pin number for the given channel name.
-
-        Args:
-            name (str): The name of the 'channel' to turn on.
-
-        Returns:
-            int: The pin number corresponding to name for the channel.
-        """
-        try:
-            pin_name = self.config['channels'][name]
-            pin_number = getattr(self, pin_name)
-        except (KeyError, AttributeError):
-            self.logger.warning(f'No pin found for {name}')
-            return False
-
-        self.logger.debug(f'Return pin_number={pin_number} for {name}')
-
-        return pin_number
-
-    def get_pin_state(self, channel_name):
-        """Get the digital pin state.
-
-        Args:
-            channel_name (str): The name of the channel to read state from.
-
-        Returns:
-            bool: True if pin is HIGH, False if LOW.
-        """
-        pin_number = self.get_channel_pin(channel_name)
-        state, timestamp = await self.board.digital_read(pin_number)
-        self.logger.info(f'{channel_name} = {state} (at {timestamp})')
-
-        return bool(state)
+        self.logger.debug(f'Setting up relay pins')
+        for pin_number in RelayPins:
+            self.board.set_pin_mode_digital_output(pin_number)
 
     def set_pin_state(self, pin_number, state):
-        """Turns on the power channel with the given name.
+        """Set the relay to the given state.
 
         Args:
-            pin_number (int): The pin number of the channel to turn on.
-            state (bool): A state that evaluates to True for "on" and False for "off".
+            pin_number (int): The pin number of the relay to turn on.
+            state (bool or PinState): A state that evaluates to True for "on" and False for "off".
 
         Returns:
             bool: True if pin state successfully set.
@@ -140,59 +109,112 @@ class PowerBoard(PanBase):
 
         return True
 
-    def turn_on(self, channel_name):
-        """Turns off the given power channel.
+    def set_relay_name(self, number, name):
+        """Sets an attribute with the given name that returns the pin number.
+
+        Args:
+            number (int): The pin number to set an attribute name for.
+            name (str): The name for the pin.
+        """
+        self.logger.debug(f'Adding {name} as board attribute with pin_number={number}')
+        setattr(self, name, number)
+
+    def get_relay_state(self, relay_name):
+        """Get the digital pin state.
+
+        Args:
+            relay_name (str): The name of the relay to read state from.
+
+        Returns:
+            bool: True if pin is HIGH, False if LOW.
+        """
+        pin_number = getattr(self, relay_name)
+        state, timestamp = await self.board.digital_read(pin_number)
+        self.logger.info(f'{relay_name} = {state} (at {timestamp})')
+
+        return bool(state)
+
+    def turn_on(self, relay_name):
+        """Turns off the given power relay.
 
         Note: This method is not asynchronous.
 
         Args:
-            channel_name (str): The name of the channel to turn on.
+            relay_name (str): The name of the relay to turn on.
         """
-        self.logger.info(f'Turning on {channel_name}')
-        return await self.set_pin_state(self.get_channel_pin(channel_name), PinState.HIGH)
+        self.logger.info(f'Turning on {relay_name}')
+        pin_number = getattr(self, relay_name)
+        return await self.set_pin_state(pin_number, PinState.HIGH)
 
-    def turn_off(self, channel_name):
-        """Turns off the given power channel.
+    def turn_off(self, relay_name):
+        """Turns off the given power relay.
 
         Note: This method is not asynchronous.
 
         Args:
-            channel_name (str): The name of the channel to turn off.
+            relay_name (str): The name of the relay to turn off.
         """
-        self.logger.info(f'Turning off {channel_name}')
-        return await self.set_pin_state(self.get_channel_pin(channel_name), PinState.LOW)
+        self.logger.info(f'Turning off {relay_name}')
+        pin_number = getattr(self, relay_name)
+        return await self.set_pin_state(pin_number, PinState.LOW)
 
-    def toggle(self, channel_name, delay=500):
+    def toggle(self, relay_name, delay=0.5):
         """Toggles the power state with optional delay.
 
         Args:
-            channel_name (str): The name of the channel to toggle.
-            delay (int): The delay between toggling the state in milliseconds.
+            relay_name (str): The name of the relay to toggle.
+            delay (int): The delay between toggling the state in seconds.
         """
-        starting_state = self.get_pin_state(channel_name)
-        self.logger.info(f'Toggling {channel_name} (with {delay} ms delay). '
+        starting_state = self.get_relay_state(relay_name)
+        self.logger.info(f'Toggling {relay_name} (with {delay} ms delay). '
                          f'Starting state = {starting_state}')
 
-        self.set_pin_state(self.get_channel_pin(channel_name), not starting_state)
+        pin_number = getattr(self, relay_name)
+        self.set_pin_state(pin_number, not starting_state)
         time.sleep(delay)
-        self.set_pin_state(self.get_channel_pin(channel_name), starting_state)
+        self.set_pin_state(pin_number, starting_state)
 
     def read_current(self):
-        """Reads the current from all channels on the board.
+        """Reads the current from all relays on the board.
 
         The Power Board uses the Infineon 24V relay shield, which has five
-        relay channels but only three MOSFETs. To read the current we poll
-        the two MOSFETs with shared channels, change which channel is read,
+        relay relays but only three MOSFETs. To read the current we poll
+        the two MOSFETs with shared relays, change which relay is read,
         and then poll again.  The third MOSFET is polled each time and averaged.
+
+        This will first set the channel select pins to low, do a reading,
+        then set to high and do another reading.
         """
-        # Toggle which channels are read.
-        self.set_pin_state(self.DSEL_0, PinState.LOW)
-        self.set_pin_state(self.DSEL_1, PinState.LOW)
+        # Set select pins to low.
+        self.set_pin_state(CurrentSelectPins.DSEL_0, PinState.LOW)
+        self.set_pin_state(CurrentSelectPins.DSEL_1, PinState.LOW)
 
-        for pin_number, pin_name in self.pin_mapping['analog']:
-            value, timestamp = self.board.analog_read(pin_number)
-            self.db.insert_current('power', dict(channel_name=value, read_time=timestamp))
+        # Read current.
+        relay_0_value, _ = self.board.digital_read(RelayPins.RELAY_0)
+        relay_1_value, _ = self.board.digital_read(RelayPins.RELAY_1)
 
+        # Set select pins to low.
+        self.set_pin_state(CurrentSelectPins.DSEL_0, PinState.HIGH)
+        self.set_pin_state(CurrentSelectPins.DSEL_1, PinState.HIGH)
+
+        # FirmataExpress polls analog pins every 19 ms, delay 500 ms before reading.
+        time.sleep(0.5)
+
+        # Read current.
+        relay_2_value, _ = self.board.digital_read(RelayPins.RELAY_2)
+        relay_3_value, _ = self.board.digital_read(RelayPins.RELAY_3)
+
+        relay_4_value, _ = self.board.digital_read(RelayPins.RELAY_4)
+
+        current_readings = {
+            self.relays['RELAY_0']['label']: relay_0_value,
+            self.relays['RELAY_1']['label']: relay_1_value,
+            self.relays['RELAY_2']['label']: relay_2_value,
+            self.relays['RELAY_3']['label']: relay_3_value,
+            self.relays['RELAY_4']['label']: relay_4_value,
+        }
+
+        self.db.insert_current('power', dict(current=current_readings))
 
     def __str__(self):
         return f'Power Distribution Board - {self.name}'
