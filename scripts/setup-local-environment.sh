810d38e4748ffcc6d2503f7b62c03f642eaed678
Wilfred Gee <wtylergee@gmail.com>
2020-12-31 06:05:30 -1000

    * cleaning up docker file to match the changes in panoptes-utils, namely simplifying and removing panuser.

--- scripts/setup-local-environment.sh
+++ scripts/setup-local-environment.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-INCLUDE_DEVELOPER=${INCLUDE_DEVELOPER:-false}
+INCLUDE_DEVELOPER=${INCLUDE_DEVELOPER:-true}
 
 TAG="${TAG:-develop}"
 
 PANDIR="${PANDIR:-${PWD}/../}"
 PANOPTES_POCS=${PANOPTES_POCS:-${PANDIR}/POCS}
 
-UTILS_IMAGE_URL="${UTILS_IMAGE_URL:-gcr.io/panoptes-exp/panoptes-utils:${TAG}}"
+UTILS_IMAGE_URL="${UTILS_IMAGE_URL:-panoptes-utils:${TAG}}"
 
 echo "Setting up local environment in ${PANOPTES_POCS}"
 cd "${PANOPTES_POCS}"
+
+build_developer() {
+  echo "Building local panoptes-pocs:developer from ${UTILS_IMAGE_URL} in ${PANOPTES_POCS}"
+  docker build \
+    --build-arg "image_url=${UTILS_IMAGE_URL}" \
+    -t "panoptes-pocs:developer" \
+    -f "${PANOPTES_POCS}/docker/developer/Dockerfile" \
+    "${PANOPTES_POCS}"
+
+  # Use the developer version for building local.
+  UTILS_IMAGE_URL="panoptes-pocs:developer"
+}
 
 build_develop() {
   echo "Building local panoptes-pocs:${TAG} from ${UTILS_IMAGE_URL} in ${PANOPTES_POCS}"
@@ -20,25 +32,18 @@ build_develop() {
     -t "panoptes-pocs:${TAG}" \
     -f "${PANOPTES_POCS}/docker/Dockerfile" \
     "${PANOPTES_POCS}"
-}
 
-build_developer() {
-  echo "Building local panoptes-pocs:developer from panoptes-pocs:${TAG} in ${PANOPTES_POCS}"
-  docker build \
-    -t "panoptes-pocs:developer" \
-    -f "${PANOPTES_POCS}/docker/developer/Dockerfile" \
-    "${PANOPTES_POCS}"
 }
 
 ####################################################################################
 # Script logic below
 ####################################################################################
 
-build_develop
-
 if [ "${INCLUDE_DEVELOPER}" = true ]; then
   build_developer
 fi
+
+build_develop
 
 cat <<EOF
 Done building the local images.
