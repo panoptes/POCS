7f13e1f256aed487daf212cd53d4990c48c6fb42
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-01-27 23:03:43 -1000

    * Simplify the install script.

--- scripts/install/install-pocs.sh
+++ scripts/install/install-pocs.sh
@@ -49,7 +49,7 @@ usage() {
 #                           removing zsh, etc. Removed Darwin options.
 #
 #############################################################
- $ $(basename $0) [--user panoptes] [--pandir /var/panoptes]
+ $ $(basename $0) [--user panoptes] [--pandir /panoptes]
 
  Options:
   USER      The PANUSER environment variable, defaults to current user (i.e. PANUSER=$USER).
@@ -63,18 +63,12 @@ PS3="Select: "
 
 # TODO should be checking to matching userid=1000
 PANUSER=${PANUSER:-$USER}
-PANDIR=${PANDIR:-/var/panoptes}
-LOGFILE="${PANDIR}/install-pocs.log"
+PANDIR=${PANDIR:-/panoptes}
+TAG_NAME=${TAG_NAME:-develop}
+LOGFILE="${PANDIR}/logs/install-pocs.log"
 OS="$(uname -s)"
 
 DOCKER_BASE=${DOCKER_BASE:-"gcr.io/panoptes-exp"}
-
-function command_exists() {
-  # https://gist.github.com/gubatron/1eb077a1c5fcf510e8e5
-  # this should be a very portable way of checking if something is on the path
-  # usage: "if command_exists foo; then echo it exists; fi"
-  type "$1" &>/dev/null
-}
 
 function make_directories() {
   sudo mkdir -p "${PANDIR}/logs"
@@ -89,14 +83,12 @@ function system_deps() {
     byobu \
     docker.io \
     docker-compose \
-    git \
     htop \
     httpie \
     jq \
     openssh-server \
-    speedometer \
-    vim-nox \
-    wget | sudo tee -a "${LOGFILE}" 2>&1
+    wget \
+    zsh | sudo tee -a "${LOGFILE}" 2>&1
 
   # Add an SSH key if one doesn't exist.
   if [[ ! -f "${HOME}/.ssh/id_rsa" ]]; then
@@ -106,23 +98,12 @@ function system_deps() {
 
   # Add to docker group if not already.
   sudo usermod -aG docker "${PANUSER}" | sudo tee -a "${LOGFILE}" 2>&1
-
-  # Source the environment variables if available.
-  cat <<EOF >>"/home/${PANUSER}/.bashrc"
-export LANG="en_US.UTF-8"
-
-# Load POCS env file if it exists
-if test -f "${PANDIR}/POCS/env"; then
-  source "${PANDIR}/POCS/env"
-fi
-EOF
 }
 
 function get_or_build_images() {
   echo "Pulling POCS docker images from Google Cloud Registry (GCR)."
 
-  sudo docker pull "${DOCKER_BASE}/panoptes-pocs:latest"
-  sudo docker pull "${DOCKER_BASE}/aag-weather:latest"
+  sudo docker pull "${DOCKER_BASE}/panoptes-pocs:${TAG_NAME}"
 }
 
 function do_install() {
