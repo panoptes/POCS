86c4946b1c0cebca251be540d9129691684191ac
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-01-29 16:11:13 -1000

    * Simulator load script actually loads simulator.

--- scripts/load-simulators.py
+++ scripts/load-simulators.py
@@ -1,12 +1,15 @@
 import time
 
-from panoptes.utils.config.server import config_server
+from panoptes.pocs.mount import create_mount_simulator
+from panoptes.pocs.scheduler import create_scheduler_from_config
+from panoptes.pocs.camera import create_cameras_from_config
 from panoptes.pocs.observatory import Observatory
 from panoptes.pocs.core import POCS
 
-print("Starting config-server")
-conf_server = config_server('/POCS/conf_files/pocs.yaml')
-
-pocs = POCS(Observatory(db_type='memory'), simulators=['all'])
+pocs = POCS(Observatory(mount=create_mount_simulator(),
+                        scheduler=create_scheduler_from_config(),
+                        cameras=create_cameras_from_config(),
+                        db_type='memory'),
+            simulators=['dome', 'night', 'weather', 'power'])
 time.sleep(1)
 print(f'POCS simulator instance created as "pocs"')
