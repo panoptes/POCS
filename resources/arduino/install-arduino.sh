8abf55712157af7ac25719171460b61bde606a93
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-03-10 10:35:57 -1000

    * Fix arduino install and add the cli to path.

--- resources/arduino/install-arduino.sh
+++ resources/arduino/install-arduino.sh
@@ -6,7 +6,7 @@ ARDUINO_PORT="${ARDUINO_PORT:-/dev/ttyACM0}"
 
 arduino-cli config init
 arduino-cli core update-index
-arduino-cli board install arduino:avr
+arduino-cli core install arduino:avr
 arduino-cli lib install ArduinoJson
 arduino-cli compile -b "${FQBN}" "${SKETCH_LOCATION}"
 arduino-cli install -p "${ARDUINO_PORT}" -b "${FQBN}" "${SKETCH_LOCATION}"
