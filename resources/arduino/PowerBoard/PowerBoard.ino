ccdc5eeadc4afba242a40ca881a10ba93692d03f
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-03-17 14:09:17 -1000

    * Use JSON for arduino input.

--- resources/arduino/PowerBoard/PowerBoard.ino
+++ resources/arduino/PowerBoard/PowerBoard.ino
@@ -57,8 +57,8 @@ void setup() {
   digitalWrite(DEN_1, HIGH);  // DEN_1 goes HIGH so Diagnosis enabled for PROFET1
   digitalWrite(DEN_2, HIGH);  // DEN_2 goes HIGH so Diagnosis enabled for PROFET2
 
-  digitalWrite(DSEL_0, LOW); // DSEL_0 LOW reads PROFET 0_0. DSEL_0 HIGH reades PROFET 0_1
-  digitalWrite(DSEL_1, LOW); // DSEL_1 LOW reads PROFET 1_0. DSEL_1 HIGH reades PROFET 1_1
+  digitalWrite(DSEL_0, LOW); // DSEL_0 LOW reads PROFET 0_0. DSEL_0 HIGH reads PROFET 0_1
+  digitalWrite(DSEL_1, LOW); // DSEL_1 LOW reads PROFET 1_0. DSEL_1 HIGH reads PROFET 1_1
 
  // Turn on all relays to start
  turn_pin_on(RELAY_0);
@@ -68,138 +68,9 @@ void setup() {
  turn_pin_on(RELAY_4);
 }
 
-// CharBuffer stores characters and supports (minimal) parsing of
-// the buffered characters.
-template <uint8_t kBufferSize>
-class CharBuffer {
-  public:
-    CharBuffer() {
-      Reset();
-    }
-    void Reset() {
-      write_cursor_ = read_cursor_ = 0;
-    }
-    bool Append(char c) {
-      if (write_cursor_ < buf_ + kBufferSize) {
-        buf_[write_cursor_++] = c;
-        return true;
-      }
-      return false;
-    }
-    bool Empty() {
-      return read_cursor_ >= write_cursor_;
-    }
-    char Next() {
-      return buf_[read_cursor_++];
-    }
-    char Peek() {
-      return buf_[read_cursor_];
-    }
-    bool ParseInt(int* output) {
-      int& v = *output;
-      v = 0;
-      size_t len = 0;
-      while (!Empty() && isdigit(Peek())) {
-        char c = Next();
-        v = v * 10 + c - '0';
-        ++len;
-        if (len > 5) {
-          return false;
-        }
-      }
-      return len > 0;
-    }
-    bool MatchAndConsume(char c) {
-      if (Empty() || Peek() != c) {
-        return false;
-      }
-      Next();
-      return true;
-    }
-
-  private:
-    char buf_[kBufferSize];
-    uint8_t write_cursor_;
-    uint8_t read_cursor_;
-};
-
-// Accumulates a line, parses it and takes the requested action if it is valid.
-class SerialInputHandler {
-  public:
-    void Handle() {
-      while (Serial && Serial.available() > 0) {
-        int c = Serial.read();
-        if (wait_for_new_line_) {
-          if (IsNewLine(c)) {
-            wait_for_new_line_ = false;
-            input_buffer_.Reset();
-          }
-        } else if (IsNewLine(c)) {
-          ProcessInputBuffer();
-          wait_for_new_line_ = false;
-          input_buffer_.Reset();
-        } else if (isprint(c)) {
-          if (!input_buffer_.Append(static_cast<char>(c))) {
-            wait_for_new_line_ = true;
-          }
-        } else {
-          // Input is not an acceptable character.
-          wait_for_new_line_ = true;
-        }
-      }
-    }
-
-  private:
-    // Allow the input line to end with NL, CR NL or CR.
-    bool IsNewLine(int c) {
-      return c == '\n' || c == '\r';
-    }
-
-    void ProcessInputBuffer() {
-      int relay_index, new_state;
-      if (input_buffer_.ParseInt(&relay_index) &&
-          input_buffer_.MatchAndConsume(',') &&
-          input_buffer_.ParseInt(&new_state) &&
-          input_buffer_.Empty()) {
-
-        int pin_num = relayArray[relay_index];
-        switch (new_state) {
-          case 1:
-            turn_pin_on(pin_num);
-            break;
-          case 2:
-            turn_pin_off(pin_num);
-            break;
-          case 3:
-            toggle_pin(pin_num);
-            break;
-          case 4:
-            toggle_pin_delay(pin_num);
-            break;
-        }
-      }
-    }
-
-    CharBuffer<8> input_buffer_;
-    bool wait_for_new_line_{false};
-} serial_input_handler;
-
 void loop() {
-
-  // Read any serial input
-  //    - Input will be two comma separated integers, the
-  //      first specifying the relayArray index and the second
-  //      the new desired state.
-  //      Example serial input:
-  //           0,1   # Turn relay index 0 on (pin RELAY_0)
-  //           0,2   # Turn relay index 0 off
-  //           0,3   # Toggle relay index 0
-  //           0,4   # Toggle relay index 0 w/ 30 sec delay
-
   serial_input_handler.Handle();
-
   delay(250);
-
   get_readings();
 
   // Simple heartbeat
@@ -207,9 +78,22 @@ void loop() {
   delay(250);
 }
 
+void handle_input() {
+  StaticJsonDocument<16> doc;
+  DeserializationError error = deserializeJson(doc, Serial, inputLength);
+
+  int pin_num = relayArray[doc["relay"]];
+  bool power_on = doc["power"]; // true=on
+
+  if (power_on == true){
+      turn_pin_on(pin_num);
+  } else {
+      turn_pin_off(pin_num);
+  }
+}
+
 void get_readings() {
   int current_readings[5];
-
   read_currents(current_readings);
 
   StaticJsonDocument<128> doc;
@@ -255,16 +139,11 @@ void read_currents(int current_readings[]) {
   int Diag1_1=analogRead(IS_1);
   int Diag2_1=analogRead(IS_2);
 
-//  float Iload0 = Diag0_0 * MULTIPLIER;
-//  float Iload1 = Diag0_1 * MULTIPLIER;
-//  float Iload2 = Diag1_0 * MULTIPLIER;
-//  float Iload3 = Diag1_1 * MULTIPLIER;
-//  float Iload4 = (Diag2_0 * Diag2_1) / 2 * MULTIPLIER;
-
   current_readings[0] = Diag0_0;
   current_readings[1] = Diag0_1;
   current_readings[2] = Diag1_0;
   current_readings[3] = Diag1_1;
+  // Average the PROFET that was read twice.
   current_readings[4] = int((Diag2_0 * Diag2_1) / 2);
 }
 
