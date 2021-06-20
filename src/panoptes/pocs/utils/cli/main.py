e915ebe1e3ea43d8275765c0f73160f2b918c519
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-06-20 08:33:23 -1000

    Power service and cli
    Adding a fastapi web service and cli for controlling the power board.
    
    * API exposes methods for getting current status, controlling relays, recording values to db, etc.  This should act as the primary control for the PowerBoard.
    * CLI interface interacts with the web service. Also contains a `setup_wizard` command that allows for interactively determining which device is hooked to which relay.
    * The CLI interface is a subcommand of the `pocs` cli.
    * Fastapi service added to docker-compose file.

---
+++ src/panoptes/pocs/utils/cli/main.py
@@ -0,0 +1,26 @@
+import typer
+
+from panoptes.pocs.utils.cli import power
+
+app = typer.Typer()
+state = {'verbose': False}
+
+app.add_typer(power.app, name="power", help='Manage the power board.')
+
+
+@app.callback()
+def main(context: typer.Context,
+         config_host: str = '127.0.0.1',
+         config_port: int = 6563,
+         verbose: bool = False):
+    state.update({
+        'config_host': config_host,
+        'config_port': config_port,
+        'verbose': verbose,
+    })
+    if verbose:
+        typer.echo(f'Command options from main: {context.params!r}')
+
+
+if __name__ == "__main__":
+    app()
---
+++ src/panoptes/pocs/utils/service/power.py
@@ -0,0 +1,77 @@
+import time
+from enum import Enum
+from typing import Union
+
+from fastapi import FastAPI
+from panoptes.pocs.sensor.power import PowerBoard
+from panoptes.utils.config.client import get_config
+from pydantic import BaseModel
+
+
+class RelayAction(str, Enum):
+    turn_on = 'turn_on'
+    turn_off = 'turn_off'
+
+
+class RelayCommand(BaseModel):
+    relay: Union[str, int]
+    command: RelayAction
+
+
+app = FastAPI()
+power_board: PowerBoard
+
+
+@app.on_event('startup')
+async def startup():
+    global power_board
+    power_board = PowerBoard(**get_config('environment.power', {}))
+
+
+@app.get('/')
+async def root():
+    """Returns the power board status."""
+    global power_board
+    return power_board.status
+
+
+@app.get('/readings')
+async def readings():
+    """Return the current readings as a dict."""
+    global power_board
+    return power_board.to_dataframe().to_dict()
+
+
+@app.get('/record')
+def control_relay():
+    """Record the current readings in the db."""
+    global power_board
+    return power_board.record()
+
+
+@app.post('/control')
+def control_relay(relay_command: RelayCommand):
+    """Control a relay via a post submission"""
+    return do_command(relay_command)
+
+
+@app.get('/relay/{relay}/control/{command}')
+def control_relay_url(relay: Union[int, str], command: str = 'turn_on'):
+    """Control a relay via a get url"""
+    return do_command(RelayCommand(relay=relay, command=command))
+
+
+def do_command(relay_command: RelayCommand):
+    """Control relay"""
+    global power_board
+    relay_id = relay_command.relay
+    try:
+        relay = power_board.relay_labels[relay_id]
+    except KeyError:
+        relay = power_board.relays[relay_id]
+
+    command_func = getattr(relay, relay_command.command)
+    # Perform function.
+    command_func()
+    time.sleep(1)  # Give it time to toggle before returning status
+    return power_board.status
