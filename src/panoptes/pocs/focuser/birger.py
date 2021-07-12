2b0f669df8490034b3c544f52309f3e8eef90929
danjampro <danjampro@sky.com>
2021-07-12 18:17:43 +1000

    fix bug (#1136)

--- src/panoptes/pocs/focuser/birger.py
+++ src/panoptes/pocs/focuser/birger.py
@@ -153,9 +153,6 @@ class Focuser(AbstractSerialFocuser):
 
         super().connect(port=self.port, baudrate=baudrate, **kwargs)
 
-        # Set the serial number
-        self._serial_number = self._get_serial_number()
-
     def move_to(self, position):
         """
         Moves focuser to a new position.
@@ -197,7 +194,7 @@ class Focuser(AbstractSerialFocuser):
             # returned then the focuser is no longer moving.
             self._is_moving = False
 
-        self.logger.debug("Moved by {} encoder units".format(moved_by))
+        self.logger.debug(f"Moved by {moved_by} encoder units")
         return self.position
 
     # Private Methods
@@ -207,6 +204,35 @@ class Focuser(AbstractSerialFocuser):
         # Set 'verbose' and 'legacy' response modes. The response from this depends on
         # what the current mode is... but after a power cycle it should be 'rm1,0', 'OK'
         self._send_command('rm1,0', response_length=0)
+
+        # Set the serial number
+        self._serial_number = self._get_serial_number()
+
+        # Get the version string of the adaptor software libray. Accessible as self.library_version
+        self._get_library_version()
+
+        # Get the hardware version of the adaptor. Accessible as self.hardware_version
+        self._get_hardware_version()
+
+        # Get basic lens info (e.g. '400mm,f28' for a 400 mm, f/2.8 lens). Accessible as
+        # self.lens_info
+        self._get_lens_info()
+
+        # Initialise the aperture motor. This also has the side effect of fully opening the iris.
+        self._initialise_aperture()
+
+        # Initalise focus. First move the focus to the close stop.
+        self._move_zero()
+
+        # Then reset the focus encoder counts to 0
+        self._zero_encoder()
+        self._min_position = 0
+
+        # Calibrate the focus with the 'Learn Absolute Focus Range' command
+        self._learn_focus_range()
+
+        # Finally move the focus to the far stop (close to where we'll want it) and record position
+        self._max_position = self._move_inf()
 
     def _send_command(self, command, *args, **kwargs):
         """
@@ -283,34 +309,6 @@ class Focuser(AbstractSerialFocuser):
             self.logger.warning('{} reported hitting a focus stop'.format(self))
 
         return amount
-
-        # Get the version string of the adaptor software libray. Accessible as self.library_version
-        self._get_library_version()
-
-        # Get the hardware version of the adaptor. Accessible as self.hardware_version
-        self._get_hardware_version()
-
-        # Get basic lens info (e.g. '400mm,f28' for a 400 mm, f/2.8 lens). Accessible as
-        # self.lens_info
-        self._get_lens_info()
-
-        # Initialise the aperture motor. This also has the side effect of fully opening the iris.
-        self._initialise_aperture()
-
-        # Initalise focus. First move the focus to the close stop.
-        self._move_zero()
-
-        # Then reset the focus encoder counts to 0
-        self._zero_encoder()
-        self._min_position = 0
-
-        # Calibrate the focus with the 'Learn Absolute Focus Range' command
-        self._learn_focus_range()
-
-        # Finally move the focus to the far stop (close to where we'll want it) and record position
-        self._max_position = self._move_inf()
-
-        self.logger.info('{} initialised'.format(self))
 
     def _get_serial_number(self):
         """ Get the Birger Focuser's serial number. """
