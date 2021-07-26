ade78f3c773705865284939b33853baec1c920e4
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-07-26 11:52:25 -1000

    Better return json from service.

--- src/panoptes/pocs/utils/service/camera.py
+++ src/panoptes/pocs/utils/service/camera.py
@@ -28,13 +28,11 @@ def gphoto(command: Command):
 
     # Build the full command.
     full_command = [shutil.which('gphoto2'), command]
-    print(f'Running {command=}')
 
+    print(f'Running {command=!r}')
     completed_proc = subprocess.run(full_command, capture_output=True)
+
     # Return the full path upon success otherwise the output from command.
-    if completed_proc.returncode:
-        print(completed_proc.stdout)
-        return {'success': False, 'message': completed_proc.stdout}
-    else:
-        print(f'Done with {command=}')
-        return {'success': True}
+    success = completed_proc.returncode >= 0
+
+    return {'success': success, 'output': completed_proc.stdout}
