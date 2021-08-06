547db7a4a5f3f766276ecd31c8cfd2df48bbb2c6
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-08-05 14:00:36 -1000

    Remote Camera Service
    * Add a service so that camera operations can be called remotely via an api.
    
    Replaces #1137

---
+++ src/panoptes/pocs/utils/service/camera.py
@@ -0,0 +1,37 @@
+import shutil
+import sys
+import subprocess
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+
+class Command(BaseModel):
+    """Accepts an arbitrary command string which is passed to gphoto2."""
+    command: str = '--auto-detect'
+
+
+app = FastAPI()
+
+
+@app.on_event('startup')
+def startup_tasks():
+    gphoto2_path = shutil.which('gphoto2')
+    if gphoto2_path is None:
+        print('Cannot find gphoto2, exiting system.')
+        sys.exit(1)
+
+
+@app.post('/gphoto')
+def gphoto(command: Command):
+    """Perform arbitrary gphoto2 command."""
+
+    # Build the full command.
+    full_command = [shutil.which('gphoto2'), command]
+
+    print(f'Running {command=!r}')
+    completed_proc = subprocess.run(full_command, capture_output=True)
+
+    # Return the full path upon success otherwise the output from command.
+    success = completed_proc.returncode >= 0
+
+    return {'success': success, 'output': completed_proc.stdout}
