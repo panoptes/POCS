696c674e324badb8d7aa501b4a9b2b2fad715c55
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-10-02 19:41:59 -1000

    * Adding a basic startup script for loading ipython session.

---
+++ scripts/pocs-ipython-startup.py
@@ -0,0 +1,14 @@
+from panoptes.pocs.core import POCS
+from panoptes.pocs.observatory import Observatory
+from panoptes.pocs.mount import create_mount_from_config
+from panoptes.pocs.camera import create_cameras_from_config
+from panoptes.pocs.scheduler import create_scheduler_from_config
+
+scheduler = create_scheduler_from_config()
+mount = create_mount_from_config()
+cameras = create_cameras_from_config()
+
+observatory = Observatory(cameras=cameras, mount=mount, scheduler=scheduler)
+
+# Add simulators if necessary.
+# pocs = POCS(observatory, simulators=[])
