d9e080ba3e68a81419639248e84febebbe41bf08
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-10-14 16:19:23 -1000

    * Better firestore nesting.

--- src/panoptes/pocs/utils/database.py
+++ src/panoptes/pocs/utils/database.py
@@ -20,11 +20,10 @@ class PocsDB(PanFileDB):
         obj_id = super().insert_current(collection, obj, store_permanently=store_permanently)
 
         if get_config('panoptes_network.use_firestore'):
-            obj['received_time'] = firestore.SERVER_TIMESTAMP
-
             # Update the "current" collection.
             current_doc = self.firestore_db.document(f'units/{self.unit_id}/current/{collection}')
-            current_doc.set(obj)
+            metadata = dict(collection=collection, received_time=firestore.SERVER_TIMESTAMP, **obj)
+            current_doc.set(metadata)
 
             obj_id = self.insert(collection, obj)
 
@@ -35,11 +34,9 @@ class PocsDB(PanFileDB):
         obj_id = super().insert(collection, obj)
 
         if get_config('panoptes_network.use_firestore'):
-            if 'received_time' not in obj:
-                obj['received_time'] = firestore.SERVER_TIMESTAMP
-
             # Add a document.
             col = self.firestore_db.collection(f'units/{self.unit_id}/metadata')
-            doc_ts, obj_id = col.add(obj, document_id=collection)
+            metadata = dict(collection=collection, received_time=firestore.SERVER_TIMESTAMP, **obj)
+            doc_ts, obj_id = col.add(metadata)
 
         return obj_id
