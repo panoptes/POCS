8e4d65cde85499bbf2995cfa36e44312176f0eb5
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-01-19 06:31:06 -1000

    * Weather only has `status` for now. * Added a basic `pocs status` that will get built up.

--- src/panoptes/pocs/utils/cli/main.py
+++ src/panoptes/pocs/utils/cli/main.py
@@ -1,17 +1,29 @@
 import typer
+from rich.console import Console
+from rich.table import Table
+from enum import Enum
 
 from panoptes.pocs.utils.cli import config
 from panoptes.pocs.utils.cli import sensor
 from panoptes.pocs.utils.cli import image
 from panoptes.pocs.utils.cli import power
+from panoptes.pocs.utils.cli import weather
+from panoptes.pocs.utils.cli.helpers import is_tmux_session_running as is_running
+
+
+class IsRunning(Enum):
+    RUNNING = f'[bold green] Running [/bold green]'
+    NOT_RUNNING = f'[bold red] Not running [/bold red]'
+
 
 app = typer.Typer()
-state = {'verbose': False}
+state = {'verbose': False, 'console': Console()}
 
 app.add_typer(config.app, name="config", help='Interact with the config server.')
 app.add_typer(sensor.app, name="sensor", help='Interact with system sensors.')
 app.add_typer(image.app, name="image", help='Interact with images.')
 app.add_typer(power.app, name="power", help='Interact with power system.')
+app.add_typer(weather.app, name="weather", help='Interact with weather station.')
 
 
 @app.callback()
@@ -26,6 +38,26 @@ def main(context: typer.Context,
     })
     if verbose:
         typer.echo(f'Command options from main: {context.params!r}')
+
+
+@app.command()
+def status():
+    """Shows POCS status."""
+    console = state['console']
+
+    status_table = Table()
+    status_table.add_column('Service')
+    status_table.add_column('Status', justify='right')
+
+    sessions = ['config-server', 'power-monitor', 'weather-reader']
+    for session in sessions:
+        session_running = IsRunning.RUNNING if is_running(session) else IsRunning.NOT_RUNNING
+        status_table.add_row(session.replace('-', ' ').title(), session_running.value)
+
+    status_table.add_row('', '')
+    status_table.add_row('POCS', 'N/A')
+
+    console.print(status_table)
 
 
 if __name__ == "__main__":
--- src/panoptes/pocs/utils/cli/weather.py
+++ src/panoptes/pocs/utils/cli/weather.py
@@ -1,4 +1,3 @@
-from contextlib import suppress
 from typing import Optional
 
 import typer
@@ -7,31 +6,15 @@ import requests
 app = typer.Typer()
 
 
-def server_running(host: str = 'localhost', port: int = 6564):
-    """Check if the config server is running"""
-    # NOTE: A bug in server_is_running means we cannot specify the port.
-    is_running = False
-    with suppress(requests.exceptions.ConnectionError):
-        is_running = requests.get(f'http://{host}:{port}').ok
-
-    if is_running is None or is_running is False:
-        run_status = '[bold red]:x: NOT RUNNING[/bold red]'
-    else:
-        run_status = '[bold green]:heavy_check_mark: RUNNING[/bold green]'
-
-    print(f'Power Monitor status: {run_status}')
-
-    return is_running
-
-
 @app.command()
 def status(
-        host: Optional[str] = typer.Option('localhost', help='Host for the power monitor service.'),
-        port: Optional[int] = typer.Option(6564, help='Port for the power monitor service.'),
+        host: Optional[str] = typer.Option('localhost', help='Host for the weather service.'),
+        port: Optional[int] = typer.Option(6564, help='Port for the weather service.'),
+        endpoint: Optional[str] = typer.Option('/current',
+                                               help='The endpoint for the weather service.')
 ):
     """Shows the status of the power monitor."""
-    server_running(host=host, port=port)
     try:
-        return requests.get(f'http://{host}:{port}').json()
+        return requests.get(f'http://{host}:{port}/{endpoint}').json()
     except requests.exceptions.ConnectionError:
         return False
