687ba0e25ef90519c014d51d896d199b20eb16a8
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-05-26 18:19:51 -1000

    Even better logging format

--- src/panoptes/pocs/core.py
+++ src/panoptes/pocs/core.py
@@ -436,8 +436,7 @@ class POCS(PanStateMachine, PanBase):
             timestamp = record['date'].replace(tzinfo=None)  # current_time is timezone naive
             age = (current_time().datetime - timestamp).total_seconds()
 
-            self.logger.debug(f"Weather Safety: {is_safe=} "
-                              f"[{age=:.0f}sec - {timestamp:%Y-%m-%d %H:%M:%S}]")
+            self.logger.debug(f"Weather Safety: {is_safe=} {age=:.0f}sec [{timestamp:%x %X}]")
 
         except Exception as e:  # pragma: no cover
             self.logger.error(f"No weather record in database: {e!r}")
