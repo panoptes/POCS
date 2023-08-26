f0b478b83d2472a64f6cf4e5222d704dec099d74
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-08-25 18:26:08 -1000

    Small updates to running cli.

--- src/panoptes/pocs/utils/cli/run.py
+++ src/panoptes/pocs/utils/cli/run.py
@@ -8,18 +8,19 @@ app = typer.Typer()
 
 
 @app.command(name='auto')
-def run_auto(confirm: Annotated[bool, typer.Option(prompt=True)]) -> None:
+def run_auto(confirm: Annotated[bool, typer.Option(prompt='Are you sure you want to run POCS automatically?')]) -> None:
     """Runs POCS automatically, like it's meant to be run."""
-    print('Running POCS automatically!')
 
     if confirm is True:
-        pocs = POCS.from_config()
-        pocs.initialize()
-
-        # Run the main loop
         try:
+            print('[green]Running POCS automatically!\tPress Ctrl-c to quit.[/green]')
+            pocs = POCS.from_config()
+            pocs.initialize()
+
             pocs.run()
         except KeyboardInterrupt:
             print('POCS interrupted by user, shutting down.')
             print(f'[bold red]Please be patient, this may take a moment while the mount parks itself.[/bold red]]')
             pocs.power_down()
+        except Exception:
+            print('[bold red]POCS encountered an error.[/bold red]')
