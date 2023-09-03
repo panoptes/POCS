8e5b5873801af2a7a79170e819021435a8a15172
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-09-02 15:11:46 -1000

    Cleaning up the weather sensor and service after updating `panoptes-aag`

--- src/panoptes/pocs/sensor/weather.py
+++ src/panoptes/pocs/sensor/weather.py
@@ -1,10 +1,4 @@
-from typing import Optional, Callable
-
-import pandas as pd
 from aag.weather import CloudSensor
-from astropy import units as u
-from panoptes.utils.time import current_time
-from streamz.dataframe import PeriodicDataFrame
 
 from panoptes.pocs.base import PanBase
 
@@ -12,92 +6,55 @@ from panoptes.pocs.base import PanBase
 class WeatherStation(PanBase):
     """POCS weather station.
 
-    Ideally just a thin wrapper.
+    Ideally just a thin wrapper with support for putting entries into the database.
     """
 
     def __init__(self,
                  port: str = None,
                  name: str = 'Weather Station',
-                 reader_callback: Callable[[dict], dict] = None,
-                 dataframe_period: int = 1,
-                 mean_interval: Optional[int] = 5,
+                 db_collection: str = 'weather',
                  *args, **kwargs):
         """Initialize the weather station.
 
         Args:
             port (str, optional): The dev port for the arduino, if not provided, search for port
                 matching the vendor (2341) and product id (0043).
-            name (str): The user-friendly name for the power board.
-            reader_callback (Callable): A callback for the serial readings. The
-                default callback will update the pin values and record data in a
-                json format, which is then made into a dataframe with the `to_dataframe`.
-            dataframe_period (int): The period to use for creating the
-                `PeriodicDataFrame`, default `2` (seconds).
-            mean_interval (int): When taking a rolling mean, use this many seconds,
-                default 5.
-            arduino_board_name (str): The name of the arduino board to match in
-                the callback and the collection name for storing in `record.
+            name (str): The user-friendly name for the weather station.
+            db_collection (str): Which collection (i.e. table) to store the values in, default 'weather'.
         """
         super().__init__(*args, **kwargs)
 
         self.port = port or self.get_config('environment.weather.port', '/dev/ttyUSB0')
         self.name = name
+        self.collection_name = db_collection
 
         self.logger.debug(f'Setting up weather station connection for {name=} on {self.port}')
-        self._ignore_readings = 5
         self.weather_station = CloudSensor(serial_port=self.port)
-
-        self.dataframe = None
-        if dataframe_period is not None:
-            self.dataframe = PeriodicDataFrame(interval=f'{dataframe_period}s',
-                                               datafn=self.to_dataframe)
-
-        self._mean_interval = mean_interval
 
         self.logger.info(f'{self.weather_station} initialized')
 
     @property
     def status(self):
-        return self.readings
+        """Returns the most recent weather reading."""
+        return self.weather_station.readings[-1]
 
-    @property
-    def readings(self):
-        """Return the rolling mean of the readings. """
-        time_start = (current_time() - self._mean_interval * u.second).to_datetime()
-        df = self.to_dataframe()[time_start:]
-        values = df.mean().astype('int').to_dict()
+    def record(self):
+        """Record the rolling mean of the power readings in the database."""
+        recent_values = self.weather_station.get_reading()
 
-        return values
-
-    def to_dataframe(self, **kwargs):
-        """Make a dataframe from the latest readings.
-
-        This method is called by a `streamz.dataframe.PeriodicDataFrame`.
-
-        """
-        try:
-            # columns = ['time', 'ac_ok'] + list(self.relay_labels.keys())
-            # df0 = pd.DataFrame(self.weather_station.readings, columns=columns)
-            df0 = pd.DataFrame(self.weather_station.readings)
-            df0.set_index(['time'], inplace=True)
-        except:
-            df0 = pd.DataFrame([], index=pd.DatetimeIndex([]))
-
-        return df0
-
-    def record(self, collection_name: str = 'weather'):
-        """Record the rolling mean of the power readings in the database.
-
-        Args:
-            collection_name (str): Where to store the results in the db. If None
-                (the default), then use `arduino_board_name`.
-
-        """
-        recent_values = self.readings
-
-        self.db.insert_current(collection_name, recent_values)
+        self.db.insert_current(self.collection_name, recent_values)
 
         return recent_values
+
+    def readings(self, num_readings: int = 20):
+        """Returns weather readings.
+
+        Args:
+            num_readings (int, optional): The number of readings to return, default
+                20. Note that the weather station itself might be configured to
+                store less than this.
+        """
+        return self.weather_station.readings[-num_readings:]
 
     def __str__(self):
         return f'{self.name} {self.weather_station}'
--- src/panoptes/pocs/utils/service/weather.py
+++ src/panoptes/pocs/utils/service/weather.py
@@ -1,25 +1,18 @@
-from enum import auto
-from typing import Union
-
 from fastapi import FastAPI
-from fastapi_utils.enums import StrEnum
 from fastapi_utils.tasks import repeat_every
 from panoptes.utils.config.client import get_config
-from pydantic import BaseModel
 
-from aag.weather import CloudSensor
-
-from panoptes.pocs.base import PanBase
+from panoptes.pocs.sensor.weather import WeatherStation
 
 app = FastAPI()
-weather_station: CloudSensor
-read_interval = get_config('environment.power.read_interval', default=60)
+weather_station: WeatherStation
+read_interval = get_config('environment.weather.read_interval', default=60)
 
 
 @app.on_event('startup')
 async def startup():
     global weather_station
-    weather_station = CloudSensor(**get_config('environment.weather', {}))
+    weather_station = WeatherStation(**get_config('environment.weather', {}))
 
 
 @app.on_event('startup')
@@ -27,7 +20,7 @@ async def startup():
 def record_readings():
     """Record the current readings in the db."""
     global weather_station
-    return weather_station.record(collection_name='power')
+    return weather_station.record()
 
 
 @app.get('/')
@@ -35,10 +28,3 @@ async def root():
     """Returns the power board status."""
     global weather_station
     return weather_station.status
-
-
-@app.get('/readings')
-async def readings():
-    """Return the current readings as a dict."""
-    global weather_station
-    return weather_station.to_dataframe().to_dict()
