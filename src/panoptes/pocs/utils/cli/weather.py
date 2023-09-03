8538c5c859b93d03ace3bc1b5cddca95628b00c3
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-09-02 18:49:17 -1000

    Simplify the weather service so that it is using the pan db for values instead of a server. This reflects what pocs knows. Should be done with power too but not in this PR.

--- src/panoptes/pocs/utils/cli/weather.py
+++ src/panoptes/pocs/utils/cli/weather.py
@@ -1,58 +1,18 @@
 import subprocess
-from dataclasses import dataclass
 
-import requests
 import typer
 from rich import print
 
-
-@dataclass
-class HostInfo:
-    host: str = 'localhost'
-    port: str = '6566'
-
-    @property
-    def url(self):
-        return f'http://{self.host}:{self.port}'
-
+from panoptes.utils.database import PanDB
 
 app = typer.Typer()
-
-
-@app.callback()
-def common(context: typer.Context,
-           host: str = typer.Option('localhost', help='Weather station host address.'),
-           port: str = typer.Option('6566', help='Weather station port.'),
-           ):
-    context.obj = HostInfo(host=host, port=port)
+DB = PanDB(db_type='file')
 
 
 @app.command(name='status')
 def status(context: typer.Context):
     """Get the status of the weather station."""
-    url = context.obj.url
-    try:
-        res = requests.get(url)
-        if res.ok:
-            return res.json()
-        else:
-            print(f'[red]{res.content.decode()}[/red]')
-    except requests.exceptions.ConnectionError:
-        print(f'[red]Cannot connect to {url}[/red]')
-
-
-@app.command(name='readings')
-def readings(context: typer.Context):
-    """Get the weather readings."""
-    url = context.obj.url + '/readings'
-    try:
-        res = requests.get(url)
-        if res.ok:
-            return res.json()
-        else:
-            print(f'[red]{res.content.decode()}[/red]')
-    except requests.exceptions.ConnectionError:
-        print(f'[red]Cannot connect to {url}[/red]')
+    return DB.get_current('weather')['data']
 
 
 @app.command()
