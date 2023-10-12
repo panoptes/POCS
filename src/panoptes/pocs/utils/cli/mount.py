51a12866833f75e0103bc5aaa8ec1aa107d90c3a
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-10-11 17:31:42 -1000

    Auto find mount (#1199)
    * iOptron mount improvements.
    
    * Move the status and coordinate matching info into the yaml file as it depends on firmware, not mount type.
    
    * Typo fix.
    
    * Compile the status and coords re with verbose flag for nice whitespace.
    
    * Provide a default status and coords format.
    
    * Basics of mount auto discovery
    
    * Basics of mount auto discovery
    
    * Slightly better handling of discovery.
    
    * Slightly better handling of discovery.
    
    * Slightly better handling of discovery.
    
    * Typo
    
    * Typo
    
    * Check for devices in use.
    
    * Check for ioptron response.
    
    * Use the `panoptes.utils.rs232.SerialData` for mount probing.
    
    * Check properly for bad devices.
    
    * Fix break.
    
    * No longer encode.
    
    * Setup for changing config items.
    
    * * Adding enum for ioptron mount types.
    * Building up check for mount, not setting values in config yet.
    
    * Parse the mount return type as int.
    
    * Move all the enum types into the shared init file for the `ioptron` folder.
    
    * Update the config after discovery.
    
    * Search the symlinks in addition to devices.
    
    * Update the udev entry for the mount. Maybe should move this to the pocs command?
    
    * Skip aag port.
    
    * Skip aag port typo fix.
    
    * Time adjustment is based on firmware, not mount.
    
    * Create a udev entry output.
    
    * Typo
    
    * Don't include subsystems in udev entry.
    
    * Skip udev entry for symlinks.
    
    * Write udev entry to file and give instructions.
    
    * Small formatting update.
    
    * Typo with missing `=`.
    
    * Show filename better.
    
    * Add `ACTION` command.
    
    * Remove 'SUBSYSTEMS'
    
    * Automatically write udev entry.
    
    * Automatically write udev entry.
    
    * Give up on the automatic
    
    * Write out the ioptron config entry.
    
    * Different instructions for copying to the file.
    
    * Only test USB tty entries.

--- src/panoptes/pocs/utils/cli/mount.py
+++ src/panoptes/pocs/utils/cli/mount.py
@@ -1,12 +1,16 @@
 import re
+from pathlib import Path
 
 import serial
 import typer
+from panoptes.utils.config.client import set_config
 from rich import print
 from typing_extensions import Annotated
 
-from panoptes.utils.rs232 import SerialData, get_serial_port_info
+from panoptes.utils.serial.device import get_serial_port_info
+from panoptes.utils.rs232 import SerialData
 from panoptes.pocs.mount import create_mount_from_config
+from panoptes.pocs.mount.ioptron import MountInfo
 
 app = typer.Typer()
 
@@ -92,9 +96,9 @@ def setup_mount(
     # Loop through all the ports and baudrates.
     for port in ports:
         for baudrate in baudrates:
-            print(f"Trying {port.device=} at {baudrate=}...")
-            if 'serial' in port.device:
+            if 'ttyUSB' not in port.device or 'ioptron' not in port.device:
                 continue
+            print(f"Trying {port.device=} at {baudrate=}...")
             device = SerialData(port=port.device, baudrate=baudrate, timeout=1)
 
             try:
@@ -102,30 +106,61 @@ def setup_mount(
                 try:
                     response = device.read()
                 except serial.SerialException:
-                    print('Device potentially being accessed by another process.')
+                    print('\tDevice potentially being accessed by another process.')
                     continue
 
                 if re.match(r'\d{4}', response):  # iOptron specific
-                    print(f"Found mount at {port.device=} at {baudrate=}.")
-                    print(f"Response: {response}")
+                    mount_type = MountInfo(int(response[0:4]))
+                    print(f'Found mount at {port.device=} at {baudrate=} with {response=}.')
+                    print(f'It looks like an iOptron {mount_type.name}.')
 
                     # Get the mainboard and handcontroller firmware version.
                     device.write(':FW1#')
                     response = device.read()
-                    mainboard_fw = response[:6]
-                    handcontroller_fw = response[6:-1]
-                    print(f'Mainboard: {mainboard_fw}')
-                    print(f'Handcontroller: {handcontroller_fw}')
+                    mainboard_fw = int(response[:6])
+                    handcontroller_fw = response[6:-1]  # Returns a string if HC not plugged in.
+                    print('Firmware:')
+                    print(f'\tMainboard: {mainboard_fw}')
+                    print(f'\tHandcontroller: {handcontroller_fw}')
 
                     # Get the RA and DEC firmware version.
                     device.write(':FW2#')
                     response = device.read()
-                    ra_fw = response[:6]
-                    dec_fw = response[6:-1]
-                    print(f'RA: {ra_fw}')
-                    print(f'DEC: {dec_fw}')
+                    ra_fw = int(response[:6])
+                    dec_fw = int(response[6:-1])
+                    print(f'\tRA: {ra_fw}')
+                    print(f'\tDEC: {dec_fw}')
 
-                    # Set config items here.
+                    command_set = 'v310' if ra_fw >= 210101 and dec_fw >= 210101 else 'v250'
+                    print(f'Suggested command set: {command_set}')
+
+                    # Get info for writing udev entry.
+                    try:
+                        udev_str = (f'ACTION=="add", '
+                                    f'SUBSYSTEM=="tty", '
+                                    f'ATTRS{{idVendor}}=="{port.vid:04x}", '
+                                    f'ATTRS{{idProduct}}=="{port.pid:04x}", '
+                                    f'ATTRS{{serial}}=="{port.serial_number}", '
+                                    f'SYMLINK+="ioptron"')
+                        udev_fn = Path('91-panoptes.rules')
+                        with udev_fn.open('w') as f:
+                            f.write(udev_str)
+
+                        print(f'Wrote udev entry to [green]{udev_fn}[/green].')
+                        print('Run the following command and then reboot for changes to take effect:')
+                        print(f'\t[green]cat {udev_fn} | sudo tee -a /etc/udev/rules.d/{udev_fn}[/green]')
+                    except Exception:
+                        pass
+
+                    # Confirm the user wants to update the config.
+                    if typer.confirm('Do you want to update the config?'):
+                        print('Updating config.')
+                        set_config('mount.brand', 'ioptron')
+                        set_config('mount.serial.port', '/dev/ioptron')
+                        set_config('mount.serial.baudrate', baudrate)
+                        set_config('mount.model', mount_type.name.lower())
+                        set_config('mount.driver', f'panoptes.pocs.mount.ioptron.{mount_type.name.lower()}')
+                        set_config('mount.commands_file', f'ioptron/{command_set}')
 
                     return typer.Exit()
             except serial.SerialTimeoutException:
