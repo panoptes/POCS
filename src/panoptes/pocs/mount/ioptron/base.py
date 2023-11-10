17dc93d6e9d3555a95db136d54990fd93ffe88fe
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-11-10 12:18:56 -1000

    Dealing with dumb older RA millisecond values.

--- src/panoptes/pocs/mount/ioptron/base.py
+++ src/panoptes/pocs/mount/ioptron/base.py
@@ -243,7 +243,12 @@ class Mount(AbstractSerialMount):
         self.logger.trace(f'Mount coordinates: {coords_match}')
 
         if coords_match is not None:
-            ra = (coords_match.group('ra') * getattr(u, self._ra_coords_units)).to(u.deg)
+            if self._ra_coords_units == 'millisecond':
+                ra = (coords_match.group('ra') * getattr(u, self._ra_coords_units)).to(u.hour).value
+                ra = (ra * u.hourangle).to(u.degree)
+            else:
+                ra = (coords_match.group('ra') * getattr(u, self._ra_coords_units)).to(u.deg)
+
             dec = (coords_match.group('dec') * getattr(u, self._dec_coords_units)).to(u.deg)
 
             dec_sign = coords_match.group('dec_sign')
