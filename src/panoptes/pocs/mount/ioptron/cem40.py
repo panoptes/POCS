1a208539750c5fca83a45cfbdfe6c484ce58fc7b
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-11-12 12:09:04 -1000

    Typo

--- src/panoptes/pocs/mount/ioptron/cem40.py
+++ src/panoptes/pocs/mount/ioptron/cem40.py
@@ -32,7 +32,7 @@ class Mount(BaseMount):
         target_set = super().set_target_coordinates(*args, **kwargs)
         self.logger.debug(f'Checking number of possible positions for {self._target_coordinates=}')
         num_possible_positions = self.query('query_positions')
-        self.logger.debug(f'Number of possible positions: {_num_possible_positions}')
+        self.logger.debug(f'Number of possible positions: {num_possible_positions}')
 
         if num_possible_positions == 0:
             self.logger.warning(f'No possible positions for {self._target_coordinates=}')
