82208af838e2b7154384df4264f4f9fb6e82dea8
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-11-12 09:08:13 -1000

    If slew wasn't a success, raise Exception.

--- src/panoptes/pocs/state/states/default/slewing.py
+++ src/panoptes/pocs/state/states/default/slewing.py
@@ -1,3 +1,6 @@
+from panoptes.pocs.utils import error
+
+
 def on_enter(event_data):
     """ Once inside the slewing state, set the mount slewing. """
     pocs = event_data.model
@@ -9,10 +12,11 @@ def on_enter(event_data):
         pocs.say("I'm slewing over to the coordinates to track the target.")
 
         # Start the mount slewing
-        pocs.observatory.mount.slew_to_target(blocking=True)
+        if pocs.observatory.mount.slew_to_target(blocking=True) is False:
+            raise error.PocsError("Mount did not successfully slew to target.")
 
         pocs.say("I'm at the target, checking pointing.")
         pocs.next_state = 'pointing'
 
     except Exception as e:
-        pocs.say("Wait a minute, there was a problem slewing. Sending to parking. {}".format(e))
+        pocs.say(f'Wait a minute, there was a problem slewing. Sending to parking. {e}')
