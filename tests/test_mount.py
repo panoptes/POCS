c87e5f6c9b5f4e0020c88a3c179c27ecdf869d9d
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-02-01 12:04:47 -1000

    Fix how config works.

--- tests/test_mount.py
+++ tests/test_mount.py
@@ -25,7 +25,7 @@ def reset_conf(config_host, config_port):
 
 def test_create_mount_simulator(config_host, config_port):
     # Use the simulator create function directly.
-    with iers_conf.set_temp('iers_degraded_accuracy', 'warn'):
+    with iers_conf.iers_degraded_accuracy.set_temp('warn'):
         mount = create_mount_simulator()
         assert isinstance(mount, AbstractMount) is True
 
