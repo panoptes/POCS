bfafd8de0e22ef7c2138dc6c1f2afcc2dd9a54ea
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-02-01 10:51:42 -1000

    Fix awkward formatting.

--- src/panoptes/pocs/scheduler/__init__.py
+++ src/panoptes/pocs/scheduler/__init__.py
@@ -36,8 +36,8 @@ def create_scheduler_from_config(config=None, observer=None, iers_url=None, *arg
 
     # Read the targets from the file
     fields_file = Path(scheduler_config.get('fields_file', 'simple.yaml'))
-    fields_dir = Path(get_config('directories.base'), default='.') / Path(
-        get_config('directories.fields', default='./conf_files/fields'))
+    base_dir = Path(str(get_config('directories.base', default='.')))
+    fields_dir = base_dir / Path(str(get_config('directories.fields', default='./conf_files/fields')))
     fields_path = fields_dir / fields_file
     logger.debug(f'Creating scheduler: {fields_path}')
 
