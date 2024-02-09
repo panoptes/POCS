f982fdd55a5f11a1c821ca8e1dd18c3304787018
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-02-09 11:21:14 -1000

    Handle missing readings. Status only shows status (and not config)

--- src/panoptes/pocs/utils/service/weather.py
+++ src/panoptes/pocs/utils/service/weather.py
@@ -52,7 +52,7 @@ def record_readings():
 async def status():
     """Returns the power board status."""
     global weather_station
-    return dict(status=weather_station.status, config=weather_station.weather_station.config)
+    return weather_station.status
 
 
 @app.get('/config')
