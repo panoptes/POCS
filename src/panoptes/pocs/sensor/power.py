61a7227260051376de6c5cb5e8d0157b1850f29b
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-02-14 07:45:07 -1000

    Minor doc changes.

--- src/panoptes/pocs/sensor/power.py
+++ src/panoptes/pocs/sensor/power.py
@@ -213,7 +213,7 @@ class PowerBoard(PanBase):
         self.change_relay_state(self.relay_labels[label], TruckerBoardCommands.TOGGLE)
 
     def cycle_relay(self, label):
-        """Cycle the relay with a default 5 second delay."""
+        """Cycle the relay with a default 5-second delay."""
         self.change_relay_state(self.relay_labels[label], TruckerBoardCommands.CYCLE_DELAY)
 
     def to_dataframe(self, **kwargs):
@@ -317,8 +317,8 @@ class PowerBoard(PanBase):
         # Create a list for the new data row and add common time and AC reading.
         new_data = [
             current_time().to_datetime(),
-            data.get(ac_key, 0),
-            data.get(battery_key, 0)
+            data.get(ac_key, pd.NA),
+            data.get(battery_key, pd.NA)
         ]
         for relay_index, read_relay in enumerate(self.relays):
             # Update the state of the pin.
