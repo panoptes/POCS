4ab8d1fe7d84c62c735521096a337d5ab07bfe0a
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-24 10:31:12 -1000

    Fastapi deprecation 1208 (#1276)
    * FastAPI startup events
    
    This was added a few months ago but seems to have since been clobbered. Testing again.
    
    * Missing global
    
    * Use objects
    
    * Passing the lifespan to the FastAPI constructor
    
    * Make lifespan async
    
    * Tying it all back together.
    
    * Warning for no status instead of error.
    
    * More missing values
    
    * Converting weather station fastapi service.
    
    * Namespace conflict
    
    * Remove old `on_startup`
    
    * Replace repeat_every
    
    * Replace repeat_every with simple thread
    
    * Join the threads.
    
    * Less verbose
    
    * Adding host info for working with remote weather stations on cli

--- src/panoptes/pocs/utils/cli/weather.py
+++ src/panoptes/pocs/utils/cli/weather.py
@@ -1,22 +1,44 @@
 import subprocess
+from dataclasses import dataclass
 
 import requests
 import typer
 from rich import print
 
+
+@dataclass
+class HostInfo:
+    host: str = 'localhost'
+    port: str = '6566'
+
+    @property
+    def url(self):
+        return f'http://{self.host}:{self.port}'
+
+
 app = typer.Typer()
 
 
+@app.callback()
+def common(context: typer.Context,
+           host: str = typer.Option('localhost', help='Weather station host address.'),
+           port: str = typer.Option('6566', help='Weather station port.'),
+           ):
+    context.obj = HostInfo(host=host, port=port)
+
+
 @app.command(name='status', help='Get the status of the weather station.')
-def status(page='status', base_url='http://localhost:6566'):
+def status(context: typer.Context, page='status'):
     """Get the status of the weather station."""
-    print(get_page(page, base_url))
+    url = context.obj.url
+    print(get_page(page, url))
 
 
 @app.command(name='config', help='Get the configuration of the weather station.')
-def config(page='config', base_url='http://localhost:6566'):
+def config(context: typer.Context, page='config'):
     """Get the configuration of the weather station."""
-    print(get_page(page, base_url))
+    url = context.obj.url
+    print(get_page(page, url))
 
 
 def get_page(page, base_url):
