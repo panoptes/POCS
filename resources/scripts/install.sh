6262c14a04cdc13c62ab26eedc468601339b0b57
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-11-19 13:20:35 -1000

    Output updates

--- resources/scripts/install.sh
+++ resources/scripts/install.sh
@@ -4,22 +4,22 @@ set -e
 echo "Installing POCS"
 
 echo "Setting up user."
-source ./setup-user.sh
+source ./setup-user.sh > install.log 2>&1
 
 echo "Fixing system time."
-source ./fix-time.sh
+source ./fix-time.sh >> install.log 2>&1
 
 echo "Installing system dependencies."
-source ./install-system-deps.sh
+source ./install-system-deps.sh >> install.log 2>&1
 
 echo "Installing POCS software."
-source ./install-pocs.sh
+source ./install-pocs.sh >> install.log 2>&1
 
 echo "Installing conda python."
-source ./install-conda.sh
+source ./install-conda.sh >> install.log 2>&1
 
 echo "Installing ZSH for a better shell."
-source ./install-zsh.sh
+source ./install-zsh.sh >> install.log 2>&1
 
 echo "Installing services so things run at startup."
-source ./install-services.sh
+source ./install-services.sh >> install.log 2>&1
