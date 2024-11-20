5690132e4c1762ef5b77249308793ab83cca8956
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-11-19 14:00:20 -1000

    Change default shell with sudo

--- resources/scripts/install-services.sh
+++ resources/scripts/install-services.sh
@@ -2,20 +2,16 @@
 
 PANUSER="${PANUSER:-panoptes}"
 
-function install_services() {
-  echo "Installing supervisor services."
+echo "Installing supervisor services."
 
-  # Make supervisor read our conf file at its current location.
-  echo "files = ${HOME}/conf_files/pocs-supervisord.conf" | sudo tee -a /etc/supervisor/supervisord.conf
+# Make supervisor read our conf file at its current location.
+echo "files = ${HOME}/conf_files/pocs-supervisord.conf" | sudo tee -a /etc/supervisor/supervisord.conf
 
-  # Change the user and home directory.
-  sed -i "s/chown=panoptes:panoptes/chown=${PANUSER}:${PANUSER}/g" "${HOME}/conf_files/pocs-supervisord.conf"
-  sed -i "s/user=panoptes/user=${PANUSER}/g" "${HOME}/conf_files/pocs-supervisord.conf"
-  sed -i "s|/home/panoptes|${HOME}|g" "${HOME}/conf_files/pocs-supervisord.conf"
+# Change the user and home directory.
+sed -i "s/chown=panoptes:panoptes/chown=${PANUSER}:${PANUSER}/g" "${HOME}/conf_files/pocs-supervisord.conf"
+sed -i "s/user=panoptes/user=${PANUSER}/g" "${HOME}/conf_files/pocs-supervisord.conf"
+sed -i "s|/home/panoptes|${HOME}|g" "${HOME}/conf_files/pocs-supervisord.conf"
 
-  # Reread the supervisord conf and restart.
-  sudo supervisorctl reread
-  sudo supervisorctl update
-}
-
-install_services
+# Reread the supervisord conf and restart.
+sudo supervisorctl reread
+sudo supervisorctl update
