918e6af757165bb18599f464d49fb6d4e8d9d50f
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-04-02 15:35:43 -1000

    More explicit getting of all simulator names.

--- tests/test_pocs.py
+++ tests/test_pocs.py
@@ -471,7 +471,7 @@ def test_run_power_down_interrupt(observatory,
 
     observatory.logger.info('start_pocs ENTER')
     # Remove weather simulator, else it would always be safe.
-    simulators = hardware.get_all_names()
+    simulators = hardware.get_simulator_names('all')
     observatory.logger.warning(f'Using simulators: {simulators}')
     pocs = POCS(observatory, simulators=simulators)
     pocs.set_config('wait_delay', 5)  # Check safety every 5 seconds.
