5af9b671cd4a1c7ff5a294822b2ffb61c1197562
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-04-03 07:54:01 -1000

    Add old alignment tools (#1287)
    * Add back old alignment script.
    
    This adds back some of the old alignment tools and makes them available. Needs to be tested with updated POCS.
    
    * Tidy
    
    * Add alignment tools file.
    
    * Fix command name to not overlap. Rearrange.
    
    * Fix escape sequence.
    
    * Only use a single camera and block.
    
    * Unpark the mount.
    
    * Fix import errors for fits.
    
    * Fix blocking of rotation.
    
    * Update the plotting code.
    
    * Update the plotting code.
    
    * Don't use tab character in title.
    
    * Small cleanups and docstrings.
    
    * Add back old alignment script.
    
    This adds back some of the old alignment tools and makes them available. Needs to be tested with updated POCS.
    
    * Tidy
    
    * Add alignment tools file.
    
    * Fix command name to not overlap. Rearrange.
    
    * Fix escape sequence.
    
    * Only use a single camera and block.
    
    * Unpark the mount.
    
    * Fix import errors for fits.
    
    * Fix blocking of rotation.
    
    * Update the plotting code.
    
    * Update the plotting code.
    
    * Don't use tab character in title.
    
    * Small cleanups and docstrings.
    
    * * Remove hard-coded paths.
    
    * * Better handling of base_dir and paths.
    
    * * Change `replace` to `with_suffix`
    
    * * Warn in yellow
    
    * * Add ability to change exptime
    
    * * Print the error.
    
    * Allow an increase of the readout time when solving plate image.
    
    * * Fix WCS path that doesn't accept `Path`s.

---
+++ src/panoptes/pocs/utils/alignment.py
@@ -0,0 +1,112 @@
+from pathlib import Path
+
+import numpy as np
+from astropy.nddata import Cutout2D
+from astropy.visualization import SqrtStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+from astropy.wcs import WCS
+from matplotlib import pyplot as plt
+from panoptes.utils.images.fits import get_solve_field, getdata
+from skimage.feature import canny
+from skimage.transform import hough_circle, hough_circle_peaks
+
+
+def analyze_polar_rotation(pole_fn: Path | str, **kwargs):
+    """Analyze the polar rotation image to get the center of the pole.
+
+    Args:
+        pole_fn (Path | str): FITS file of polar center
+    Returns:
+        tuple(int): Polar center XY coordinates
+    """
+    get_solve_field(pole_fn, **kwargs)
+
+    wcs = WCS(pole_fn.as_posix())
+
+    pole_cx, pole_cy = wcs.all_world2pix(360, 90, 1)
+
+    return pole_cx, pole_cy
+
+
+def analyze_ra_rotation(rotate_fn: Path | str):
+    """Analyze the RA rotation image to get the center of rotation.
+
+    Args:
+        rotate_fn (Path | str): FITS file of RA rotation image
+    Returns:
+        tuple(int): RA axis center of rotation XY coordinates
+    """
+    d0 = getdata(rotate_fn.as_posix())
+
+    # Get center
+    position = (d0.shape[1] // 2, d0.shape[0] // 2)
+    size = (1500, 1500)
+    d1 = Cutout2D(d0, position, size)
+
+    d1.data = d1.data / d1.data.max()
+
+    # Get edges for rotation
+    rotate_edges = canny(d1.data, sigma=1.0)
+
+    rotate_hough_radii = np.arange(100, 500, 50)
+    rotate_hough_res = hough_circle(rotate_edges, rotate_hough_radii)
+    rotate_accums, rotate_cx, rotate_cy, rotate_radii = hough_circle_peaks(
+        rotate_hough_res, rotate_hough_radii, total_num_peaks=1
+    )
+
+    return d1.to_original_position((rotate_cx[-1], rotate_cy[-1]))
+
+
+def plot_center(pole_fn, rotate_fn, pole_center, rotate_center):
+    """ Overlay the celestial pole and RA rotation axis images.
+
+    Args:
+        pole_fn (str): FITS file of polar center
+        rotate_fn (str): FITS file of RA rotation image
+        pole_center (tuple(int)): Polar center XY coordinates
+        rotate_center (tuple(int)): RA axis center of rotation XY coordinates
+    Returns:
+        matplotlib.Figure: Plotted image
+    """
+    d0 = getdata(pole_fn) - 0.  # Easy cast to float
+    d1 = getdata(rotate_fn) - 0.  # Easy cast to float
+
+    d0 /= d0.max()
+    d1 /= d1.max()
+
+    pole_cx, pole_cy = pole_center
+    rotate_cx, rotate_cy = rotate_center
+
+    d_x = pole_center[0] - rotate_center[0]
+    d_y = pole_center[1] - rotate_center[1]
+
+    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(20, 14))
+
+    # Show rotation center in red
+    ax.scatter(rotate_cx, rotate_cy, color='r', marker='x', lw=5)
+
+    # Show polar center in green
+    ax.scatter(pole_cx, pole_cy, color='g', marker='x', lw=5)
+
+    # Show both images in background
+    norm = ImageNormalize(stretch=SqrtStretch())
+    ax.imshow(d0 + d1, cmap='Greys_r', norm=norm, origin='lower')
+
+    # Show an arrow
+    delta_cy = pole_cy - rotate_cy
+    delta_cx = pole_cx - rotate_cx
+    if (np.abs(delta_cy) > 25) or (np.abs(delta_cx) > 25):
+        ax.arrow(
+            rotate_cx,
+            rotate_cy,
+            delta_cx,
+            delta_cy,
+            fc='r',
+            ec='r',
+            width=20,
+            length_includes_head=True
+        )
+
+    ax.set_title(f"dx: {d_x:0.2f} pix   dy: {d_y:0.2f} pix")
+
+    return fig
