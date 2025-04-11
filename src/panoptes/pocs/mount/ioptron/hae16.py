df8fe85faf088c379202e6dc7a474a72e9918b57
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-04-10 18:17:48 -1000

    Pass the version better.

--- src/panoptes/pocs/mount/ioptron/hae16.py
+++ src/panoptes/pocs/mount/ioptron/hae16.py
@@ -4,6 +4,5 @@ from panoptes.pocs.mount.ioptron.cem40 import Mount as BaseMount
 class Mount(BaseMount):
 
     def __init__(self, location, mount_version='0012', *args, **kwargs):
-        self._mount_version = mount_version
-        super(Mount, self).__init__(location, *args, **kwargs)
+        super(Mount, self).__init__(location, mount_version=mount_version, *args, **kwargs)
         self.logger.success('iOptron HAE16 mount created')
