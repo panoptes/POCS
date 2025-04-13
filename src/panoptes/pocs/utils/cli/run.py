e5ca496abefc919d18eedb4e4dbd60779de9efeb
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-04-12 21:44:34 -1000

    * Don't double print.

--- src/panoptes/pocs/utils/cli/run.py
+++ src/panoptes/pocs/utils/cli/run.py
@@ -420,10 +420,7 @@ def run_quick_alignment(
 
             if results:
                 print(f'Camera {cam_id} alignment results:')
-                print(
-                    f"\tDelta (degrees): dx={results.dx_deg:.02f} dy={results.dy_deg:.02f} "
-                    f"dra={results.dx_deg:.02f} ddec={results.dy_deg:.02f}"
-                )
+                print(f"\tDelta (degrees): dx={results.dx_deg:.02f} dy={results.dy_deg:.02f}")
 
                 # Plot.
                 fig = plot_alignment_diff(cam_id, files, results)
