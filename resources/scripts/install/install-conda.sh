d483ee584ed5978767f25757b2e0a6e2615aa3de
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-09-29 11:39:04 -1000

    * Better set up of micromamba

--- resources/scripts/install/install-conda.sh
+++ resources/scripts/install/install-conda.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-CONDA_URL="micro.mamba.pm/install.sh"
-PANDIR="${PANDIR:-${HOME}/POCS}"
+CONDA_URL="https://micro.mamba.pm/install.sh"
 
 echo "Installing micromamba"
 
-wget -q "${CONDA_URL}" -O install-micromamba.sh
+wget --no-check-certificate "${CONDA_URL}" -O install-micromamba.sh
 sh install-micromamba.sh
 rm install-micromamba.sh
+
+echo "micromamba activate" >> ~/.bashrc
+source ~/.bashrc
+
+~/.local/bin/micromamba shell init -s bash -r ~/micromamba
+~/.local/bin/micromamba shell init -s zsh -r ~/micromamba
+
+~/.local/bin/micromamba config append channels conda-forge
+~/.local/bin/micromamba config set channel_priority strict
+
+~/.local/bin/micromamba install python=3.12 hatch click=8.2.1 -y
+
+echo "micromamba activate" >> ~/.zshrc
+
+~/.local/bin/micromamba clean --all
